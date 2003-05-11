ce656c5514a3a24eae97096833bb4be4319779f2
darras <darras@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2003-05-11 11:09:51 +0000

    ChannelChooser improved

--- tvbrowser/src/tvbrowser/ui/programtable/ChannelChooser.java
+++ tvbrowser/src/tvbrowser/ui/programtable/ChannelChooser.java
@@ -26,115 +26,226 @@
 
 package tvbrowser.ui.programtable;
 
-import javax.swing.*;
-import java.awt.event.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-import devplugin.Channel;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JWindow;
+import javax.swing.event.MouseInputListener;
+
+import tvbrowser.core.ChannelList;
 import tvbrowser.ui.finder.FinderItem;
-import tvbrowser.ui.programtable.ScrollableTablePanel;
-import tvbrowser.core.*;
+import devplugin.Channel;
 
 /**
  * TV-Browser
  *
  * @author Martin Oberhauser
  */
-public class ChannelChooser extends JPanel implements MouseListener {
-
-    private JWindow win=null;
-    private ScrollableTablePanel tablePanel;
-    private JLabel label;
-
-    public ChannelChooser(ScrollableTablePanel tablePanel) {
-       label=new JLabel(new ImageIcon("imgs/down16.gif"));
-        add(label);
-        addMouseListener(this);
-        this.tablePanel=tablePanel;
-    }
-
-    public void mousePressed(MouseEvent e) {
-        win=new ChannelChooserWindow(tablePanel);
-        win.setLocation(getLocationOnScreen());
-        win.setVisible(true);
-    }
-    public void mouseReleased(MouseEvent e) {}
-    public void mouseExited(MouseEvent e) {}
-    public void mouseEntered(MouseEvent e) {}
-    public void mouseClicked(MouseEvent e) {}
-}
 
 
-class ChannelChooserWindow extends JWindow implements MouseListener {
+public class ChannelChooser extends JPanel implements MouseListener  {
 
-    private ScrollableTablePanel tablePanel;
+	private JWindow win=null;
+	private ScrollableTablePanel tablePanel;
+	private JLabel label;
+	private Frame parent;
 
-    public ChannelChooserWindow(ScrollableTablePanel tablePanel) {
-        super();
-        this.tablePanel=tablePanel;
-        JPanel contentPane=(JPanel)getContentPane();
-        contentPane.setLayout(new BorderLayout());
+	public ChannelChooser(Frame parent, ScrollableTablePanel tablePanel) {
+	   label=new JLabel(new ImageIcon("imgs/down16.gif"));
+		add(label);
+		addMouseListener(this);
+		this.tablePanel=tablePanel;
+		this.parent=parent;
+	}
 
-        JPanel listPane=new JPanel(new GridLayout(0,1,5,0));
-        Object[] list=ChannelList.getSubscribedChannels();
+	public void mousePressed(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {
+		if (win==null) {
+			win=new ChannelChooserWindow(parent, tablePanel);
+		}
+		win.setLocation(getLocationOnScreen());
+		win.setVisible(true);
+	}
+   
+	public void mouseClicked(MouseEvent e) {}
 
-        for (int i=0;i<list.length;i++) {
-          Channel channel = (Channel) list[i];
-          listPane.add(new ChannelItem(this, channel));
-        }
-
-        contentPane.add(new JScrollPane(listPane),BorderLayout.CENTER);
-
-        addMouseListener(this);
-        setSize(100,200);
-    }
-
-    public void setChannel(Channel channel) {
-        tablePanel.scrollTo(channel);
-    }
-
-    public void mouseClicked(MouseEvent e) {}
-    public void mousePressed(MouseEvent e) {}
-    public void mouseReleased(MouseEvent e) {}
-    public void mouseEntered(MouseEvent e) {}
-    public void mouseExited(MouseEvent e) {
-        setVisible(false);
-    }
 
 }
 
-class ChannelItem extends JLabel implements MouseListener {
+class ChannelChooserWindow extends JWindow implements MouseInputListener {
+
+	private ScrollableTablePanel tablePanel;
+	private JPanel contentPane;
+	private ChannelItem curSelectedItem;
+	private int visibleRows=20;
+	private int upperInx;
+	private JLabel upLabel, downLabel;
+	private Channel[] list;
+	private ChannelItem[] channelItems;
+    
+
+	public ChannelChooserWindow(Frame parent, ScrollableTablePanel tablePanel) {
+		super(parent);
+		this.tablePanel=tablePanel;
+        
+		contentPane=(JPanel)getContentPane();
+		contentPane.setLayout(new BorderLayout());
+		contentPane.setBorder(BorderFactory.createLineBorder(Color.black));
+		JPanel listPane=new JPanel(new GridLayout(0,1));
+      
+		upLabel=new JLabel(new ImageIcon("imgs/Up16.gif"));
+		downLabel=new JLabel(new ImageIcon("imgs/Down16.gif"));
+   
+		list=ChannelList.getSubscribedChannels();
+        
+		upperInx=0;
+        
+		if (list.length<visibleRows) {
+			visibleRows=list.length;
+		}
+        
+		channelItems=new ChannelItem[visibleRows];
+		listPane.add(upLabel);
+		for (int i=0;i<visibleRows;i++) {
+			channelItems[i]=new ChannelItem(this,list[i]);
+			listPane.add(channelItems[i]);
+		}
+		listPane.add(downLabel);
+		updateList();
+        
+		contentPane.add(listPane,BorderLayout.CENTER);
+		       
+		contentPane.addMouseListener(this);
+		contentPane.addMouseMotionListener(this);
+        
+		this.pack();
+      
+	}
+    
+	private void updateList() {
+        
+		for (int i=0;i<visibleRows;i++) {
+			channelItems[i].setChannel(list[i+upperInx]);           
+		}
+        
+		upLabel.setEnabled(upperInx!=0);
+		downLabel.setEnabled(upperInx<list.length-visibleRows);
+        
+	}
+    
+	private JLabel getItem(int x, int y) {
+        
+		Component comp=contentPane.getComponentAt(x,y);
+		comp=comp.getComponentAt(x-comp.getX(),y-comp.getY());
+        
+		if (comp instanceof JLabel) {
+			return (JLabel)comp;
+		}
+		return null;
+        
+	}
+
+	public void setChannel(Channel channel) {
+		tablePanel.scrollTo(channel);
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		JLabel item=getItem(e.getX(),e.getY());
+		if (item==upLabel) {
+			if (upLabel.isEnabled()) {
+				upperInx--;
+				updateList();
+			}
+		}
+		else if (item==downLabel) {
+			if (downLabel.isEnabled()) {
+				upperInx++;
+				updateList();
+			}
+		}else if (curSelectedItem!=null) {
+			curSelectedItem.mouseClicked();
+		}
+	}
+    
+	public void mousePressed(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {
+		setVisible(false);
+	}
+	public void mouseDragged(MouseEvent e) {
+	}
+    
+	public void mouseMoved(MouseEvent e) {
+		JLabel item=getItem(e.getX(),e.getY());
+		if (item==null) {
+			if (curSelectedItem!=null) {
+				curSelectedItem.mouseExited();
+			}
+			curSelectedItem=null;
+		}
+        
+		else if (item instanceof ChannelItem) {
+			ChannelItem chItem=(ChannelItem)item;
+			if (chItem!=curSelectedItem) {
+				if (curSelectedItem!=null) {
+					curSelectedItem.mouseExited();
+				}
+				curSelectedItem=chItem;
+				chItem.mouseEntered();
+			}
+		}
+        
+	}
+    
+    
+
+}
+
+class ChannelItem extends JLabel {
   
-    private ChannelChooserWindow mParent;
-    private Channel mChannel;
+	private ChannelChooserWindow mParent;
+	private Channel mChannel;
     
     
-    public ChannelItem(ChannelChooserWindow parent, Channel channel) {
-        super(channel.getName());
+	public ChannelItem(ChannelChooserWindow parent, Channel channel) {
+		super(channel.getName());
         
-        mParent = parent;
-        mChannel = channel;
+		mParent = parent;
+		mChannel = channel;
         
-        addMouseListener(this);
-        setOpaque(false);
-    }
+		setOpaque(false);
+	}
+    
+	public void setChannel(Channel ch) {
+		mChannel=ch;
+		setText(ch.getName());
+	}
 
-    public void mouseClicked(MouseEvent evt) {
-        mParent.setVisible(false);
-        mParent.setChannel(mChannel);
-    }
-    public void mousePressed(MouseEvent e) {
-    }
-    public void mouseReleased(MouseEvent e) {
-    }
-    public void mouseExited(MouseEvent e) {
-       setOpaque(false);
-       updateUI();
-    }
+	public void mouseClicked() {
+		mParent.setChannel(mChannel);
+	}
+   
+   
+	public void mouseExited() {
+	   setOpaque(false);
+	   updateUI();
+	}
 
-    public void mouseEntered(MouseEvent e) {
-      setOpaque(true);
-      setBackground(FinderItem.MARKED_BG_COLOR);
-      updateUI();
-    }
+	public void mouseEntered() {
+		setOpaque(true);
+		setBackground(FinderItem.MARKED_BG_COLOR);
+		updateUI();
+	}
 }
