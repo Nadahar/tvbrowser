a7653995aa9ccc05bfd13566a3324009fda3978e
darras <darras@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2003-05-25 16:07:40 +0000

    prepared for release 0.9.4

--- tvbrowser/src/favoritesplugin/FavoritesPlugin.java
+++ tvbrowser/src/favoritesplugin/FavoritesPlugin.java
@@ -179,7 +179,7 @@ public class FavoritesPlugin extends Plugin {
       "Automatically marks your favorite programs and passes them to other Plugins." );
     String author = "Til Schneider, www.murfman.de" ;
     
-    return new PluginInfo(name, desc, author, new Version(1, 1));
+    return new PluginInfo(name, desc, author, new Version(1, 2));
   }
   
   
