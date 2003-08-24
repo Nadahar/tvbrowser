958967a518fb8e0a56fef8c8e99cadfa4ee49f52
darras <darras@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2003-08-24 23:40:39 +0000

    new feature: system tray

---
+++ trunk/tvbrowser/src/com/gc/systray/SystemTrayIconListener.java
@@ -0,0 +1,45 @@
+package com.gc.systray;
+
+import java.awt.Point;
+
+/**
+ * This intreface should be implemented by a class that want to manage
+ * the icon events.
+ *
+ * @author <a href="mail:gciubotaru@yahoo.com">George Ciubotaru</a>
+ * @version 1.0
+ */
+public interface SystemTrayIconListener
+{
+    /**
+     * This method will be called when the icon is left clicked
+     *
+     * @param pos the mouse position on the screen when mouse click
+     * @param source the SystemTrayIconManager instance
+     */
+    public void mouseClickedLeftButton(Point pos, SystemTrayIconManager source);
+
+    /**
+     * This method will be called when the icon is right clicked
+     *
+     * @param pos the mouse position on the screen when mouse click
+     * @param source the SystemTrayIconManager instance
+     */
+    public void mouseClickedRightButton(Point pos, SystemTrayIconManager source);
+
+    /**
+     * This method will be called when the icon is double right clicked
+     *
+     * @param pos the mouse position on the screen when mouse click
+     * @param source the SystemTrayIconManager instance
+     */
+    public void mouseRightDoubleClicked(Point pos, SystemTrayIconManager source);
+
+    /**
+     * This method will be called when the icon is double left clicked
+     *
+     * @param pos the mouse position on the screen when mouse click
+     * @param source the SystemTrayIconManager instance
+     */
+    public void mouseLeftDoubleClicked(Point pos, SystemTrayIconManager source);
+}
