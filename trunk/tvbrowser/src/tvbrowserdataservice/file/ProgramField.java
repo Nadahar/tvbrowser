92fd6f1b9081802a2a46cc9cbeebc5c4f0e62710
til132 <til132@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2003-10-04 12:39:00 +0000

    Created API for reading, writing and updating the TV-Browser-TV-data-format

---
+++ trunk/tvbrowser/src/tvbrowserdataservice/file/ProgramField.java
@@ -0,0 +1,265 @@
+/*
+ * TV-Browser
+ * Copyright (C) 04-2003 Martin Oberhauser (darras@users.sourceforge.net)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * CVS information:
+ *  $RCSfile$
+ *   $Source$
+ *     $Date$
+ *   $Author$
+ * $Revision$
+ */
+package tvbrowserdataservice.file;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+
+import java.util.logging.Level;
+
+/**
+ * 
+ * 
+ * @author Til Schneider, www.murfman.de
+ */
+public class ProgramField implements Cloneable {
+  
+  private static java.util.logging.Logger mLog
+    = java.util.logging.Logger.getLogger(ProgramField.class.getName());
+  
+  private static final String TEXT_CHARSET = "UTF-8";
+
+  private int mTypeId;
+
+  private ProgramFieldType mType;
+  
+  private byte[] mData;
+  
+  private int mDataFormat;
+  
+  
+  
+  public ProgramField() {
+    mDataFormat = ProgramFieldType.UNKOWN_FORMAT;
+    mType = null;
+  }
+  
+  
+  
+  public Object clone() {
+    try {
+      return super.clone();
+    }
+    catch (CloneNotSupportedException exc) {
+      // This will never happen, since this class implements Cloneable
+      return null;
+    }
+  }
+
+
+
+  /**
+   * @return
+   */
+  public int getTypeId() {
+    return mTypeId;
+  }
+
+
+
+  /**
+   * @param type
+   */
+  public ProgramFieldType getType() {
+    if (mType == null) {
+      mType = ProgramFieldType.getTypeForId(mTypeId);
+    }
+    
+    return mType;
+  }
+
+
+
+  /**
+   * @param type
+   */
+  public void setType(ProgramFieldType type) {
+    mType = type;
+    mTypeId = type.getTypeId();
+  }
+
+
+
+  /**
+   * @return
+   */
+  public byte[] getBinaryData() {
+    return mData;
+  }
+
+
+
+  /**
+   * @param data
+   */
+  public void setBinaryData(byte[] data) {
+    mDataFormat = ProgramFieldType.BINARY_FORMAT;
+    
+    mData = data;
+  }
+  
+  
+  
+  public String getTextData() {
+    try {
+      return new String(mData, TEXT_CHARSET);
+    }
+    catch (UnsupportedEncodingException exc) {
+      // This will never happen, because UTF-8 is always supported
+      mLog.log(Level.SEVERE, "Charset " + TEXT_CHARSET + " is not supported", exc);
+      
+      return null;
+    }
+  }
+
+
+
+  public void setTextData(String text) {
+    mDataFormat = ProgramFieldType.TEXT_FORMAT;
+
+    try {
+      mData = text.getBytes(TEXT_CHARSET);
+    }
+    catch (UnsupportedEncodingException exc) {
+      // This will never happen, because UTF-8 is always supported
+      mLog.log(Level.SEVERE, "Charset " + TEXT_CHARSET + " is not supported", exc);
+    }
+  }
+  
+  
+  
+  public int getIntData() {
+    return dataToInt(mData);
+  }
+  
+  
+  
+  public void setIntData(int value) {
+    mDataFormat = ProgramFieldType.INT_FORMAT;
+    
+    mData = intToData(value);
+  }
+
+
+
+  public int getTimeData() {
+    return dataToInt(mData);
+  }
+  
+  
+  
+  public void setTimeData(int minutesAfter1970) {
+    mDataFormat = ProgramFieldType.TIME_FORMAT;
+    
+    mData = intToData(minutesAfter1970);
+  }
+
+
+
+  private static int dataToInt(byte[] data) {
+    return (((int) data[0] & 0xFF) << (3 * 8))
+         | (((int) data[1] & 0xFF) << (2 * 8))
+         | (((int) data[2] & 0xFF) << (1 * 8))
+         | (((int) data[3] & 0xFF) << (0 * 8));
+  }
+
+
+
+  private static byte[] intToData(int value) {
+    byte[] data = new byte[4];
+    
+    data[0] = (byte) (value >> (3 * 8));
+    data[1] = (byte) (value >> (2 * 8));
+    data[2] = (byte) (value >> (1 * 8));
+    data[3] = (byte) (value >> (0 * 8));
+    
+    return data;
+  }
+
+
+
+  private void checkFormat() throws FileFormatException {
+    // Check whether the field data has the right format
+    if (! getType().isRightFormat(mDataFormat)) {
+      throw new FileFormatException("The field " + getType().getName()
+        + " must have the " + ProgramFieldType.getFormatName(getType().getFormat())
+        + " but it has the " + ProgramFieldType.getFormatName(mDataFormat));
+    }
+  }
+
+
+
+  public void readFromStream(InputStream stream)
+    throws IOException, FileFormatException
+  {
+    mTypeId = stream.read();
+    mType = null;
+
+    mDataFormat = ProgramFieldType.UNKOWN_FORMAT;
+    
+    int dataLength = ((stream.read() & 0xFF) << 16)
+                   | ((stream.read() & 0xFF) << 8)
+                   |  (stream.read() & 0xFF);
+    
+    mData = new byte[dataLength];
+    int offset = 0;
+    while (offset < dataLength) {
+      int len = stream.read(mData, offset, dataLength - offset);
+      if (len == -1) {
+        throw new FileFormatException("Unexpected end of file");
+      }
+      offset += len;
+    }
+  }
+
+
+
+  public void writeToStream(OutputStream stream)
+    throws IOException, FileFormatException
+  {
+    // Check whether the field has the right format
+    checkFormat();
+
+    stream.write(mTypeId);
+    
+    if (mData == null) {
+      stream.write(0); // Length highest byte
+      stream.write(0); // Length middle byte
+      stream.write(0); // Length lowest byte
+    } else {
+      // Write the data length
+      stream.write((byte) (mData.length >> 16));
+      stream.write((byte) (mData.length >> 8));
+      stream.write((byte) (mData.length));
+      
+      // Write the data
+      stream.write(mData);
+    }
+  }
+
+}
---
+++ trunk/tvbrowser/src/tvbrowserdataservice/file/ProgramFrame.java
@@ -0,0 +1,177 @@
+/*
+ * TV-Browser
+ * Copyright (C) 04-2003 Martin Oberhauser (darras@users.sourceforge.net)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * CVS information:
+ *  $RCSfile$
+ *   $Source$
+ *     $Date$
+ *   $Author$
+ * $Revision$
+ */
+package tvbrowserdataservice.file;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+/**
+ * 
+ * 
+ * @author Til Schneider, www.murfman.de
+ */
+public class ProgramFrame {
+
+  private int mId;
+
+  private ArrayList mProgramFieldList;
+  
+  
+  
+  public ProgramFrame(int id) {
+    mId = id;
+    
+    mProgramFieldList = new ArrayList();
+  }
+
+
+
+  public ProgramFrame() {
+    this(-1);
+  }
+
+  
+  
+  /**
+   * @return
+   */
+  public int getId() {
+    return mId;
+  }
+
+
+
+  /**
+   * @param id
+   */
+  public void setId(int id) {
+    mId = id;
+  }
+
+
+
+  public int getProgramFieldCount() {
+    return mProgramFieldList.size();
+  }
+  
+  
+  
+  public ProgramField getProgramFieldAt(int index) {
+    return (ProgramField) mProgramFieldList.get(index);
+  }
+
+
+
+  public void removeProgramFieldAt(int index) {
+    mProgramFieldList.remove(index);
+  }
+
+
+
+  public void removeAllProgramFields() {
+    mProgramFieldList.clear();
+  }
+
+
+
+  public void addProgramField(ProgramField frame) {
+    mProgramFieldList.add(frame);
+  }
+
+
+
+  public int getProgramFieldIndexForTypeId(int typeId) {
+    for (int i = 0; i < getProgramFieldCount(); i++) {
+      if (getProgramFieldAt(i).getTypeId() == typeId) {
+        return i;
+      }
+    }
+    
+    // Nothing found
+    return -1;
+  }
+
+
+
+  private void checkFormat() throws FileFormatException {
+    // Check whether the ID was set
+    if (mId == -1) {
+      throw new FileFormatException("The program frame's ID was not set");
+    }
+    
+    // Check whether there are duplicate fields
+    for (int i = 0; i < getProgramFieldCount(); i++) {
+      ProgramField field = getProgramFieldAt(i);
+      // Check whether there is a field that has the same type ID than this one
+      for (int j = i + 1; j < getProgramFieldCount(); j++) {
+        ProgramField cmp = getProgramFieldAt(j);
+        
+        if (field.getTypeId() == cmp.getTypeId()) {
+          throw new FileFormatException("Program field #" + i + " and "
+            + " program field #" + j + " have the same type ID: "
+            + field.getTypeId());
+        }
+      }
+    }
+  }
+  
+
+
+  public void readFromStream(InputStream stream)
+    throws IOException, FileFormatException
+  {
+    mId = stream.read();
+    
+    int fieldCount = stream.read();
+    mProgramFieldList.clear();
+    mProgramFieldList.ensureCapacity(fieldCount);
+    for (int i = 0; i < fieldCount; i++) {
+      ProgramField field = new ProgramField();
+      field.readFromStream(stream);
+      mProgramFieldList.add(field);
+    }
+  }
+
+
+
+  public void writeToStream(OutputStream stream)
+    throws IOException, FileFormatException
+  {
+    checkFormat();
+    
+    stream.write(mId);
+    
+    stream.write(getProgramFieldCount());
+    for (int i = 0; i < getProgramFieldCount(); i++) {
+      ProgramField field = getProgramFieldAt(i);
+      field.writeToStream(stream);
+    }
+  }
+
+
+}
