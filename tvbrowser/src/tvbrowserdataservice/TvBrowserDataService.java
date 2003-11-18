d30927957cec528109b4315254d9990b6b2cc0e3
darras <darras@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2003-11-18 20:55:36 +0000

    prepared for release 0.9.7

--- tvbrowser/src/tvbrowserdataservice/TvBrowserDataService.java
+++ tvbrowser/src/tvbrowserdataservice/TvBrowserDataService.java
@@ -67,9 +67,9 @@ public class TvBrowserDataService extends AbstractTvDataService {
   private static final int MAX_LAST_UPDATE_DAYS = 5;
 
   private static final Mirror[] DEFAULT_MIRROR_LIST = new Mirror[] {
-    //new Mirror("http://www.murfman.de/tvdata"),
-    //new Mirror("http://tvbrowser.waidi.net"),
-    //new Mirror("http://tvbrowser.dyndns.tv"),
+    new Mirror("http://www.murfman.de/tvdata"),
+    new Mirror("http://tvbrowser.waidi.net"),
+    new Mirror("http://tvbrowser.dyndns.tv"),
     new Mirror("http://webspace-free.de/Member/TV"),
   };
 
