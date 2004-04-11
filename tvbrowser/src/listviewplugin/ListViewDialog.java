0553b714030521357e14f7a5a6cbeb2c39524fcc
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2004-04-11 15:33:39 +0000

    New Plugin: ListViewPlugin

---
+++ tvbrowser/src/listviewplugin/ListViewDialog.java
@@ -0,0 +1,123 @@
+/*
+ * Created on 11.04.2004
+ */
+package listviewplugin;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Iterator;
+import java.util.Vector;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
+
+import devplugin.Channel;
+import devplugin.Date;
+import devplugin.Plugin;
+import devplugin.Program;
+import favoritesplugin.FavoritesPlugin;
+
+/**
+ * Creates a Dialog with a List of Programs
+ * 
+ * @author bodo
+ */
+public class ListViewDialog extends JDialog {
+
+    /**
+     * Creates the Dialog
+     * @param frame Frame for modal
+     * @param plugin Plugin for reference
+     */
+    public ListViewDialog(Frame frame, Plugin plugin) {
+        super(frame, true);
+        setTitle(mLocalizer.msg("viewList", "View List:"));
+
+        generateList();
+        createGUI();
+    }
+
+    /**
+     *  Genereates the List of Programs
+     */
+    private void generateList() {
+        _programList = new Vector();
+
+        Channel[] channels = Plugin.getPluginManager().getSubscribedChannels();
+
+        Date date = new Date();
+        for (int i = 0; i < channels.length; i++) {
+            Iterator it = Plugin.getPluginManager().getChannelDayProgram(date,
+                    channels[i]);
+            while ((it != null) && (it.hasNext())) {
+                Program program = (Program) it.next();
+                if (program.isOnAir()) {
+                    _programList.add(program);
+                }
+            }
+        }
+
+    }
+
+    /**
+     *  Creates the GUI
+     */
+    private void createGUI() {
+        JPanel content = (JPanel) this.getContentPane();
+        content.setLayout(new BorderLayout());
+        content.add(new JLabel(mLocalizer.msg("nowRunning", "Now Running:")), BorderLayout.NORTH);
+
+        _programJList = new JList(_programList);
+        
+        _programJList.setCellRenderer(new ListViewProgramCellRenderer());
+                
+        _programJList.addMouseListener(new MouseAdapter() {
+
+            public void mouseClicked(MouseEvent e) {
+                if (SwingUtilities.isRightMouseButton(e)) {
+                    int inx = _programJList.locationToIndex(e.getPoint());
+                    Program p = (Program) _programJList.getModel().getElementAt(inx);
+                    _programJList.setSelectedIndex(inx);
+                    JPopupMenu menu = devplugin.Plugin.getPluginManager()
+                            .createPluginContextMenu(p,
+                                    FavoritesPlugin.getInstance());
+                    menu.show(_programJList, e.getX() - 15, e.getY() - 15);
+                }
+            }
+        });
+
+        JScrollPane scroll = new JScrollPane(_programJList);
+
+        content.add(scroll, BorderLayout.CENTER);
+
+        JPanel buttonPn = new JPanel(new FlowLayout(FlowLayout.TRAILING));
+        content.add(buttonPn, BorderLayout.SOUTH);
+        
+        JButton okButton = new JButton(mLocalizer.msg("ok", "OK"));
+        okButton.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent evt) {
+            dispose();
+          }
+        });
+        buttonPn.add(okButton);
+    }
+    
+    /** The localizer used by this class. */  
+    private static final util.ui.Localizer mLocalizer
+      = util.ui.Localizer.getLocalizerFor(ListViewDialog.class );
+    
+    private JList _programJList;
+    private Vector _programList;
+}
---
+++ tvbrowser/src/listviewplugin/ListViewPlugin.java
@@ -0,0 +1,102 @@
+/*
+ * Created on 11.04.2004
+ */
+package listviewplugin;
+
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.event.ComponentEvent;
+
+import javax.swing.SwingUtilities;
+
+import util.ui.UiUtilities;
+import devplugin.Plugin;
+import devplugin.PluginInfo;
+import devplugin.Version;
+
+/**
+ * This Plugin shows a List of current running Programs
+ * 
+ * @author bodo
+ */
+public class ListViewPlugin extends Plugin {
+   
+    /** Translator */
+    private static final util.ui.Localizer mLocalizer = util.ui.Localizer
+            .getLocalizerFor(ListViewPlugin.class);
+
+    /**
+     * Returns Informations about this Plugin
+     */
+    public PluginInfo getInfo() {
+        String name = mLocalizer.msg("pluginName", "View List Plugin");
+        String desc = mLocalizer
+                .msg("description",
+                        "Shows a List of current running Programs");
+        String author = "Bodo Tasche";
+        return new PluginInfo(name, desc, author, new Version(1, 0));
+    }
+
+    /**
+     * Creates the Dialog
+     */
+    public void execute() {
+        final ListViewDialog dlg = new ListViewDialog(getParentFrame(), this);
+        
+        Runnable runLater = new Runnable() {
+
+            public void run() {
+
+
+                dlg.pack();
+                dlg.addComponentListener(new java.awt.event.ComponentAdapter() {
+
+                    public void componentResized(ComponentEvent e) {
+                        _dimensionListDialog = e.getComponent().getSize();
+                    }
+
+                    public void componentMoved(ComponentEvent e) {
+                        e.getComponent().getLocation(_locationListDialog);
+                    }
+                });
+
+                if ((_locationListDialog != null)
+                        && (_dimensionListDialog != null)) {
+                    dlg.setLocation(_locationListDialog);
+                    dlg.setSize(_dimensionListDialog);
+                    dlg.show();
+                } else {
+                    dlg.setSize(500, 600);
+                    UiUtilities.centerAndShow(dlg);
+                    _locationListDialog = dlg.getLocation();
+                    _dimensionListDialog = dlg.getSize();
+                }
+            }
+        };
+        SwingUtilities.invokeLater(runLater);
+
+    }
+
+    /**
+     * Returns the Button-Text
+     */
+    public String getButtonText() {
+        return mLocalizer.msg("buttonName", "View Liste");
+    }
+
+    public String getMarkIconName() {
+        return "listviewplugin/listview16.gif";
+    }
+
+    /**
+     * Returns the Button-Icon 
+     */
+    public String getButtonIconName() {
+        return "listviewplugin/listview16.gif";
+    }
+
+    /** Needed for Position */
+    private Point _locationListDialog = null;
+    /** Needed for Position */
+    private Dimension _dimensionListDialog = null;
+}
