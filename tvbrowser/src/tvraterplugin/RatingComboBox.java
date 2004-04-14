76cc450810f08e1e6d3852d150cc449030d85a99
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2004-04-14 17:21:13 +0000

    Every Rating-Category can have it's own text now

---
+++ tvbrowser/src/tvraterplugin/RatingComboBox.java
@@ -0,0 +1,75 @@
+/*
+ * Created on 14.04.2004
+ */
+package tvraterplugin;
+
+import java.awt.Component;
+
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+
+/**
+ * The RatingComboBox shows a specific Rating in a ComboBox
+ * 
+ * @author Bodo
+ */
+public class RatingComboBox extends JComboBox implements ListCellRenderer {
+   
+    /**
+     * Creates the Rating
+     * @param rating Rating to show
+     * @param type Type to show (Erotic etc)
+     */
+    public RatingComboBox(Rating rating, Object type) {
+        super(values);
+        _rating = rating;
+        _type = type;
+        
+        if ((_rating != null) && (_rating.getIntValue(type) >= 0)) {
+            setSelectedIndex(_rating.getIntValue(type));
+        }
+		setRenderer(this);
+    }
+
+    /*
+     * Renders the selection
+     * @see javax.swing.ListCellRenderer#getListCellRendererComponent(javax.swing.JList, java.lang.Object, int, boolean, boolean)
+     */
+    public Component getListCellRendererComponent(JList list, Object obj,
+            int index, boolean isSelected, boolean hasFocus) {
+
+        JLabel label = new JLabel();
+        label.setOpaque(true);
+        if (obj instanceof Integer) {
+            label.setIcon(RatingIconTextFactory.getImageIconForRating(((Integer)obj).intValue()));
+            label.setText(RatingIconTextFactory.getStringForRating(_type, ((Integer)obj).intValue()));
+        }
+        
+        if (isSelected || hasFocus) {
+            label.setBackground(list.getSelectionBackground());
+            label.setForeground(list.getSelectionForeground());
+        } else {
+            label.setBackground(list.getBackground());
+            label.setForeground(list.getForeground());
+        }
+        
+        return label;
+    }
+    
+    /**
+     * Type to show
+     */
+    private Object _type;
+    /**
+     * Rating to use
+     */
+    private Rating _rating;
+    
+    /**
+     * Selection
+     */
+	private static Integer[] values = { new Integer(0), new Integer(1), new Integer(2), new Integer(3), new Integer(4), new Integer(5) };
+}
--- tvbrowser/src/tvraterplugin/RatingIconTextFactory.java
+++ tvbrowser/src/tvraterplugin/RatingIconTextFactory.java
@@ -15,54 +15,69 @@ import util.ui.Localizer;
 public class RatingIconTextFactory {
 
     /** Localizer */
-    private static final Localizer _mLocalizer = Localizer
-            .getLocalizerFor(RatingIconTextFactory.class);
+    private static final Localizer _mLocalizer = Localizer.getLocalizerFor(RatingIconTextFactory.class);
 
-	/** Icons used to show the Ratings */
-   	final static ImageIcon _icons[] = new ImageIcon[] {
+    /** Icons used to show the Ratings */
+    final static ImageIcon _icons[] = new ImageIcon[] {
             new ImageIcon(RatingIconTextFactory.class.getResource("0.gif")),
             new ImageIcon(RatingIconTextFactory.class.getResource("1.gif")),
             new ImageIcon(RatingIconTextFactory.class.getResource("2.gif")),
             new ImageIcon(RatingIconTextFactory.class.getResource("3.gif")),
             new ImageIcon(RatingIconTextFactory.class.getResource("4.gif")),
-            new ImageIcon(RatingIconTextFactory.class.getResource("5.gif"))
-    };
+            new ImageIcon(RatingIconTextFactory.class.getResource("5.gif"))};
 
-	/**
-	 *	Returns the Icon for a specific Rating
+    /**
+     * Returns the Icon for a specific Rating
      */
-	public static ImageIcon getImageIconForRating(int rating) {
-	    if ((rating < 0) || (rating > _icons.length)) {
-	        return new ImageIcon();
-	    }
-	            
-	    return _icons[rating];
-	}
+    public static ImageIcon getImageIconForRating(int rating) {
+        if ((rating < 0) || (rating > _icons.length)) { return new ImageIcon(); }
 
-	
-	public static String getStringForRating(int rateing) {
+        return _icons[rating];
+    }
+
+    public static String getStringForRating(Object type, int rateing) {
         String ratingText;
 
+        String prefix = "";
+        
+        if (type == Rating.OVERALL) {
+            prefix = "Overall.";
+        } else if (type == Rating.ACTION) {
+            prefix = "Action.";
+        } else if (type == Rating.ENTITLEMENT) {
+            prefix = "Entitlement.";
+        } else if (type == Rating.EROTIC) {
+            prefix = "Erotic.";
+        } else if (type == Rating.FUN) {
+            prefix = "Fun.";
+        } else if (type == Rating.TENSION) {
+            prefix = "Tension.";
+        }
+        
+        
         switch (rateing) {
         case 0:
-            ratingText = _mLocalizer.msg("crap", "crap");
+            ratingText = _mLocalizer.msg(prefix+"crap", "crap");
             break;
         case 1:
-            ratingText = _mLocalizer.msg("mediocre", "mediocre");
+            ratingText = _mLocalizer.msg(prefix+"mediocre", "mediocre");
             break;
         case 2:
-            ratingText = _mLocalizer.msg("nice", "nice");
+            ratingText = _mLocalizer.msg(prefix+"nice", "nice");
             break;
         case 3:
-            ratingText = _mLocalizer.msg("enjoyable", "enjoyable");
+            ratingText = _mLocalizer.msg(prefix+"enjoyable", "enjoyable");
             break;
         case 4:
-            ratingText = _mLocalizer.msg("excellent", "excellent");
+            ratingText = _mLocalizer.msg(prefix+"excellent", "excellent");
+            break;
+        case 5:
+            ratingText = _mLocalizer.msg(prefix+"super", "super");
             break;
         default:
-            ratingText = _mLocalizer.msg("super", "super");
+            ratingText = "-";
             break;
         }
         return ratingText;
-	}
+    }
 }
