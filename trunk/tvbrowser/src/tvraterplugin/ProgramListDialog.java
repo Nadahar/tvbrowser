330f11c29a3b929ddc9ab363c0db0de05e7af178
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2004-04-16 13:49:39 +0000

    Using same Texts like TVBrowser usually does (Close, not OK etc..), setting default-buttons

--- trunk/tvbrowser/src/tvraterplugin/ProgramListDialog.java
+++ trunk/tvbrowser/src/tvraterplugin/ProgramListDialog.java
@@ -44,7 +44,7 @@ public class ProgramListDialog extends JDialog {
      */
     public ProgramListDialog(Frame parent, String title) {
         super(parent, true);
-        setTitle(mLocalizer.msg("ListDialog", "Listening"));
+        setTitle(mLocalizer.msg("ListDialogTitle", "Programs with \"{0}\"", title));
 
         _title = title;
 
@@ -117,14 +117,16 @@ public class ProgramListDialog extends JDialog {
         JPanel buttonPn = new JPanel(new FlowLayout(FlowLayout.TRAILING));
         content.add(buttonPn, BorderLayout.SOUTH);
 
-        JButton okButton = new JButton(mLocalizer.msg("ok", "OK"));
-        okButton.addActionListener(new ActionListener() {
+        JButton closeButton = new JButton(mLocalizer.msg("close", "Close"));
+        closeButton.addActionListener(new ActionListener() {
 
             public void actionPerformed(ActionEvent evt) {
                 dispose();
             }
         });
-        buttonPn.add(okButton);
+        buttonPn.add(closeButton);
+
+        getRootPane().setDefaultButton(closeButton);
         pack();
     }
 
