f9741007ecf9563a2cd425e33cc5f324ca49ae54
darras <darras@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2004-04-25 12:40:18 +0000

    prepared for release 0.9.8

--- trunk/tvbrowser/src/tvbrowser/ui/programtable/DefaultProgramTableModel.java
+++ trunk/tvbrowser/src/tvbrowser/ui/programtable/DefaultProgramTableModel.java
@@ -106,7 +106,6 @@ public class DefaultProgramTableModel implements ProgramTableModel, ChangeListen
     if (channelArr == null) {
       throw new NullPointerException("shownChannelArr is null!");
     }
-    System.out.println("now "+channelArr.length+" channels available");
     mChannelArr = channelArr;
     
 	  mProgramColumn=new ArrayList[mChannelArr.length];
@@ -119,8 +118,7 @@ public class DefaultProgramTableModel implements ProgramTableModel, ChangeListen
   
   
   public void setProgramFilter(ProgramFilter filter) {
-    System.out.println("new Filter set: "+filter);
-  	mProgramFilter=filter;
+    mProgramFilter=filter;
     fireTableDataChanged();
     updateTableContent();
   }
@@ -172,8 +170,6 @@ public class DefaultProgramTableModel implements ProgramTableModel, ChangeListen
       monitor.setValue(0);
     }
     
-    System.out.println("number of channels: "+mChannelArr.length);
-    
     for (int i = 0; i < mChannelArr.length; i++) {
       
       mProgramColumn[i].clear();
@@ -196,11 +192,9 @@ public class DefaultProgramTableModel implements ProgramTableModel, ChangeListen
     }
     boolean showEmptyColumns = mProgramFilter instanceof tvbrowser.core.filters.ShowAllFilter;
     
-      System.out.println("ShowAllFilter is NOT set");
       ArrayList newShownColumns = new ArrayList();
       ArrayList newShownChannels = new ArrayList();
       for (int i=0; i<mProgramColumn.length; i++) {
-        //System.out.println("# of progs in col "+i+": "+mProgramColumn[i].size());
         if (showEmptyColumns || mProgramColumn[i].size()>0) {
           newShownColumns.add(mProgramColumn[i]);
           newShownChannels.add(mChannelArr[i]);
@@ -212,7 +206,6 @@ public class DefaultProgramTableModel implements ProgramTableModel, ChangeListen
       newShownColumns.toArray(mShownProgramColumn);
       newShownChannels.toArray(mShownChannelArr);
     
-    if (mShownProgramColumn!=null) System.out.println("number of shown columns AFTER: "+mShownProgramColumn.length);
      
     
     SwingUtilities.invokeLater(new Runnable() {
