e450f59718034fab383d8f6c134122ad82c096a3
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2004-04-27 16:09:46 +0000

    Bug in Case-Sensitvity removed

--- tvbrowser/src/tvraterplugin/Database.java
+++ tvbrowser/src/tvraterplugin/Database.java
@@ -1,36 +1,31 @@
 /*
- * TV-Browser
- * Copyright (C) 04-2003 Martin Oberhauser (martin_oat@yahoo.de)
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * TV-Browser Copyright (C) 04-2003 Martin Oberhauser (martin_oat@yahoo.de)
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
  */
-
 package tvraterplugin;
-
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-
 import devplugin.Program;
-
 /**
- * This class holds the "Database" for the Ratings. Could be 
- * replaced by a real Database in a later Version...
+ * This class holds the "Database" for the Ratings. Could be replaced by a real
+ * Database in a later Version...
  * 
  * @author bodo tasche
  */
@@ -41,123 +36,122 @@ public class Database {
 	private HashMap _personalrating = new HashMap();
 	/** The changes since the last contact with the Server */
 	private ArrayList _changedpersonal = new ArrayList();
-
 	/**
 	 * Gets the overall Ratings in this Database
+	 * 
 	 * @return all overall Ratings
 	 */
 	public Collection getOverallRating() {
 		return _overalrating.values();
 	}
-
 	/**
 	 * Gets one overall Rating for a specific Program
+	 * 
 	 * @param program get Rating for this Program
 	 * @return the overall Rating
 	 */
 	public Rating getOverallRating(Program program) {
 		return getOverallRating(program.getTitle().toLowerCase());
 	}
-
 	/**
 	 * Gets one overall Rating for a specific title
-	 * @param title get rating for this Title  
+	 * 
+	 * @param title get rating for this Title
 	 * @return the overall Rating
 	 */
 	public Rating getOverallRating(String title) {
 		if (_overalrating.get(title.toLowerCase()) != null) {
-			return (Rating)_overalrating.get(title.toLowerCase());
+			return (Rating) _overalrating.get(title.toLowerCase());
 		}
 		return null;
 	}
-	
 	/**
 	 * Sets the Rating
+	 * 
 	 * @param rating
 	 */
 	public void setOverallRating(Rating rating) {
 		_overalrating.put(rating.getTitle().toLowerCase(), rating);
 	}
-	
 	/**
 	 * Gets the personal Ratings in this Database
+	 * 
 	 * @return all personal Ratings
 	 */
 	public Collection getPersonalRating() {
 		return _personalrating.values();
 	}
-
 	/**
 	 * Gets one personal Rating for a specific Program
+	 * 
 	 * @param program get Rating for this Program
 	 * @return the personal Rating
 	 */
 	public Rating getPersonalRating(Program program) {
-		return getPersonalRating(program.getTitle().toLowerCase());
+		Rating rating = getPersonalRating(program.getTitle().toLowerCase());
+		if (rating == null) {
+			rating = getPersonalRating(program.getTitle());
+		}
+		return rating;
 	}
-
 	/**
 	 * Gets one personal Rating for a specific title
-	 * @param title get rating for this Title  
+	 * 
+	 * @param title get rating for this Title
 	 * @return the personal Rating
 	 */
 	public Rating getPersonalRating(String title) {
 		if (_personalrating.get(title.toLowerCase()) != null) {
-			return (Rating)_personalrating.get(title.toLowerCase());
+			return (Rating) _personalrating.get(title.toLowerCase());
+		} else if (_personalrating.get(title) != null) {
+			return (Rating) _personalrating.get(title);
 		}
-
+		
 		return null;
 	}
-
-
 	/**
 	 * Saves the personal Rating in the Database
-	 * @param rating save this Rating 
+	 * 
+	 * @param rating save this Rating
 	 */
 	public void setPersonalRating(Rating rating) {
 		if (_personalrating.get(rating.getTitle().toLowerCase()) == null) {
 			_personalrating.put(rating.getTitle().toLowerCase(), rating);
-		} 
-		
+		}
 		if (!_changedpersonal.contains(rating)) {
 			_changedpersonal.add(rating);
 		}
 	}
-
 	public ArrayList getChangedPersonal() {
 		return _changedpersonal;
 	}
-
 	/**
 	 * Empties the ChangedPersonal List
 	 */
 	public void clearChangedPersonal() {
 		_changedpersonal = new ArrayList();
 	}
-
 	/**
 	 * Empties the ChangedPersonal List
 	 */
 	public void clearOverall() {
 		_overalrating = new HashMap();
-	}	
-	
+	}
 	/**
-	 * Called by the host-application during start-up. 
-	 * Loads the data.
-	 *
+	 * Called by the host-application during start-up. Loads the data.
+	 * 
 	 * @see #writeData(ObjectOutputStream)
 	 */
-	public void readData(ObjectInputStream in) throws IOException, ClassNotFoundException {
+	public void readData(ObjectInputStream in) throws IOException,
+			ClassNotFoundException {
 		_personalrating = (HashMap) in.readObject();
 		_overalrating = (HashMap) in.readObject();
 		_changedpersonal = (ArrayList) in.readObject();
 	}
-
 	/**
-	 * Counterpart to loadData. Called when the application shuts down.
-	 * Saves the data.
-	 *
+	 * Counterpart to loadData. Called when the application shuts down. Saves
+	 * the data.
+	 * 
 	 * @see #readData(ObjectInputStream)
 	 */
 	public void writeData(ObjectOutputStream out) throws IOException {
@@ -165,5 +159,4 @@ public class Database {
 		out.writeObject(_overalrating);
 		out.writeObject(_changedpersonal);
 	}
-
 }
