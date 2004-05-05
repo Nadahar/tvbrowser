63720a8e5b3f2266a9fe62dd5354af6c0305a21a
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2004-05-05 20:40:40 +0000

    Fixed Nullpointer in Updater.java

--- tvbrowser/src/tvraterplugin/RatingCellRenderer.java
+++ tvbrowser/src/tvraterplugin/RatingCellRenderer.java
@@ -1,27 +1,23 @@
 /*
- * TV-Browser
- * Copyright (C) 04-2003 Martin Oberhauser (martin_oat@yahoo.de)
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * TV-Browser Copyright (C) 04-2003 Martin Oberhauser (martin_oat@yahoo.de)
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
  */
-
 package tvraterplugin;
 
 import java.awt.Component;
-
-import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.ListCellRenderer;
@@ -32,7 +28,6 @@ import javax.swing.ListCellRenderer;
  * @author bodo
  */
 public class RatingCellRenderer extends JLabel implements ListCellRenderer {
-
 
     public RatingCellRenderer() {
         setOpaque(true);
@@ -46,30 +41,39 @@ public class RatingCellRenderer extends JLabel implements ListCellRenderer {
      */
     public Component getListCellRendererComponent(JList list, Object obj,
             int index, boolean isSelected, boolean hasFocus) {
-        
-        if (obj == null) {
-            return new JLabel("Empty");
+       
+        if (obj == null) { 
+            return new JLabel("");
         }
+
         if (obj instanceof Rating) {
-        	Rating rating = (Rating) obj;
-        	Rating personal = TVRaterPlugin.getInstance().getDatabase().getPersonalRating(rating.getTitle());
-        	
-            int overall = rating.getIntValue(Rating.OVERALL);
 
-            if ((personal == null) || (personal.getIntValue(Rating.OVERALL) == -1)){
-                ImageIcon rateing = RatingIconTextFactory.getImageIconForRating(overall);
-                this.setIcon(rateing);
-            } else {
-                int personaloverall = personal.getIntValue(Rating.OVERALL);
-            	CompositeIcon composite = new CompositeIcon(
-            			RatingIconTextFactory.getImageIconForRating(overall),
-            			RatingIconTextFactory.getImageIconForRating(personaloverall)
-            	);
-            	this.setIcon(composite);
+            Rating rating = TVRaterPlugin.getInstance().getDatabase()
+                    .getOverallRating(((Rating) obj).getTitle());
+            
+            Rating personal = TVRaterPlugin.getInstance().getDatabase()
+                    .getPersonalRating(((Rating) obj).getTitle());
+
+            int overall = -1;
+            int personaloverall = -1;
+
+            if (personal != null) {
+                personaloverall = personal.getIntValue(Rating.OVERALL);
             }
-			
-            this.setText(((Rating) obj).getTitle());
 
+            if (rating != null) {
+                overall = rating.getIntValue(Rating.OVERALL);
+            }
+
+            CompositeIcon composite = new CompositeIcon(
+                     RatingIconTextFactory.getImageIconForRating(overall),
+                     RatingIconTextFactory
+                             .getImageIconForRating(personaloverall));
+            
+            this.setIcon(composite);
+        
+            this.setText(((Rating) obj).getTitle());
+            
             if (isSelected || hasFocus) {
                 this.setBackground(list.getSelectionBackground());
                 this.setForeground(list.getSelectionForeground());
@@ -77,11 +81,9 @@ public class RatingCellRenderer extends JLabel implements ListCellRenderer {
                 this.setBackground(list.getBackground());
                 this.setForeground(list.getForeground());
             }
-
+            
             return this;
         }
-
         return new JLabel(obj.toString());
     }
-
 }
