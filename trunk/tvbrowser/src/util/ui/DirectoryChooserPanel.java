7dfa0e6efc33ef1518b9370c5a6a7748aa1787a4
darras <darras@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2004-06-08 21:43:26 +0000

    DirectoryChooserPanel moved to util.ui.

---
+++ trunk/tvbrowser/src/util/ui/DirectoryChooserPanel.java
@@ -0,0 +1,84 @@
+/*
+ * TV-Browser
+ * Copyright (C) 04-2003 Martin Oberhauser (darras@users.sourceforge.net)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * CVS information:
+ *  $RCSfile$
+ *   $Source$
+ *     $Date$
+ *   $Author$
+ * $Revision$
+ */
+ 
+package util.ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+public class DirectoryChooserPanel extends JPanel {
+ 
+  /** The localizer for this class. */
+         private static final util.ui.Localizer mLocalizer
+         = util.ui.Localizer.getLocalizerFor(DirectoryChooserPanel.class);
+ 
+  private JTextField mTextField;
+  private JButton mBtn;
+  private JLabel mLabel;
+  
+  public DirectoryChooserPanel(String title, String text) {
+    
+    setLayout(new BorderLayout(7,0));
+    mLabel=new JLabel(title);
+    mLabel.setBorder(BorderFactory.createEmptyBorder(0,13,0,0));
+    add(mLabel,BorderLayout.WEST);
+    
+    mTextField=new JTextField(text);
+    add(mTextField,BorderLayout.CENTER);
+    
+    mBtn=new JButton(mLocalizer.msg("change","change"));
+    mBtn.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent event) {
+        JFileChooser fc =new JFileChooser();
+        fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        fc.setApproveButtonText("Ok");
+        fc.setCurrentDirectory(new File(mTextField.getText()));
+        int retVal=fc.showOpenDialog(getParent());
+        if (retVal==JFileChooser.APPROVE_OPTION) {
+          File f=fc.getSelectedFile();
+          mTextField.setText(f.getAbsolutePath());
+        }
+      }
+    });
+    
+    add(mBtn,BorderLayout.EAST); 
+     
+  }
+  
+  public void setEnabled(boolean enabled) {
+    super.setEnabled(enabled);
+    mTextField.setEnabled(enabled);
+    mBtn.setEnabled(enabled);  
+    mLabel.setEnabled(enabled);
+  }
+  
+  public String getText() {
+    return mTextField.getText();
+  }
+}
