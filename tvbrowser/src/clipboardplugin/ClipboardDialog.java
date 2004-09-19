ba336b48e6e944508293985311c66d817ef0f5a7
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2004-09-19 14:56:07 +0000

    ClipBoardPlugin first commit

---
+++ tvbrowser/src/clipboardplugin/ClipboardDialog.java
@@ -0,0 +1,283 @@
+/*
+ * Created on 11.04.2004
+ */
+package clipboardplugin;
+
+import java.awt.BorderLayout;
+import java.awt.Frame;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Calendar;
+import java.util.Vector;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
+
+import util.ui.ImageUtilities;
+import util.ui.ProgramListCellRenderer;
+import util.ui.UiUtilities;
+import devplugin.Plugin;
+import devplugin.Program;
+
+/**
+ * Creates a Dialog with a List of Programs
+ * 
+ * @author bodo
+ */
+public class ClipboardDialog extends JDialog {
+
+    /** The localizer used by this class. */
+    private static final util.ui.Localizer mLocalizer = util.ui.Localizer.getLocalizerFor(ClipboardDialog.class);
+
+    /** JList component */
+    private JList mProgramJList;
+
+    /** ParentFrame */
+    private Frame mFrame;
+
+    /** Vector with Programs */
+    private Vector mClipList;
+
+    /** Plugin that called this Dialog */
+    private Plugin mPlugin;
+
+    /**
+     * Creates the Dialog
+     * 
+     * @param frame Frame for modal
+     * @param plugin Plugin for reference
+     * @param clipboard
+     */
+    public ClipboardDialog(Frame frame, Plugin plugin, Vector clipboard) {
+        super(frame, true);
+        setTitle(mLocalizer.msg("viewList", "View List:"));
+        mClipList = clipboard;
+        mFrame = frame;
+        mPlugin = plugin;
+        createGUI();
+    }
+
+    /**
+     * Returns the current Time in minutes
+     * 
+     * @return Time in minutes
+     */
+    private int getCurrentTime() {
+        Calendar cal = Calendar.getInstance();
+        return cal.get(Calendar.HOUR_OF_DAY) * 60 + cal.get(Calendar.MINUTE);
+    }
+
+    /**
+     * Creates the GUI
+     */
+    private void createGUI() {
+        JPanel content = (JPanel) this.getContentPane();
+        content.setLayout(new BorderLayout());
+        content.setBorder(UiUtilities.DIALOG_BORDER);
+        mProgramJList = new JList(mClipList);
+
+        mProgramJList.setCellRenderer(new ProgramListCellRenderer());
+
+        mProgramJList.addMouseListener(new MouseAdapter() {
+
+            public void mouseClicked(MouseEvent e) {
+                if (SwingUtilities.isRightMouseButton(e)) {
+                    int inx = mProgramJList.locationToIndex(e.getPoint());
+                    if (inx == -1) { return; }
+                    Program p = (Program) mProgramJList.getModel().getElementAt(inx);
+                    mProgramJList.setSelectedIndex(inx);
+                    JPopupMenu menu = devplugin.Plugin.getPluginManager().createPluginContextMenu(p, mPlugin);
+                    menu.show(mProgramJList, e.getX() - 15, e.getY() - 15);
+                } else if (SwingUtilities.isLeftMouseButton(e) && (e.getClickCount() == 2)) {
+                    int inx = mProgramJList.locationToIndex(e.getPoint());
+
+                    if (inx == -1) { return; }
+                    Program p = (Program) mProgramJList.getModel().getElementAt(inx);
+
+                    Plugin plugin = devplugin.Plugin.getPluginManager().getDefaultContextMenuPlugin();
+                    if (plugin != null) {
+                        plugin.execute(p);
+                    }
+                }
+            }
+        });
+
+        JScrollPane scroll = new JScrollPane(mProgramJList);
+
+        content.add(scroll, BorderLayout.CENTER);
+
+        JPanel buttonRight = new JPanel(new GridBagLayout());
+
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(0, 5, 5, 0);
+        c.anchor = GridBagConstraints.NORTH;
+        c.gridwidth = GridBagConstraints.REMAINDER;
+
+        JButton upButton = new JButton(new ImageIcon("imgs/up16.gif"));
+        upButton.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent arg0) {
+                upItems();
+            }
+        });
+        buttonRight.add(upButton, c);
+
+        JButton downButton = new JButton(new ImageIcon("imgs/down16.gif"));
+        downButton.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent arg0) {
+                downItems();
+            }
+        });
+        buttonRight.add(downButton, c);
+
+        c.weighty = 1.0;
+
+        JButton deleteButton = new JButton(ImageUtilities.createImageIconFromJar("clipboardplugin/Delete16.gif", getClass()));
+
+        deleteButton.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent arg0) {
+                deleteItems();
+            }
+        });
+
+        buttonRight.add(deleteButton, c);
+
+        content.add(buttonRight, BorderLayout.EAST);
+
+        JButton sendButton = new JButton();
+
+        sendButton.setIcon(ImageUtilities.createImageIconFromJar("clipboardplugin/send.png", getClass()));
+        sendButton.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                showSendDialog(mPlugin);
+            }
+
+        });
+
+        JPanel buttonPn = new JPanel(new BorderLayout());
+        buttonPn.setBorder(BorderFactory.createEmptyBorder(5, 0, 5, 0));
+
+        content.add(buttonPn, BorderLayout.SOUTH);
+
+        buttonPn.add(sendButton, BorderLayout.WEST);
+
+        JButton closeButton = new JButton(mLocalizer.msg("close", "Close"));
+        closeButton.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent evt) {
+                dispose();
+            }
+        });
+
+        buttonPn.add(closeButton, BorderLayout.EAST);
+        getRootPane().setDefaultButton(closeButton);
+
+    }
+
+    /**
+     * Move Item Down
+     */
+    protected void downItems() {
+        moveSelectedItems(1);
+    }
+
+    /**
+     * Move Item Up
+     */
+    protected void upItems() {
+        moveSelectedItems(-1);
+    }
+
+    /**
+     * Moves the selected Items
+     * 
+     * @param nrRows # Rows to Move
+     */
+    protected void moveSelectedItems(int nrRows) {
+        int[] selection = mProgramJList.getSelectedIndices();
+        if (selection.length == 0) return;
+
+        int insertPos = selection[0] + nrRows;
+        if (insertPos < 0) {
+            insertPos = 0;
+        }
+        if (insertPos > mClipList.size() - selection.length) {
+            insertPos = mClipList.size() - selection.length;
+        }
+
+        // Markierte Zeilen merken und entfernen
+        Object[] selectedRows = new Object[selection.length];
+        for (int i = selectedRows.length - 1; i >= 0; i--) {
+            selectedRows[i] = mClipList.elementAt(selection[i]);
+            mClipList.removeElementAt(selection[i]);
+        }
+
+        // Zeilen wieder einf�gen
+        for (int i = 0; i < selectedRows.length; i++) {
+            mClipList.insertElementAt(selectedRows[i], insertPos + i);
+        }
+
+        // Zeilen markieren
+        mProgramJList.getSelectionModel().setSelectionInterval(insertPos, insertPos + selection.length - 1);
+
+        // Scrollen
+        int scrollPos = insertPos;
+        if (nrRows > 0) scrollPos += selection.length;
+        mProgramJList.ensureIndexIsVisible(scrollPos);
+        mProgramJList.updateUI();
+    }
+
+    /**
+     * Delete-Button was pressed.
+     */
+    protected void deleteItems() {
+        if ((mClipList.size() == 0) || (mProgramJList.getSelectedIndex() >= mClipList.size())) { return; }
+
+        Object[] obj = mProgramJList.getSelectedValues();
+        for (int i = 0; i < obj.length; i++) {
+            mClipList.remove(obj[i]);
+            Program prg = (Program) obj[i];
+            prg.unmark(mPlugin);
+        }
+
+        if (mClipList.size() == 0) {
+            hide();
+        }
+
+        mProgramJList.updateUI();
+    }
+
+    /**
+     * Shows the Send-Dialog
+     * 
+     * @param plugin Plugin that called
+     * @param dialog
+     */
+    private void showSendDialog(Plugin plugin) {
+        Program[] prgList = new Program[mClipList.size()];
+
+        for (int i = 0; i < mClipList.size(); i++) {
+            prgList[i] = (Program) mClipList.get(i);
+        }
+
+        SendDialog send = new SendDialog(plugin, this, prgList);
+
+        send.show();
+    }
+
+}
---
+++ tvbrowser/src/clipboardplugin/SendDialog.java
@@ -0,0 +1,181 @@
+/*
+ * Created on 25.06.2004
+ */
+package clipboardplugin;
+
+import java.awt.Dialog;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Vector;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import util.ui.Localizer;
+import devplugin.Plugin;
+import devplugin.Program;
+
+/**
+ * Ein Dialog, der es erlaubt, Programme an andere Plugins weiter zu reichen
+ * 
+ * @author bodum
+ */
+public class SendDialog extends JDialog {
+
+    /** Translator */
+    private static final Localizer mLocalizer = Localizer.getLocalizerFor(SendDialog.class);
+
+    /**
+     * Programs to send
+     */
+    private Program[] mPrograms;
+
+    /**
+     * List of Plugins
+     */
+    private JComboBox mPluginList;
+
+    /**
+     * The Sender-Plugin
+     */
+    private Plugin mPlugin;
+
+    /**
+     * Create the Dialog
+     * 
+     * @param plug Sender-Plugin
+     * @param owner Owner Frame
+     * @param prg List of Programs to send
+     */
+    public SendDialog(Plugin plug, Frame owner, Program[] prg) {
+        super(owner, true);
+        mPrograms = prg;
+        mPlugin = plug;
+        createDialog();
+        setLocationRelativeTo(owner);
+    }
+
+    /**
+     * Create the Dialog
+     * 
+     * @param plug Sender-Plugin
+     * @param owner Owner Frame
+     * @param prg List of Programs to send
+     */
+    public SendDialog(Plugin plug, Dialog owner, Program[] prg) {
+        super(owner, true);
+        mPrograms = prg;
+        mPlugin = plug;
+        createDialog();
+        setLocationRelativeTo(owner);
+    }
+
+    /**
+     * Creates the Dialog
+     */
+    private void createDialog() {
+        setTitle(mLocalizer.msg("title", "Send to other Plugin"));
+
+        JPanel panel = (JPanel) this.getContentPane();
+
+        panel.setLayout(new GridBagLayout());
+
+        // get the installed plugins
+        Plugin[] installedPluginArr = Plugin.getPluginManager().getInstalledPlugins();
+
+        Arrays.sort(installedPluginArr, new ObjectComperator());
+
+        // create a list of those who support multiple program execution
+        Vector selectablePluginList = new Vector();
+        for (int i = 0; i < installedPluginArr.length; i++) {
+            if ((installedPluginArr[i] != mPlugin) && (installedPluginArr[i].supportMultipleProgramExecution())) {
+                selectablePluginList.add(installedPluginArr[i]);
+            }
+        }
+
+        mPluginList = new JComboBox(selectablePluginList);
+
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.gridwidth = GridBagConstraints.RELATIVE;
+        c.weightx = 0;
+        c.weighty = 0;
+        c.insets = new Insets(5, 5, 5, 0);
+        c.anchor = GridBagConstraints.WEST;
+
+        panel.add(new JLabel(mLocalizer.msg("sendTo", "Send programs to")), c);
+
+        c.gridwidth = GridBagConstraints.REMAINDER;
+        c.weightx = 1.0;
+        c.weighty = 0;
+        c.insets = new Insets(5, 5, 5, 5);
+
+        panel.add(mPluginList, c);
+
+        c.gridwidth = GridBagConstraints.REMAINDER;
+        c.anchor = GridBagConstraints.SOUTHEAST;
+        c.weightx = 1.0;
+        c.weighty = 1.0;
+
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+
+        JButton sendButton = new JButton(mLocalizer.msg("send", "Send"));
+
+        sendButton.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent evt) {
+                send();
+                hide();
+            }
+        });
+
+        buttonPanel.add(sendButton);
+
+        JButton cancelButton = new JButton(mLocalizer.msg("cancel", "Cancel"));
+
+        cancelButton.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent evt) {
+                hide();
+            }
+        });
+
+        buttonPanel.add(cancelButton);
+
+        c.insets = new Insets(5, 5, 5, 0);
+        panel.add(buttonPanel, c);
+
+        pack();
+        setResizable(false);
+    }
+
+    /**
+     * Sends the Data to the selected Plugin
+     */
+    protected void send() {
+        Plugin plug = (Plugin) mPluginList.getSelectedItem();
+
+        plug.execute(mPrograms);
+    }
+
+    /**
+     * Comperator needed to Sort List of Plugins
+     */
+    private class ObjectComperator implements Comparator {
+
+        public int compare(Object o1, Object o2) {
+            return o1.toString().compareTo(o2.toString());
+        }
+
+    }
+}
