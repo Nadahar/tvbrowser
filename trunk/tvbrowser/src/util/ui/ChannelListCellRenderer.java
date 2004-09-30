5eac20cd5abe0c276379faf1a023bb8444737f76
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2004-09-30 14:57:44 +0000

    Now using ChannelLabel

--- trunk/tvbrowser/src/util/ui/ChannelListCellRenderer.java
+++ trunk/tvbrowser/src/util/ui/ChannelListCellRenderer.java
@@ -1,5 +1,27 @@
 /*
- * Created on 28.09.2004
+ * TV-Browser
+ * Copyright (C) 04-2003 Martin Oberhauser (martin_oat@yahoo.de)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * CVS information:
+ *  $RCSfile$
+ *   $Source$
+ *     $Date$
+ *   $Author$
+ * $Revision$
  */
 package util.ui;
 
@@ -13,7 +35,7 @@ import devplugin.Channel;
 
 
 /**
- * @author bodum
+ * A ListCellRenderer for Channel-Lists
  */
 public class ChannelListCellRenderer extends DefaultListCellRenderer {
 
@@ -22,7 +44,14 @@ public class ChannelListCellRenderer extends DefaultListCellRenderer {
         
         
         if (value instanceof Channel) {
-            label.setIcon(((Channel)value).getIcon());
+            
+            ChannelLabel cLabel = new ChannelLabel();
+            cLabel.setChannel((Channel)value);
+            cLabel.setOpaque(label.isOpaque());
+            cLabel.setBackground(label.getBackground());
+            cLabel.setForeground(label.getForeground());
+            
+            return cLabel;
         }
         
         return label;
