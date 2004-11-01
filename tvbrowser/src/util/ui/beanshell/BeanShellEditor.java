04e9eaf195798fc4b610c7a01a01468b7ee55424
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2004-11-01 19:23:34 +0000

    Added Editor for BeanShell (maybee add Syntax-Highlighter later)

---
+++ tvbrowser/src/util/ui/beanshell/BeanShellEditor.java
@@ -0,0 +1,19 @@
+/*
+ * Created on 01.11.2004
+ */
+package util.ui.beanshell;
+
+import javax.swing.JTextArea;
+
+
+/**
+ * @author bodum
+ */
+public class BeanShellEditor extends JTextArea {
+	public BeanShellEditor() {
+	   setTabSize(1);
+	   setLineWrap(false);
+    }
+	
+	
+}
