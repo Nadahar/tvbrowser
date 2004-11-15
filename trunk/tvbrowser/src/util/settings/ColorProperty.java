25ab1aa505fc5ef6c2a891e8a77756bfa51a35e5
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2004-11-15 16:22:52 +0000

    Fixed loading/saving of Property

--- trunk/tvbrowser/src/util/settings/ColorProperty.java
+++ trunk/tvbrowser/src/util/settings/ColorProperty.java
@@ -65,20 +65,20 @@ public final class ColorProperty extends Property {
 
             } else {
                 try {
-                    
                     if ((asString.length() == 9) && (asString.startsWith("#"))) {
-                        
-                        int red = Integer.parseInt(asString.substring(1,2));
-                        int green = Integer.parseInt(asString.substring(3,4));
-                        int blue = Integer.parseInt(asString.substring(5,6));
-                        int alpha = Integer.parseInt(asString.substring(7,8));
+                        int red = Integer.parseInt(asString.substring(1,3),16);
+                        int green = Integer.parseInt(asString.substring(3,5),16);
+                        int blue = Integer.parseInt(asString.substring(5,7),16);
+                        int alpha = Integer.parseInt(asString.substring(7,9),16);
                         
                         Color c = new Color(red, green, blue, alpha);
+                        mCachedValue = c;
                     } else {
                         mCachedValue = mDefaultColor;
                     }
                     //mCachedValue = Integer.parseInt(asString);
                 } catch (Exception exc) {
+                    exc.printStackTrace();
                     mCachedValue = mDefaultColor;
                 }
             }
@@ -97,18 +97,26 @@ public final class ColorProperty extends Property {
         if (color == mDefaultColor) {
             setProperty(null);
         } else {
-
+            mCachedValue = color;
+            
             String value = "#";
 
-            value += Integer.toString(color.getRed(), 16);
-            value += Integer.toString(color.getGreen(), 16);
-            value += Integer.toString(color.getBlue(), 16);
-            value += Integer.toString(color.getAlpha(), 16);
+            value += addZero(Integer.toString(color.getRed(), 16),2);
+            value += addZero(Integer.toString(color.getGreen(), 16),2);
+            value += addZero(Integer.toString(color.getBlue(), 16),2);
+            value += addZero(Integer.toString(color.getAlpha(), 16),2);
 
             setProperty(value);
         }
     }
 
+    private String addZero(String str, int len) {
+        while (str.length() < len) {
+            str = "0" + str;
+        }
+        return str;
+    }
+    
     /*
      * (non-Javadoc)
      * 
