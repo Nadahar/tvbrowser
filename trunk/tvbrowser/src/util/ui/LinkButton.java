e12912e9dfe58f4aabb76525aa6b7a5185f51847
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2005-01-15 15:04:37 +0000

    Added comments and new Constructor

--- trunk/tvbrowser/src/util/ui/LinkButton.java
+++ trunk/tvbrowser/src/util/ui/LinkButton.java
@@ -13,16 +13,39 @@ import javax.swing.plaf.metal.MetalButtonUI;
 
 
 /**
+ * A Button for Web-Links
  * @author bodum
  */
 public class LinkButton extends JButton implements ActionListener{
 
+    /** URL */
     private String mUrl;
     
+    /**
+     * Create a Link-Button for URL
+     * @param url Url to show
+     */
+    public LinkButton(String url) {
+        super("<html><FONT COLOR='BLUE'><u>"+url+"</u></FONT></html>");
+        mUrl = url;
+        createButton();
+    }
+    
+    /**
+     * Create a Link-Button for URL
+     * @param text Text to show
+     * @param url URL to use
+     */
     public LinkButton(String text, String url) {
         super("<html><FONT COLOR='BLUE'><u>"+text+"</u></FONT></html>");
         mUrl = url;
-        
+        createButton();
+    }
+
+    /**
+     * creates the Button 
+     */
+    private void createButton() {
         setBorder(BorderFactory.createEmptyBorder());
         setCursor(new Cursor(Cursor.HAND_CURSOR));
         setContentAreaFilled(false);
@@ -31,7 +54,7 @@ public class LinkButton extends JButton implements ActionListener{
         setUI(new MetalButtonUI());
         addActionListener(this);
     }
-
+    
     /* (non-Javadoc)
      * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
      */
