049df33f5c1de51159a0b16769fca1814264eb8a
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2005-02-13 13:47:06 +0000

    Settings added for EMail-Plugin

--- tvbrowser/src/emailplugin/EMailPlugin.java
+++ tvbrowser/src/emailplugin/EMailPlugin.java
@@ -1,31 +1,63 @@
 /*
- * Created on 11.02.2005
+ * EMailPlugin by Bodo Tasche
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * CVS information:
+ *  $RCSfile$
+ *   $Source$
+ *     $Date$
+ *   $Author$
+ * $Revision$
  */
 package emailplugin;
 
 import java.awt.event.ActionEvent;
+import java.io.File;
 import java.net.URLEncoder;
+import java.util.Properties;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 
+import util.exc.ErrorHandler;
 import util.ui.ImageUtilities;
 import util.ui.Localizer;
+import util.ui.UiUtilities;
 import devplugin.ActionMenu;
 import devplugin.Plugin;
 import devplugin.PluginInfo;
 import devplugin.Program;
 import devplugin.ProgramFieldType;
+import devplugin.SettingsTab;
 import devplugin.Version;
 
 /**
+ * This Plugin makes it possible to send an email with short
+ * info's about the Program
+ * 
  * @author bodum
  */
 public class EMailPlugin extends Plugin {
     /** Translator */
     private static final Localizer mLocalizer = Localizer.getLocalizerFor(EMailPlugin.class);
-
+    /** Properties */
+    private Properties mSettings;
+    
     /*
      * (non-Javadoc)
      * 
@@ -83,6 +115,34 @@ public class EMailPlugin extends Plugin {
         createMail(programArr);
     }    
     
+	/* (non-Javadoc)
+	 * @see devplugin.Plugin#getSettingsTab()
+	 */
+	public SettingsTab getSettingsTab() {
+
+		EMailSettingsTab tab = new EMailSettingsTab(mSettings);
+		
+		return tab;
+	}
+	
+	/* (non-Javadoc)
+	 * @see devplugin.Plugin#loadSettings(java.util.Properties)
+	 */
+	public void loadSettings(Properties settings) {
+		mSettings = settings;
+	}
+	
+	/* (non-Javadoc)
+	 * @see devplugin.Plugin#storeSettings()
+	 */
+	public Properties storeSettings() {
+		return mSettings;
+	}
+	
+    /**
+     * Create the Mail
+     * @param program Programs to show in the Mail
+     */
     private void createMail(Program[] program) {
     	
     	String emailText = new String();
@@ -91,12 +151,26 @@ public class EMailPlugin extends Plugin {
     		emailText += program[i].getChannel().getName() + " " + program[i].getDateString() + ", " + program[i].getTimeString() + "-" + program[i].getTimeFieldAsString(ProgramFieldType.END_TIME_TYPE) + " " + program[i].getTitle() + "\n";
     	}
 
-    	try {
-        	Runtime.getRuntime().exec("/opt/MozillaThunderbird/bin/thunderbird.sh mailto:?body=" + URLEncoder.encode(emailText, "UTF8").replaceAll("\\+", "%20"));
-    	} catch (Exception e) {
-    		e.printStackTrace();
+    	if (mSettings.getProperty("application", "").trim().equals("")) {	
+    		JOptionPane.showMessageDialog(UiUtilities.getBestDialogParent(getParentFrame()), 
+    				mLocalizer.msg("SpecifyApp", "Please specify a Application in the Settings for this Plugin."));
+    		return;
     	}
     	
-    	System.out.println(emailText.trim());
+    	File f = new File(mSettings.getProperty("application"));
+    	
+    	if (!f.exists() || f.isDirectory()) {
+    		JOptionPane.showMessageDialog(UiUtilities.getBestDialogParent(getParentFrame()), 
+    				mLocalizer.msg("AppNotFound", "Application wasn't found. Please specify a Application in the Settings for this Plugin."));
+    		return;
+    	}
+    	
+    	try {
+    		String param = mSettings.getProperty("parameter", "").replaceAll("\\{0\\}", "mailto:?body=" + URLEncoder.encode(emailText, "UTF8").replaceAll("\\+", "%20"));
+    		Runtime.getRuntime().exec(mSettings.getProperty("application") + " " + param);
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    		ErrorHandler.handle(mLocalizer.msg("ErrorWhileStarting", "Error while starting Mail-Application"), e);
+    	}
     }
 }
