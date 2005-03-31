38dda50038a95e7abeb6651b6afd386c2438395f
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2005-03-31 20:05:27 +0000

    Source-Reformat

--- trunk/tvbrowser/src/emailplugin/EMailSettingsTab.java
+++ trunk/tvbrowser/src/emailplugin/EMailSettingsTab.java
@@ -54,106 +54,117 @@ import devplugin.SettingsTab;
  * @author bodum
  */
 public class EMailSettingsTab implements SettingsTab {
-    /** Translator */
-    private static final Localizer mLocalizer = Localizer.getLocalizerFor(EMailSettingsTab.class);
-    /** Settings */
-    private Properties mSettings;
-    /** Application-Path */
-    private JTextField mApplication;
-    /** Parameters */
-    private JTextField mParameter;
-    /** The Settings-Panel*/
-    private JPanel mPanel;
-    
-	/**
-	 * Creates the SettingsTab
-	 * @param settings Settings to use
-	 */
-	public EMailSettingsTab(Properties settings) {
-		mSettings = settings;
-	}
+  /** Translator */
+  private static final Localizer mLocalizer = Localizer.getLocalizerFor(EMailSettingsTab.class);
 
-	/* (non-Javadoc)
-	 * @see devplugin.SettingsTab#createSettingsPanel()
-	 */
-	public JPanel createSettingsPanel() {
-		mPanel = new JPanel();
-	
-		mPanel.setBorder(BorderFactory.createTitledBorder(mLocalizer.msg("configuration", "Configuration")));
-		
-		FormLayout layout = new FormLayout(
-				"3dlu, pref, 3dlu, pref:grow, 3dlu, pref, 3dlu",
-				"3dlu, pref, 3dlu, pref, 3dlu, pref, 3dlu"
-		);
-		mPanel.setLayout(layout);
-		
-		CellConstraints cc = new CellConstraints();
-		
-		mPanel.add(new JLabel(mLocalizer.msg("Application", "Application") +":"), cc.xy(2,2));
-		
-		mApplication = new JTextField(mSettings.getProperty("application")); 
-		
-		mPanel.add(mApplication, cc.xy(4, 2));
-		
-		JButton appFinder = new JButton("...");
-		appFinder.addActionListener(new ActionListener() {
+  /** Settings */
+  private Properties mSettings;
 
-			public void actionPerformed(ActionEvent e) {
-				findApplictation();
-			}
-			
-		});
-		
-		mPanel.add(appFinder, cc.xy(6,2));
-		
-		mPanel.add(new JLabel(mLocalizer.msg("Parameter", "Parameter")+ ":"), cc.xy(2,4));
+  /** Application-Path */
+  private JTextField mApplication;
 
-		mParameter = new JTextField(mSettings.getProperty("parameter", "{0}"));
-		
-		mPanel.add(mParameter, cc.xyw(4, 4, 3));
+  /** Parameters */
+  private JTextField mParameter;
 
-		JTextArea ta = new JTextArea(mLocalizer.msg("Desc", "Desc"));
-	    ta.setWrapStyleWord(true);
-	    ta.setLineWrap(true);
-	    ta.setOpaque(false);
-	    ta.setEditable(false);
-	    ta.setFocusable(false);
-		
-	    mPanel.add(ta, cc.xyw(2,6,4));
-		
-		return mPanel;
-	}
+  /** The Settings-Panel */
+  private JPanel mPanel;
 
-	/**
-	 * Opens a FileChooser and let the User open a File
-	 */
-	public void findApplictation() {
-	    JFileChooser chooser = new JFileChooser(new File(mSettings.getProperty("application", "")));
-	    int returnVal = chooser.showOpenDialog(UiUtilities.getBestDialogParent(mPanel));
-	    if(returnVal == JFileChooser.APPROVE_OPTION) {
-	    	mApplication.setText(chooser.getSelectedFile().getAbsolutePath());
-	    }	
-	}
-	
-	/* (non-Javadoc)
-	 * @see devplugin.SettingsTab#saveSettings()
-	 */
-	public void saveSettings() {
-		mSettings.put("application", mApplication.getText());
-		mSettings.put("parameter", mParameter.getText());
-	}
+  /**
+   * Creates the SettingsTab
+   * 
+   * @param settings Settings to use
+   */
+  public EMailSettingsTab(Properties settings) {
+    mSettings = settings;
+  }
 
-	/* (non-Javadoc)
-	 * @see devplugin.SettingsTab#getIcon()
-	 */
-	public Icon getIcon() {
-		return new ImageIcon(ImageUtilities.createImageFromJar("emailplugin/email.gif", EMailSettingsTab.class));
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see devplugin.SettingsTab#createSettingsPanel()
+   */
+  public JPanel createSettingsPanel() {
+    mPanel = new JPanel();
 
-	/* (non-Javadoc)
-	 * @see devplugin.SettingsTab#getTitle()
-	 */
-	public String getTitle() {
-		return mLocalizer.msg("name", "Send EMail");
-	}
+    mPanel.setBorder(BorderFactory.createTitledBorder(mLocalizer.msg("configuration", "Configuration")));
+
+    FormLayout layout = new FormLayout("3dlu, pref, 3dlu, pref:grow, 3dlu, pref, 3dlu",
+        "3dlu, pref, 3dlu, pref, 3dlu, pref, 3dlu");
+    mPanel.setLayout(layout);
+
+    CellConstraints cc = new CellConstraints();
+
+    mPanel.add(new JLabel(mLocalizer.msg("Application", "Application") + ":"), cc.xy(2, 2));
+
+    mApplication = new JTextField(mSettings.getProperty("application"));
+
+    mPanel.add(mApplication, cc.xy(4, 2));
+
+    JButton appFinder = new JButton("...");
+    appFinder.addActionListener(new ActionListener() {
+
+      public void actionPerformed(ActionEvent e) {
+        findApplictation();
+      }
+
+    });
+
+    mPanel.add(appFinder, cc.xy(6, 2));
+
+    mPanel.add(new JLabel(mLocalizer.msg("Parameter", "Parameter") + ":"), cc.xy(2, 4));
+
+    mParameter = new JTextField(mSettings.getProperty("parameter", "{0}"));
+
+    mPanel.add(mParameter, cc.xyw(4, 4, 3));
+
+    JTextArea ta = new JTextArea(mLocalizer.msg("Desc", "Desc"));
+    ta.setWrapStyleWord(true);
+    ta.setLineWrap(true);
+    ta.setOpaque(false);
+    ta.setEditable(false);
+    ta.setFocusable(false);
+
+    mPanel.add(ta, cc.xyw(2, 6, 4));
+
+    return mPanel;
+  }
+
+  /**
+   * Opens a FileChooser and let the User open a File
+   */
+  public void findApplictation() {
+    JFileChooser chooser = new JFileChooser(new File(mSettings.getProperty("application", "")));
+    int returnVal = chooser.showOpenDialog(UiUtilities.getBestDialogParent(mPanel));
+    if (returnVal == JFileChooser.APPROVE_OPTION) {
+      mApplication.setText(chooser.getSelectedFile().getAbsolutePath());
+    }
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see devplugin.SettingsTab#saveSettings()
+   */
+  public void saveSettings() {
+    mSettings.put("application", mApplication.getText());
+    mSettings.put("parameter", mParameter.getText());
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see devplugin.SettingsTab#getIcon()
+   */
+  public Icon getIcon() {
+    return new ImageIcon(ImageUtilities.createImageFromJar("emailplugin/email.gif", EMailSettingsTab.class));
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see devplugin.SettingsTab#getTitle()
+   */
+  public String getTitle() {
+    return mLocalizer.msg("name", "Send EMail");
+  }
 }
