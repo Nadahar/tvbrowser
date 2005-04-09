26ee370aaf58a52fd4e25fe963d9c22f9cd828fc
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2005-04-09 09:23:51 +0000

    Possible infinite loop removed

--- trunk/tvbrowser/src/tvdataservice/MutableProgram.java
+++ trunk/tvbrowser/src/tvdataservice/MutableProgram.java
@@ -216,7 +216,8 @@ public class MutableProgram implements Program {
   public final String getDateString() {
     Date d = getDate();
     if (d == null) {
-      System.out.println(this.toString());
+      System.out.println(mChannel.getName() + " at " + getHours() + ":" + getMinutes()
+        + ", NO DATE : '" + getTitle() + "'");
       return "";
     }
     return getDate().toString();
