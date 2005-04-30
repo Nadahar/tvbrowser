096b1d3d891d5c35fc34b11670f26da5ad0796be
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2005-04-30 16:06:48 +0000

    EMail-Plugin now uses the new Param-Handler

--- tvbrowser/src/emailplugin/EMailPlugin.java
+++ tvbrowser/src/emailplugin/EMailPlugin.java
@@ -35,6 +35,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 import util.exc.ErrorHandler;
+import util.paramhandler.ParamParser;
 import util.ui.ImageUtilities;
 import util.ui.Localizer;
 import util.ui.UiUtilities;
@@ -42,136 +43,157 @@ import devplugin.ActionMenu;
 import devplugin.Plugin;
 import devplugin.PluginInfo;
 import devplugin.Program;
-import devplugin.ProgramFieldType;
 import devplugin.SettingsTab;
 import devplugin.Version;
 
 /**
- * This Plugin makes it possible to send an email with short
- * info's about the Program
+ * This Plugin makes it possible to send an email with short info's about the
+ * Program
  * 
  * @author bodum
  */
 public class EMailPlugin extends Plugin {
-    /** Translator */
-    private static final Localizer mLocalizer = Localizer.getLocalizerFor(EMailPlugin.class);
-    /** Properties */
-    private Properties mSettings;
-    
-    /*
-     * (non-Javadoc)
-     * 
-     * @see devplugin.Plugin#getInfo()
-     */
-    public PluginInfo getInfo() {
-        String name = mLocalizer.msg("pluginName", "EMail export");
-        String desc = mLocalizer.msg("description",
-                "Send a EMail with an external Program");
-        String author = "Bodo Tasche";
-        return new PluginInfo(name, desc, author, new Version(0, 1));
-    }	
-    
-    /*
-     * (non-Javadoc)
-     * 
-     * @see devplugin.Plugin#getMarkIconName()
-     */
-    public String getMarkIconName() {
-        return "emailplugin/email.gif";
-    }
+  /** Translator */
+  private static final Localizer mLocalizer = Localizer.getLocalizerFor(EMailPlugin.class);
 
-    /*
-     *  (non-Javadoc)
-     * @see devplugin.Plugin#getContextMenuActions(devplugin.Program)
-     */
-    public ActionMenu getContextMenuActions(final Program program) {
-        AbstractAction action = new AbstractAction() {
-            public void actionPerformed(ActionEvent evt) {
-                Program[] programArr = { program };
-                createMail(programArr);
-            }
-        };
-        action.putValue(Action.NAME, mLocalizer.msg("contextMenuText","Send via EMail"));
-        action.putValue(Action.SMALL_ICON, new ImageIcon(ImageUtilities.createImageFromJar("emailplugin/email.gif", EMailPlugin.class)));
-        
-        return new ActionMenu(action);
-    }    
-    
-    /*
-     * (non-Javadoc)
-     * 
-     * @see devplugin.Plugin#canReceivePrograms()
-     */
-    public boolean canReceivePrograms() {
-        return true;
-    }    
-    
-    /**
-     * This method is invoked for multiple program execution.
-     * 
-     * @see #supportMultipleProgramExecution()
-     */
-    public void receivePrograms(Program[] programArr) {
+  /** Properties */
+  private Properties mSettings;
+
+  /** The Default-Parameters */
+  public static final String DEFAULT_PARAMETER = "{channel_name} - {title}\n{leadingZero(start_day,\"2\")}.{leadingZero(start_month,\"2\")}.{start_year} {leadingZero(start_hour,\"2\")}:{leadingZero(start_minute,\"2\")}-{leadingZero(end_hour,\"2\")}:{leadingZero(end_minute,\"2\")}\n\n{splitAt(short_info,\"78\")}\n\n";
+  
+  /*
+   * (non-Javadoc)
+   * 
+   * @see devplugin.Plugin#getInfo()
+   */
+  public PluginInfo getInfo() {
+    String name = mLocalizer.msg("pluginName", "EMail export");
+    String desc = mLocalizer.msg("description", "Send a EMail with an external Program");
+    String author = "Bodo Tasche";
+    return new PluginInfo(name, desc, author, new Version(0, 1));
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see devplugin.Plugin#getMarkIconName()
+   */
+  public String getMarkIconName() {
+    return "emailplugin/email.gif";
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see devplugin.Plugin#getContextMenuActions(devplugin.Program)
+   */
+  public ActionMenu getContextMenuActions(final Program program) {
+    AbstractAction action = new AbstractAction() {
+      public void actionPerformed(ActionEvent evt) {
+        Program[] programArr = { program };
         createMail(programArr);
-    }    
-    
-	/* (non-Javadoc)
-	 * @see devplugin.Plugin#getSettingsTab()
-	 */
-	public SettingsTab getSettingsTab() {
+      }
+    };
+    action.putValue(Action.NAME, mLocalizer.msg("contextMenuText", "Send via EMail"));
+    action.putValue(Action.SMALL_ICON, new ImageIcon(ImageUtilities.createImageFromJar("emailplugin/email.gif",
+        EMailPlugin.class)));
 
-		EMailSettingsTab tab = new EMailSettingsTab(mSettings);
-		
-		return tab;
-	}
-	
-	/* (non-Javadoc)
-	 * @see devplugin.Plugin#loadSettings(java.util.Properties)
-	 */
-	public void loadSettings(Properties settings) {
-		mSettings = settings;
-	}
-	
-	/* (non-Javadoc)
-	 * @see devplugin.Plugin#storeSettings()
-	 */
-	public Properties storeSettings() {
-		return mSettings;
-	}
-	
-    /**
-     * Create the Mail
-     * @param program Programs to show in the Mail
-     */
-    private void createMail(Program[] program) {
-    	
-    	String emailText = new String();
-    	
-    	for (int i = 0; i < program.length; i++) {
-    		emailText += program[i].getDateString() + ", "+program[i].getChannel().getName() + "\n" + 
-			             program[i].getTimeString() + " - " + program[i].getTimeFieldAsString(ProgramFieldType.END_TIME_TYPE) + "   " + program[i].getTitle() + "\n\n";
-    	}
+    return new ActionMenu(action);
+  }
 
-    	if (mSettings.getProperty("application", "").trim().equals("")) {	
-    		JOptionPane.showMessageDialog(UiUtilities.getBestDialogParent(getParentFrame()), 
-    				mLocalizer.msg("SpecifyApp", "Please specify a Application in the Settings for this Plugin."));
-    		return;
-    	}
-    	
-    	File f = new File(mSettings.getProperty("application"));
-    	
-    	if (!f.exists() || f.isDirectory()) {
-    		JOptionPane.showMessageDialog(UiUtilities.getBestDialogParent(getParentFrame()), 
-    				mLocalizer.msg("AppNotFound", "Application wasn't found. Please specify a Application in the Settings for this Plugin."));
-    		return;
-    	}
-    	
-    	try {
-    		String param = mSettings.getProperty("parameter", "").replaceAll("\\{0\\}", "mailto:?body=" + URLEncoder.encode(emailText, "UTF8").replaceAll("\\+", "%20"));
-    		Runtime.getRuntime().exec(mSettings.getProperty("application") + " " + param);
-    	} catch (Exception e) {
-    		e.printStackTrace();
-    		ErrorHandler.handle(mLocalizer.msg("ErrorWhileStarting", "Error while starting Mail-Application"), e);
-    	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see devplugin.Plugin#canReceivePrograms()
+   */
+  public boolean canReceivePrograms() {
+    return true;
+  }
+
+  /**
+   * This method is invoked for multiple program execution.
+   * 
+   * @see #supportMultipleProgramExecution()
+   */
+  public void receivePrograms(Program[] programArr) {
+    createMail(programArr);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see devplugin.Plugin#getSettingsTab()
+   */
+  public SettingsTab getSettingsTab() {
+
+    EMailSettingsTab tab = new EMailSettingsTab(mSettings);
+
+    return tab;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see devplugin.Plugin#loadSettings(java.util.Properties)
+   */
+  public void loadSettings(Properties settings) {
+    mSettings = settings;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see devplugin.Plugin#storeSettings()
+   */
+  public Properties storeSettings() {
+    return mSettings;
+  }
+
+  /**
+   * Create the Mail
+   * 
+   * @param program Programs to show in the Mail
+   */
+  private void createMail(Program[] program) {
+    String param = mSettings.getProperty("paramToUse", DEFAULT_PARAMETER);
+    StringBuffer result = new StringBuffer();
+    ParamParser parser = new ParamParser();
+
+    int i = 0;
+
+    while (!parser.hasErrors() && (i < program.length)) {
+      String prgResult = parser.analyse(param, (Program) program[i]);
+      result.append(prgResult);
+      i++;
     }
+
+    if (parser.hasErrors()) {
+      JOptionPane.showMessageDialog(UiUtilities.getLastModalChildOf(getParentFrame()), parser.getErrorString(), "Error", JOptionPane.ERROR_MESSAGE);
+      return;
+    }
+    
+    if (mSettings.getProperty("application", "").trim().equals("")) {
+      JOptionPane.showMessageDialog(UiUtilities.getBestDialogParent(getParentFrame()), mLocalizer.msg("SpecifyApp",
+          "Please specify a Application in the Settings for this Plugin."));
+      return;
+    }
+
+    File f = new File(mSettings.getProperty("application"));
+
+    if (!f.exists() || f.isDirectory()) {
+      JOptionPane.showMessageDialog(UiUtilities.getBestDialogParent(getParentFrame()), mLocalizer.msg("AppNotFound",
+          "Application wasn't found. Please specify a Application in the Settings for this Plugin."));
+      return;
+    }
+
+    try {
+      String execparam = mSettings.getProperty("parameter", "").replaceAll("\\{0\\}",
+          "mailto:?body=" + URLEncoder.encode(result.toString(), mSettings.getProperty("encoding", "UTF-8")).replaceAll("\\+", "%20"));
+      Runtime.getRuntime().exec(mSettings.getProperty("application") + " " + execparam);
+    } catch (Exception e) {
+      e.printStackTrace();
+      ErrorHandler.handle(mLocalizer.msg("ErrorWhileStarting", "Error while starting Mail-Application"), e);
+    }
+  }
 }
