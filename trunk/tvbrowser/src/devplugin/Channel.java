0b5b1fada69b49fb67e0e6f58f3a6da9e9c54ba0
darras <darras@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2005-05-26 20:18:12 +0000

    channel categories updated

--- trunk/tvbrowser/src/devplugin/Channel.java
+++ trunk/tvbrowser/src/devplugin/Channel.java
@@ -41,14 +41,16 @@ import util.ui.ImageUtilities;
 public class Channel {
 
   public static final int CATEGORY_NONE = 0;
-  public static final int CATEGORY_RADIO = 1;
-  public static final int CATEGORY_CINEMA = 1 << 1;
-  public static final int CATEGORY_DIGITAL = 1 << 2;
-  public static final int CATEGORY_SPECIAL_MUSIC = 1 << 3;
-  public static final int CATEGORY_SPECIAL_SPORT = 1 << 4;
-  public static final int CATEGORY_SPECIAL_NEWS = 1 << 5;
-  public static final int CATEGORY_SPECIAL_OTHER = 1 << 6;
-  public static final int CATEGORY_EVENTS = 1 << 7;
+  public static final int CATEGORY_TV = 1;
+  public static final int CATEGORY_RADIO = 1 << 1;
+  public static final int CATEGORY_CINEMA = 1 << 2;
+  public static final int CATEGORY_EVENTS = 1 << 3;
+  public static final int CATEGORY_DIGITAL = 1 << 4;
+  public static final int CATEGORY_SPECIAL_MUSIC = 1 << 5;
+  public static final int CATEGORY_SPECIAL_SPORT = 1 << 6;
+  public static final int CATEGORY_SPECIAL_NEWS = 1 << 7;
+  public static final int CATEGORY_SPECIAL_OTHER = 1 << 8;
+
 
 
   private TvDataService mDataService;
