89ca1c950894e291a198c61f0a78b6ce909c02f5
darras <darras@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2005-09-04 13:11:12 +0000

    created deployment folder; moved files from txt, win, installer, jsmooth into deployment folder

--- tvbrowser/macosx/public-mac.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ..
-#ant runtime-mac
-cd macosx
-#freeze tvbrowser.packproj
-./mkdmg -v -format UDZO -s build/.app.pkg build/.core.pkg build/install.mpkg build/readme.rtf -i ../runtime/tvbrowser_mac/tvbrowser.dmg
-
