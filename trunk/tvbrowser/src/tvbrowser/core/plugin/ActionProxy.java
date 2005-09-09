bac08a9243fe4cb245c77be512e5267761bdbd91
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2005-09-09 18:25:21 +0000

    Added Javadoc

--- trunk/tvbrowser/src/tvbrowser/core/plugin/ActionProxy.java
+++ trunk/tvbrowser/src/tvbrowser/core/plugin/ActionProxy.java
@@ -1,3 +1,28 @@
+/*
+ * TV-Browser
+ * Copyright (C) 04-2003 Martin Oberhauser (martin_oat@yahoo.de)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * CVS information:
+ *  $RCSfile$
+ *   $Source$
+ *     $Date$
+ *   $Author$
+ * $Revision$
+ */
 package tvbrowser.core.plugin;
 
 import java.awt.event.ActionEvent;
@@ -5,11 +30,25 @@ import java.beans.PropertyChangeListener;
 
 import javax.swing.Action;
 
+/**
+ * A ActionProxy handles Erros throwns by Plugins and enables the User to 
+ * deactivate those Plugins
+ * 
+ * @author bodum
+ *
+ */
 public class ActionProxy implements Action {
-
-  Action mAction;
-  PluginProxy mPluginProxy;
+  /** Action to use*/
+  private Action mAction;
+  /** PluginProxy to use */
+  private PluginProxy mPluginProxy;
   
+  /**
+   * Create the Proxy
+   * 
+   * @param pluginProxy PluginProxy to use 
+   * @param action Action that gets a better Error-Handling
+   */
   public ActionProxy(PluginProxy pluginProxy, Action action) {
     mPluginProxy = pluginProxy;
     mAction = action;
@@ -41,10 +80,8 @@ public class ActionProxy implements Action {
 
   public void actionPerformed(ActionEvent e) {
     try {
-      System.out.println("XXXXXXXXXXXX");
       mAction.actionPerformed(e);
     } catch (RuntimeException ex) {
-      System.out.println("HANDLE ERROR");
       mPluginProxy.handlePluginException(ex);
       
     }
