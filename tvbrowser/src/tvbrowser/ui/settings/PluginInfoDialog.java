e7bc60638d2efe5829eb24a20f04caf73f216438
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2005-09-17 06:59:13 +0000

    Added Info-Dialog

---
+++ tvbrowser/src/tvbrowser/ui/settings/PluginInfoDialog.java
@@ -0,0 +1,157 @@
+package tvbrowser.ui.settings;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JEditorPane;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+
+import util.ui.BrowserLauncher;
+import util.ui.html.ExtendedHTMLDocument;
+import util.ui.html.ExtendedHTMLEditorKit;
+import util.ui.html.HTMLTextHelper;
+
+import com.jgoodies.forms.factories.Borders;
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
+
+import devplugin.PluginInfo;
+
+/**
+ * The Info-Dialog shows Informations about an Plugin
+ * 
+ * @author bodum
+ */
+public class PluginInfoDialog extends JDialog {
+  /** Translation */
+  private static final util.ui.Localizer mLocalizer
+  = util.ui.Localizer.getLocalizerFor(PluginInfoDialog.class);
+  
+  /** Infos about the Plugin */
+  private PluginInfo mPluginInfo;
+  /** Icon */
+  private Icon mPluginIcon;
+
+  /**
+   * Create the Dialog 
+   * @param dialog Parent Dialog
+   * @param pluginInfo Info's about the Plugin
+   */
+  public PluginInfoDialog(JDialog dialog, PluginInfo pluginInfo) {
+    this(dialog, null, pluginInfo);
+  }
+
+  /**
+   * Create the Dialog
+   * @param dialog Parent Dialog
+   * @param icon Icon of the Plugin
+   * @param pluginInfo Info's about the Plugin
+   */
+  public PluginInfoDialog(JDialog dialog, Icon icon, PluginInfo pluginInfo) {
+    super(dialog, true);
+    setTitle(pluginInfo.getName());
+    mPluginInfo = pluginInfo;
+    
+    if (icon == null) {
+      icon = new ImageIcon("imgs/Jar16.gif");
+    }
+    
+    mPluginIcon = icon;
+    initGui();
+  }
+  
+  /**
+   * Create the GUI
+   */
+  private void initGui() {
+    
+    JPanel panel = (JPanel) getContentPane();
+    panel.setBorder(Borders.DLU4_BORDER);
+    panel.setLayout(new FormLayout("fill:default:grow, default", "fill:default:grow, 3dlu, default"));
+    
+    CellConstraints cc = new CellConstraints();
+    
+    JEditorPane infoPanel = new JEditorPane();
+    
+    infoPanel.setEditorKit(new ExtendedHTMLEditorKit());
+
+    ExtendedHTMLDocument doc = (ExtendedHTMLDocument) infoPanel.getDocument();
+    
+    infoPanel.setEditable(false);
+    infoPanel.setText(generateHtml(doc));
+        
+    infoPanel.addHyperlinkListener(new HyperlinkListener() {
+      public void hyperlinkUpdate(HyperlinkEvent evt) {
+        if (evt.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+          URL url = evt.getURL();
+          if (url != null) {
+            BrowserLauncher.openURL(url.toString());
+          }
+        }
+      }
+    });
+    
+    panel.add(new JScrollPane(infoPanel), cc.xyw(1,1,2));
+    
+    JButton ok = new JButton(mLocalizer.msg("ok", "OK"));
+    
+    ok.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        setVisible(false);
+      }
+    });
+    
+    panel.add(ok, cc.xy(2,3));
+    
+    setSize(500, 350);
+  }
+
+  /**
+   * Generate the HML for the EditorPane
+   * @param doc Document to use
+   * @return Html-Text
+   */
+  private String generateHtml(ExtendedHTMLDocument doc) {
+    StringBuffer html = new StringBuffer();
+    
+    html.append("<html><style type=\"text/css\" media=\"screen\">"
+                + "<!--" +
+                    "body {font-size:12px;font-family:Dialog;}" +
+                    "h1 {font-size:12px;font-family:Dialog;font-weight:bold;}" +
+                "-->" + 
+                "</style><body>");
+    
+    html.append("<table><tr><td valign=\"top\">");
+    
+    html.append(doc.createCompTag(new JLabel(mPluginIcon)));
+    
+    html.append("</td><td valign=\"top\"><b>").append(mPluginInfo.getName()).append("</b></td></tr></table>");
+    
+    html.append("<i>").append(mLocalizer.msg("version", "Version")).append(" ").append(mPluginInfo.getVersion()).append("</i><br>");
+    
+    html.append("<h1>").append(mLocalizer.msg("author", "Author")).append("</h1>");
+    html.append(HTMLTextHelper.convertTextToHtml(mPluginInfo.getAuthor(), true));
+
+    if (mPluginInfo.getLicense() != null) {
+      html.append("<h1>").append(mLocalizer.msg("licence", "Licence")).append("</h1>");
+      html.append(HTMLTextHelper.convertTextToHtml(mPluginInfo.getLicense(), true));
+    }
+
+    html.append("<h1>").append(mLocalizer.msg("description", "Description")).append("</h1>");
+    html.append(HTMLTextHelper.convertTextToHtml(mPluginInfo.getDescription(), true));
+
+    html.append("</body></html>");
+    
+    return html.toString();
+  }
+  
+}
