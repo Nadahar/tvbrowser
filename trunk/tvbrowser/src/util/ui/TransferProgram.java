18ba984643797bd3a3c6fb4cb952db92883693a3
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2005-09-23 22:23:29 +0000

    Needed Transferable to support DragAndDrop from the ProgramTable to the PluginTree.

---
+++ trunk/tvbrowser/src/util/ui/TransferProgram.java
@@ -0,0 +1,47 @@
+package util.ui;
+
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+
+
+import devplugin.Program;
+
+/**
+ * A class for Drag'n'Drop of Programs. 
+ * @author René Mach
+ */
+public class TransferProgram implements Transferable {
+  private Program mProgram;
+  private DataFlavor mPF;
+  
+  /**
+   * Set up the transferEntries.
+   * @param program The selected program.
+   */
+  public TransferProgram(Program program) {
+    mProgram = program;
+    mPF = new DataFlavor(Program.class,"Program");
+  }
+  
+  public DataFlavor[] getTransferDataFlavors() {
+    DataFlavor[] f = {mPF};
+    return f;
+  }
+
+  public boolean isDataFlavorSupported(DataFlavor e) {
+    if(e.equals(mPF))
+      return true;
+    else
+      return false;
+  }
+  
+  public Object getTransferData(DataFlavor e) throws UnsupportedFlavorException, IOException {
+    if(e.equals(mPF))
+      return mProgram;
+    else
+      return null;
+  }
+
+}
