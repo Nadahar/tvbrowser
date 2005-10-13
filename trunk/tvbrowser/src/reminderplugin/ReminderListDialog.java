401548cd9e0ba2cb88b1b5bde0d002e0a4f3404a
darras <darras@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2005-10-13 21:41:19 +0000

    Bugfix: don't export reminder array of length 0

--- trunk/tvbrowser/src/reminderplugin/ReminderListDialog.java
+++ trunk/tvbrowser/src/reminderplugin/ReminderListDialog.java
@@ -31,16 +31,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Arrays;
 
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JDialog;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
+import javax.swing.*;
 
 import util.ui.ChannelLabel;
 import util.ui.ImageUtilities;
@@ -58,41 +49,41 @@ import devplugin.Program;
 public class ReminderListDialog extends JDialog {
 
   private static final util.ui.Localizer mLocalizer
-    = util.ui.Localizer.getLocalizerFor(ReminderListDialog.class);
-  
+          = util.ui.Localizer.getLocalizerFor(ReminderListDialog.class);
+
   private static final String[] REMIND_MSG_ARR = ReminderFrame.REMIND_MSG_ARR;
   private static final int[] REMIND_VALUE_ARR = ReminderFrame.REMIND_VALUE_ARR;
 
   private static final int DONT_REMEMBER = 0;
 
   private ReminderList reminderList;
-  
+
   private JPanel mListPanel;
   private JScrollPane mScrollPane;
-  
+
   /** The Reminder-Plugin */
   private Plugin mPlugin;
-  
+
   public ReminderListDialog(Plugin plugin, Frame parent, ReminderList list) {
     super(parent,true);
-    
+
     mPlugin = plugin;
-    
+
     String msg;
-    
+
     reminderList=list;
     setTitle(mLocalizer.msg("title", "Reminder"));
-    
+
     JPanel contentpane=(JPanel)getContentPane();
     contentpane.setLayout(new BorderLayout(0,12));
     contentpane.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
     msg = mLocalizer.msg("labelText", "You will be reminded for the following programs:");
     JLabel label = new JLabel(msg);
     contentpane.add(label,BorderLayout.NORTH);
-    
+
     mListPanel=new JPanel();
     mListPanel.setLayout(new BoxLayout(mListPanel,BoxLayout.Y_AXIS));
-    
+
     if (list!=null) {
       ReminderListItem[] items = list.getReminderItems();
       Arrays.sort(items);
@@ -100,24 +91,24 @@ public class ReminderListDialog extends JDialog {
         mListPanel.add(createListItemPanel(items[i]));
       }
     }
-    
+
     mScrollPane = new JScrollPane(mListPanel);
     mScrollPane.getVerticalScrollBar().setUnitIncrement(30);
     mScrollPane.getHorizontalScrollBar().setUnitIncrement(30);
     contentpane.add(mScrollPane, BorderLayout.CENTER);
-    
+
     JPanel btnPanel=new JPanel(new BorderLayout());
-    
+
     JButton closeBtn = new JButton(mLocalizer.msg("close", "Close"));
     getRootPane().setDefaultButton(closeBtn);
-    
+
     closeBtn.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         hide();
       }
     });
 
-    
+
     btnPanel.add(closeBtn,BorderLayout.EAST);
 
     JButton sendBtn = new JButton(new ImageIcon("imgs/SendToPlugin.png"));
@@ -125,47 +116,52 @@ public class ReminderListDialog extends JDialog {
     sendBtn.setToolTipText(mLocalizer.msg("send", "Send Programs to another Plugin"));
     sendBtn.addActionListener(new ActionListener() {
 
-        public void actionPerformed(ActionEvent e) {
-           showSendDialog();            
-        }
-        
+      public void actionPerformed(ActionEvent e) {
+        showSendDialog();
+      }
+
     });
 
-    btnPanel.add(sendBtn, BorderLayout.WEST);   
-    
+    btnPanel.add(sendBtn, BorderLayout.WEST);
+
     contentpane.add(btnPanel,BorderLayout.SOUTH);
   }
-  
+
   private void showSendDialog() {
-      
-      ReminderListItem[] items = reminderList.getReminderItems();
-      Program[] programArr = new Program[items.length];
-      for (int i=0; i<items.length; i++) {
-        programArr[i] = items[i].getProgram();
-      }
-      
+
+    ReminderListItem[] items = reminderList.getReminderItems();
+    Program[] programArr = new Program[items.length];
+    for (int i=0; i<items.length; i++) {
+      programArr[i] = items[i].getProgram();
+    }
+
+    if (programArr.length > 0) {
       SendToPluginDialog send = new SendToPluginDialog(mPlugin, this, programArr);
       send.show();
-  }  
-  
+    }
+    else {
+      JOptionPane.showMessageDialog(this, mLocalizer.msg("noProgramToExport","There are no programs to export."));
+    }
+  }
+
   private void removeReminderListItem(ReminderListItem item, JPanel panel) {
-    reminderList.remove(item.getProgramItem());    
+    reminderList.remove(item.getProgramItem());
     mListPanel.remove(panel);
     mScrollPane.updateUI();
   }
-  
+
   private JPanel createListItemPanel(final ReminderListItem item) {
-    
+
     final Program prog=item.getProgram();
-    
+
     final JPanel result=new JPanel(new BorderLayout());
     result.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
     JPanel panel1=new JPanel(new BorderLayout());
     JPanel panel2=new JPanel(new BorderLayout());
-    
-    
+
+
     final JComboBox box1=new JComboBox(REMIND_MSG_ARR);
-    
+
     box1.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent event) {
         int inx = box1.getSelectedIndex();
@@ -187,22 +183,22 @@ public class ReminderListDialog extends JDialog {
       }
     }
     box1.setSelectedIndex(idx);
-    
+
     JLabel dateLabel=new JLabel(prog.getDate().toString());
     ChannelLabel channelLabel=new ChannelLabel(prog.getChannel());
-    
+
     dateLabel.setPreferredSize(new java.awt.Dimension(100,(int)dateLabel.getPreferredSize().getHeight()));
-    
+
     JPanel panel3=new JPanel(new BorderLayout());
     panel3.add(channelLabel,BorderLayout.NORTH);
     panel1.add(dateLabel,BorderLayout.NORTH);
     panel1.add(panel3,BorderLayout.CENTER);
-    
+
     JPanel panel4=new JPanel();
     panel4.setLayout(new BoxLayout(panel4,BoxLayout.Y_AXIS));
     panel2.add(panel4,BorderLayout.NORTH);
-    
-    
+
+
     JPanel panel5=new JPanel(new BorderLayout());
     panel4.add(box1);
     panel4.add(panel5);
@@ -215,14 +211,14 @@ public class ReminderListDialog extends JDialog {
       }
     });
     panel5.add(deleteBtn,BorderLayout.EAST);
-    
+
     result.add(panel1,BorderLayout.WEST);
     result.add(panel2,BorderLayout.EAST);
-    
+
     ProgramPanel panel = new ProgramPanel(prog);
     panel.addPluginContextMenuMouseListener(mPlugin);
     result.add(panel, BorderLayout.CENTER);
-    
+
     return result;
   }
 
