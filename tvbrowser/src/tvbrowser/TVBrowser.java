935d2fd385556ffa0813606aeaa287c4179f13d9
darras <darras@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2005-10-16 13:24:48 +0000

    prepared for release (2.1-beta1)

--- tvbrowser/src/tvbrowser/TVBrowser.java
+++ tvbrowser/src/tvbrowser/TVBrowser.java
@@ -80,7 +80,7 @@ public class TVBrowser {
 
   private static String curLookAndFeel;
 
-  public static final devplugin.Version VERSION=new devplugin.Version(2,9,true,"2.1 (cvs)");
+  public static final devplugin.Version VERSION=new devplugin.Version(2,9,true,"2.1 Beta 1");
 
 
   public static final String MAINWINDOW_TITLE="TV-Browser "+VERSION.toString();
