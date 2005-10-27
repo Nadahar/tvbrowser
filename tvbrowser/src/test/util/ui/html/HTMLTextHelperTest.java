a8140165b92adde20dc4063786fe5b1717417c91
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2005-10-27 19:38:44 +0000

    Bug #1318001: Links were not correctly recognised when the http:// was missing. Added JUnit-Test ;)

---
+++ tvbrowser/src/test/util/ui/html/HTMLTextHelperTest.java
@@ -0,0 +1,17 @@
+package test.util.ui.html;
+
+import util.ui.html.HTMLTextHelper;
+import junit.framework.TestCase;
+
+public class HTMLTextHelperTest extends TestCase {
+
+  public void testConvertTextToHtml() {
+    
+    assertEquals("asfssdf lkasf <a href=\"http://www.heise.de\">http://www.heise.de</a> kjshd slkas fjösldkfas", 
+                HTMLTextHelper.convertTextToHtml("asfssdf lkasf http://www.heise.de kjshd slkas fjösldkfas", true));
+    assertEquals("asfssdf lkasf <a href=\"http://www.heise.de\">www.heise.de</a> kjshd slkas fjösldkfas", 
+                HTMLTextHelper.convertTextToHtml("asfssdf lkasf www.heise.de kjshd slkas fjösldkfas", true));
+    
+  }
+  
+}
