14db9150d5679bcba200cfb798246c6b54a91485
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2005-10-31 09:58:14 +0000

    Changed on the suggestions of Bodo. Removed mActionCommand.

--- trunk/tvbrowser/src/justmark/GroupUnmarkAction.java
+++ trunk/tvbrowser/src/justmark/GroupUnmarkAction.java
@@ -6,29 +6,30 @@ import java.awt.event.ActionListener;
 import devplugin.ContextMenuAction;
 import devplugin.PluginTreeNode;
 
-
 /**
  * Unmark all marked programs of a PluginTreeNode.
  * 
  * @author René Mach
- *
+ * 
  */
-public class GroupUnmarkAction extends ContextMenuAction implements ActionListener{
+public class GroupUnmarkAction extends ContextMenuAction implements
+    ActionListener {
 
   private static final long serialVersionUID = 1L;
   private PluginTreeNode mNode;
-  
+
   /**
    * The Constructor.
    * 
-   * @param node The PluginTreeNode to unmark it's programs.
+   * @param node
+   *          The PluginTreeNode to unmark it's programs.
    */
-  public GroupUnmarkAction(PluginTreeNode node){
+  public GroupUnmarkAction(PluginTreeNode node) {
     super();
-    mNode = node;    
+    mNode = node;
   }
-  
+
   public void actionPerformed(ActionEvent e) {
-    JustMark.getInstance().handleAction(mNode,e);
-  }  
+    JustMark.getInstance().handleAction(mNode, e);
+  }
 }
