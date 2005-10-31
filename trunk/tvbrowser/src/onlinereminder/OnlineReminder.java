2456e9459a8903f28588c524eff51be79820d698
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2005-10-31 05:35:52 +0000

    OnlineReminder, first Version. ToDo: Dialog, update Server

---
+++ trunk/tvbrowser/src/onlinereminder/OnlineReminder.java
@@ -0,0 +1,163 @@
+/*
+ * TV-Browser
+ * Copyright (C) 04-2003 Martin Oberhauser (martin_oat@yahoo.de)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * CVS information:
+ *  $RCSfile$
+ *   $Source$
+ *     $Date$
+ *   $Author$
+ * $Revision$
+ */
+package onlinereminder;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Iterator;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.ImageIcon;
+
+import util.ui.ImageUtilities;
+import util.ui.Localizer;
+import captureplugin.CapturePlugin;
+import devplugin.ActionMenu;
+import devplugin.ContextMenuAction;
+import devplugin.Plugin;
+import devplugin.PluginInfo;
+import devplugin.PluginTreeNode;
+import devplugin.Program;
+import devplugin.Version;
+
+/**
+ * This Plugin contacts TVAddicted and stores Reminder Online.
+ * The Reminder will be used to Inform the User via Mail, ICQ etc.
+ * 
+ * @author bodum
+ */
+public class OnlineReminder extends Plugin {
+  /** Localisation */
+  private static final Localizer mLocalizer = Localizer.getLocalizerFor(OnlineReminder.class);
+  /** Configuration */
+  private Configuration mConfig;
+  
+  /**
+   * Creates the Plugin
+   */
+  public OnlineReminder() {
+    mConfig = new Configuration();
+  }
+
+  public PluginInfo getInfo() {
+    String name = mLocalizer.msg("pluginName", "Online Reminder");
+    String desc = mLocalizer.msg("description", "Remindes you via EMail, AIM or Jabber Messages.");
+    String author = "Bodo Tasche";
+    return new PluginInfo(name, desc, author, new Version(0, 10));
+  }
+
+  protected String getMarkIconName() {
+    return "onlinereminder/icons/bell16.png";
+  }
+
+  public ActionMenu getContextMenuActions(Program program) {
+    if (!program.equals(getPluginManager().getExampleProgram()))
+      if (program.isExpired() || program.isOnAir())
+        return null;
+
+    ContextMenuAction menu = new ContextMenuAction();
+
+    final Program prog = program;
+
+    if (!mConfig.getProgramList().contains(prog)) {
+      menu.setSmallIcon(createImageIcon("onlinereminder/icons/bell16.png"));
+      menu.setText(mLocalizer.msg("contextMenuRemind","Add to Online-Reminder"));
+      menu.setActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+          mConfig.addProgram(prog);
+          getRootNode().addProgram(prog);
+          getRootNode().update();
+        }
+      });
+    } else {
+      menu.setSmallIcon(createImageIcon("onlinereminder/icons/bell16.png"));
+      menu.setText(mLocalizer.msg("contextMenuRemove","Remove from Online-Reminder"));
+      menu.setActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+          mConfig.removeProgram(prog);
+          getRootNode().removeProgram(prog);
+          getRootNode().update();
+        }
+      });
+    }
+
+    return new ActionMenu(menu);
+  }
+
+  public ActionMenu getButtonAction() {
+    AbstractAction action = new AbstractAction() {
+
+      public void actionPerformed(ActionEvent evt) {
+      }
+    };
+    action.putValue(Action.NAME, mLocalizer.msg("pluginName", "Online Reminder"));
+    action.putValue(Action.SMALL_ICON, new ImageIcon(ImageUtilities.createImageFromJar(
+        "onlinereminder/icons/bell16.png", CapturePlugin.class)));
+    action.putValue(BIG_ICON, new ImageIcon(ImageUtilities.createImageFromJar("onlinereminder/icons/bell22.png",
+        CapturePlugin.class)));
+
+    return new ActionMenu(action);
+  }
+
+  public boolean canUseProgramTree() {
+    return true;
+  }
+
+  public boolean canReceivePrograms() {
+    return true;
+  }
+
+  public void writeData(ObjectOutputStream out) throws IOException {
+    mConfig.writeData(out);
+  }
+  
+  public void readData(ObjectInputStream in) throws IOException, ClassNotFoundException {
+    mConfig = new Configuration(in);
+    
+    Iterator it = mConfig.getProgramList().iterator();
+
+    PluginTreeNode root = getRootNode();
+    
+    while (it.hasNext()) {
+      root.addProgram((Program)it.next());    
+    }
+    
+    root.update();
+  }
+  
+  public void receivePrograms(Program[] programArr) {
+    for (int i = 0; i < programArr.length; i++) {
+      if (mConfig.addProgram(programArr[i])) {
+          getRootNode().addProgram(programArr[i]);
+      }
+    }
+    getRootNode().update();
+  }
+}
