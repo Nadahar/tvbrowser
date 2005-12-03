e49255a7471b6189476dd0813f9181197a0f3ba7
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2005-12-03 23:59:17 +0000

    IniFile-Reader added. This class is needed for parsing of .theme-Files

---
+++ tvbrowser/src/util/io/IniFileReader.java
@@ -0,0 +1,126 @@
+/*
+ * TV-Browser
+ * Copyright (C) 04-2003 Martin Oberhauser (darras@users.sourceforge.net)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * CVS information:
+ *  $RCSfile$
+ *   $Source$
+ *     $Date$
+ *   $Author$
+ * $Revision$
+ */
+package util.io;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.regex.Pattern;
+
+/**
+ * This class reads Ini-Files.
+ * 
+ * Ini-Files are Property-Files with [sections] and Key=Value Text
+ * 
+ * Each [Section] is loaded into an unique HashMap. 
+ */
+public class IniFileReader {
+  /** HashMap for the Sections */
+  private HashMap mSections;
+  
+  /**
+   * Loads the Ini-File
+   * 
+   * @param iniFile File to load
+   * @throws IOException
+   */
+  public IniFileReader(File iniFile) throws IOException{
+    readIni(new BufferedInputStream(new FileInputStream(iniFile)));
+  }
+
+  /**
+   * Loads the Ini from a InputStream
+   * 
+   * @param stream Stream with Ini-File
+   * @throws IOException
+   */
+  public IniFileReader(InputStream stream) throws IOException {
+    readIni(stream);
+  }
+
+  /**
+   * Reads the Ini from a Stream
+   * 
+   * @param stream Stream with Ini-File
+   * @throws IOException
+   */
+  private void readIni(InputStream stream) throws IOException {
+    
+    BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+
+    String str;
+    
+    mSections = new HashMap();
+    
+    HashMap map = new HashMap();
+    
+    Pattern section = Pattern.compile("^\\[.*\\]\\w*");
+    Pattern keyvalue = Pattern.compile("^.*=.*\\w*");
+    
+    while ((str = reader.readLine()) != null) {
+      
+      if (section.matcher(str).matches()) {
+        str = str.trim();
+        str = str.substring(1, str.length()-1);
+        
+        map = new HashMap();
+        mSections.put(str, map);
+      } else if (keyvalue.matcher(str).matches()){
+        String key = str.substring(0, str.indexOf('='));
+        String value = str.substring(str.indexOf('=')+1);
+        map.put(key, value);
+      }
+    }
+    
+  }
+  
+  /**
+   * Get a specific "Section" of the Ini-File. Each Key=Value is stored
+   * in the HashMap as Key = Value
+   * 
+   * @param section Section to return
+   * @return Specific Section as HashMap
+   */
+  public HashMap getSection(String section) {
+    return (HashMap) mSections.get(section);
+  }
+ 
+  /**
+   * Gets all available Sections in the loaded Ini-File
+   * @return
+   */
+  public String[] getAllSections() {
+    ArrayList list = new ArrayList(mSections.keySet());
+    return (String[])list.toArray(new String[0]);
+  }
+ 
+}
