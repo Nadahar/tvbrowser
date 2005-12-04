8f1de949bb7da9f2b1a014fb2d04f3c8a92d30eb
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2005-12-04 18:53:34 +0000

    Added small JavaDoc

--- trunk/tvbrowser/src/util/misc/JavaVersion.java
+++ trunk/tvbrowser/src/util/misc/JavaVersion.java
@@ -1,7 +1,35 @@
+/*
+ * TV-Browser
+ * Copyright (C) 04-2003 Martin Oberhauser (martin_oat@yahoo.de)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * CVS information:
+ *  $RCSfile$
+ *   $Source$
+ *     $Date$
+ *   $Author$
+ * $Revision$
+ */
 package util.misc;
 
+/**
+ * This class detects the Java-Version
+ */
 public class JavaVersion {
-
+  
   public static final int VERSION_NOTFOUND = 0;
 
   public static final int VERSION_1_0 = 1;
@@ -20,6 +48,10 @@ public class JavaVersion {
 
   public static final int VERSION_1_7 = 8;
 
+  /**
+   * Returns the Version of Java. 
+   * @return VERSION_ - Value acording to the Java-Version
+   */
   public static int getVersion() {
 
     String[] ver = System.getProperty("java.version").split("\\.");
@@ -66,9 +98,5 @@ public class JavaVersion {
     }
 
     return -1;
-  }
-
-  public static void main(String[] args) {
-    System.out.println(getVersion());
   }
 }
