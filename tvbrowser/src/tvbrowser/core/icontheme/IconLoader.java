6181b02cee1bb2b3151e6449bb24595c60b28908
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2005-12-04 15:10:29 +0000

    Icon-Theme-System added

---
+++ tvbrowser/src/tvbrowser/core/icontheme/IconLoader.java
@@ -0,0 +1,160 @@
+/*
+ * TV-Browser
+ * Copyright (C) 04-2003 Martin Oberhauser (martin_oat@yahoo.de)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * CVS information:
+ *  $RCSfile$
+ *   $Source$
+ *     $Date$
+ *   $Author$
+ * $Revision$
+ */
+package tvbrowser.core.icontheme;
+
+import java.io.File;
+
+import javax.swing.ImageIcon;
+
+import tvbrowser.core.Settings;
+import util.ui.ImageUtilities;
+import devplugin.Plugin;
+
+/**
+ * The IconLoader manages the IconThemes and loads an Icon.
+ * 
+ * If the Icon was not found the the current Icon-Theme it tries to
+ * load the Icon in the Default-IconTheme.
+ * 
+ * IconThemes are Directories that are based on the Icon-Theme Specifications
+ * 
+ * http://standards.freedesktop.org/icon-theme-spec/icon-theme-spec-latest.html
+ * 
+ * The Icon-Names are based on the Freedesktop Icon Naming Spec:
+ * http://cvs.freedesktop.org/[*]checkout[*]/icon-theme/default-icon-theme/spec/icon-naming-spec.xml
+ * (please remove the [ ])
+ */
+public class IconLoader {
+  /** Singelton */
+  private static IconLoader mInstance;
+  /** Icon Themes to Load Icons from*/
+  private IconTheme mDefaultIconTheme, mIconTheme;
+  
+  /**
+   * Private Constructor
+   * 
+   * It creates the IconThemes
+   */
+  private IconLoader() {
+    File defaultIconDir = new File(Settings.getDefaultSettings().getProperty("icontheme", "icons/tango"));
+    
+    File iconDir = new File(Settings.propIcontheme.toString());
+    
+    if (!iconDir.exists() || !iconDir.isFile()) {
+      iconDir = defaultIconDir;
+    }
+    
+    mDefaultIconTheme = new IconTheme(defaultIconDir);
+    mDefaultIconTheme.loadTheme();
+    
+    if (defaultIconDir != iconDir) {
+      mIconTheme = new IconTheme(iconDir);
+      if (!mIconTheme.loadTheme()) {
+        mIconTheme = mDefaultIconTheme;
+      }
+    } else {
+      mIconTheme = mDefaultIconTheme;
+    }
+    
+  }
+  
+  /**
+   * Get an Instance of the IconLoader
+   * @return Instance
+   */
+  public static IconLoader getInstance() {
+    if (mInstance == null) {
+      mInstance = new IconLoader();
+    }
+    
+    return mInstance;
+  }
+
+  /**
+   * Load a specific Icon
+   * 
+   * @param category Category of the Icon
+   * @param icon Name of the Icon without File-Extension
+   * @param size Size in Pixel
+   * @return Icon if found, null if no Icon was found
+   */
+  public ImageIcon getIconFromTheme(String category, String icon, int size) {
+    return getIconFromTheme(null, category, icon, size);
+  }
+  
+  /**
+   * Load a specific Icon
+   *
+   * @param plugin Plugin that wants to use the Icon
+   * @param category Category of the Icon
+   * @param icon Name of the Icon without File-Extension
+   * @param size Size in Pixel
+   * @return Icon if found, null if no Icon was found
+   */
+  public ImageIcon getIconFromTheme(Plugin plugin, String category, String icon, int size) {
+    // First Try: Current Icon Theme
+    ImageIcon imageIcon = mIconTheme.getIcon(category, icon, size);
+    
+    if (imageIcon != null) {
+      return imageIcon;
+    }
+    
+    // Second Try: Default Icon Theme
+    if (mIconTheme != mDefaultIconTheme) {
+      imageIcon = mDefaultIconTheme.getIcon(category, icon, size);
+      
+      if (imageIcon != null) {
+        return imageIcon;
+      }
+    }
+ 
+    // Third Try: Icon in Plugin-Jar
+    if(plugin != null) {
+      StringBuffer buffer = new StringBuffer(plugin.getClass().getPackage().getName()).append("/icons/").append(size).append("x").append(size).append("/").append(category).append("/").append(icon).append(".png");
+      
+      if (plugin.getClass().getResourceAsStream(buffer.toString()) != null) {
+        try {
+          imageIcon = ImageUtilities.createImageIconFromJar(buffer.toString(), plugin.getClass()); 
+          
+          if (imageIcon != null)
+            return imageIcon;
+        } catch (Exception e) {
+        }
+      }
+    }
+    
+    // Last Try: Icon in tvbrowser.jar
+    StringBuffer buffer = new StringBuffer("/icons/").append(size).append("x").append(size).append("/").append(category).append("/").append(icon).append(".png");
+     
+    if (getClass().getResourceAsStream(buffer.toString()) != null) {
+      imageIcon = ImageUtilities.createImageIconFromJar(buffer.toString(), getClass()); 
+      return imageIcon;
+    }
+
+    // Failed, return null
+    return null;
+  }
+}
---
+++ tvbrowser/src/tvbrowser/core/icontheme/IconTheme.java
@@ -0,0 +1,264 @@
+/*
+ * TV-Browser
+ * Copyright (C) 04-2003 Martin Oberhauser (martin_oat@yahoo.de)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * CVS information:
+ *  $RCSfile$
+ *   $Source$
+ *     $Date$
+ *   $Author$
+ * $Revision$
+ */
+package tvbrowser.core.icontheme;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.logging.Level;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+import util.io.IniFileReader;
+import util.ui.ImageUtilities;
+import util.ui.UiUtilities;
+
+/**
+ * This class implements the IconTheme-Loading
+ * 
+ * Most of the Code is based on the FreeDesktop Specs
+ * http://standards.freedesktop.org/icon-theme-spec/icon-theme-spec-latest.html
+ */
+public class IconTheme {
+  /** Base-Directory of the IconTheme */
+  private File mIconBaseDirectory;
+  /** Logger */
+  private static java.util.logging.Logger mLog = java.util.logging.Logger.getLogger(IconTheme.class.getName());
+  /** Name and Comment */
+  private String mThemeName, mThemeComment;
+  /** Directory-Entries in the index.theme File*/
+  private ArrayList mDirectories;
+  
+  /**
+   * Create the IconTheme
+   * 
+   * The Directory must contain a index.theme-File. 
+   * For Details please look into the Specs.
+   * 
+   * @param iconDir Directory for this Theme
+   */
+  public IconTheme(File iconDir) {
+    mIconBaseDirectory = iconDir;
+  }
+
+  /**
+   * Load the Theme-File
+   * @return true if successfull
+   */
+  public boolean loadTheme() {
+    return loadThemeFile();
+  }
+
+  /**
+   * Load the .theme-File and parses it 
+   * @return true if successfull
+   */
+  private boolean loadThemeFile() {
+    try {
+      File theme = new File(mIconBaseDirectory, "index.theme");
+
+      if (!theme.exists()) {
+        return false;
+      }
+
+      mDirectories = new ArrayList();
+      IniFileReader iniReader = new IniFileReader(theme);
+
+      HashMap iconSection = iniReader.getSection("Icon Theme");
+      
+      mThemeName = (String)iconSection.get("Name");
+      mThemeComment = (String)iconSection.get("Comment");
+
+      String[] directories = ((String)iconSection.get("Directories")).split(",");
+      
+      int len = directories.length;
+      for (int i = 0;i<len;i++) {
+        HashMap dirMap = iniReader.getSection(directories[i]);
+        
+        String context = (String)dirMap.get("Context"); 
+        String type = (String)dirMap.get("Type");
+        int size = parseInt((String)dirMap.get("Size"));
+        int maxsize = parseInt((String)dirMap.get("MaxSize"));
+        int minsize = parseInt((String)dirMap.get("MinSize"));
+        int threshold = parseInt((String)dirMap.get("Threshold"));
+        
+        Directory dir = new Directory(directories[i],context, type, size, maxsize, minsize, threshold);
+        mDirectories.add(dir);
+      }
+      
+      return true;
+    } catch (Exception e) {
+      mLog.log(Level.SEVERE, "Problems loading Icon theme", e);
+      e.printStackTrace();
+    }
+
+    return false;
+  }
+
+  /**
+   * Parse an Integer
+   * @param str String that contains an Integer
+   * @return -1 if wrong, otherwise the Value of the String
+   */
+  private int parseInt(String str) {
+    try {
+      int num = Integer.parseInt(str);
+      return num;
+    } catch (Exception e) {
+    }
+    return -1;
+  }
+  
+  /**
+   * Get the Name of the Theme
+   * @return Name
+   */
+  public String getName() {
+    return mThemeName;
+  }
+  
+  /**
+   * Get the Comment of the Theme
+   * @return Comment
+   */
+  public String getComment() {
+    return mThemeComment;
+  }
+ 
+  /**
+   * Get an Icon from this Theme.
+   * This Method tries to find an exact size match, if it wasn't found
+   * it tries to find another Version of the Icon and rescales it.
+   * 
+   * @param category Category of the Icon
+   * @param icon Name of the Icon without File-Extension
+   * @param size Size of the Icon
+   * @return Icon or Null if Icon was not found
+   */
+  public ImageIcon getIcon(String category, String icon, int size) {
+    // First Try, find exact size matching Icon
+    Iterator it = mDirectories.iterator();
+    while (it.hasNext()) {
+      Directory dir = (Directory)it.next();
+      if (category.equalsIgnoreCase(category) && sizeMatches(dir, size)) {
+        StringBuffer iconFile = new StringBuffer(dir.getName()).append("/").append(icon).append(".png");
+        File file = new File(mIconBaseDirectory, iconFile.toString());
+        
+        if (file.exists()) {
+          return new ImageIcon(ImageUtilities.createImage(file.getAbsolutePath()));
+        }
+      }
+    }
+    
+    // Second Try, find best fitting Icon
+    int minSize = Integer.MAX_VALUE;
+    String closestMatch = null;
+    
+    it = mDirectories.iterator();
+    while (it.hasNext()) {
+      Directory dir = (Directory)it.next();
+      int distance = sizeDistance(dir, size); 
+      if (category.equalsIgnoreCase(category) && distance < minSize) {
+        StringBuffer iconFile = new StringBuffer(dir.getName()).append("/").append(icon).append(".png");
+        File file = new File(mIconBaseDirectory, iconFile.toString());
+
+        if (file.exists()) {
+          closestMatch = file.getAbsolutePath();
+          minSize = distance;
+        }
+        
+      }
+    }
+
+    // Found closest match, resize it
+    if (closestMatch != null) {
+      Icon closestIcon = new ImageIcon(ImageUtilities.createImage(closestMatch));
+      return (ImageIcon) UiUtilities.scaleIcon(closestIcon, size, size);
+    }
+    
+    return null;
+  }
+
+  /**
+   * Tests if the Size of the Directory matches 
+   * @param dir Directory to test
+   * @param size Size that is needed
+   * @return true if Size matches
+   */
+  private boolean sizeMatches(Directory dir, int size) {
+    if (dir.getType().equals("Fixed")) {
+      return size == dir.getSize();
+    }
+    
+    if (dir.getType().equals("Scaled")) {
+      return (dir.getMinSize() <= size) && (size <= dir.getMaxSize());
+    }
+    
+    if (dir.getType().equals("Threshold")) {
+      return (dir.getSize() - dir.getThreshold() <= size) && (size <= dir.getSize() + dir.getThreshold()); 
+    }
+    
+    return false;
+  }
+  
+  /**
+   * Returns the Distance between the size and the size of the Directory
+   * @param dir Directory to test
+   * @param size Size that is needed
+   * @return Distance between needed Size and the Size of the Directory
+   */
+  private int sizeDistance(Directory dir, int size) {
+
+    if (dir.getType().equals("Fixed")) {
+      return Math.abs(dir.getSize()-size);
+    }
+    
+    if (dir.getType().equals("Scaled")) {
+      if (size < dir.getMinSize()) {
+        return dir.getMinSize() - size;
+      }
+      if (size > dir.getMaxSize()) {
+        return size - dir.getMaxSize();
+      }
+      return 0;
+    }
+    
+    if (dir.getType().equals("Threshold")) {
+      if (size < (dir.getSize() - dir.getThreshold())) {
+        return dir.getMinSize() - size;
+      }
+      if (size > dir.getSize() + dir.getThreshold()) {
+        return size - dir.getMaxSize();
+      }
+      return 0;
+    }
+    
+    return 0;
+  }
+ 
+}
