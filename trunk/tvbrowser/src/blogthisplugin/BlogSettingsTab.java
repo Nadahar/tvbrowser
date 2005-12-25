608dac5bdc2421a53c7f53d913cc9629216af2cc
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2005-12-25 21:59:41 +0000

    Added Settingstab

---
+++ trunk/tvbrowser/src/blogthisplugin/BlogSettingsTab.java
@@ -0,0 +1,191 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * CVS information:
+ *  $RCSfile$
+ *   $Source$
+ *     $Date$
+ *   $Author$
+ * $Revision$
+ */
+package blogthisplugin;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Properties;
+
+import javax.swing.Icon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import util.ui.Localizer;
+
+import com.jgoodies.forms.factories.Borders;
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
+
+import devplugin.SettingsTab;
+
+/**
+ * Settings for the BlogSettings
+ * 
+ * @author bodum
+ */
+public class BlogSettingsTab implements SettingsTab {
+    /** Translator */
+    private static final Localizer mLocalizer = Localizer
+            .getLocalizerFor(BlogSettingsTab.class);
+
+    /** Plugin */
+    private BlogThisPlugin mPlugin;
+
+    /** Settings for this Plugin */
+    private Properties mSettings;
+
+    /** ComboBox for Blog-Service */
+    private JComboBox mServiceCombo;
+
+    /** Label for the Url-Inputfield */
+    private JLabel mServiceUrlLabel;
+
+    /** Textfield for Url */
+    private JTextField mServiceUrlField;
+
+    /**
+     * Create Plugin-Settingstab
+     * 
+     * @param plugin Plugin
+     * @param settings Settings of this Plugin
+     */
+    public BlogSettingsTab(BlogThisPlugin plugin, Properties settings) {
+        mPlugin = plugin;
+        mSettings = settings;
+    }
+
+    /**
+     * Create the SettingsPanel
+     */
+    public JPanel createSettingsPanel() {
+        JPanel settingsPanel = new JPanel(new FormLayout(
+                "pref, 3dlu, pref, fill:pref:grow, pref",
+                "pref, 3dlu, pref, 3dlu, pref"));
+        settingsPanel.setBorder(Borders.DLU4_BORDER);
+
+        String[] services = { "Blogger.com", "Blogg.de", "Wordpress",
+                "b2evolution" };
+
+        mServiceCombo = new JComboBox(services);
+
+        if (mSettings.getProperty("BlogService", "").equals(
+                BlogThisPlugin.BLOGGER)) {
+            mServiceCombo.setSelectedIndex(0);
+        } else if (mSettings.getProperty("BlogService", "").equals(
+                BlogThisPlugin.BLOGG)) {
+            mServiceCombo.setSelectedIndex(1);
+        } else if (mSettings.getProperty("BlogService", "").equals(
+                BlogThisPlugin.WORDPRESS)) {
+            mServiceCombo.setSelectedIndex(2);
+        } else if (mSettings.getProperty("BlogService", "").equals(
+                BlogThisPlugin.B2EVOLUTION)) {
+            mServiceCombo.setSelectedIndex(3);
+        }
+
+        mServiceCombo.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                updateAfterServiceSelection();
+            }
+        });
+
+        mServiceUrlLabel = new JLabel(mLocalizer.msg("Url", "Url")+":");
+        mServiceUrlLabel.setEnabled(mServiceCombo.getSelectedIndex() >= 2);
+
+        mServiceUrlField = new JTextField();
+        mServiceUrlField.setEnabled(mServiceCombo.getSelectedIndex() >= 2);
+        mServiceUrlField.setText(mSettings.getProperty("BlogUrl"));
+
+        CellConstraints cc = new CellConstraints();
+        settingsPanel.add(new JLabel(mLocalizer.msg("Service", "Blog-Service")+":"), cc.xy(1, 1));
+        settingsPanel.add(mServiceCombo, cc.xy(3, 1));
+        settingsPanel.add(mServiceUrlLabel, cc.xy(1, 3));
+        settingsPanel.add(mServiceUrlField, cc.xyw(3, 3, 3));
+
+        JButton extended = new JButton(mLocalizer.msg("Extended", "Extended"));
+        settingsPanel.add(extended, cc.xy(5, 5));
+
+        return settingsPanel;
+    }
+
+    /**
+     * Updates the GUI after a new Service is selected
+     */
+    private void updateAfterServiceSelection() {
+        mServiceUrlLabel.setEnabled(mServiceCombo.getSelectedIndex() >= 2);
+        mServiceUrlField.setEnabled(mServiceCombo.getSelectedIndex() >= 2);
+
+        if (mServiceCombo.getSelectedIndex() == 2) {
+            mServiceUrlField.setText(BlogThisPlugin.URL_WORDPRESS);
+        } else if (mServiceCombo.getSelectedIndex() == 3) {
+            mServiceUrlField.setText(BlogThisPlugin.URL_B2EVOLUTION);
+        } else {
+            mServiceUrlField.setText("");
+        }
+    }
+
+    /**
+     * Saves the Settings
+     */
+    public void saveSettings() {
+        switch (mServiceCombo.getSelectedIndex()) {
+        case 0:
+            mSettings.setProperty("BlogService", BlogThisPlugin.BLOGGER);
+            break;
+        case 1:
+            mSettings.setProperty("BlogService", BlogThisPlugin.BLOGG);
+            break;
+        case 2:
+            mSettings.setProperty("BlogService", BlogThisPlugin.WORDPRESS);
+            break;
+        case 3:
+            mSettings.setProperty("BlogService", BlogThisPlugin.B2EVOLUTION);
+            break;
+
+        default:
+            break;
+        }
+        mSettings.setProperty("BlogUrl", mServiceUrlField.getText());
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see devplugin.SettingsTab#getIcon()
+     */
+    public Icon getIcon() {
+        return mPlugin.createImageIcon("apps", "internet-web-browser", 16);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see devplugin.SettingsTab#getTitle()
+     */
+    public String getTitle() {
+        return mLocalizer.msg("Name", "Blog this!");
+    }
+
+}
