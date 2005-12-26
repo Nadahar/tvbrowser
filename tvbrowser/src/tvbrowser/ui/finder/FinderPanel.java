fe0f3c01156bf8ef7f0764a868ba7a236c54d1c2
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2005-12-26 17:27:35 +0000

    Added right-click to FinderPanel

--- tvbrowser/src/tvbrowser/ui/finder/FinderPanel.java
+++ tvbrowser/src/tvbrowser/ui/finder/FinderPanel.java
@@ -24,15 +24,15 @@
  * $Revision$
  */
 
-
- /**
-  * TV-Browser
-  * @author Martin Oberhauser
-  */
+/**
+ * TV-Browser
+ * @author Martin Oberhauser
+ */
 
 package tvbrowser.ui.finder;
 
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.*;
 
@@ -40,83 +40,83 @@ import devplugin.Date;
 import tvbrowser.core.*;
 import tvbrowser.ui.mainframe.MainFrame;
 
-
 class FinderItemRenderer extends DefaultListCellRenderer {
-  
+
   private FinderItem mCurSelectedItem;
-  
+
   public FinderItemRenderer() {
   }
 
- 
   public void setSelectedItem(FinderItem item) {
-    mCurSelectedItem=item;
+    mCurSelectedItem = item;
   }
- 
-  public Component getListCellRendererComponent(JList list, Object value,
-                int index, boolean isSelected, boolean cellHasFocus) {
-     
-    FinderItem comp = (FinderItem)value;
-    
+
+  public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected,
+      boolean cellHasFocus) {
+
+    FinderItem comp = (FinderItem) value;
+
     if (cellHasFocus) {
       comp.setBorder(BorderFactory.createLineBorder(Color.black));
-    }else{
-      comp.setBorder(BorderFactory.createEmptyBorder(1,1,1,1));
-    }     
-    
-    if (value==mCurSelectedItem) {
-      comp.setChoosen();  
-    }    
-    else if (isSelected) {
-      comp.setSelected();          
+    } else {
+      comp.setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1));
     }
-    else {
-      comp.setOpaque(false);          
-    }
-    
-    comp.setEnabled(TvDataBase.getInstance().dataAvailable(comp.getDate()));
-    return comp;    
-  }
- 
 
-} 
+    if (value == mCurSelectedItem) {
+      comp.setChoosen();
+    } else if (isSelected) {
+      comp.setSelected();
+    } else {
+      comp.setOpaque(false);
+    }
+
+    comp.setEnabled(TvDataBase.getInstance().dataAvailable(comp.getDate()));
+    return comp;
+  }
+
+}
 
 public class FinderPanel extends JScrollPane implements MouseListener, MouseMotionListener, KeyListener {
+
+  private static final util.ui.Localizer mLocalizer
+  = util.ui.Localizer.getLocalizerFor(FinderPanel.class);  
   
   private DateListener mDateChangedListener;
-  
+
   private JList mList;
+
   private DefaultListModel mModel;
+
   private FinderItemRenderer mRenderer;
-  
-  private int mCurMouseItemInx=-1;
+
+  private int mCurMouseItemInx = -1;
+
   private Date mCurChoosenDate;
 
   private Date mToday;
 
   public FinderPanel() {
-    
-    mModel=new DefaultListModel();
-    mList=new JList(mModel);
+
+    mModel = new DefaultListModel();
+    mList = new JList(mModel);
     mList.setOpaque(false);
-    
-    mRenderer=new FinderItemRenderer();
+
+    mRenderer = new FinderItemRenderer();
     mList.setCellRenderer(mRenderer);
-    
+
     setViewportView(mList);
 
-    mToday=Date.getCurrentDate();
+    mToday = Date.getCurrentDate();
     mModel.removeAllElements();
-    for (int i=-1;i<28;i++) {
-      mModel.addElement(new FinderItem(mList,mToday.addDays(i), mToday));
+    for (int i = -1; i < 28; i++) {
+      mModel.addElement(new FinderItem(mList, mToday.addDays(i), mToday));
     }
-    
-    mList.addMouseMotionListener(this);
-    mList.addMouseListener(this); 
-    mList.addKeyListener(this);
-    
-    markDate(mToday);
 
+    mList.addMouseMotionListener(this);
+    mList.addMouseListener(this);
+    mList.addKeyListener(this);
+
+    markDate(mToday);
 
   }
 
@@ -125,16 +125,16 @@ public class FinderPanel extends JScrollPane implements MouseListener, MouseMoti
    * the 'today' label.
    */
   public void updateContent() {
-    Date date=Date.getCurrentDate();
+    Date date = Date.getCurrentDate();
     if (date.equals(mToday)) {
       return;
     }
 
     mToday = date;
     mModel.removeAllElements();
-    for (int i=-1;i<28;i++) {
+    for (int i = -1; i < 28; i++) {
       Date d = mToday.addDays(i);
-      FinderItem fi = new FinderItem(mList,d, mToday);
+      FinderItem fi = new FinderItem(mList, d, mToday);
       mModel.addElement(fi);
       if (d.equals(mCurChoosenDate)) {
         mRenderer.setSelectedItem(fi);
@@ -145,27 +145,27 @@ public class FinderPanel extends JScrollPane implements MouseListener, MouseMoti
   }
 
   public void setDateListener(DateListener dateChangedListener) {
-    mDateChangedListener=dateChangedListener;
+    mDateChangedListener = dateChangedListener;
   }
-  
+
   public Date getSelectedDate() {
     return mCurChoosenDate;
   }
-  
+
   public devplugin.ProgressMonitor getProgressMonitorForDate(Date date) {
-    Object[] o=mModel.toArray();
-    for (int i=0;i<o.length;i++) {
-      FinderItem item=(FinderItem)o[i];
+    Object[] o = mModel.toArray();
+    for (int i = 0; i < o.length; i++) {
+      FinderItem item = (FinderItem) o[i];
       mRenderer.setSelectedItem(item);
-      
+
       if (item.getDate().equals(date)) {
         return item;
-      }      
+      }
     }
-    
+
     return null;
   }
-  
+
   public void markDate(Date d) {
     markDate(d, null);
   }
@@ -180,87 +180,111 @@ public class FinderPanel extends JScrollPane implements MouseListener, MouseMoti
 
   /**
    * This is a non blocking method
+   * 
    * @param d
    * @param callback
    */
   public void markDate(final Date d, Runnable callback) {
-    
+
     if (d.equals(mCurChoosenDate)) {
-      if (callback!=null) {
+      if (callback != null) {
         callback.run();
       }
       return;
-    }    
-    
-    Object[] o=mModel.toArray();
-    for (int i=0;i<o.length;i++) {
-      final FinderItem item=(FinderItem)o[i];
+    }
+
+    Object[] o = mModel.toArray();
+    for (int i = 0; i < o.length; i++) {
+      final FinderItem item = (FinderItem) o[i];
       if (item.getDate().equals(d)) {
         if (item.isEnabled()) {
-          mCurChoosenDate=d;       
+          mCurChoosenDate = d;
           mRenderer.setSelectedItem(item);
           mList.setSelectedValue(item, true);
           item.startProgress(mDateChangedListener, callback);
-          /*if (callback!=null) {
-            callback.run();
-          }*/
+          /*
+           * if (callback!=null) { callback.run(); }
+           */
           return;
-        }          
-      } 
+        }
+      }
     }
   }
 
-	
-	public void mouseClicked(MouseEvent event) {
-    int index=mList.locationToIndex(event.getPoint());    
-		markDate(((FinderItem)mModel.getElementAt(index)).getDate());
+  public void mouseClicked(MouseEvent event) {
+    int index = mList.locationToIndex(event.getPoint());
+    markDate(((FinderItem) mModel.getElementAt(index)).getDate());
     MainFrame.getInstance().addKeyboardAction();
-	}
+  }
 
+  public void mouseEntered(MouseEvent arg0) {
+  }
 
-	public void mouseEntered(MouseEvent arg0) {		
-	}
+  public void mouseExited(MouseEvent arg0) {
+    mList.clearSelection();
+    mCurMouseItemInx = -1;
+  }
 
-	
-	public void mouseExited(MouseEvent arg0) {
-		mList.clearSelection();
-    mCurMouseItemInx=-1;
-	}
+  public void mousePressed(MouseEvent arg0) {
+    if (arg0.isPopupTrigger()) {
+      showPopup(arg0);
+    }
+  }
 
-	
-	public void mousePressed(MouseEvent arg0) {}
+  public void mouseReleased(MouseEvent arg0) {
+    if (arg0.isPopupTrigger()) {
+      showPopup(arg0);
+    }
+  }
 
-	
-	public void mouseReleased(MouseEvent arg0) {}
-
-
-	public void mouseDragged(MouseEvent arg0) {}
-
-
-	public void mouseMoved(MouseEvent event) {
+  /**
+   * Show the Popup of the FinderPanel
+   */
+  private void showPopup(MouseEvent e) {
     
-    int index=mList.locationToIndex(event.getPoint());
+    JPopupMenu menu = new JPopupMenu();
+    
+    JMenuItem update = new JMenuItem(mLocalizer.msg("update", "Update"));
+    
+    update.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        MainFrame.getInstance().updateTvData();
+      };
+    });
+    
+    menu.add(update);
+    
+    int x = e.getX() + ((JComponent)e.getSource()).getX();
+    int y = e.getY() + ((JComponent)e.getSource()).getY();
+    
+    menu.show(this, x, y);
+  }
+
+  public void mouseDragged(MouseEvent arg0) {
+  }
+
+  public void mouseMoved(MouseEvent event) {
+
+    int index = mList.locationToIndex(event.getPoint());
     if (index != mCurMouseItemInx) {
-      mCurMouseItemInx=index;
+      mCurMouseItemInx = index;
       mList.setSelectedIndex(index);
     }
-		
-	}
 
-	
-	public void keyPressed(KeyEvent event) {
-		if (event.getKeyCode()==KeyEvent.VK_SPACE) {
-      FinderItem item = (FinderItem)mList.getSelectedValue();
+  }
+
+  public void keyPressed(KeyEvent event) {
+    if (event.getKeyCode() == KeyEvent.VK_SPACE) {
+      FinderItem item = (FinderItem) mList.getSelectedValue();
       if (item != null) {
         markDate(item.getDate());
       }
     }
-	}
+  }
 
-	
-	public void keyReleased(KeyEvent arg0) {}
+  public void keyReleased(KeyEvent arg0) {
+  }
 
-	
-	public void keyTyped(KeyEvent arg0) { }
+  public void keyTyped(KeyEvent arg0) {
+  }
 }
-
