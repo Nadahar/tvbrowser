aad945952f1393f02e0472255a203326ef9cc8de
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2006-03-02 08:21:51 +0000

    Refactored KeyboardAction

--- tvbrowser/src/tvbrowser/ui/programtable/KeyboardAction.java
+++ tvbrowser/src/tvbrowser/ui/programtable/KeyboardAction.java
@@ -3,57 +3,59 @@ package tvbrowser.ui.programtable;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.SwingUtilities;
+
 /**
  * A class for keyboard selection of programs in the ProgramTable.
  * 
  * @author René Mach
- *
+ * 
  */
 public class KeyboardAction implements ActionListener {
-  
+
+  public static final int KEY_UP = 0;
+  public static final int KEY_DOWN = 1;
+  public static final int KEY_RIGHT = 2;
+  public static final int KEY_LEFT = 3;
+  public static final int KEY_CONTEXTMENU = 4;
+  public static final int KEY_DESELECT = 5;
+  public static final int KEY_MIDDLECLICK = 6;
+  public static final int KEY_DOUBLECLICK = 7;
+
   private ProgramTableScrollPane mScrollPane;
-  private boolean mUp,mRight,mDown,mLeft,mContextMenu,mDeselect,mMiddleClick,mDoubleClick;
-  
+
+  private int mType;
+
   /**
    * 
    * @param pane The ProgramTableScrollPane to start the action on.
-   * @param up Action to go up?
-   * @param right Action to go right?
-   * @param down Action to go down?
-   * @param left Action to go left?
-   * @param context Action to open context menu?
-   * @param deselect Action to deselect the selected program?
-   * @param middleClick Action to start the middle click Plugin?
-   * @param doubleClick Action to start the double click Plugin?
-   */  
-  public KeyboardAction(ProgramTableScrollPane pane, boolean up, boolean right, boolean down, boolean left, boolean context, boolean deselect , boolean middleClick, boolean doubleClick) {
+   * @param type The Type of Action ( KEY_UP, KEY_DOWN, ...)
+   */
+  public KeyboardAction(ProgramTableScrollPane pane, int type) {
     mScrollPane = pane;
-    mUp = up;
-    mRight = right;
-    mDown = down;
-    mLeft = left;
-    mContextMenu = context;
-    mDeselect = deselect;
-    mMiddleClick = middleClick;
-    mDoubleClick = doubleClick;    
+    mType = type;
   }
-  
+
   public void actionPerformed(ActionEvent e) {
-    if(mUp)
-      mScrollPane.up();
-    if(mRight)
-      mScrollPane.right();
-    if(mDown)
-      mScrollPane.down();
-    if(mLeft)
-      mScrollPane.left();
-    if(mContextMenu)
-      mScrollPane.showPopupMenu();
-    if(mDeselect)
-      mScrollPane.deSelectItem();
-    if(mMiddleClick)
-      mScrollPane.handleMiddleClick();
-    if(mDoubleClick)
-      mScrollPane.handleDoubleClick();
+    SwingUtilities.invokeLater(new Runnable() {
+      public void run() {
+        if (mType == KEY_UP)
+          mScrollPane.up();
+        if (mType == KEY_DOWN)
+          mScrollPane.down();
+        if (mType == KEY_LEFT)
+          mScrollPane.left();
+        if (mType == KEY_RIGHT)
+          mScrollPane.right();
+        if (mType == KEY_CONTEXTMENU)
+          mScrollPane.showPopupMenu();
+        if (mType == KEY_DESELECT)
+          mScrollPane.deSelectItem();
+        if (mType == KEY_MIDDLECLICK)
+          mScrollPane.handleMiddleClick();
+        if (mType == KEY_DOUBLECLICK)
+          mScrollPane.handleDoubleClick();
+      };
+    });
   }
 }
