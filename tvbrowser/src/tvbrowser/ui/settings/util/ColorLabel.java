e2fec0661b24cd2760d5b0618935007274266539
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2006-03-06 16:10:50 +0000

    Removed unneded IMports

--- tvbrowser/src/tvbrowser/ui/settings/util/ColorLabel.java
+++ tvbrowser/src/tvbrowser/ui/settings/util/ColorLabel.java
@@ -1,52 +1,54 @@
 /*
-* TV-Browser
-* Copyright (C) 04-2003 Martin Oberhauser (martin@tvbrowser.org)
-*
-* This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License
-* as published by the Free Software Foundation; either version 2
-* of the License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-*
-* CVS information:
-*  $RCSfile$
-*   $Source$
-*     $Date$
-*   $Author$
-* $Revision$
-*/
+ * TV-Browser
+ * Copyright (C) 04-2003 Martin Oberhauser (martin@tvbrowser.org)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * CVS information:
+ *  $RCSfile$
+ *   $Source$
+ *     $Date$
+ *   $Author$
+ * $Revision$
+ */
 
 package tvbrowser.ui.settings.util;
 
-import javax.swing.*;
-
-import util.settings.ColorProperty;
-
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.GraphicsEnvironment;
 import java.awt.image.BufferedImage;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
 /**
- * Created by: Martin Oberhauser (martin@tvbrowser.org)
- * Date: 01.05.2005
+ * Created by: Martin Oberhauser (martin@tvbrowser.org) 
+ * Date: 01.05.2005 
  * Time: 14:21:28
  */
 
 public class ColorLabel extends JLabel {
 
-  private Color mColor,mStandardColor;
+  private Color mColor, mStandardColor;
 
   public ColorLabel(Color color) {
     super();
     mStandardColor = null;
-    setColor(color);    
+    setColor(color);
   }
 
   public void setColor(Color color) {
@@ -57,17 +59,19 @@ public class ColorLabel extends JLabel {
   public Color getColor() {
     return mColor;
   }
-  
+
   /**
    * Returns the standard color of this label
+   * 
    * @return Color
    */
   public Color getStandardColor() {
     return mStandardColor;
   }
-  
+
   /**
    * Sets the standard color of this label
+   * 
    * @param color Standard color
    */
   public void setStandardColor(Color color) {
@@ -75,18 +79,18 @@ public class ColorLabel extends JLabel {
   }
 
   private Icon createIcon() {
-        BufferedImage img = new BufferedImage(25, 10, BufferedImage.TYPE_INT_RGB);
+    BufferedImage img = new BufferedImage(25, 10, BufferedImage.TYPE_INT_RGB);
 
-        Graphics2D g = GraphicsEnvironment.getLocalGraphicsEnvironment().createGraphics(img);
+    Graphics2D g = GraphicsEnvironment.getLocalGraphicsEnvironment().createGraphics(img);
 
-        g.setColor(Color.WHITE);
-        g.fillRect(0,0,25,10);
-        g.setColor(mColor);
-        g.fillRect(0,0,25,10);
+    g.setColor(Color.WHITE);
+    g.fillRect(0, 0, 25, 10);
+    g.setColor(mColor);
+    g.fillRect(0, 0, 25, 10);
 
-        ImageIcon icon = new ImageIcon(img);
+    ImageIcon icon = new ImageIcon(img);
 
-        return icon;
-    }
+    return icon;
+  }
 
 }
