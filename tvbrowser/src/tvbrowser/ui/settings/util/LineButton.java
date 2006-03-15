b92bf2329f16ca61da3fa8f52d0de98bbd323af7
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2006-03-15 10:27:15 +0000

    Frq #1450262: Add GPL-Header

--- tvbrowser/src/tvbrowser/ui/settings/util/LineButton.java
+++ tvbrowser/src/tvbrowser/ui/settings/util/LineButton.java
@@ -1,3 +1,28 @@
+/*
+ * TV-Browser
+ * Copyright (C) 04-2003 Martin Oberhauser (martin@tvbrowser.org)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * CVS information:
+ *  $RCSfile$
+ *   $Source$
+ *     $Date$
+ *   $Author$
+ * $Revision$
+ */
 package tvbrowser.ui.settings.util;
 
 import java.awt.Color;
@@ -7,6 +32,11 @@ import java.awt.image.BufferedImage;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
+/**
+ * A Simple Button that contains a Line as Icon.
+ * 
+ * @author bodum
+ */
 public class LineButton extends JButton {
 
   public LineButton() {
