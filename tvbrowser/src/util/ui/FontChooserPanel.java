d96b445670607100572193b3ed840fb0c7f0c0c8
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2006-03-19 16:12:43 +0000

    Added new Constructor

--- tvbrowser/src/util/ui/FontChooserPanel.java
+++ tvbrowser/src/util/ui/FontChooserPanel.java
@@ -37,84 +37,92 @@ import javax.swing.JPanel;
 public class FontChooserPanel extends JPanel {
 
   /** The localizer for this class. */
-       private static final util.ui.Localizer mLocalizer
-       = util.ui.Localizer.getLocalizerFor(FontChooserPanel.class);
+  private static final util.ui.Localizer mLocalizer = util.ui.Localizer.getLocalizerFor(FontChooserPanel.class);
 
   private JComboBox mFontCB, mStyleCB, mSizeCB;
+
   private JLabel mTitle;
 
-    private static java.awt.GraphicsEnvironment ge =
-      java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment();
-    private static final String[] FONTNAMES = ge.getAvailableFontFamilyNames();
+  private static java.awt.GraphicsEnvironment ge = java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment();
 
-    private static final Integer[] FONTSIZES=new Integer[17];
+  private static final String[] FONTNAMES = ge.getAvailableFontFamilyNames();
 
-    private static final String[] FONTSTYLES={
-                mLocalizer.msg("plain", "plain"),
-                mLocalizer.msg("bold", "bold"),
-                mLocalizer.msg("italic", "italic")};
+  private static final Integer[] FONTSIZES = new Integer[17];
 
-    {
-      for (int i=0;i<FONTSIZES.length;i++) {
-        FONTSIZES[i]=new Integer(i+8);
-      }
+  private static final String[] FONTSTYLES = { mLocalizer.msg("plain", "plain"), mLocalizer.msg("bold", "bold"),
+      mLocalizer.msg("italic", "italic") };
+
+  {
+    for (int i = 0; i < FONTSIZES.length; i++) {
+      FONTSIZES[i] = new Integer(i + 8);
     }
+  }
 
   public FontChooserPanel(String title, Font font, boolean style) {
     setLayout(new BorderLayout());
-    mTitle=new JLabel(title);
-    add(mTitle,BorderLayout.NORTH);
-    JPanel panel1=new JPanel(new FlowLayout());
+    if (mTitle != null) {
+      mTitle = new JLabel(title);
+      add(mTitle, BorderLayout.NORTH);
+    }
+    JPanel panel1 = new JPanel(new FlowLayout());
 
-    mFontCB=new JComboBox(FONTNAMES);
-    mStyleCB=new JComboBox(FONTSTYLES);
-    mSizeCB=new JComboBox(FONTSIZES);
+    mFontCB = new JComboBox(FONTNAMES);
+    mStyleCB = new JComboBox(FONTSTYLES);
+    mSizeCB = new JComboBox(FONTSIZES);
 
     panel1.add(mFontCB);
-    
-    if(style)
+
+    if (style)
       panel1.add(mStyleCB);
-    
+
     panel1.add(mSizeCB);
 
-    add(panel1,BorderLayout.CENTER);
+    add(panel1, BorderLayout.CENTER);
 
     if (font != null) {
       selectFont(font);
-    }      
+    }
   }
-    
+
   public FontChooserPanel(String title, Font font) {
-    this(title,font,true);
+    this(title, font, true);
   }
 
-
+  /**
+   * Creates a Font-Chooser without Title-Bar
+   * 
+   * @param font
+   * @since 2.2
+   */
+  public FontChooserPanel(Font font) {
+    this(null, font, true);
+  }
+  
   public FontChooserPanel(String title) {
-    this (title, null);
+    this(title, null);
   }
 
   public void selectFont(Font font) {
-    for (int i=0;i<mFontCB.getItemCount();i++) {
-      String item=(String)mFontCB.getItemAt(i);
+    for (int i = 0; i < mFontCB.getItemCount(); i++) {
+      String item = (String) mFontCB.getItemAt(i);
       if (item.equals(font.getName())) {
         mFontCB.setSelectedIndex(i);
         break;
       }
     }
 
-    for (int i=0;i<mSizeCB.getItemCount();i++) {
-      Integer item=(Integer)mSizeCB.getItemAt(i);
+    for (int i = 0; i < mSizeCB.getItemCount(); i++) {
+      Integer item = (Integer) mSizeCB.getItemAt(i);
       if (item.intValue() == font.getSize()) {
         mSizeCB.setSelectedIndex(i);
       }
     }
 
-    if (font.getStyle()==Font.BOLD) {
+    if (font.getStyle() == Font.BOLD) {
       mStyleCB.setSelectedIndex(1);
-    }else if (font.getStyle()==Font.ITALIC) {
+    } else if (font.getStyle() == Font.ITALIC) {
       mStyleCB.setSelectedIndex(2);
     }
-
 
   }
 
@@ -123,27 +131,22 @@ public class FontChooserPanel extends JPanel {
     mFontCB.setEnabled(enabled);
     mStyleCB.setEnabled(enabled);
     mSizeCB.setEnabled(enabled);
-    mTitle.setEnabled(enabled);
-
+    if (mTitle != null)
+      mTitle.setEnabled(enabled);
   }
 
   public Font getChosenFont() {
     Font result;
     int style;
-    int inx=mStyleCB.getSelectedIndex();
-    if (inx==0) {
-      style=Font.PLAIN;
-    }else if (inx==1) {
-      style=Font.BOLD;
-    }else {
-      style=Font.ITALIC;
+    int inx = mStyleCB.getSelectedIndex();
+    if (inx == 0) {
+      style = Font.PLAIN;
+    } else if (inx == 1) {
+      style = Font.BOLD;
+    } else {
+      style = Font.ITALIC;
     }
-    result=new Font(
-      (String)mFontCB.getSelectedItem(),
-      style,
-      ((Integer)mSizeCB.getSelectedItem()).intValue()
-    );
-
+    result = new Font((String) mFontCB.getSelectedItem(), style, ((Integer) mSizeCB.getSelectedItem()).intValue());
 
     return result;
   }
