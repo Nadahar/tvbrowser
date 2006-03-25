1fac48df755d43a94d58d77e73adb4b389d81950
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2006-03-25 21:03:54 +0000

    Set Font of Contextmenu

--- tvbrowser/src/tvbrowser/core/contextmenu/ContextMenuManager.java
+++ tvbrowser/src/tvbrowser/core/contextmenu/ContextMenuManager.java
@@ -295,6 +295,7 @@ public class ContextMenuManager {
       } else if (menuIf instanceof ConfigMenuItem) {
         JMenuItem item = new JMenuItem(menuIf.toString());
         item.setIcon(IconLoader.getInstance().getIconFromTheme("categories", "preferences-desktop", 16));
+        item.setFont(MenuUtil.CONTEXT_MENU_PLAINFONT);
         item.addActionListener(new ActionListener() {
           public void actionPerformed(java.awt.event.ActionEvent e) {
             MainFrame.getInstance().showSettingsDialog(SettingsItem.CONTEXTMENU);
