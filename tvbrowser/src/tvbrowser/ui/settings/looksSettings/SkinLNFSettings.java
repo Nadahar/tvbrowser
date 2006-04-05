fc16f3006653e36da84752d8b5484a9c8245d221
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2006-04-05 15:58:05 +0000

    LnF-Settings : Added Config Dialogs

---
+++ tvbrowser/src/tvbrowser/ui/settings/looksSettings/SkinLNFSettings.java
@@ -0,0 +1,19 @@
+package tvbrowser.ui.settings.looksSettings;
+
+import javax.swing.JDialog;
+
+public class SkinLNFSettings extends JDialog {
+
+  public SkinLNFSettings(JDialog parent) {
+    super(parent, true);
+    
+    createGui();
+  }
+
+  private void createGui() {
+    
+    
+    
+  }
+  
+}
