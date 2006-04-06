a76efdc98cba56e930df2c8b28f5d69cb177bf49
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2006-04-06 15:27:54 +0000

    Add Settings for SkinLnF

--- tvbrowser/src/tvbrowser/ui/settings/looksSettings/SkinLNFSettings.java
+++ tvbrowser/src/tvbrowser/ui/settings/looksSettings/SkinLNFSettings.java
@@ -28,18 +28,24 @@ package tvbrowser.ui.settings.looksSettings;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JFileChooser;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
+import tvbrowser.core.Settings;
 import util.ui.UiUtilities;
 import util.ui.WindowClosingIf;
 
 import com.jgoodies.forms.builder.ButtonBarBuilder;
 import com.jgoodies.forms.factories.Borders;
+import com.jgoodies.forms.factories.DefaultComponentFactory;
 import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.Sizes;
 
 /**
  * Settings for the Skin LnF
@@ -49,6 +55,7 @@ import com.jgoodies.forms.layout.FormLayout;
 public class SkinLNFSettings extends JDialog implements WindowClosingIf {
   /** Translation */
   private static final util.ui.Localizer mLocalizer = util.ui.Localizer.getLocalizerFor(SkinLNFSettings.class);
+  private JTextField mThemePack;
  
   /**
    * Create the Dialog
@@ -66,11 +73,34 @@ public class SkinLNFSettings extends JDialog implements WindowClosingIf {
   private void createGui() {
     JPanel content = (JPanel) getContentPane();
     
-    content.setLayout(new FormLayout("pref, 3dlu, fill:pref:grow", "pref, 3dlu, pref, fill:3dlu:grow, pref"));
+    content.setLayout(new FormLayout("5dlu, fill:150dlu:grow, 3dlu, pref, 3dlu", "pref, 5dlu, pref, 5dlu, pref, 5dlu, pref, fill:3dlu:grow, pref"));
     content.setBorder(Borders.DLU4_BORDER);
     
     CellConstraints cc = new CellConstraints();
     
+    content.add(DefaultComponentFactory.getInstance().createSeparator(mLocalizer.msg("help", "Help")), cc.xyw(1,1,5));
+    
+    content.add(UiUtilities.createHtmlHelpTextArea(mLocalizer.msg("skinLFInfo", "Skin Info")), cc.xyw(2,3,3));
+    
+    mThemePack = new JTextField(Settings.propSkinLFThemepack.getString());
+    JButton chooseBtn = new JButton(mLocalizer.msg("choose", "Choose"));
+
+    content.add(DefaultComponentFactory.getInstance().createSeparator(mLocalizer.msg("chooseThemepack", "Choose Themepack")), cc.xyw(1,5,5));
+
+    content.add(mThemePack, cc.xy(2,7));
+    content.add(chooseBtn, cc.xy(4,7));
+    
+    chooseBtn.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        JFileChooser fileChooser=new JFileChooser(new File("themepacks"));
+        fileChooser.setFileFilter(new util.ui.ExtensionFileFilter(".zip","SkinLF themepacks (*.zip)"));
+        fileChooser.showOpenDialog(SkinLNFSettings.this);
+        File f=fileChooser.getSelectedFile();
+        if (f!=null) {
+          mThemePack.setText(f.getAbsolutePath());
+        }
+      }
+    });
     
     JButton ok = new JButton(mLocalizer.msg("ok", "OK"));
     ok.addActionListener(new ActionListener() {
@@ -91,11 +121,11 @@ public class SkinLNFSettings extends JDialog implements WindowClosingIf {
 
     JPanel panel = new JPanel(new FlowLayout(FlowLayout.RIGHT, 0, 0));
     panel.add(bar.getPanel());
-    content.add(panel, cc.xyw(1,5,3));
+    content.add(panel, cc.xyw(1,9,5));
    
     UiUtilities.registerForClosing(this);
     
-    pack();
+    setSize(Sizes.dialogUnitXAsPixel(300, this), Sizes.dialogUnitYAsPixel(150, this));
   }
 
   /**
@@ -109,6 +139,7 @@ public class SkinLNFSettings extends JDialog implements WindowClosingIf {
    * OK was pressed
    */
   protected void okPressed() {
+    Settings.propSkinLFThemepack.setString(mThemePack.getText());
     setVisible(false);
   }
 
