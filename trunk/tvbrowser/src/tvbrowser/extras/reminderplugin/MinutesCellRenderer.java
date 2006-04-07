53ede3fbed1cd9c636eac7126fcee5b0d27080b6
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2006-04-07 22:14:07 +0000

    RFE #1466356 Added edit icon to the time column in the Reminder table.

--- trunk/tvbrowser/src/tvbrowser/extras/reminderplugin/MinutesCellRenderer.java
+++ trunk/tvbrowser/src/tvbrowser/extras/reminderplugin/MinutesCellRenderer.java
@@ -3,14 +3,27 @@ package tvbrowser.extras.reminderplugin;
 import java.awt.Component;
 
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
+
+import tvbrowser.core.icontheme.IconLoader;
+
 public class MinutesCellRenderer extends DefaultTableCellRenderer {
-  JLabel label;
+  private JPanel mPanel;
+  private JLabel mTextLabel, mIconLabel;
   
   public MinutesCellRenderer() {
-    label = new JLabel();
+    mPanel = new JPanel(new FormLayout("pref,pref:grow,pref,2dlu","pref:grow"));
+    CellConstraints cc = new CellConstraints();
+    mTextLabel = new JLabel();
+    mIconLabel = new JLabel(IconLoader.getInstance().getIconFromTheme("actions","document-edit",16));
+    
+    mPanel.add(mTextLabel, cc.xy(1,1));
+    mPanel.add(mIconLabel, cc.xy(3,1));
   }
   
   public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
@@ -19,13 +32,16 @@ public class MinutesCellRenderer extends DefaultTableCellRenderer {
     if (value instanceof Integer) {
       Integer minutes = (Integer) value;
       
-      label.setText(ReminderFrame.getStringForMinutes(minutes.intValue()));
+      mTextLabel.setText(ReminderFrame.getStringForMinutes(minutes.intValue()));
       
-      label.setOpaque(def.isOpaque());
-      label.setForeground(def.getForeground());
-      label.setBackground(def.getBackground());
+      mTextLabel.setOpaque(def.isOpaque());
+      mTextLabel.setForeground(def.getForeground());
+      mTextLabel.setBackground(def.getBackground());
       
-      return label;
+      mPanel.setOpaque(def.isOpaque());
+      mPanel.setBackground(def.getBackground());
+      
+      return mPanel;
     }
     
     return def;
