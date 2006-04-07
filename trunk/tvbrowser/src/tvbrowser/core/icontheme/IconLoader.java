fc80d1d535172e9e2024e365fd5dc34a2b1013b2
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2006-04-07 16:35:57 +0000

    Fixed problem with loading icons from Plugin, the category instead of the icon name was used, so the icon couldn't be found.

--- trunk/tvbrowser/src/tvbrowser/core/icontheme/IconLoader.java
+++ trunk/tvbrowser/src/tvbrowser/core/icontheme/IconLoader.java
@@ -222,8 +222,8 @@ public class IconLoader {
     
     // Forth Try: Icon in Plugin-Jar
     if(plugin != null) {
-      StringBuffer buffer = new StringBuffer("/").append(plugin.getClass().getPackage().getName()).append("/icons/").append(icon.getSize()).append("x").append(icon.getSize()).append("/").append(icon.getCategory()).append("/").append(icon.getCategory()).append(".png");
-            
+      StringBuffer buffer = new StringBuffer("/").append(plugin.getClass().getPackage().getName()).append("/icons/").append(icon.getSize()).append("x").append(icon.getSize()).append("/").append(icon.getCategory()).append("/").append(icon.getName()).append(".png");
+      
       if (plugin.getClass().getResourceAsStream(buffer.toString()) != null) {
         try {
           imageIcon = ImageUtilities.createImageIconFromJar(buffer.toString(), plugin.getClass()); 
