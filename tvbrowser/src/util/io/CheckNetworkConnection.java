df8bd053a8c9f2cece7f1f91d479db1f7bce1fae
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2006-04-10 20:26:41 +0000

    Removed unneeded Import

--- tvbrowser/src/util/io/CheckNetworkConnection.java
+++ tvbrowser/src/util/io/CheckNetworkConnection.java
@@ -40,7 +40,6 @@ import javax.swing.BorderFactory;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JProgressBar;
 import javax.swing.SwingUtilities;
 
 import tvbrowser.ui.mainframe.MainFrame;
