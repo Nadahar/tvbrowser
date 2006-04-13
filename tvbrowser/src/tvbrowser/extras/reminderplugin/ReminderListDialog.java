f504719f86f6cff14f926b69f752e00cc6e281eb
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2006-04-13 16:34:53 +0000

    Add Config-Button

--- tvbrowser/src/tvbrowser/extras/reminderplugin/ReminderListDialog.java
+++ tvbrowser/src/tvbrowser/extras/reminderplugin/ReminderListDialog.java
@@ -45,41 +45,44 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.SwingUtilities;
 
+import tvbrowser.core.icontheme.IconLoader;
+import tvbrowser.core.plugin.PluginManagerImpl;
+import tvbrowser.ui.mainframe.MainFrame;
+import util.ui.ProgramTableCellRenderer;
+import util.ui.SendToPluginDialog;
+import util.ui.UiUtilities;
+import util.ui.WindowClosingIf;
+
 import com.jgoodies.forms.builder.ButtonBarBuilder;
 import com.jgoodies.forms.factories.Borders;
 import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
 
-import tvbrowser.core.icontheme.IconLoader;
-import tvbrowser.core.plugin.PluginManagerImpl;
-import util.ui.ProgramTableCellRenderer;
-import util.ui.SendToPluginDialog;
-import util.ui.UiUtilities;
-import util.ui.WindowClosingIf;
 import devplugin.Program;
+import devplugin.SettingsItem;
 
 /**
  * TV-Browser
- *
+ * 
  * @author Martin Oberhauser
  */
 public class ReminderListDialog extends JDialog implements WindowClosingIf {
 
-  private static final util.ui.Localizer mLocalizer
-          = util.ui.Localizer.getLocalizerFor(ReminderListDialog.class);
+  private static final util.ui.Localizer mLocalizer = util.ui.Localizer.getLocalizerFor(ReminderListDialog.class);
 
   private ReminderList reminderList;
+
   private JTable mTable;
 
   public ReminderListDialog(Frame parent, ReminderList list) {
-    super(parent,true);
+    super(parent, true);
     UiUtilities.registerForClosing(this);
-    
-    reminderList=list;
+
+    reminderList = list;
     setTitle(mLocalizer.msg("title", "Reminder"));
     createGui();
   }
-  
+
   private void createGui() {
     JPanel panel = (JPanel) getContentPane();
 
@@ -98,7 +101,7 @@ public class ReminderListDialog extends JDialog implements WindowClosingIf {
           mTable.getRootPane().dispatchEvent(e);
       }
     });
-    
+
     mTable.addMouseListener(new MouseAdapter() {
 
       public void mousePressed(MouseEvent evt) {
@@ -116,32 +119,33 @@ public class ReminderListDialog extends JDialog implements WindowClosingIf {
       public void mouseClicked(MouseEvent e) {
         if (SwingUtilities.isLeftMouseButton(e) && (e.getClickCount() == 1)) {
           int column = mTable.columnAtPoint(e.getPoint());
-          
-          if(column == 1) {
+
+          if (column == 1) {
             int row = mTable.rowAtPoint(e.getPoint());
             int height = mTable.getRowHeight(row);
-            
-            ((MinutesCellRenderer)mTable.getCellRenderer(row,column)).trackSingleClick(e.getPoint(),mTable,height,row,column);
+
+            ((MinutesCellRenderer) mTable.getCellRenderer(row, column)).trackSingleClick(e.getPoint(), mTable, height,
+                row, column);
           }
         }
         if (SwingUtilities.isLeftMouseButton(e) && (e.getClickCount() == 2)) {
           int column = mTable.columnAtPoint(e.getPoint());
-          
+
           if (column == 1)
             return;
-          
+
           int row = mTable.rowAtPoint(e.getPoint());
-          
+
           mTable.changeSelection(row, 0, false, false);
           Program p = (Program) mTable.getModel().getValueAt(row, 0);
-          
+
           PluginManagerImpl.getInstance().handleProgramDoubleClick(p, ReminderPlugin.getInstance());
         }
         if (SwingUtilities.isMiddleMouseButton(e) && (e.getClickCount() == 1)) {
           int row = mTable.rowAtPoint(e.getPoint());
           mTable.changeSelection(row, 0, false, false);
           Program p = (Program) mTable.getModel().getValueAt(row, 0);
-          
+
           PluginManagerImpl.getInstance().handleProgramMiddleClick(p, ReminderPlugin.getInstance());
         }
         mTable.updateUI();
@@ -154,20 +158,33 @@ public class ReminderListDialog extends JDialog implements WindowClosingIf {
 
     ButtonBarBuilder builder = ButtonBarBuilder.createLeftToRightBuilder();
 
+    JButton config = new JButton(IconLoader.getInstance().getIconFromTheme("categories", "preferences-desktop", 16));
+
+    config.setToolTipText(mLocalizer.msg("config", "Configure Reminder"));
+    
+    config.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        MainFrame.getInstance().showSettingsDialog(SettingsItem.REMINDER);
+      }
+    });
+
+    builder.addFixed(config);
+    builder.addRelatedGap();
+    
     JButton send = new JButton(IconLoader.getInstance().getIconFromTheme("actions", "edit-copy", 16));
-    send.setToolTipText(mLocalizer.msg("send","Send to other Plugins"));
+    send.setToolTipText(mLocalizer.msg("send", "Send to other Plugins"));
 
     send.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         showSendDialog();
       }
     });
-    
+
     builder.addFixed(send);
     builder.addRelatedGap();
 
     JButton delete = new JButton(IconLoader.getInstance().getIconFromTheme("actions", "edit-delete", 16));
-    delete.setToolTipText(mLocalizer.msg("delete","Remove selected programs from reminder list"));
+    delete.setToolTipText(mLocalizer.msg("delete", "Remove selected programs from reminder list"));
 
     delete.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
@@ -176,10 +193,10 @@ public class ReminderListDialog extends JDialog implements WindowClosingIf {
     });
 
     builder.addFixed(delete);
+
     builder.addRelatedGap();
 
-
-    JButton ok = new JButton(mLocalizer.msg("close","Close"));
+    JButton ok = new JButton(mLocalizer.msg("close", "Close"));
 
     ok.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
@@ -197,31 +214,30 @@ public class ReminderListDialog extends JDialog implements WindowClosingIf {
     pack();
 
     getRootPane().setDefaultButton(ok);
-    
+
     setSize(new Dimension(550, 350));
   }
-  
+
   private void installTableModel(ReminderTableModel model) {
     mTable.setModel(model);
     mTable.getColumnModel().getColumn(0).setCellRenderer(new ProgramTableCellRenderer());
     mTable.getColumnModel().getColumn(1).setCellEditor(new MinutesCellEditor());
     mTable.getColumnModel().getColumn(1).setCellRenderer(new MinutesCellRenderer());
   }
-  
+
   private void deleteItems() {
     int[] selected = mTable.getSelectedRows();
-    
+
     if (selected.length < 1 && mTable.getRowCount() > 0) {
-      int i = JOptionPane.showConfirmDialog(this,mLocalizer.msg("deleteQuestion",
-          "Should all Reminders be deleted?"),mLocalizer.msg("delTitle","Delete Reminder"),
-          JOptionPane.YES_NO_CANCEL_OPTION);
-      
-      if(i == 0) {
+      int i = JOptionPane.showConfirmDialog(this, mLocalizer.msg("deleteQuestion", "Should all Reminders be deleted?"),
+          mLocalizer.msg("delTitle", "Delete Reminder"), JOptionPane.YES_NO_CANCEL_OPTION);
+
+      if (i == 0) {
         mTable.getSelectionModel().setSelectionInterval(0, mTable.getRowCount() - 1);
         selected = mTable.getSelectedRows();
       }
     }
-    
+
     if (selected.length > 0) {
       Arrays.sort(selected);
 
@@ -246,32 +262,32 @@ public class ReminderListDialog extends JDialog implements WindowClosingIf {
   private void showSendDialog() {
     int[] rows = mTable.getSelectedRows();
     Program[] programArr;
-    
-    if(rows == null || rows.length == 0) {
+
+    if (rows == null || rows.length == 0) {
       ReminderListItem[] items = reminderList.getReminderItems();
       programArr = new Program[items.length];
-      
-      for (int i=0; i<items.length; i++)
+
+      for (int i = 0; i < items.length; i++)
         programArr[i] = items[i].getProgram();
-    }
-    else {
+    } else {
       ArrayList programs = new ArrayList();
-      
+
       for (int i = 0; i < rows.length; i++)
-        programs.add((Program)mTable.getValueAt(rows[i],0));
-      
+        programs.add((Program) mTable.getValueAt(rows[i], 0));
+
       programArr = new Program[programs.size()];
       programs.toArray(programArr);
     }
-      
+
     if (programArr.length > 0) {
       SendToPluginDialog send = new SendToPluginDialog(null, this, programArr);
       send.setVisible(true);
     }
   }
-  
+
   /**
    * Shows the Popup
+   * 
    * @param e Mouse-Event
    */
   private void showPopup(MouseEvent e) {
