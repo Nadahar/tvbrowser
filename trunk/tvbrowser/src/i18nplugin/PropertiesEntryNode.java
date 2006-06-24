2ddbbfd69f56d8071f5cb9cb9936890284e33028
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2006-06-24 07:09:12 +0000

    RFE #1511604 : Finished Tree

---
+++ trunk/tvbrowser/src/i18nplugin/PropertiesEntryNode.java
@@ -0,0 +1,41 @@
+/*
+ * TV-Browser
+ * Copyright (C) 04-2003 Martin Oberhauser (martin_oat@yahoo.de)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * CVS information:
+ *  $RCSfile$
+ *   $Source$
+ *     $Date: 2006-06-05 21:02:43 +0200 (Mo, 05 Jun 2006) $
+ *   $Author: darras $
+ * $Revision: 2466 $
+ */
+package i18nplugin;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+/**
+ * Entry for a Property
+ * 
+ * @author bodum
+ */
+public class PropertiesEntryNode extends DefaultMutableTreeNode {
+
+  public PropertiesEntryNode(String name) {
+    super(name);
+  }
+  
+}
