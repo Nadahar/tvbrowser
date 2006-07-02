65fa160d50455a8406bfa0a83329016ebd3c53f7
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2006-07-02 10:29:11 +0000

    Made Dialog always on Front

--- tvbrowser/src/tvbrowser/ui/update/SoftwareUpdateDlg.java
+++ tvbrowser/src/tvbrowser/ui/update/SoftwareUpdateDlg.java
@@ -24,11 +24,11 @@
  * $Revision$
  */
 
-
 package tvbrowser.ui.update;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.Dialog;
 import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -52,6 +52,8 @@ import javax.swing.event.HyperlinkListener;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
+import com.jgoodies.forms.builder.ButtonBarBuilder;
+
 import tvbrowser.core.Settings;
 import util.browserlauncher.Launch;
 import util.exc.TvBrowserException;
@@ -59,54 +61,64 @@ import util.ui.WindowClosingIf;
 import util.ui.html.ExtendedHTMLEditorKit;
 
 public class SoftwareUpdateDlg extends JDialog implements ActionListener, ListSelectionListener, WindowClosingIf {
-	
-	/** The localizer for this class. */
-	private static final util.ui.Localizer mLocalizer
-	= util.ui.Localizer.getLocalizerFor(SoftwareUpdateDlg.class);
-	
-	
-	private JButton mCloseBtn, mDownloadBtn;
+
+  /** The localizer for this class. */
+  private static final util.ui.Localizer mLocalizer = util.ui.Localizer.getLocalizerFor(SoftwareUpdateDlg.class);
+
+  private JButton mCloseBtn, mDownloadBtn;
+
   private JLabel mInfoLb;
-	private JList mList;
-	private Frame mParent;
-	private JEditorPane mDescriptionPane;
-	
-	public SoftwareUpdateDlg(Frame parent) {
-		
-		
-		super(parent,true);
-		mParent=parent;
-		setTitle(mLocalizer.msg("title","Download plugins"));
-		
-		JPanel contentPane=(JPanel)getContentPane();
-		contentPane.setLayout(new BorderLayout(0,10));
-		contentPane.setBorder(BorderFactory.createEmptyBorder(10,10,11,11));
-		JPanel btnPanel=new JPanel(new BorderLayout(10,0));
-		mCloseBtn=new JButton(mLocalizer.msg("close","Close"));
-		mCloseBtn.addActionListener(this);
-		
-    mDownloadBtn = new JButton(mLocalizer.msg("download","Download selected items"));
+
+  private JList mList;
+
+  private JEditorPane mDescriptionPane;
+
+  public SoftwareUpdateDlg(Dialog parent) {
+    super(parent, true);
+    createGui();
+  }
+
+  
+  public SoftwareUpdateDlg(Frame parent) {
+    super(parent, true);
+    createGui();
+  }
+  
+  private void createGui() {
+    setTitle(mLocalizer.msg("title", "Download plugins"));
+
+    JPanel contentPane = (JPanel) getContentPane();
+    contentPane.setLayout(new BorderLayout(0, 10));
+    contentPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 11, 11));
+    mCloseBtn = new JButton(mLocalizer.msg("close", "Close"));
+    mCloseBtn.addActionListener(this);
+
+    mDownloadBtn = new JButton(mLocalizer.msg("download", "Download selected items"));
     mDownloadBtn.addActionListener(this);
     mDownloadBtn.setEnabled(false);
+
+    ButtonBarBuilder builder = new ButtonBarBuilder();
     
-    btnPanel.add(mDownloadBtn, BorderLayout.CENTER);
-    btnPanel.add(mCloseBtn, BorderLayout.EAST);
-    
-		 
+    builder.addGlue();
+    builder.addFixed(mDownloadBtn);
+    builder.addRelatedGap();
+    builder.addFixed(mCloseBtn);
+
     mList = new JList();
     mList.setCellRenderer(new SoftwareUpdateItemRenderer());
     mList.addListSelectionListener(this);
-		
+
     JPanel northPn = new JPanel();
-    northPn.add(new JLabel(mLocalizer.msg("header","Hier k&ouml;nnen neue Plugins heruntergeladen und installiert werden.")));
-    
+    northPn.add(new JLabel(mLocalizer.msg("header",
+        "Hier k&ouml;nnen neue Plugins heruntergeladen und installiert werden.")));
+
     JPanel southPn = new JPanel(new BorderLayout());
-    
+
     mDescriptionPane = new JEditorPane();
-    
+
     mDescriptionPane.setEditorKit(new ExtendedHTMLEditorKit());
     mDescriptionPane.setEditable(false);
-    
+
     mDescriptionPane.addHyperlinkListener(new HyperlinkListener() {
       public void hyperlinkUpdate(HyperlinkEvent evt) {
         if (evt.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
@@ -117,159 +129,130 @@ public class SoftwareUpdateDlg extends JDialog implements ActionListener, ListSe
         }
       }
     });
-    
-    southPn.add(btnPanel, BorderLayout.SOUTH);
-    
-    JSplitPane splitPn = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, true, new JScrollPane(mList), new JScrollPane(mDescriptionPane));
+
+    southPn.add(builder.getPanel(), BorderLayout.SOUTH);
+
+    JSplitPane splitPn = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, true, new JScrollPane(mList), new JScrollPane(
+        mDescriptionPane));
     splitPn.setDividerLocation(150);
-    
+
     contentPane.add(northPn, BorderLayout.NORTH);
     contentPane.add(splitPn, BorderLayout.CENTER);
     contentPane.add(southPn, BorderLayout.SOUTH);
-    
-    if(Settings.propUpdateDialogWidth.getInt() == -1 
-        || Settings.propUpdateDialogHeight.getInt() == -1)
-		  this.setSize(450,400);
+
+    if (Settings.propUpdateDialogWidth.getInt() == -1 || Settings.propUpdateDialogHeight.getInt() == -1)
+      this.setSize(450, 400);
     else
-      this.setSize(Settings.propUpdateDialogWidth.getInt(),
-          Settings.propUpdateDialogHeight.getInt());
-    
+      this.setSize(Settings.propUpdateDialogWidth.getInt(), Settings.propUpdateDialogHeight.getInt());
+
     setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
-    
+
     addWindowListener(new WindowAdapter() {
       public void windowClosing(WindowEvent e) {
         close();
       }
-    });    
-	}
-	
-	
+    });
+  }
+
   private void updateDescription(SoftwareUpdateItem item) {
     if (item == null) {
-        mDescriptionPane.setText("");  
-    }
-    else {
+      mDescriptionPane.setText("");
+    } else {
       StringBuffer content = new StringBuffer();
       String author = item.getProperty("author");
       String website = item.getWebsite();
       devplugin.Version version = item.getVersion();
-      
-      content.append("<html><head>"
-          + "<style type=\"text/css\" media=\"screen\"><!--"
-          + "body { font-family: Dialog; }"
-          + "h1 { font-size: medium; }"
-          + "td { text-align: left; font-style: bold; font-size: small;}"
-          + "th { text-align: right; font-style: plain; font-size: small; }"
-          + "--></style>" +
-          "</head>")
-        .append("<body><h1>")
-        .append(item.getName())
-        .append("</h1>")
-        .append("<p>")
-        .append(item.getDescription())
-        .append("</p><br><table>");
-      
+
+      content.append(
+          "<html><head>" + "<style type=\"text/css\" media=\"screen\"><!--" + "body { font-family: Dialog; }"
+              + "h1 { font-size: medium; }" + "td { text-align: left; font-style: bold; font-size: small;}"
+              + "th { text-align: right; font-style: plain; font-size: small; }" + "--></style>" + "</head>").append(
+          "<body><h1>").append(item.getName()).append("</h1>").append("<p>").append(item.getDescription()).append(
+          "</p><br><table>");
+
       if (author != null) {
-        content.append("<tr><th>")
-               .append(mLocalizer.msg("author","author"))
-               .append(":</th><td>")
-               .append(author)
-               .append("</td></tr>");
+        content.append("<tr><th>").append(mLocalizer.msg("author", "author")).append(":</th><td>").append(author)
+            .append("</td></tr>");
       }
       if (version != null) {
-        content.append("<tr><th>")
-               .append(mLocalizer.msg("version","version"))
-               .append("</th><td>")
-               .append(version)
-               .append("</td></tr>");
+        content.append("<tr><th>").append(mLocalizer.msg("version", "version")).append("</th><td>").append(version)
+            .append("</td></tr>");
       }
       if (website != null) {
-        content.append("<tr><th>")
-               .append(mLocalizer.msg("website","website"))
-               .append("</th><td><a href=\"")
-               .append(website)
-               .append("\">")
-               .append(website)
-               .append("</a></td></tr>");
-      }  
+        content.append("<tr><th>").append(mLocalizer.msg("website", "website")).append("</th><td><a href=\"").append(
+            website).append("\">").append(website).append("</a></td></tr>");
+      }
       content.append("</table></body></html>");
-      
+
       mDescriptionPane.setText(content.toString());
     }
   }
-    
-	public void actionPerformed(ActionEvent event) {
+
+  public void actionPerformed(ActionEvent event) {
     if (event.getSource() == mCloseBtn) {
-		  close();
-    }
-    else if (event.getSource() == mDownloadBtn) {
+      close();
+    } else if (event.getSource() == mDownloadBtn) {
       Object[] o = mList.getSelectedValues();
       int successfullyDownloadedItems = 0;
-      for (int i = 0; i<o.length; i++) {
-        SoftwareUpdateItem item = (SoftwareUpdateItem)o[i];
+      for (int i = 0; i < o.length; i++) {
+        SoftwareUpdateItem item = (SoftwareUpdateItem) o[i];
         try {
           item.download();
           successfullyDownloadedItems++;
         } catch (TvBrowserException e) {
-           util.exc.ErrorHandler.handle(e); 
+          util.exc.ErrorHandler.handle(e);
         }
       }
-      if (successfullyDownloadedItems>0) {
-        JOptionPane.showMessageDialog(null,mLocalizer.msg("restartprogram","please restart tvbrowser before..."));
+      if (successfullyDownloadedItems > 0) {
+        JOptionPane.showMessageDialog(null, mLocalizer.msg("restartprogram", "please restart tvbrowser before..."));
         close();
       }
     }
-	}
-	
-	public void setSoftwareUpdateItems(SoftwareUpdateItem[] items) {		
+  }
+
+  public void setSoftwareUpdateItems(SoftwareUpdateItem[] items) {
     mList.setListData(items);
   }
-	
-	
 
   public void valueChanged(ListSelectionEvent event) {
     JList list = (JList) event.getSource();
-    
+
     Object[] items = list.getSelectedValues();
     if (items.length == 1) {
-      updateDescription((SoftwareUpdateItem)items[0]);
-    }
-    else {
+      updateDescription((SoftwareUpdateItem) items[0]);
+    } else {
       updateDescription(null);
     }
-    
+
     if (items.length == 0) {
       mDownloadBtn.setEnabled(false);
-    }
-    else {
+    } else {
       mDownloadBtn.setEnabled(true);
     }
-    
-    
+
   }
-  
+
   class SoftwareUpdateItemRenderer extends DefaultListCellRenderer {
-      
-    public Component getListCellRendererComponent(JList list, Object value,
-              int index, boolean isSelected, boolean cellHasFocus) {  
-       
-      JLabel label = (JLabel) super.getListCellRendererComponent(list, value,
-                index, isSelected, cellHasFocus);
+
+    public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected,
+        boolean cellHasFocus) {
+
+      JLabel label = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
 
       if (value instanceof SoftwareUpdateItem) {
-        SoftwareUpdateItem item = (SoftwareUpdateItem)value;      
+        SoftwareUpdateItem item = (SoftwareUpdateItem) value;
         label.setText(item.getName());
       }
       return label;
-        
+
     }
-      
+
   }
 
   public void close() {
     Settings.propUpdateDialogWidth.setInt(getWidth());
     Settings.propUpdateDialogHeight.setInt(getHeight());
-    
+
     setVisible(false);
   }
 
