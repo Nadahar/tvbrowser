28b5a11caeca0105b57b1d0c59e314d05e75db8a
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2006-07-08 15:21:42 +0000

    Bug #1519252: Possible NumbeerforamtError in EyeTg #1519252: Possible NumbeerforamtError in EyeTVV

--- tvbrowser/src/captureplugin/drivers/elgatodriver/ElgatoConnection.java
+++ tvbrowser/src/captureplugin/drivers/elgatodriver/ElgatoConnection.java
@@ -30,7 +30,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 
 import util.misc.AppleScriptRunner;
-
 import captureplugin.CapturePlugin;
 import devplugin.Channel;
 import devplugin.Date;
@@ -42,222 +41,257 @@ import devplugin.Program;
  * @author bodum
  */
 public class ElgatoConnection {
-  /** AppleScript Runner */
-  private AppleScriptRunner mAppleScript = new AppleScriptRunner();
+    /** AppleScript Runner */
+    private AppleScriptRunner mAppleScript = new AppleScriptRunner();
 
-  /** Script to get the Channelllist */
-  private static final String CHANNELLIST = "set chList to {}\n" + "tell application \"EyeTV\"\n"
-      + "  repeat with ch in channels\n" + "    set end of chList to channel number of contents of ch\n"
-      + "    set end of chList to name of contents of ch\n" + "  end repeat\n" + "end tell\n"
-      + "set text item delimiters to \"¥\"\n" + "set outString to chList as text\n" + "outString";
+    /** Script to get the Channelllist */
+    private static final String CHANNELLIST = "set chList to {}\n"
+            + "tell application \"EyeTV\"\n" + "  repeat with ch in channels\n"
+            + "    set end of chList to channel number of contents of ch\n"
+            + "    set end of chList to name of contents of ch\n"
+            + "  end repeat\n" + "end tell\n"
+            + "set text item delimiters to \"¥\"\n"
+            + "set outString to chList as text\n" + "outString";
 
-  /** Script for switching of Channels */
-  private static final String SWITCHCHANNEL = "tell application \"EyeTV\"\n" + "set current channel to \"{0}\"\n"
-      + "end tell";
+    /** Script for switching of Channels */
+    private static final String SWITCHCHANNEL = "tell application \"EyeTV\"\n"
+            + "set current channel to \"{0}\"\n" + "end tell";
 
-  /** Script for creation of recordings */
-  private static final String CREATERECORDING = "on stringToList from theString for myDelimiters\n"
-      + "  tell AppleScript\n"
-      + "    set theSavedDelimiters to AppleScript's text item delimiters\n"
-      + "    set text item delimiters to myDelimiters\n"
-      + "    \n"
-      + "    set outList to text items of theString\n"
-      + "    set text item delimiters to theSavedDelimiters\n"
-      + "    \n"
-      + "    return outList\n"
-      + "  end tell\n"
-      + "end stringToList\n"
-      + "\n"
-      + "\n"
-      + "on getdateForISOdate(theISODate, theISOTime)\n"
-      + "  local myDate\n"
-      + "  -- converts an ISO format (YYYY-MM-DD) and time to a date object\n"
-      + "  set monthConstants to {January, February, March, April, May, June, July, August, September, October, November, December} \n"
-      + " \n" + "  set theISODate to (stringToList from (theISODate) for \"-\")\n" + "  \n"
-      + "  set myDate to date theISOTime\n" + "  \n" + "  tell theISODate\n" + "    set year of myDate to item 1\n"
-      + "    set month of myDate to item (item 2) of monthConstants\n" + "    set day of myDate to item 3\n"
-      + "  end tell\n" + " \n" + "  return myDate\n" + "end getdateForISOdate\n" + "\n"
-      + "set dateob to getdateForISOdate(\"{0}\", \"{1}\")\n" + "\n" + 
-      "tell application \"EyeTV\"\n"
-      + "  make new program with properties {start time:dateob, duration:{2}, title:\"{3}\", channel number:{4}, description : \"{5}\"}\n"
-      + "end tell";
+    /** Script for creation of recordings */
+    private static final String CREATERECORDING = "on stringToList from theString for myDelimiters\n"
+            + "  tell AppleScript\n"
+            + "    set theSavedDelimiters to AppleScript's text item delimiters\n"
+            + "    set text item delimiters to myDelimiters\n"
+            + "    \n"
+            + "    set outList to text items of theString\n"
+            + "    set text item delimiters to theSavedDelimiters\n"
+            + "    \n"
+            + "    return outList\n"
+            + "  end tell\n"
+            + "end stringToList\n"
+            + "\n"
+            + "\n"
+            + "on getdateForISOdate(theISODate, theISOTime)\n"
+            + "  local myDate\n"
+            + "  -- converts an ISO format (YYYY-MM-DD) and time to a date object\n"
+            + "  set monthConstants to {January, February, March, April, May, June, July, August, September, October, November, December} \n"
+            + " \n"
+            + "  set theISODate to (stringToList from (theISODate) for \"-\")\n"
+            + "  \n"
+            + "  set myDate to date theISOTime\n"
+            + "  \n"
+            + "  tell theISODate\n"
+            + "    set year of myDate to item 1\n"
+            + "    set month of myDate to item (item 2) of monthConstants\n"
+            + "    set day of myDate to item 3\n"
+            + "  end tell\n"
+            + " \n"
+            + "  return myDate\n"
+            + "end getdateForISOdate\n"
+            + "\n"
+            + "set dateob to getdateForISOdate(\"{0}\", \"{1}\")\n"
+            + "\n"
+            + "tell application \"EyeTV\"\n"
+            + "  make new program with properties {start time:dateob, duration:{2}, title:\"{3}\", channel number:{4}, description : \"{5}\"}\n"
+            + "end tell";
 
-  /** List all Recordings */
-  private final String LISTRECORDINGS = "set recList to {}\n"
-      + "\n"
-      + "script x\n"
-      + "  on getIsoDate(dateObj)\n"
-      + "    tell dateObj\n"
-      + "     return (its year as string) & \"-\" & text 2 thru 3 of ((100 + (its month as integer)) as string) & \"-\" & text 2 thru 3 of ((100 + (its day)) as string)\n"
-      + "    end tell\n"
-      + "  end getIsoDate\n"
-      + "  \n"
-      + "  on getIsoTime(dateObj)\n"
-      + "    tell dateObj\n"
-      + "      return text 2 thru 3 of ((100 + ((its time) div hours as integer)) as string) & \":\" & text 2 thru 3 of ((100 + ((its time) mod hours div minutes)) as string)\n"
-      + "    end tell\n" + "  end getIsoTime\n" + "end script\n" + "\n" + "tell application \"EyeTV\"\n"
-      + "  repeat with prog in programs\n" + "    set end of recList to channel number of prog\n"
-      + "    set end of recList to x's getIsoDate(start time of prog)\n"
-      + "    set end of recList to x's getIsoTime(start time of prog)\n"
-      + "    set end of recList to duration of prog\n" + "    set end of recList to unique ID of prog\n"
-      + "    set end of recList to title of prog\n" + "  end repeat\n" + "end tell\n" + "\n"
-      + "set text item delimiters to \"¥\"\n" + "set outString to reclist as text\n" + "outString";
+    /** List all Recordings */
+    private final String LISTRECORDINGS = "set recList to {}\n"
+            + "\n"
+            + "script x\n"
+            + "  on getIsoDate(dateObj)\n"
+            + "    tell dateObj\n"
+            + "     return (its year as string) & \"-\" & text 2 thru 3 of ((100 + (its month as integer)) as string) & \"-\" & text 2 thru 3 of ((100 + (its day)) as string)\n"
+            + "    end tell\n"
+            + "  end getIsoDate\n"
+            + "  \n"
+            + "  on getIsoTime(dateObj)\n"
+            + "    tell dateObj\n"
+            + "      return text 2 thru 3 of ((100 + ((its time) div hours as integer)) as string) & \":\" & text 2 thru 3 of ((100 + ((its time) mod hours div minutes)) as string)\n"
+            + "    end tell\n" + "  end getIsoTime\n" + "end script\n" + "\n"
+            + "tell application \"EyeTV\"\n"
+            + "  repeat with prog in programs\n"
+            + "    set end of recList to channel number of prog\n"
+            + "    set end of recList to x's getIsoDate(start time of prog)\n"
+            + "    set end of recList to x's getIsoTime(start time of prog)\n"
+            + "    set end of recList to duration of prog\n"
+            + "    set end of recList to unique ID of prog\n"
+            + "    set end of recList to title of prog\n" + "  end repeat\n"
+            + "end tell\n" + "\n" + "set text item delimiters to \"¥\"\n"
+            + "set outString to reclist as text\n" + "outString";
 
-  /** Remove a specific Recording */
-  private final String REMOVERECORDING = "tell application \"EyeTV\"\n" + "  repeat with transmission in programs\n"
-      + "    if unique ID of transmission is {0} then\n" + "      delete transmission\n" + "    end if\n"
-      + "  end repeat\n" + "end tell";
+    /** Remove a specific Recording */
+    private final String REMOVERECORDING = "tell application \"EyeTV\"\n"
+            + "  repeat with transmission in programs\n"
+            + "    if unique ID of transmission is {0} then\n"
+            + "      delete transmission\n" + "    end if\n" + "  end repeat\n"
+            + "end tell";
 
-  /** Map with Program - Elgato ID Mappings */
-  private HashMap mProgramMapping = new HashMap();
+    /** Map with Program - Elgato ID Mappings */
+    private HashMap mProgramMapping = new HashMap();
 
-  /**
-   * Get the List of all available Channels
-   * 
-   * @return All available Channels
-   */
-  public ElgatoChannel[] getAvailableChannels() {
-    ArrayList list = new ArrayList();
+    /**
+     * Get the List of all available Channels
+     * 
+     * @return All available Channels
+     */
+    public ElgatoChannel[] getAvailableChannels() {
+        ArrayList list = new ArrayList();
 
-    String res = null;
-    try {
-        res = mAppleScript.executeScript(CHANNELLIST);
-    } catch (IOException e) {
-        e.printStackTrace();
+        String res = null;
+        try {
+            res = mAppleScript.executeScript(CHANNELLIST);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (res == null) {
+            return new ElgatoChannel[0];
+        }
+
+        String[] result = res.split("¥");
+
+        for (int i = 0; i < result.length; i += 2) {
+            ElgatoChannel channel = new ElgatoChannel(Integer
+                    .parseInt(result[i]), result[i + 1]);
+            list.add(channel);
+        }
+
+        return (ElgatoChannel[]) list.toArray(new ElgatoChannel[list.size()]);
     }
-    
-    if (res == null) {
-      return new ElgatoChannel[0];
-    }
-      
-    String[] result = res.split("¥");
 
-    for (int i = 0; i < result.length; i += 2) {
-      ElgatoChannel channel = new ElgatoChannel(Integer.parseInt(result[i]), result[i + 1]);
-      list.add(channel);
-    }
+    /**
+     * @return List of all current Recordings
+     */
+    public Program[] getAllRecordings(ElgatoConfig conf) {
+        ArrayList programs = new ArrayList();
 
-    return (ElgatoChannel[]) list.toArray(new ElgatoChannel[list.size()]);
-  }
+        mProgramMapping = new HashMap();
 
-  /**
-   * @return List of all current Recordings
-   */
-  public Program[] getAllRecordings(ElgatoConfig conf) {
-    ArrayList programs = new ArrayList();
+        String res = null;
+        try {
+            res = mAppleScript.executeScript(LISTRECORDINGS);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-    mProgramMapping = new HashMap();
+        if (res == null) {
+            return new Program[0];
+        }
 
-    String res = null;
-    try {
-        res = mAppleScript.executeScript(LISTRECORDINGS);
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-    
-    if (res == null) {
-      return new Program[0];
-    }
-    
-    String[] result = res.split("¥");
+        String[] result = res.split("¥");
 
-    for (int i = 0; i < result.length; i += 6) {
-      int channel = Integer.parseInt(result[i]);
-      String id = result[i + 4].replace(',', '.');
-      String title = result[i + 5].trim();
+        for (int i = 0; i < result.length; i += 6) {
+            try {
+                int channel = Integer.parseInt(result[i]);
+                String id = result[i + 4].replace(',', '.');
+                String title = result[i + 5].trim();
 
-      String[] dateStr = result[i + 1].split("-");
+                String[] dateStr = result[i + 1].split("-");
 
-      Date date = new Date(Integer.parseInt(dateStr[0]), Integer.parseInt(dateStr[1]), Integer.parseInt(dateStr[2]));
+                Date date = new Date(Integer.parseInt(dateStr[0]), Integer
+                        .parseInt(dateStr[1]), Integer.parseInt(dateStr[2]));
 
-      String[] hourStr = result[i + 2].split(":");
+                String[] hourStr = result[i + 2].split(":");
 
-      int hour = Integer.parseInt(hourStr[0]);
-      int min = Integer.parseInt(hourStr[1]);
+                int hour = Integer.parseInt(hourStr[0]);
+                int min = Integer.parseInt(hourStr[1]);
 
-      Channel chan = conf.getChannelForElgatoId(channel);
+                Channel chan = conf.getChannelForElgatoId(channel);
 
-      if (chan != null) {
-        Iterator it = CapturePlugin.getPluginManager().getChannelDayProgram(date, chan);
+                if (chan != null) {
+                    Iterator it = CapturePlugin.getPluginManager()
+                            .getChannelDayProgram(date, chan);
 
-        if (it != null)
-          while (it.hasNext()) {
-            Program prog = (Program) it.next();
+                    if (it != null)
+                        while (it.hasNext()) {
+                            Program prog = (Program) it.next();
 
-            if ((prog.getHours() == hour) && (prog.getMinutes() == min)
-                && (prog.getTitle().trim().toLowerCase().equals(title.trim().toLowerCase()))) {
-              programs.add(prog);
-              mProgramMapping.put(prog, id);
+                            if ((prog.getHours() == hour)
+                                    && (prog.getMinutes() == min)
+                                    && (prog.getTitle().trim().toLowerCase()
+                                            .equals(title.trim().toLowerCase()))) {
+                                programs.add(prog);
+                                mProgramMapping.put(prog, id);
+                            }
+                        }
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-          }
-      }
+        }
+
+        return (Program[]) programs.toArray(new Program[programs.size()]);
     }
 
-    return (Program[]) programs.toArray(new Program[programs.size()]);
-  }
+    /**
+     * Record Program
+     * 
+     * @param conf Config
+     * @param prg Program to record
+     * @param length Length of Program
+     * @return true if successfull
+     */
+    public boolean addToRecording(ElgatoConfig conf, Program prg, int length) {
+        String date = prg.getDate().getYear() + "-" + prg.getDate().getMonth()
+                + "-" + prg.getDate().getDayOfMonth();
 
-  /**
-   * Record Program
-   * 
-   * @param conf Config
-   * @param prg Program to record
-   * @param length Length of Program
-   * @return true if successfull
-   */
-  public boolean addToRecording(ElgatoConfig conf, Program prg, int length) {
-    String date = prg.getDate().getYear() + "-" + prg.getDate().getMonth() + "-" + prg.getDate().getDayOfMonth();
+        String time = prg.getHours() + ":" + prg.getMinutes();
 
-    String time = prg.getHours() + ":" + prg.getMinutes();
+        String call = CREATERECORDING.replaceAll("\\{0\\}", date);
+        call = call.replaceAll("\\{1\\}", time);
+        call = call.replaceAll("\\{2\\}", Integer.toString(length));
+        call = call.replaceAll("\\{3\\}", prg.getTitle());
+        call = call.replaceAll("\\{4\\}", Integer.toString(conf
+                .getElgatoChannel(prg.getChannel()).getNumber()));
+        call = call.replaceAll("\\{5\\}", prg.getShortInfo().replaceAll("\"",
+                "\\\\\\\\\"").replace('\n', ' '));
 
-    String call = CREATERECORDING.replaceAll("\\{0\\}", date);
-    call = call.replaceAll("\\{1\\}", time);
-    call = call.replaceAll("\\{2\\}", Integer.toString(length));
-    call = call.replaceAll("\\{3\\}", prg.getTitle());
-    call = call.replaceAll("\\{4\\}", Integer.toString(conf.getElgatoChannel(prg.getChannel()).getNumber()));
-    call = call.replaceAll("\\{5\\}", prg.getShortInfo().replaceAll("\"", "\\\\\\\\\"").replace('\n', ' '));
-    
-    String res = null;
-    try {
-        res = mAppleScript.executeScript(call);
-    } catch (IOException e) {
-        e.printStackTrace();
+        String res = null;
+        try {
+            res = mAppleScript.executeScript(call);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (res == null)
+            return false;
+
+        if (res.startsWith("program id"))
+            return true;
+
+        return false;
     }
 
-    if (res == null)
-      return false;
-
-    if (res.startsWith("program id"))
-      return true;
-
-    return false;
-  }
-
-  /**
-   * Remove Recording
-   * 
-   * @param prg Remove recording of this Program
-   */
-  public void removeRecording(Program prg) {
-    String id = (String) mProgramMapping.get(prg);
-    try {
-        mAppleScript.executeScript(REMOVERECORDING.replaceAll("\\{0\\}", id));
-    } catch (IOException e) {
-        e.printStackTrace();
+    /**
+     * Remove Recording
+     * 
+     * @param prg Remove recording of this Program
+     */
+    public void removeRecording(Program prg) {
+        String id = (String) mProgramMapping.get(prg);
+        try {
+            mAppleScript.executeScript(REMOVERECORDING
+                    .replaceAll("\\{0\\}", id));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  /**
-   * Switch to Channel of Program
-   * 
-   * @param conf Config to use
-   * @param prg Switch to Channel of Program
-   */
-  public void switchToChannel(ElgatoConfig conf, Program prg) {
-    try {
-        mAppleScript.executeScript(SWITCHCHANNEL.replaceAll("\\{0\\}", Integer.toString(conf.getElgatoChannel(
-            prg.getChannel()).getNumber())));
-    } catch (IOException e) {
-        e.printStackTrace();
+    /**
+     * Switch to Channel of Program
+     * 
+     * @param conf Config to use
+     * @param prg Switch to Channel of Program
+     */
+    public void switchToChannel(ElgatoConfig conf, Program prg) {
+        try {
+            mAppleScript.executeScript(SWITCHCHANNEL.replaceAll("\\{0\\}",
+                    Integer.toString(conf.getElgatoChannel(prg.getChannel())
+                            .getNumber())));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-  }
 }
