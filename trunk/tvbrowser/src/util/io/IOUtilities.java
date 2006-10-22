747a74d9363d447007ecab33e948a548a21a4f14
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2006-10-22 16:18:54 +0000

    Added Image-Feature to XmlTvPDS

--- trunk/tvbrowser/src/util/io/IOUtilities.java
+++ trunk/tvbrowser/src/util/io/IOUtilities.java
@@ -26,6 +26,8 @@
 
 package util.io;
 
+import util.ui.TimeFormatter;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
@@ -48,8 +50,6 @@ import java.util.logging.Level;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
-
-import util.ui.TimeFormatter;
 
 /**
  * A utilities class for I/O stuff. It constists of serveral static
@@ -684,5 +684,49 @@ public class IOUtilities {
     
     return data;
   }
-  
+
+
+    /**
+     * Reads all Bytes from a File into an byte array
+     *
+     * @param file Read Bytes from this File
+     * @return Byte-Array or NULL if too large
+     * @throws IOException Read-Exception
+     *
+     * @since  2.5
+     */
+    public static byte[] getBytesFromFile(File file) throws IOException {
+        InputStream is = new FileInputStream(file);
+
+        // Get the size of the file
+        long length = file.length();
+
+        // You cannot create an array using a long type.
+        // It needs to be an int type.
+        // Before converting to an int type, check
+        // to ensure that file is not larger than Integer.MAX_VALUE.
+        if (length > Integer.MAX_VALUE) {
+            return null;
+        }
+
+        // Create the byte array to hold the data
+        byte[] bytes = new byte[(int) length];
+
+        // Read in the bytes
+        int offset = 0;
+        int numRead = 0;
+        while (offset < bytes.length
+                && (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
+            offset += numRead;
+        }
+
+        // Ensure all the bytes have been read in
+        if (offset < bytes.length) {
+            throw new IOException("Could not completely read file " + file.getName());
+        }
+
+        // Close the input stream and return bytes
+        is.close();
+        return bytes;
+    }
 }
