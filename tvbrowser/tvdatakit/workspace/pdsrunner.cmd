d9245de11f4299c11dfb072920f5c6e7e226a900
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2006-10-29 11:56:27 +0000

    Added headless and increased memory limit

--- tvbrowser/tvdatakit/workspace/pdsrunner.cmd
+++ tvbrowser/tvdatakit/workspace/pdsrunner.cmd
@@ -2,4 +2,4 @@
 
 call setClasspath.cmd
 
-java -cp lib\PDSRunner.jar;%PDS_CLASSPATH% primarydatamanager.PDSRunner %*
+java -Xmx256m -Djava.awt.headless=true -cp lib\PDSRunner.jar;%PDS_CLASSPATH% primarydatamanager.PDSRunner %*
--- tvbrowser/tvdatakit/workspace/pdsrunner.sh
+++ tvbrowser/tvdatakit/workspace/pdsrunner.sh
@@ -2,4 +2,4 @@
 
 PDS_CLASSPATH=classes:lib/poi-3.0-alpha2-20060616.jar:lib/xercesImpl.jar
 
-java -cp lib/PDSRunner.jar:"$PDS_CLASSPATH" primarydatamanager.PDSRunner $*
+java -Xmx256m -Djava.awt.headless=true -cp lib/PDSRunner.jar:"$PDS_CLASSPATH" primarydatamanager.PDSRunner $*
