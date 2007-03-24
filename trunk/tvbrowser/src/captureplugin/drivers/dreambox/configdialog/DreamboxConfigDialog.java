66e7a3786e473eb412ff4c9862d5032671aa20aa
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2007-03-24 12:17:09 +0000

    RFE 1687331: Dreambox interface

---
+++ trunk/tvbrowser/src/captureplugin/drivers/dreambox/configdialog/DreamboxConfigDialog.java
@@ -0,0 +1,279 @@
+/*
+ * CapturePlugin by Andreas Hessel (Vidrec@gmx.de), Bodo Tasche
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * CVS information:
+ *  $RCSfile$
+ *   $Source$
+ *     $Date: 2007-01-03 09:06:40 +0100 (Mi, 03 Jan 2007) $
+ *   $Author: bananeweizen $
+ * $Revision: 2979 $
+ */
+package captureplugin.drivers.dreambox.configdialog;
+
+import captureplugin.CapturePlugin;
+import captureplugin.drivers.dreambox.DreamboxConfig;
+import captureplugin.drivers.dreambox.DreamboxDevice;
+import captureplugin.drivers.dreambox.connector.DreamboxChannel;
+import captureplugin.drivers.dreambox.connector.DreamboxConnector;
+import com.jgoodies.forms.builder.ButtonBarBuilder;
+import com.jgoodies.forms.factories.Borders;
+import com.jgoodies.forms.factories.DefaultComponentFactory;
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
+import util.ui.ChannelTableCellRenderer;
+import util.ui.Localizer;
+import util.ui.UiUtilities;
+import util.ui.WindowClosingIf;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.table.TableCellEditor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Collection;
+
+/**
+ * The configuration dialog for the dreambox
+ */
+public class DreamboxConfigDialog extends JDialog implements WindowClosingIf {
+    /**
+     * Translator
+     */
+    private static final Localizer mLocalizer = Localizer.getLocalizerFor(DreamboxConfigDialog.class);
+
+    /** Configuration */
+    private DreamboxConfig mConfig;
+    /** Device */
+    private DreamboxDevice mDevice;
+    /** Was ok pressed ? */
+    private boolean mOkPressed;
+    /** IP-Address of the dreambox */
+    private JTextField mDreamboxAddress;
+    /** Device Name of the dreambox */
+    private JTextField mDeviceName;
+    /** Table with channel mappings */
+    private JTable mTable;
+
+    /**
+     * Create the Dialog
+     * @param frame Parent-Frame
+     * @param device Device to configure
+     * @param config Config for the Device
+     */
+    public DreamboxConfigDialog(JFrame frame, DreamboxDevice device, DreamboxConfig config) {
+        super(frame, true);
+        mConfig = config.clone();
+        mDevice = device;
+        createGui();
+    }
+
+    /**
+     * Create the Dialog
+     * @param frame Parent-Frame
+     * @param device Device to configure
+     * @param config Config for the Device
+     */
+    public DreamboxConfigDialog(JDialog frame, DreamboxDevice device, DreamboxConfig config) {
+        super(frame, true);
+        mConfig = config.clone();
+        mDevice = device;
+        createGui();
+    }
+
+    /**
+     * Create the GUI
+     */
+    private void createGui() {
+        setTitle(mLocalizer.msg("title", "Configure Dreambox"));
+
+        UiUtilities.registerForClosing(this);
+
+        JPanel panel = (JPanel) getContentPane();
+        panel.setBorder(Borders.DLU4_BORDER);
+
+        panel.setLayout(new FormLayout("5dlu, fill:min:grow",
+                "pref, 3dlu, pref, 3dlu, pref, 3dlu, fill:min:grow, 3dlu, pref"));
+
+        CellConstraints cc = new CellConstraints();
+
+        panel.add(DefaultComponentFactory.getInstance().createSeparator(mLocalizer.msg("misc", "Miscellaneous")), cc.xyw(1, 1, 2));
+
+        JPanel miscPanel = new JPanel(new FormLayout("right:pref, 3dlu, fill:min:grow, 3dlu, pref, 3dlu, pref", "pref, 3dlu,pref, 3dlu, pref"));
+
+        miscPanel.add(new JLabel(mLocalizer.msg("name","Name:")), cc.xy(1, 1));
+        mDeviceName = new JTextField(mDevice.getName());
+        miscPanel.add(mDeviceName, cc.xy(3, 1));
+
+
+        miscPanel.add(new JLabel(mLocalizer.msg("ipaddress", "IP address")), cc.xy(1, 3));
+        mDreamboxAddress = new JTextField(mConfig.getDreamboxAddress());
+        miscPanel.add(mDreamboxAddress, cc.xy(3, 3));
+
+/*        JButton find = new JButton("Suchen...");
+        miscPanel.add(find, cc.xy(5,1));
+  */
+        JButton help = new JButton(CapturePlugin.getInstance().createImageIcon("apps", "help-browser", 16));
+        help.setToolTipText(mLocalizer.msg("help", "Help"));
+        help.setOpaque(false);
+        help.setBorder(Borders.EMPTY_BORDER);
+        miscPanel.add(help, cc.xy(7, 3));
+
+        panel.add(miscPanel, cc.xy(2, 3));
+
+        ButtonBarBuilder refresh = new ButtonBarBuilder();
+
+        refresh.addGlue();
+
+        JButton refreshButton = new JButton(mLocalizer.msg("refresh", "Refresh channellist"));
+        refreshButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                refreshChannelList();
+            }
+        });
+        refreshButton.setIcon(CapturePlugin.getInstance().createImageIcon("apps", "view-refresh", 16));
+
+        refresh.addGriddedButtons(new JButton[]{refreshButton});
+
+        miscPanel.add(refresh.getPanel(), cc.xy(3, 5));
+
+        panel.add(DefaultComponentFactory.getInstance().createSeparator(mLocalizer.msg("channel", "Channel assignment")), cc.xyw(1, 5, 2));
+
+        mTable = new JTable(new ConfigTableModel(mConfig));
+        mTable.getTableHeader().setReorderingAllowed(false);
+        mTable.getColumnModel().getColumn(0).setCellRenderer(new ChannelTableCellRenderer());
+        mTable.getColumnModel().getColumn(1).setCellRenderer(new DreamboxChannelRenderer());
+        mTable.getColumnModel().getColumn(1).setCellEditor(new DreamboxChannelEditor(mConfig));
+
+        panel.add(new JScrollPane(mTable), cc.xy(2, 7));
+
+        ButtonBarBuilder builder = new ButtonBarBuilder();
+
+        JButton ok = new JButton(Localizer.getLocalization(Localizer.I18N_OK));
+        ok.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                okPressed();
+            }
+        });
+
+        JButton cancel = new JButton(Localizer.getLocalization(Localizer.I18N_CANCEL));
+        cancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                close();
+            }
+        });
+
+        builder.addGlue();
+        builder.addGriddedButtons(new JButton[]{ok, cancel});
+
+        getRootPane().setDefaultButton(ok);
+
+        panel.add(builder.getPanel(), cc.xyw(1, 9, 2));
+
+        setSize(400, 400);
+    }
+
+    /**
+     * Refresh all Channels and update the table
+     */
+    private void refreshChannelList() {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Thread(new Runnable() {
+                    public void run() {
+                        mConfig.setDreamboxAddress(mDreamboxAddress.getText());
+
+                        DreamboxConnector connect = new DreamboxConnector(mConfig.getDreamboxAddress());
+                        Collection<DreamboxChannel> channels = null;
+
+                        try {
+                            channels = connect.getChannels();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                        if (channels == null) {
+                            JOptionPane.showMessageDialog(DreamboxConfigDialog.this, mLocalizer.msg("errorText", "Sorry, could not load channellist from Dreambox."),
+                                    mLocalizer.msg("errorTitle", "Error"), JOptionPane.ERROR_MESSAGE);
+                        } else {
+                            System.out.println(channels);
+                            mConfig.setDreamboxChannels(channels.toArray(new DreamboxChannel[0]));
+                        }
+                        mTable.updateUI();
+
+                        JOptionPane.showMessageDialog(DreamboxConfigDialog.this, mLocalizer.msg("okText", "Channellist updated."),
+                                mLocalizer.msg("okTitle", "Updated"), JOptionPane.INFORMATION_MESSAGE);
+
+                    }
+                }).start();
+
+            }
+        });
+    }
+
+    /**
+     * OK was pressed, config gets saved
+     */
+    private void okPressed() {
+        mOkPressed = true;
+
+        if (mTable.isEditing()) {
+            TableCellEditor editor = mTable.getCellEditor();
+            if (editor != null)
+                editor.stopCellEditing();
+        }
+        
+        mConfig.setDreamboxAddress(mDreamboxAddress.getText());
+
+        setVisible(false);
+    }
+
+    /**
+     * @return true, if ok was pressed
+     */
+    public boolean wasOkPressed() {
+        return mOkPressed;
+    }
+
+    /**
+     * @return current configuration
+     */
+    public DreamboxConfig getConfig() {
+        return mConfig;
+    }
+
+    /**
+     * Close the Dialog
+     */
+    public void close() {
+        setVisible(false);
+    }
+
+    /**
+     * @return Name of the Device
+     */
+    public String getDeviceName() {
+        return mDeviceName.getText();
+    }
+}
---
+++ trunk/tvbrowser/src/captureplugin/drivers/dreambox/connector/DreamboxConnector.java
@@ -0,0 +1,143 @@
+/*
+ * CapturePlugin by Andreas Hessel (Vidrec@gmx.de), Bodo Tasche
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * CVS information:
+ *  $RCSfile$
+ *   $Source$
+ *     $Date: 2007-01-03 09:06:40 +0100 (Mi, 03 Jan 2007) $
+ *   $Author: bananeweizen $
+ * $Revision: 2979 $
+ */
+package captureplugin.drivers.dreambox.connector;
+
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.TreeMap;
+
+/**
+ * Connector for the Dreambox
+ */
+public class DreamboxConnector {
+    /** get list of bouquets */
+    private final String BOUQUETLIST = "1:7:1:0:0:0:0:0:0:0:(type == 1) || (type == 17) || (type == 195) || (type == 25)FROM BOUQUET \"bouquets.tv\" ORDER BY bouquet";
+    /** IP-Address of the dreambox */
+    private String mAddress;
+
+    /**
+     * Constructor
+     * @param address IP-Address of the dreambox
+     */
+    public DreamboxConnector(String address) {
+        mAddress = address;
+    }
+
+    /**
+     * @param service Service-ID
+     * @return Data of specific service
+     */
+    public TreeMap<String, String> getServiceData(String service) {
+        try {
+            URL url = new URL("http://" + mAddress + "/web/fetchchannels?ServiceListBrowse=" + service);
+            InputStream stream = url.openStream();
+
+            SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser();
+
+            DreamboxHandler handler = new DreamboxHandler();
+
+            saxParser.parse(stream, handler);
+
+            return handler.getData();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     * @return All channels available in the dreambox
+     */
+    public Collection<DreamboxChannel> getChannels() {
+        try {
+            ArrayList<DreamboxChannel> allChannels = new ArrayList<DreamboxChannel>();
+
+            TreeMap<String, String> bouquets = getServiceData(URLEncoder.encode(BOUQUETLIST, "UTF8"));
+            Iterator<String> it = bouquets.keySet().iterator();
+
+            for (String key : bouquets.keySet()) {
+                TreeMap<String, String> map = getServiceData(URLEncoder.encode(key, "UTF8"));
+
+                for (String mkey : map.keySet()) {
+                    allChannels.add(new DreamboxChannel(mkey, map.get(mkey)));
+                }
+            }
+
+            return allChannels;
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private void listTimers() {
+        try {
+            URL url = new URL("http://" + mAddress + "/web/timerlist");
+            InputStream stream = url.openStream();
+
+            SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser();
+
+            DreamboxTimerHandler handler = new DreamboxTimerHandler();
+
+            saxParser.parse(stream, handler);
+
+            ArrayList timers = handler.getTimers();
+            System.out.println(timers.size());
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new DreamboxConnector("192.168.1.140").listTimers();
+    }
+
+}
