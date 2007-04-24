dfc7385d87855edbf0973c87551cd1bd6a1f33d4
bananeweizen <bananeweizen@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2007-04-24 20:17:58 +0000

    RFE 1706842: have more abilities to leave full screen mode

---
+++ tvbrowser/src/tvbrowser/core/contextmenu/LeaveFullScreenMenuItem.java
@@ -0,0 +1,37 @@
+package tvbrowser.core.contextmenu;
+
+import util.ui.Localizer;
+import devplugin.ActionMenu;
+import devplugin.ContextMenuIf;
+import devplugin.Program;
+
+public class LeaveFullScreenMenuItem implements ContextMenuIf {
+  public static final Localizer mLocalizer = Localizer.getLocalizerFor(LeaveFullScreenMenuItem.class);
+
+  public static String LEAVEFULLSCREEN = "######LEAVEFULLSCREEN######";
+  
+  private static LeaveFullScreenMenuItem mInstance;
+  
+  public static LeaveFullScreenMenuItem getInstance() {
+    if (mInstance == null) {
+      mInstance = new LeaveFullScreenMenuItem();
+    }
+    return mInstance;
+  }
+  
+  
+  private LeaveFullScreenMenuItem() { }
+  
+  public ActionMenu getContextMenuActions(Program program) {
+    return null;
+  }
+
+  public String getId() {
+    return LEAVEFULLSCREEN;
+  }
+
+  public String toString() {
+    return mLocalizer.msg("leaveFullScreen", "Leave fullscreen mode");
+  }
+  
+}
