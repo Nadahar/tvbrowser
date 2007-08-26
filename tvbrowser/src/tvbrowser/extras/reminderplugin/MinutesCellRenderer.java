2670a00c3cc24b36209b6117ad64b797ddb845c2
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2007-08-26 21:30:15 +0000

    Changed popup activation. Added JavaDoc.

--- tvbrowser/src/tvbrowser/extras/reminderplugin/MinutesCellRenderer.java
+++ tvbrowser/src/tvbrowser/extras/reminderplugin/MinutesCellRenderer.java
@@ -1,9 +1,31 @@
+/*
+ * TV-Browser
+ * Copyright (C) 04-2003 Martin Oberhauser (darras@users.sourceforge.net)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * CVS information:
+ *     $Date$
+ *   $Author$
+ * $Revision$
+ */
 package tvbrowser.extras.reminderplugin;
 
 import java.awt.Component;
 import java.awt.Point;
 import java.awt.Rectangle;
-import java.awt.event.KeyEvent;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -15,10 +37,16 @@ import com.jgoodies.forms.layout.FormLayout;
 
 import tvbrowser.core.icontheme.IconLoader;
 
+/**
+ * The cell renderer for the minutest column of the remider list.
+ */
 public class MinutesCellRenderer extends DefaultTableCellRenderer {
   private JPanel mPanel;
   private JLabel mTextLabel, mIconLabel;
   
+  /**
+   * Creates an instance of this class.
+   */
   public MinutesCellRenderer() {
     mPanel = new JPanel(new FormLayout("pref,pref:grow,pref,2dlu","pref:grow"));
     CellConstraints cc = new CellConstraints();
@@ -50,7 +78,17 @@ public class MinutesCellRenderer extends DefaultTableCellRenderer {
     return def;
   }
   
-  public void  trackSingleClick(Point p, JTable table, int y, int row,int column) {
+  /**
+   * Tracks a single click on the minutes entry and opens
+   * the cell editor if the point is on the edit picture.
+   * 
+   * @param p The point on that was clicked.
+   * @param table The table that was clicked
+   * @param y The height of the clicked cell.
+   * @param row The row the click was in.
+   * @param column The column the click was in.
+   */
+  public void trackSingleClick(Point p, JTable table, int y, int row,int column) {
     for(int i = 0; i < row; i++)
       p.move(p.x,p.y - table.getRowHeight(i));      
     
@@ -59,7 +97,7 @@ public class MinutesCellRenderer extends DefaultTableCellRenderer {
     
     if(rect.contains(p)) {
       table.editCellAt(row,column);
-      ((MinutesCellEditor)table.getCellEditor()).getComboBox().processKeyEvent(new KeyEvent(mIconLabel,KeyEvent.KEY_PRESSED,0,0,KeyEvent.VK_DOWN,KeyEvent.CHAR_UNDEFINED)); 
+      ((MinutesCellEditor)table.getCellEditor()).getComboBox().showPopup(); 
     }
   }
   
