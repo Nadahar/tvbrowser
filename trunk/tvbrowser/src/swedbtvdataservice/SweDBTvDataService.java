212dfad4397e05b57dd92e51c4bfd488807431c8
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2007-09-20 10:25:09 +0000

    Fixed missing licence entry.

--- trunk/tvbrowser/src/swedbtvdataservice/SweDBTvDataService.java
+++ trunk/tvbrowser/src/swedbtvdataservice/SweDBTvDataService.java
@@ -438,7 +438,7 @@ public class SweDBTvDataService extends devplugin.AbstractTvDataService {
     return new devplugin.PluginInfo(SweDBTvDataService.class,
             mLocalizer.msg("PluginInfo.name","SweDB TV Data Service Plugin"),
             mLocalizer.msg("PluginInfo.description","A TV Data Service plugin which uses XMLTV-data from TV.SWEDB.SE"),
-            "Inforama",mLocalizer.msg("PluginInfo.support","Support the SWEDB crew - Don't forget to register with http://tv.swedb.se/"));
+            "Inforama",null,mLocalizer.msg("PluginInfo.support","Support the SWEDB crew - Don't forget to register with http://tv.swedb.se/"));
   }
 
   class IconLoader {
