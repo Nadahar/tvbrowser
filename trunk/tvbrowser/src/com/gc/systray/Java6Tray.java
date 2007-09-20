7d86204479e3f85b04508d07a8f5024976f81956
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2007-09-20 11:28:10 +0000

    Added logging of Exception.

--- trunk/tvbrowser/src/com/gc/systray/Java6Tray.java
+++ trunk/tvbrowser/src/com/gc/systray/Java6Tray.java
@@ -29,6 +29,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
+import java.util.logging.Level;
 
 import javax.imageio.ImageIO;
 import javax.swing.JDialog;
@@ -94,7 +95,7 @@ public class Java6Tray implements SystemTrayIf {
       
       return value;
     }catch(Exception e) {
-      mLog.severe("Java 6 Tray could be inited.");
+      mLog.log(Level.SEVERE, "Java 6 Tray could not be inited.", e);
       mClass = null;
       return false;
     }
