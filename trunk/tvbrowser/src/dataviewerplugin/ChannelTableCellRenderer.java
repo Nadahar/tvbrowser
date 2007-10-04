5ff81e5b9ac1b821e0186980e7ee3783e9f8c2a6
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2007-10-04 08:45:21 +0000

    Added DataViewerPlugin.

---
+++ trunk/tvbrowser/src/dataviewerplugin/ChannelTableCellRenderer.java
@@ -0,0 +1,33 @@
+package dataviewerplugin;
+
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
+/**
+ * DataViewer for TV-Browser.
+ * 
+ * @author René Mach
+ * 
+ */
+public class ChannelTableCellRenderer extends DefaultTableCellRenderer {
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
+  public Component getTableCellRendererComponent(JTable table, Object value,
+      boolean isSelected, boolean hasFocus, int row, int column) {
+    JLabel label = (JLabel) super.getTableCellRendererComponent(table, value,
+        isSelected, hasFocus, row, column);
+
+   
+      label.setForeground(Color.black);
+      label.setBackground(new Color(255, 221, 221));
+      label.setHorizontalAlignment(JLabel.RIGHT);
+   return label;
+  }
+}
