7fcd201cb844bd054ca6aea513bc95e0b176091a
jo_mormes <jo_mormes@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2007-11-30 16:35:17 +0000

    More items, recognize different spellings

--- tvbrowser/src/primarydatamanager/primarydataservice/util/InfoCategoryParser.java
+++ tvbrowser/src/primarydatamanager/primarydataservice/util/InfoCategoryParser.java
@@ -6,71 +6,104 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
- * This class helps guessing the category (movie, series, show) for a program using
- * the genre
- *
+ * This class helps guessing the category (movie, series, show) for a program
+ * using the genre
+ * 
  * @since 2.6
  */
 public class InfoCategoryParser {
 
-  private final String[] MOVIES = {"spielfilm", "tv movie", "pilotfilm", "romantische komödie", "psychothriller", "dokumentarfilm",
-          "horrorkomödie", "komödie", "thriller", "western", "science fiction", "science-fiction", "musikfilm", "heimatfilm",
-          "tv-film", "horrorthriller", "jugendfilm", "tv-krimi", "erotikfilm", "drama", "animationsfilm", "abenteuerfilm",
-          "zeichentrickkomödie", "kinderfilm", "actionfilm", "kriminalfilm", "horrorfilm", "actionthriller", "actionkomödie",
-          "tv-drama", "kriegsdrama", "familiendrama", "gesellschaftsdrama", "literaturverfilmung", "experimentalfilm", "actiondrama",
-          "kriegsfilm", "episodenfilm", "italowestern", "jugenddrama", "roadmovie", "gaunerkomödie", "beziehungskomödie",
-          "familienfilm", "familienfilm", "bikerfilm", "liebesdrama", "sportfilm", "agentenfilm", "teeniekomödie",
-          "katastrophenfilm", "krimikomödie", "politthriller", "tragikomödie", "thrillerkomödie", "romanze", "tv-thriller",
-          "westernkomödie", "romantikkomödie", "mysteryfilm", "science-fiction-komödie", "erotikthriller", "kurzfilm", "action-thriller",
-          "krimi-thriller", "wildlife-serie", "sci-fi-komödie", "krimi-komödie", "kino-klassiker", "fantasy-komödie", "katastrophen-thriller",
-          "gangster-komödie", "fantasy-abenteuer", "sci-fi-abenteuer", "alien-action", "krimi/thriller", "milieufilm",
-          "biographischer film", "portraitfilm (entwicklungsfilme)", "alltags-/familienfilm", "krimi/thriller", "klassiker/weltliteratur",
-          "märchenfilm", "gesellschaftskomödie", "tanzfilm", "zeichentrick-/puppentrickfilm" 
-  };
-  private final String[] SERIES = {"serie", "serienspecial", "soap", "sitcom", "familien-serie", "anwaltsserie", "krimi-serie",
-          "action-serie", "science fiction-serie", "mystery-serie", "drama-serie", "abenteuer/mystery-serie",
-          "action/abenteuer-serie", "arztserie", "dokumentarserie", "anime-serie", "abenteuerserie", "krimiserie", "actionserie",
-          "science-fiction-serie", "zeichentrickserie", "polizeiserie", "erotikserie", "animationsserie", "puppentrickserie",
-          "comedyserie", "familienserie", "kinderserie", "krankenhausserie", "westernserie", "agentenserie", "dramaserie",
-          "kriegsserie", "horrorserie", "thrillerserie", "monumentalserie", "sci-fi-serie", "abenteuer-serie"};
-  private final String[] NEWS = {"nachrichten"};
-  private final String[] SHOW = {"show"};
-  private final String[] MAGAZINE = {"magazin", "reportage", "news-magazin", "news + sportnachrichten", "wetter", "society magazin",
-          "erotik-magazin", "erotikmagazin", "wissensmagazin", "motorsportmagazin", "kinomagazin", "reisereportage", "buchjournal",
-          "reisemagazin", "service-magazin", "fußballmagazin", "wissens-magazin", "kinder-magazin"};
+  private final String[] MOVIES = { "abenteuerfilm", "actiondrama",
+      "actionfilm", "actionkomödie", "actionthriller", "agentenfilm",
+      "alienaction", "alltags/familienfilm", "animationsfilm",
+      "beziehungskomödie", "bikerfilm", "biographischerfilm", "dokumentarfilm",
+      "drama", "episodenfilm", "erotikfilm", "erotikthriller",
+      "experimentalfilm", "familiendrama", "familienfilm", "fantasyabenteuer",
+      "fantasykomödie", "fernsehfilm", "gangsterkomödie", "gaunerkomödie",
+      "gesellschaftsdrama", "gesellschaftskomödie", "heimatfilm", "horrorfilm",
+      "horrorkomödie", "horrorthriller", "italowestern", "jugenddrama",
+      "jugendfilm", "katastrophenfilm", "katastrophenthriller", "kinderfilm",
+      "kinoklassiker", "klassiker/weltliteratur", "komödie", "kriegsdrama",
+      "kriegsfilm", "krimi/thriller", "krimikomödie", "kriminalfilm",
+      "krimithriller", "kurzfilm", "liebesdrama", "literaturverfilmung",
+      "märchenfilm", "milieufilm", "musikfilm", "mysteryfilm", "pilotfilm",
+      "politthriller", "portraitfilm(entwicklungsfilme)", "psychothriller",
+      "roadmovie", "romantikkomödie", "romantischekomödie", "romanze",
+      "sciencefiction", "sciencefictionkomödie", "scifiabenteuer",
+      "scifikomödie", "spielfilm", "sportfilm", "tanzfilm", "teeniekomödie",
+      "thriller", "thrillerkomödie", "tragikomödie", "tvdrama", "tvfilm",
+      "tvkrimi", "tvmovie", "tvthriller", "western", "westernkomödie",
+      "wildlifeserie", "zeichentrick/puppentrickfilm", "zeichentrickkomödie"
 
-  private ArrayList<String> mMovies, mSeries, mNews, mShows, mMagazines, mUnknown;
+  };
+
+  private final String[] SERIES = { "abenteuer/mysteryserie", "abenteuerserie",
+      "action/abenteuerserie", "actionserie", "agentenserie",
+      "animationsserie", "animeserie", "anwaltsserie", "arztserie",
+      "comedyserie", "dokumentarserie", "dramaserie", "erotikserie",
+      "familienserie", "horrorserie", "kinderserie", "krankenhausserie",
+      "kriegsserie", "krimiserie", "monumentalserie", "mysteryserie",
+      "polizeiserie", "puppentrickserie", "sciencefictionserie", "scifiserie",
+      "serie", "serienspecial", "sitcom", "soap", "thrillerserie",
+      "westernserie", "zeichentrickserie", "zoodokusoap" };
+
+  private final String[] NEWS = { "nachrichten", "nachrichtenmagazin",
+      "regionalnachrichten" };
+
+  private final String[] SHOW = { "musik", "show", "talkshow" };
+
+  private final String[] MAGAZINE = { "auslandsmagazin", "automagazin",
+      "boulevardmagazin", "buchjournal", "diskussion", "doku", "dokumentation",
+      "erfindermagazin", "erotikmagazin", "erotikmagazin", "filmmagazin",
+      "fußballmagazin", "geschichtsmagazin", "gespräch", "gesundheitsmagazin",
+      "interview", "kindermagazin", "kinomagazin", "kulturmagazin",
+      "ländermagazin", "lifestylemagazin", "literaturmagazin", "magazin",
+      "medienmagazin", "motorsportmagazin", "news+sportnachrichten",
+      "newsmagazin", "politikmagazin", "politmagazin", "ratgebermagazin",
+      "regionalmagazin", "reisedokumentation", "reisemagazin",
+      "reiseundreportagemagazin", "reisereportage", "reportage",
+      "reportageundreisemagazin", "servicemagazin", "societymagazin",
+      "sozialmagazin", "technikmagazin", "umweltmagazin", "verbrauchermagazin",
+      "wetter", "wirtschaftsmagazin", "wissenschaftsmagazin", "wissensmagazin" };
+
+  private ArrayList<String> mMovies, mSeries, mNews, mShows, mMagazines,
+      mUnknown;
 
   /**
    * Constructor
    */
   public InfoCategoryParser() {
-    mMovies    = new ArrayList<String>(Arrays.asList(MOVIES));
-    mSeries    = new ArrayList<String>(Arrays.asList(SERIES));
-    mNews      = new ArrayList<String>(Arrays.asList(NEWS));
-    mShows     = new ArrayList<String>(Arrays.asList(SHOW));
+    mMovies = new ArrayList<String>(Arrays.asList(MOVIES));
+    mSeries = new ArrayList<String>(Arrays.asList(SERIES));
+    mNews = new ArrayList<String>(Arrays.asList(NEWS));
+    mShows = new ArrayList<String>(Arrays.asList(SHOW));
     mMagazines = new ArrayList<String>(Arrays.asList(MAGAZINE));
 
     mUnknown = new ArrayList<String>();
   }
 
   public boolean isMovie(String s) {
+    s = s.replaceAll("(-| )", "");
     return mMovies.contains(s);
   }
 
   public boolean isSeries(String s) {
+    s = s.replaceAll("(-| )", "");
     return mSeries.contains(s);
   }
 
   public boolean isNews(String s) {
+    s = s.replaceAll("(-| )", "");
     return mNews.contains(s);
   }
 
   public boolean isShows(String s) {
+    s = s.replaceAll("(-| )", "");
     return mShows.contains(s);
   }
 
   public boolean isMagazineOrInfotainment(String s) {
+    s = s.replaceAll("(-| )", "");
     return mMagazines.contains(s);
   }
 
