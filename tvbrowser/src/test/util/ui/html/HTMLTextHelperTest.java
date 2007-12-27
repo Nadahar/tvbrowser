3ced0905a81279317ec2fbbcda28a814b1e8a9ed
Bananeweizen <bananeweizen@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2007-12-27 16:31:04 +0000

    adapt test case to current implementation

--- tvbrowser/src/test/util/ui/html/HTMLTextHelperTest.java
+++ tvbrowser/src/test/util/ui/html/HTMLTextHelperTest.java
@@ -7,7 +7,7 @@ public class HTMLTextHelperTest extends TestCase {
 
   public void testConvertTextToHtml() {
     
-    assertEquals("asfssdf lkasf <a href=\"http://www.heise.de\">http://www.heise.de</a> kjshd slkas fjösldkfas", 
+    assertEquals("asfssdf lkasf <a href=\"http://www.heise.de\">www.heise.de</a> kjshd slkas fjösldkfas", 
                 HTMLTextHelper.convertTextToHtml("asfssdf lkasf http://www.heise.de kjshd slkas fjösldkfas", true));
     assertEquals("asfssdf lkasf <a href=\"http://www.heise.de\">www.heise.de</a> kjshd slkas fjösldkfas", 
                 HTMLTextHelper.convertTextToHtml("asfssdf lkasf www.heise.de kjshd slkas fjösldkfas", true));
