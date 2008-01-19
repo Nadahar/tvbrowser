be177f5bb9c53637bddd87326b74f509e53cfa7a
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2008-01-19 21:17:50 +0000

    TVD-146 : Add Mirror-System

---
+++ tvbrowser/src/swedbtvdataservice/DataFoxSettingsPanel.java
@@ -0,0 +1,18 @@
+package swedbtvdataservice;
+
+import tvdataservice.SettingsPanel;
+
+public class DataFoxSettingsPanel extends SettingsPanel {
+
+  public DataFoxSettingsPanel() {
+    createGui();
+  }
+
+  private void createGui() {
+      
+  }
+
+  public void ok() {
+  }
+
+}
