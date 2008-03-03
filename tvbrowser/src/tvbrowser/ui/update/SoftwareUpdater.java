82d055d9f0d027c766c85f18f071d4d4209b6dc1
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2008-03-03 12:48:34 +0000

    Removed unneeded imports.

--- tvbrowser/src/tvbrowser/ui/update/SoftwareUpdater.java
+++ tvbrowser/src/tvbrowser/ui/update/SoftwareUpdater.java
@@ -29,10 +29,8 @@ package tvbrowser.ui.update;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.regex.Matcher;
