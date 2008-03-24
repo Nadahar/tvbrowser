2fe1eebca8c7cc8efb1c646ab53fc6292f2d2814
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2008-03-24 12:30:06 +0000

    [TVB-218] Title filter for Reminder list dialog

--- tvbrowser/src/tvbrowser/extras/reminderplugin/ReminderTableModel.java
+++ tvbrowser/src/tvbrowser/extras/reminderplugin/ReminderTableModel.java
@@ -1,17 +1,101 @@
 package tvbrowser.extras.reminderplugin;
 
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.ArrayList;
+
+import javax.swing.JComboBox;
 import javax.swing.table.AbstractTableModel;
 
 import util.ui.Localizer;
 
+/**
+ * The model for the reminder table.
+ */
 public class ReminderTableModel extends AbstractTableModel {
-
-  private Localizer mLocalizer = Localizer.getLocalizerFor(ReminderTableModel.class);
+  private static Localizer mLocalizer = Localizer.getLocalizerFor(ReminderTableModel.class);
+  private static String mLastSelectedTitle = mLocalizer.msg("all","All");
   
+  private ReminderList mList;
   private ReminderListItem[] mProgramItems;
+  private JComboBox mTitleFilterBox;
 
-  public ReminderTableModel(ReminderList list) {
-    mProgramItems = list.getReminderItems();
+  private boolean mHandleBoxSelection;
+  
+  /**
+   * Creates an instance of this class.
+   * <p>
+   * @param list The list with the reminders.
+   * @param titleFilterBox The title filter selection combo box.
+   */
+  public ReminderTableModel(ReminderList list, JComboBox titleFilterBox) {
+    mList = list;
+    mTitleFilterBox = titleFilterBox;
+    mHandleBoxSelection = true;
+    
+    ItemListener[] itemListeners = titleFilterBox.getItemListeners();
+    
+    for(ItemListener itemListener : itemListeners) {
+      titleFilterBox.removeItemListener(itemListener);
+    }
+    
+    insertAvailableTitles();
+    
+    mTitleFilterBox.addItemListener(new ItemListener() {
+      public void itemStateChanged(ItemEvent e) {
+        if(e.getStateChange() == ItemEvent.SELECTED && mHandleBoxSelection) {
+          mProgramItems = getItemsForTitleSelection();
+          mLastSelectedTitle = e.getItem().toString();
+          fireTableDataChanged();
+        }
+      }
+    });
+    
+    mProgramItems = getItemsForTitleSelection();
+  }
+  
+  private void insertAvailableTitles() {
+    mHandleBoxSelection = false;
+    mTitleFilterBox.removeAllItems();
+    ReminderListItem[] allItems = mList.getReminderItems();
+    
+    mTitleFilterBox.addItem(mLocalizer.msg("all","All"));
+        
+    for(ReminderListItem item : allItems) {
+      boolean found = false;
+      
+      for(int i = 0; i < mTitleFilterBox.getItemCount(); i++) {
+        if(mTitleFilterBox.getItemAt(i).equals(item.getProgram().getTitle())) {
+          found = true;
+          break;
+        }
+      }
+      
+      if(!found) {
+        mTitleFilterBox.addItem(item.getProgram().getTitle());
+      }
+    }
+    
+    mHandleBoxSelection = true;
+    
+    mTitleFilterBox.setSelectedItem(mLastSelectedTitle);
+  }
+  
+  private ReminderListItem[] getItemsForTitleSelection() {
+    if(mTitleFilterBox.getSelectedIndex() != 0) {
+      ArrayList<ReminderListItem> filteredList = new ArrayList<ReminderListItem>();
+      ReminderListItem[] allItems = mList.getReminderItems();
+      
+      for(ReminderListItem item : allItems) {
+        if(mTitleFilterBox.getSelectedItem().equals(item.getProgram().getTitle())) {
+          filteredList.add(item);
+        }
+      }
+      
+      return filteredList.toArray(new ReminderListItem[filteredList.size()]);
+    }
+    
+    return mList.getReminderItems();
   }
 
   public String getColumnName(int column) {
