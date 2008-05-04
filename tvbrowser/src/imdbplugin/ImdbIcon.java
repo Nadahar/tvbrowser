83c4bc7aa2a4122e659a8c6b8215591c43499109
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2008-05-04 19:35:51 +0000

    Add first version of imdb icon

---
+++ tvbrowser/src/imdbplugin/ImdbIcon.java
@@ -0,0 +1,35 @@
+package imdbplugin;
+
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Color;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+public class ImdbIcon implements Icon {
+  private ImdbRating mRating;
+
+  private static Icon mImdbIcon;
+  int mLength = 0;
+  public ImdbIcon(ImdbRating rating) {
+    mRating = rating;
+    mLength = 15 * rating.getRating() / 100;
+  }
+
+  public int getIconWidth() {
+    return 16;
+  }
+
+  public int getIconHeight() {
+    return 16;
+  }
+
+  public void paintIcon(Component c, Graphics g, int x, int y) {
+    if (mImdbIcon == null) {
+      mImdbIcon = new ImageIcon(getClass().getResource("rating.png"));
+    }
+    mImdbIcon.paintIcon(c, g, x, y);
+    g.setColor(Color.yellow);
+    g.fill3DRect(x, y+7, mLength, 5, true);
+  }
+}
