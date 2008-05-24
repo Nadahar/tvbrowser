c3417b94fef349af89e81bed5880e5a1c30699ec
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2008-05-24 12:29:22 +0000

    Set maximum memory usage to 256MB.

--- tvbrowser/deployment/win/launcher/src/launcher/JarWinJavaExeLauncher.java
+++ tvbrowser/deployment/win/launcher/src/launcher/JarWinJavaExeLauncher.java
@@ -41,7 +41,7 @@ public class JarWinJavaExeLauncher {
     String startApp = null;
     
     for(int i = 0; i < args.length; i++) {
-      if(args[i].toLowerCase().startsWith("-d"))
+      if(args[i].toLowerCase().startsWith("-d") || args[i].toLowerCase().startsWith("-x"))
         dValues.add(args[i]);
       else if(args[i].toLowerCase().startsWith("/start"))
         startApp = args[++i];
