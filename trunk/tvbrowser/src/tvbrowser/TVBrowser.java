b5de2779caf948bafe4ee0e1f7691ce8236cc45b
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2008-05-25 12:46:46 +0000

    Set version to 2.7beta1.

--- trunk/tvbrowser/src/tvbrowser/TVBrowser.java
+++ trunk/tvbrowser/src/tvbrowser/TVBrowser.java
@@ -138,7 +138,8 @@ public class TVBrowser {
   */
   /** The string array with the names of the earlier versions. */
   public static final String[] ALL_VERSIONS = new String[]{
-          "2.7 (SVN)" + mVersionAppendix,
+          "2.7beta1" + mVersionAppendix,
+          "2.7 (SVN)",
           "2.6.3", "2.6.3beta",
           "2.6.2",
           "2.6.1", "2.6.1beta", "2.6.1 (SVN)",
