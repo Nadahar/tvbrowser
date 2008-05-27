9dca70f497208055992ff74c3fffb48626939f6b
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2008-05-27 19:09:30 +0000

    [TVB-277] Remove shell scripts, replace them with ant, remove excel pds

--- trunk/tvbrowser/tvdatakit/workspace/pdsrunner.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-call setClasspath.cmd
-
-java -Xmx256m -Djava.awt.headless=true -cp lib\PDSRunner.jar;%PDS_CLASSPATH% primarydatamanager.PDSRunner %*
--- trunk/tvbrowser/tvdatakit/workspace/pdsrunner.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-PDS_CLASSPATH=classes:lib/poi-3.0-alpha2-20060616.jar:lib/xercesImpl.jar
-
-java -Xmx256m -Djava.awt.headless=true -cp lib/PDSRunner.jar:"$PDS_CLASSPATH" primarydatamanager.PDSRunner $*
