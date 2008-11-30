787105a0092d3e420d608c789800e0a4c8afa64e
Bananeweizen <bananeweizen@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2008-11-30 16:28:32 +0000

    [TVBP-25] forgot 2 files in last commit

--- tvbrowser-plugins/plugin-mediathek/src/main/java/mediathekplugin/MediathekProgramNode.java
+++ tvbrowser-plugins/plugin-mediathek/src/main/java/mediathekplugin/MediathekProgramNode.java
@@ -1,10 +1,22 @@
 package mediathekplugin;
 
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+
+import javax.swing.AbstractAction;
+
+import util.ui.SearchFormSettings;
+import util.ui.SearchHelper;
+import devplugin.Channel;
+import devplugin.Plugin;
 import devplugin.PluginTreeNode;
 import devplugin.Program;
 import devplugin.ProgramItem;
 
 public class MediathekProgramNode extends PluginTreeNode {
+
+  private static final util.ui.Localizer mLocalizer = util.ui.Localizer
+      .getLocalizerFor(MediathekProgramNode.class);
 
   @Override
   public boolean contains(Program prog, boolean recursive) {
@@ -26,10 +38,31 @@ public class MediathekProgramNode extends PluginTreeNode {
     return new Program[0];
   }
 
-  public MediathekProgramNode(String title) {
+  public MediathekProgramNode(final String title) {
     super(title);
     setGroupingByDateEnabled(false);
     getMutableTreeNode().setShowLeafCountEnabled(false);
+    final MediathekPlugin plugin = MediathekPlugin.getInstance();
+    addAction(new AbstractAction(mLocalizer.msg("action.search",
+        "Search programs"), Plugin.getPluginManager().getIconFromTheme(plugin,
+        "action", "system-search", 16)) {
+
+      public void actionPerformed(ActionEvent e) {
+        ArrayList<Channel> channels = new ArrayList<Channel>();
+        
+        for (Channel channel : Plugin.getPluginManager()
+            .getSubscribedChannels()) {
+          if (plugin.isSupportedChannel(channel)) {
+            channels.add(channel);
+          }
+        }
+        final SearchFormSettings searchSettings = new SearchFormSettings(title);
+        Channel[] array = new Channel[channels.size()];
+        channels.toArray(array);
+        searchSettings.setChannels(array);
+        SearchHelper.search(plugin.getFrame(), searchSettings);
+      }
+    });
   }
 
 }
