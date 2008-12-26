84218da2b91fc5d7010fff8286f9f0033d4bf4d1
nildog <nildog@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2008-12-26 10:54:32 +0000

    fix character from UTF-8 conversion

--- tvbrowser-plugins/plugin-tvpearl/src/main/java/tvpearlplugin/PearlInfoDialog.java
+++ tvbrowser-plugins/plugin-tvpearl/src/main/java/tvpearlplugin/PearlInfoDialog.java
@@ -47,180 +47,179 @@ import devplugin.Plugin;
 
 public class PearlInfoDialog extends JDialog implements WindowClosingIf
 {
-    protected static final util.ui.Localizer mLocalizer = util.ui.Localizer.getLocalizerFor(PearlInfoDialog.class);
+	protected static final util.ui.Localizer mLocalizer = util.ui.Localizer.getLocalizerFor(PearlInfoDialog.class);
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private JScrollPane mScrollPane;
-    private JEditorPane mInfoPane;
-    private JButton mCloseBn;
-    private TVPProgram mProgram;
+	private JScrollPane mScrollPane;
+	private JEditorPane mInfoPane;
+	private JButton mCloseBn;
+	private TVPProgram mProgram;
 
-    public PearlInfoDialog(Frame parent, TVPProgram program)
-    {
-        super(parent, true);
+	public PearlInfoDialog(Frame parent, TVPProgram program)
+	{
+		super(parent, true);
 
-        mProgram = program;
-        createGUI();
-    }
+		mProgram = program;
+		createGUI();
+	}
 
-    public PearlInfoDialog(Dialog parent, TVPProgram program)
-    {
-        super(parent, true);
+	public PearlInfoDialog(Dialog parent, TVPProgram program)
+	{
+		super(parent, true);
 
-        mProgram = program;
-        createGUI();
-    }
+		mProgram = program;
+		createGUI();
+	}
 
-    private void createGUI()
-    {
-        setTitle(mLocalizer.msg("title", "TV Pearl Info"));
-        UiUtilities.registerForClosing(this);
+	private void createGUI()
+	{
+		setTitle(mLocalizer.msg("title", "TV Pearl Info"));
+		UiUtilities.registerForClosing(this);
 
-        JPanel main = new JPanel(new BorderLayout());
-        main.setBorder(UiUtilities.DIALOG_BORDER);
-        main.setPreferredSize(new Dimension(500, 350));
-        setContentPane(main);
+		JPanel main = new JPanel(new BorderLayout());
+		main.setBorder(UiUtilities.DIALOG_BORDER);
+		main.setPreferredSize(new Dimension(500, 350));
+		setContentPane(main);
 
-        mInfoPane = new JEditorPane();
-        mInfoPane.setEditorKit(new ExtendedHTMLEditorKit());
-        mInfoPane.setEditable(false);
-        mInfoPane.addHyperlinkListener(new HyperlinkListener()
-        {
-            public void hyperlinkUpdate(HyperlinkEvent evt)
-            {
-                if (evt.getEventType() == HyperlinkEvent.EventType.ACTIVATED)
-                {
-                    URL url = evt.getURL();
-                    if (url != null)
-                    {
-                        Launch.openURL(url.toString());
-                    }
-                }
-            }
-        });
-        mScrollPane = new JScrollPane(mInfoPane);
-        main.add(mScrollPane, BorderLayout.CENTER);
+		mInfoPane = new JEditorPane();
+		mInfoPane.setEditorKit(new ExtendedHTMLEditorKit());
+		mInfoPane.setEditable(false);
+		mInfoPane.addHyperlinkListener(new HyperlinkListener()
+		{
+			public void hyperlinkUpdate(HyperlinkEvent evt)
+			{
+				if (evt.getEventType() == HyperlinkEvent.EventType.ACTIVATED)
+				{
+					URL url = evt.getURL();
+					if (url != null)
+					{
+						Launch.openURL(url.toString());
+					}
+				}
+			}
+		});
+		mScrollPane = new JScrollPane(mInfoPane);
+		main.add(mScrollPane, BorderLayout.CENTER);
 
-        JPanel buttonPn = new JPanel(new FlowLayout(FlowLayout.TRAILING));
-        main.add(buttonPn, BorderLayout.SOUTH);
+		JPanel buttonPn = new JPanel(new FlowLayout(FlowLayout.TRAILING));
+		main.add(buttonPn, BorderLayout.SOUTH);
 
-        if (mProgram != null)
-        {
-            JButton GotoBn = new JButton(mLocalizer.msg("goto", "Goto"));
-            GotoBn.setVisible(mProgram.getProgramID().length() != 0);
+		if (mProgram != null)
+		{
+			JButton GotoBn = new JButton(mLocalizer.msg("goto", "Goto"));
+			GotoBn.setVisible(mProgram.getProgramID().length() != 0);
 
-            GotoBn.addActionListener(new ActionListener()
-            {
-                public void actionPerformed(ActionEvent evt)
-                {
-                    Plugin.getPluginManager().scrollToProgram(mProgram.getProgram());
-                }
-            });
-            buttonPn.add(GotoBn);
-        }
+			GotoBn.addActionListener(new ActionListener()
+			{
+				public void actionPerformed(ActionEvent evt)
+				{
+					Plugin.getPluginManager().scrollToProgram(mProgram.getProgram());
+				}
+			});
+			buttonPn.add(GotoBn);
+		}
 
-        mCloseBn = new JButton(Localizer.getLocalization(Localizer.I18N_CLOSE));
-        mCloseBn.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent evt)
-            {
-                dispose();
-            }
-        });
-        buttonPn.add(mCloseBn);
-        getRootPane().setDefaultButton(mCloseBn);
+		mCloseBn = new JButton(Localizer.getLocalization(Localizer.I18N_CLOSE));
+		mCloseBn.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent evt)
+			{
+				dispose();
+			}
+		});
+		buttonPn.add(mCloseBn);
+		getRootPane().setDefaultButton(mCloseBn);
 
-        pack();
+		pack();
 
-        if (mProgram != null)
-        {
-            ExtendedHTMLDocument doc = (ExtendedHTMLDocument) mInfoPane.getDocument();
-            mInfoPane.setText(createHtmlText(doc, mProgram));
-            mScrollPane.getVerticalScrollBar().setValue(0);
-        }
-        toFront();
-    }
+		if (mProgram != null)
+		{
+			ExtendedHTMLDocument doc = (ExtendedHTMLDocument) mInfoPane.getDocument();
+			mInfoPane.setText(createHtmlText(doc, mProgram));
+			mScrollPane.getVerticalScrollBar().setValue(0);
+		}
+		toFront();
+	}
 
-    public void close()
-    {
-        dispose();
-    }
+	public void close()
+	{
+		dispose();
+	}
 
-    private String createHtmlText(ExtendedHTMLDocument doc, TVPProgram program)
-    {
-        Font tFont = new Font("Verdana", Font.BOLD, 18);
-        Font bFont = new Font("Verdana", Font.PLAIN, 11);
-        String titleFont = tFont.getFamily();
-        String titleSize = bFont.getFamily();
-        String bodyFont = String.valueOf(tFont.getSize());
-        String bodyFontSize = String.valueOf(bFont.getSize());
+	private String createHtmlText(ExtendedHTMLDocument doc, TVPProgram program)
+	{
+		Font tFont = new Font("Verdana", Font.BOLD, 18);
+		Font bFont = new Font("Verdana", Font.PLAIN, 11);
+		String titleFont = tFont.getFamily();
+		String titleSize = bFont.getFamily();
+		String bodyFont = String.valueOf(tFont.getSize());
+		String bodyFontSize = String.valueOf(bFont.getSize());
 
-        StringBuffer buffer = new StringBuffer();
-        buffer.append("<html><body>");
-        buffer.append("<table width=\"100%\" style=\"font-family:");
-        buffer.append(bodyFont);
-        buffer.append(";\"><tr>");
-        buffer.append("<td width=\"60\">");
-        buffer.append("<p \"align=center\">");
-        buffer.append("</p></td><td>");
-        buffer.append("<div style=\"color:#ff0000; font-size:");
-        buffer.append(bodyFontSize).append(";\"><b>");
-        buffer.append(TVPearlPlugin.getInstance().getDayName(program.getStart(), false)).append(", ");
-        buffer.append(DateFormat.getDateInstance().format(program.getStart().getTime()));
-        buffer.append(" � ");
-        buffer.append(DateFormat.getTimeInstance(DateFormat.SHORT).format(program.getStart().getTime()));
-        buffer.append(" � ");
-        buffer.append(program.getChannel());
-        buffer.append("</b></div><div style=\"color:#003366; font-size:");
-        buffer.append(titleSize);
-        buffer.append("; line-height:2.5em; font-family:");
-        buffer.append(titleFont).append("\"><b>");
-        buffer.append(program.getTitle());
-        buffer.append("</b></div>");
-        buffer.append("</td></tr>");
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("<html><body>");
+		buffer.append("<table width=\"100%\" style=\"font-family:");
+		buffer.append(bodyFont);
+		buffer.append(";\"><tr>");
+		buffer.append("<td width=\"60\">");
+		buffer.append("<p \"align=center\">");
+		buffer.append("</p></td><td>");
+		buffer.append("<div style=\"color:#ff0000; font-size:");
+		buffer.append(bodyFontSize).append(";\"><b>");
+		buffer.append(TVPearlPlugin.getInstance().getDayName(program.getStart(), false)).append(", ");
+		buffer.append(DateFormat.getDateInstance().format(program.getStart().getTime()));
+		buffer.append(" · ");
+		buffer.append(DateFormat.getTimeInstance(DateFormat.SHORT).format(program.getStart().getTime()));
+		buffer.append(" · ");
+		buffer.append(program.getChannel());
+		buffer.append("</b></div><div style=\"color:#003366; font-size:");
+		buffer.append(titleSize);
+		buffer.append("; line-height:2.5em; font-family:");
+		buffer.append(titleFont).append("\"><b>");
+		buffer.append(program.getTitle());
+		buffer.append("</b></div>");
+		buffer.append("</td></tr>");
 
-        addSeperator(doc, buffer);
+		addSeperator(doc, buffer);
 
-        buffer.append("<tr><td valign=\"top\" style=\"color:#808080; font-size:");
-        buffer.append(bodyFontSize).append("\"><b>");
-        buffer.append(mLocalizer.msg("author", "Author"));
-        buffer.append("</b></td><td valign=\"middle\" style=\"font-size:font-size:");
-        buffer.append(bodyFontSize).append("\">");
-        buffer.append(program.getAuthor());
-        buffer.append("</td></tr>");
+		buffer.append("<tr><td valign=\"top\" style=\"color:#808080; font-size:");
+		buffer.append(bodyFontSize).append("\"><b>");
+		buffer.append(mLocalizer.msg("author", "Author"));
+		buffer.append("</b></td><td valign=\"middle\" style=\"font-size:font-size:");
+		buffer.append(bodyFontSize).append("\">");
+		buffer.append(program.getAuthor());
+		buffer.append("</td></tr>");
 
-        addSeperator(doc, buffer);
+		addSeperator(doc, buffer);
 
-        buffer.append("<tr><td valign=\"top\" style=\"color:#808080; font-size:");
-        buffer.append(bodyFontSize).append("\"><b>");
-        buffer.append(mLocalizer.msg("info", "Info"));
-        buffer.append("</b></td><td valign=\"middle\" style=\"font-size:font-size:");
-        buffer.append(bodyFontSize).append("\">");
-        buffer.append(program.getInfo().replaceAll("\n", "<br>"));
-        buffer.append("</td></tr>");
+		buffer.append("<tr><td valign=\"top\" style=\"color:#808080; font-size:");
+		buffer.append(bodyFontSize).append("\"><b>");
+		buffer.append(mLocalizer.msg("info", "Info"));
+		buffer.append("</b></td><td valign=\"middle\" style=\"font-size:font-size:");
+		buffer.append(bodyFontSize).append("\">");
+		buffer.append(program.getInfo().replaceAll("\n", "<br>"));
+		buffer.append("</td></tr>");
 
-        addSeperator(doc, buffer);
+		addSeperator(doc, buffer);
 
-        buffer.append("<tr><td colspan=\"2\" valign=\"top\" align=\"center\" style=\"color:#808080; font-size:");
-        buffer.append(bodyFontSize).append("\">");
-        buffer.append("<a href=\"");
-        buffer.append(program.getContentUrl()).append("\">");
-        buffer.append(mLocalizer.msg("forum", "Forum entry"));
-        buffer.append("</a>");
-        buffer.append("</td></tr>");
+		buffer.append("<tr><td colspan=\"2\" valign=\"top\" align=\"center\" style=\"color:#808080; font-size:");
+		buffer.append(bodyFontSize).append("\">");
+		buffer.append("<a href=\"");
+		buffer.append(program.getContentUrl()).append("\">");
+		buffer.append(mLocalizer.msg("forum", "Forum entry"));
+		buffer.append("</a>");
+		buffer.append("</td></tr>");
 
-        buffer.append("</table>");
+		buffer.append("</table>");
 
-        buffer.append("</body></html>");
+		buffer.append("</body></html>");
 
-        return buffer.toString();
-    }
+		return buffer.toString();
+	}
 
-    private void addSeperator(ExtendedHTMLDocument doc, StringBuffer buffer)
-    {
-        buffer.append("<tr><td colspan=\"2\">");
-        buffer.append("<div style=\"font-size:0;\">").append(doc.createCompTag(new HorizontalLine())).append(
-                "</div></td></tr>");
-    }
+	private void addSeperator(ExtendedHTMLDocument doc, StringBuffer buffer)
+	{
+		buffer.append("<tr><td colspan=\"2\">");
+		buffer.append("<div style=\"font-size:0;\">").append(doc.createCompTag(new HorizontalLine())).append("</div></td></tr>");
+	}
 }
