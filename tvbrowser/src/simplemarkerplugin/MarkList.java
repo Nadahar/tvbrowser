8d1224207f95f94a09eec8207a7c794b9c30fee2
Bananeweizen <bananeweizen@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2009-01-24 17:12:56 +0000

    [TVB-494] have selectabe grouping for nodes of marker plugin tree

--- tvbrowser/src/simplemarkerplugin/MarkList.java
+++ tvbrowser/src/simplemarkerplugin/MarkList.java
@@ -78,7 +78,7 @@ public class MarkList extends Vector<Program> {
   public MarkList(String name) {
     mName = name;
     mMarkIcon = SimpleMarkerPlugin.getInstance().getIconForFileName(null);
-    mId = name+System.currentTimeMillis();
+    mId = name + System.currentTimeMillis();
     mMarkPriority = Program.MIN_MARK_PRIORITY;
   }
 
@@ -110,11 +110,10 @@ public class MarkList extends Vector<Program> {
   public MarkList(ObjectInputStream in) throws IOException,
       ClassNotFoundException {
     int version = in.readInt();
-    
-    if(version >= 3)
+
+    if (version >= 3)
       mMarkPriority = in.readInt();
-    
-    
+
     if (version >= 1) {
       mName = (String) in.readObject();
       mId = in.readUTF();
@@ -140,39 +139,52 @@ public class MarkList extends Vector<Program> {
 
       mMarkIconPath = (String) in.readObject();
 
-      if (mMarkIconPath == null || mMarkIconPath.compareTo("null") == 0
-          || (version > 3 && !(new File(SimpleMarkerPlugin.getPluginManager().getTvBrowserSettings().getTvBrowserUserHome() + mMarkIconPath)).isFile()))
+      if (mMarkIconPath == null
+          || mMarkIconPath.compareTo("null") == 0
+          || (version > 3 && !(new File(SimpleMarkerPlugin.getPluginManager()
+              .getTvBrowserSettings().getTvBrowserUserHome()
+              + mMarkIconPath)).isFile()))
         mMarkIcon = SimpleMarkerPlugin.getInstance().getIconForFileName(null);
       else {
-                
-        if(version == 1) {
-          File dir = new File(SimpleMarkerPlugin.getPluginManager().getTvBrowserSettings().getTvBrowserUserHome(),"simplemarkericons");
+
+        if (version == 1) {
+          File dir = new File(SimpleMarkerPlugin.getPluginManager()
+              .getTvBrowserSettings().getTvBrowserUserHome(),
+              "simplemarkericons");
           File src = new File(mMarkIconPath);
-          
-          if(!dir.isDirectory())
+
+          if (!dir.isDirectory())
             dir.mkdir();
 
-          String ext =  mMarkIconPath;
+          String ext = mMarkIconPath;
           ext = ext.substring(ext.lastIndexOf("."));
 
           try {
-            IOUtilities.copy(src, new File(dir,mName+ext));
-          }catch(Exception e) {e.printStackTrace();}
-          
+            IOUtilities.copy(src, new File(dir, mName + ext));
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
+
           mMarkIconPath = dir + "/" + mName + ext;
         }
-        
-        if(version <= 3 ) {
-          mMarkIconPath = mMarkIconPath.substring(mMarkIconPath.lastIndexOf("simplemarkericons")-1);
-          
-          if(!new File(SimpleMarkerPlugin.getPluginManager().getTvBrowserSettings().getTvBrowserUserHome() + mMarkIconPath).isFile()) {
-            mMarkIcon = SimpleMarkerPlugin.getInstance().getIconForFileName(null);
+
+        if (version <= 3) {
+          mMarkIconPath = mMarkIconPath.substring(mMarkIconPath
+              .lastIndexOf("simplemarkericons") - 1);
+
+          if (!new File(SimpleMarkerPlugin.getPluginManager()
+              .getTvBrowserSettings().getTvBrowserUserHome()
+              + mMarkIconPath).isFile()) {
+            mMarkIcon = SimpleMarkerPlugin.getInstance().getIconForFileName(
+                null);
             return;
           }
         }
-                
+
         mMarkIcon = SimpleMarkerPlugin.getInstance().getIconForFileName(
-            SimpleMarkerPlugin.getPluginManager().getTvBrowserSettings().getTvBrowserUserHome() + mMarkIconPath);
+            SimpleMarkerPlugin.getPluginManager().getTvBrowserSettings()
+                .getTvBrowserUserHome()
+                + mMarkIconPath);
       }
     }
   }
@@ -212,7 +224,7 @@ public class MarkList extends Vector<Program> {
       public void actionPerformed(ActionEvent e) {
         if (contains(p)) {
           removeElement(p);
-          SimpleMarkerPlugin.getInstance().revalidate(new Program[] {p});
+          SimpleMarkerPlugin.getInstance().revalidate(new Program[] { p });
         } else {
           addElement(p);
           p.mark(SimpleMarkerPlugin.getInstance());
@@ -235,7 +247,7 @@ public class MarkList extends Vector<Program> {
       action.putValue(Action.SMALL_ICON, mMarkIcon);
       action.putValue(Program.MARK_PRIORITY, getMarkPriority());
     }
-    
+
     return action;
   }
 
@@ -247,12 +259,15 @@ public class MarkList extends Vector<Program> {
    * 
    */
   public void handleAction(PluginTreeNode node) {
+    if (node == null) {
+      return;
+    }
     Program[] programs = node.getPrograms();
-    
+
     for (Program p : programs) {
       remove(p);
     }
-    
+
     SimpleMarkerPlugin.getInstance().revalidate(programs);
 
     createNodes(mRootNode, true);
@@ -270,70 +285,87 @@ public class MarkList extends Vector<Program> {
     mRootNode = root;
     root.removeAllChildren();
 
-    PluginTreeNode programNode = new PluginTreeNode(Localizer.getLocalization(Localizer.I18N_PROGRAMS));
-    PluginTreeNode dateNode = new PluginTreeNode(SimpleMarkerPlugin.mLocalizer
-        .msg("days", "Days"));
-    programNode.setGroupingByDateEnabled(false);
-        
+    final SimpleMarkerSettings settings = SimpleMarkerPlugin.getInstance()
+        .getSettings();
+    PluginTreeNode programRoot = null;
+    PluginTreeNode dateRoot = null;
+    if (settings.isGroupingByBoth()) {
+      programRoot = new PluginTreeNode(Localizer
+          .getLocalization(Localizer.I18N_PROGRAMS));
+      programRoot.setGroupingByDateEnabled(false);
+      dateRoot = new PluginTreeNode(SimpleMarkerPlugin.mLocalizer.msg("days",
+          "Days"));
+    }
+
     updateTable();
     Hashtable<String, LinkedList<Program>> sortedPrograms = getSortedPrograms();
-    
-    GroupUnmarkAction menu = getUnmarkAction(dateNode);
-    
-    if(!sortedPrograms.isEmpty()) {
-      dateNode.addAction(menu);
-      menu = getUnmarkAction(dateNode);
-      programNode.addAction(menu);
-      
+
+    if (!sortedPrograms.isEmpty()) {
+      if (settings.isGroupingByBoth()) {
+        dateRoot.addAction(getUnmarkAction(dateRoot));
+        programRoot.addAction(getUnmarkAction(programRoot));
+      }
+
       root.removeAllActions();
-      menu = getUnmarkAction(dateNode);
-      root.addAction(menu);
-    }
+      root.addAction(getUnmarkAction(root));
 
-    for (String name : sortedPrograms.keySet()) {
-      LinkedList<Program> progList = sortedPrograms.get(name);
-      PluginTreeNode curNode = programNode.addNode(name);
+      final Date currentDate = Date.getCurrentDate();
+      for (String name : sortedPrograms.keySet()) {
+        LinkedList<Program> sameTitlePrograms = sortedPrograms.get(name);
 
-      // Create context menu entry
-      menu = getUnmarkAction(curNode);
-      curNode.addAction(menu);
-      curNode.setGroupingByDateEnabled(false);
+        PluginTreeNode titleNode = null;
+        if (settings.isGroupingByBoth()) {
+          titleNode = programRoot.addNode(name);
+        } else if (settings.isGroupingByTitle()) {
+          titleNode = root.addNode(name);
+        }
+        if (titleNode != null) {
+          titleNode.addAction(getUnmarkAction(titleNode));
+          titleNode.setGroupingByDateEnabled(false);
+        }
 
-      for (Program program : progList) {
-        PluginTreeNode prog = curNode.addProgram(program);
-        prog.setNodeFormatter(new NodeFormatter() {
-          public String format(ProgramItem pitem) {
-            Program p = pitem.getProgram();
-            Date progDate = p.getDate();
-            String progdate;
+        for (Program program : sameTitlePrograms) {
+          if (titleNode != null) {
+            PluginTreeNode prog = titleNode.addProgram(program);
+            prog.setNodeFormatter(new NodeFormatter() {
+              public String format(ProgramItem pitem) {
+                Program p = pitem.getProgram();
+                Date progDate = p.getDate();
+                String progdate;
 
-            Date currentDate = Date.getCurrentDate();
-            
-            if (progDate.equals(currentDate.addDays(-1))) {
-              progdate = Localizer.getLocalization(Localizer.I18N_YESTERDAY);
-            }
-            else if (progDate.equals(currentDate)) {
-              progdate = Localizer.getLocalization(Localizer.I18N_TODAY);
-            } 
-            else if (progDate.equals(currentDate.addDays(1))) {
-              progdate = Localizer.getLocalization(Localizer.I18N_TOMORROW);
-            }
-            else {
-              progdate = p.getDateString();
-            }
+                if (progDate.equals(currentDate.addDays(-1))) {
+                  progdate = Localizer
+                      .getLocalization(Localizer.I18N_YESTERDAY);
+                } else if (progDate.equals(currentDate)) {
+                  progdate = Localizer.getLocalization(Localizer.I18N_TODAY);
+                } else if (progDate.equals(currentDate.addDays(1))) {
+                  progdate = Localizer.getLocalization(Localizer.I18N_TOMORROW);
+                } else {
+                  progdate = p.getDateString();
+                }
 
-            return (progdate + "  " + p.getTimeString() + "  " + p.getChannel());
+                return (progdate + "  " + p.getTimeString() + "  " + p
+                    .getChannel());
+              }
+            });
           }
-        });
-        dateNode.addProgram(program);
+          if (settings.isGroupingByBoth()) {
+            dateRoot.addProgram(program);
+          } else if (settings.isGroupingByDate()) {
+            root.addProgram(program);
+          }
+        }
+      }
+      if (settings.isGroupingByBoth()) {
+        root.add(programRoot);
+        root.add(dateRoot);
+      }
+
+      if (update) {
+        root.update();
       }
     }
-    root.add(programNode);
-    root.add(dateNode);
-
-    if (update) {
-      root.update();
-    }
+    SimpleMarkerPlugin.getInstance().addGroupingActions(root);
   }
 
   private GroupUnmarkAction getUnmarkAction(PluginTreeNode dateNode) {
@@ -345,7 +377,7 @@ public class MarkList extends Vector<Program> {
   }
 
   protected void updateNode() {
-    if(mRootNode != null)
+    if (mRootNode != null)
       createNodes(mRootNode, true);
   }
 
@@ -359,8 +391,9 @@ public class MarkList extends Vector<Program> {
         addElement(updatedProg);
       } else if (containingProgram.getProgramState() == Program.IS_VALID_STATE) {
         addElement(containingProgram);
-      }
-      else if(containingProgram.getDate().compareTo(Date.getCurrentDate()) >= 0 && containingProgram.getProgramState() == Program.WAS_DELETED_STATE && !deletedPrograms.contains(containingProgram)) {
+      } else if (containingProgram.getDate().compareTo(Date.getCurrentDate()) >= 0
+          && containingProgram.getProgramState() == Program.WAS_DELETED_STATE
+          && !deletedPrograms.contains(containingProgram)) {
         deletedPrograms.add(containingProgram);
       }
     }
@@ -386,13 +419,16 @@ public class MarkList extends Vector<Program> {
 
   protected void setMarkIconFileName(String fileName) {
     mMarkIconPath = "/simplemarkericons/" + fileName;
-    mMarkIcon = SimpleMarkerPlugin.getInstance().getIconForFileName(SimpleMarkerPlugin.getPluginManager().getTvBrowserSettings().getTvBrowserUserHome() + mMarkIconPath);
+    mMarkIcon = SimpleMarkerPlugin.getInstance().getIconForFileName(
+        SimpleMarkerPlugin.getPluginManager().getTvBrowserSettings()
+            .getTvBrowserUserHome()
+            + mMarkIconPath);
   }
 
   protected Icon getMarkIcon() {
     return mMarkIcon;
   }
-  
+
   protected String getMarkIconPath() {
     return mMarkIconPath;
   }
@@ -403,16 +439,17 @@ public class MarkList extends Vector<Program> {
   public Hashtable<String, LinkedList<Program>> getSortedPrograms() {
     return mProgram;
   }
-  
+
   /**
    * Return the programs with the given title.
    * 
-   * @param title The title of the programs to get.
+   * @param title
+   *          The title of the programs to get.
    * @return The program with the given title.
    */
   public Program[] getProgramsWithTitle(String title) {
     LinkedList<Program> list = mProgram.get(title);
-    return(list.toArray(new Program[list.size()]));
+    return (list.toArray(new Program[list.size()]));
   }
 
   /**
@@ -425,54 +462,56 @@ public class MarkList extends Vector<Program> {
   public Program[] removeProgramsWithTitle(String title) {
     LinkedList<Program> list = mProgram.remove(title);
     Program[] programs = list.toArray(new Program[list.size()]);
-    
+
     for (Program p : programs)
       remove(p);
-    
+
     SimpleMarkerPlugin.getInstance().revalidate(programs);
-    
+
     return programs;
   }
-  
+
   /**
    * @return The ProgramReceiveTarget of this list.
    */
   public ProgramReceiveTarget getReceiveTarget() {
-    return new ProgramReceiveTarget(SimpleMarkerPlugin.getInstance(),SimpleMarkerPlugin.mLocalizer.msg("list.export", "Add to'{0}'",mName),mId);
+    return new ProgramReceiveTarget(SimpleMarkerPlugin.getInstance(),
+        SimpleMarkerPlugin.mLocalizer.msg("list.export", "Add to'{0}'", mName),
+        mId);
   }
-  
+
   /**
    * Gets the id String of this MarkList
    * 
-   * @return The id String of this MarkList. 
+   * @return The id String of this MarkList.
    */
   public String getId() {
     return mId;
   }
-  
+
   public String toString() {
     return mName;
   }
-  
+
   public void addElement(Program p) {
-    if(isEmpty())
+    if (isEmpty())
       super.addElement(p);
     else {
       int index = 0;
       Comparator<Program> c = ProgramUtilities.getProgramComparator();
-      
-      for(int i = 0; i < size(); i++)  {
-        int value = c.compare(elementAt(i),p);
-        if(value < 1)
-          index = i+1;
+
+      for (int i = 0; i < size(); i++) {
+        int value = c.compare(elementAt(i), p);
+        if (value < 1)
+          index = i + 1;
         else
           break;
       }
-      
-      insertElementAt(p,index);
+
+      insertElementAt(p, index);
     }
   }
-  
+
   /**
    * Gets the mark priority of this list.
    * 
@@ -481,11 +520,12 @@ public class MarkList extends Vector<Program> {
   public int getMarkPriority() {
     return mMarkPriority;
   }
-  
+
   /**
    * Sets the mark priority of this list.
    * 
-   * @param value The new mark priority.
+   * @param value
+   *          The new mark priority.
    */
   public void setMarkPriority(int value) {
     mMarkPriority = value;
