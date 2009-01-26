76f5c08a26c50d300c874b88f4b6c79d1f3499c6
Bananeweizen <Bananeweizen@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2009-01-26 22:09:21 +0000

    [TVBP-73]: have filter for all channels supported by Zattoo

---
+++ trunk/tvbrowser-plugins/plugin-zattoo/src/main/java/zattooplugin/ZattooFilterComponent.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright Michael Keppler
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package zattooplugin;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import util.ui.Localizer;
+import devplugin.PluginsFilterComponent;
+import devplugin.Program;
+
+public final class ZattooFilterComponent extends PluginsFilterComponent {
+
+  private static final Localizer mLocalizer = Localizer
+      .getLocalizerFor(ZattooFilterComponent.class);
+
+  @Override
+  public String getUserPresentableClassName() {
+    return mLocalizer.msg("name", "Zattoo channels");
+  }
+
+  public boolean accept(Program program) {
+    return ZattooPlugin.getInstance().isChannelSupported(program.getChannel());
+  }
+
+  public int getVersion() {
+    return 0;
+  }
+
+  public void read(ObjectInputStream arg0, int arg1) throws IOException,
+      ClassNotFoundException {
+    // no filter settings
+  }
+
+  public void write(ObjectOutputStream arg0) throws IOException {
+    // no filter settings
+  }
+
+  public JPanel getSettingsPanel() {
+    JPanel descPanel = new JPanel();
+    descPanel.add(new JLabel(mLocalizer.msg("desc",
+        "Accepts all programs of channels which are supported by Zattoo.")));
+    return descPanel;
+  }
+
+}
