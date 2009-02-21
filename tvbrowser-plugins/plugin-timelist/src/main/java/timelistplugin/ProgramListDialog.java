7ff0192cc36543701b4426f81660a27e97920f19
Bananeweizen <bananeweizen@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2009-02-21 15:39:04 +0000

    multiple cleanups of issues from PMD, Findbugs and Lint4j fixed POM of country code plugin force 1.5 compiler in eclipse settings for plugins project

--- tvbrowser-plugins/plugin-timelist/src/main/java/timelistplugin/ProgramListDialog.java
+++ tvbrowser-plugins/plugin-timelist/src/main/java/timelistplugin/ProgramListDialog.java
@@ -1,24 +1,18 @@
 /*
- * TimeListPlugin by Michael Keppler
+ * Copyright Michael Keppler
  * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- * VCS information:
- *     $Date: 2007-09-20 17:59:16 +0200 (Do, 20 Sep 2007) $
- *   $Author: bananeweizen $
- * $Revision: 3885 $
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package timelistplugin;
 
@@ -56,7 +50,7 @@ import devplugin.Program;
  * @author bananeweizen
  * 
  */
-public class ProgramListDialog extends JDialog implements WindowClosingIf {
+public final class ProgramListDialog extends JDialog implements WindowClosingIf {
 
   /**
    * vector of programs shown in the dialog
