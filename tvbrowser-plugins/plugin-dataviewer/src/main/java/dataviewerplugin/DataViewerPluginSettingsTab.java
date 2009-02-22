ca756fe36ef7176d9df69e28179e6ec13bef4ac1
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2009-02-22 20:40:18 +0000

    [TVBP-80] Use program duration for error calculation

--- tvbrowser-plugins/plugin-dataviewer/src/main/java/dataviewerplugin/DataViewerPluginSettingsTab.java
+++ tvbrowser-plugins/plugin-dataviewer/src/main/java/dataviewerplugin/DataViewerPluginSettingsTab.java
@@ -18,16 +18,21 @@ import devplugin.SettingsTab;
  */
 public final class DataViewerPluginSettingsTab implements SettingsTab {
   private JSpinner mGapSpinner;
+  private JSpinner mDurationSpinner;
   
   public JPanel createSettingsPanel() {
     CellConstraints cc = new CellConstraints();
-    PanelBuilder pb = new PanelBuilder(new FormLayout("5dlu,default,5dlu,default,5dlu,default","5dlu,default"));
+    PanelBuilder pb = new PanelBuilder(new FormLayout("5dlu,default,5dlu,default,5dlu,default","5dlu,default,5dlu,default"));
     
     mGapSpinner = new JSpinner(new SpinnerNumberModel(DataViewerPlugin.getInstance().getAcceptableGap(),1,15,1));
+    mDurationSpinner = new JSpinner(new SpinnerNumberModel(DataViewerPlugin.getInstance().getAcceptableDuration(),6,18,1));
     
     pb.addLabel(DataViewerPlugin.mLocalizer.msg("settings1","Don't show gaps less than/equal to"), cc.xy(2,2));
     pb.add(mGapSpinner, cc.xy(4,2));
     pb.addLabel(DataViewerPlugin.mLocalizer.msg("settings2","minutes as error."), cc.xy(6,2));
+    pb.addLabel(DataViewerPlugin.mLocalizer.msg("settings3","Show error for days with programs with duartion more than"), cc.xy(2,4));
+    pb.add(mDurationSpinner, cc.xy(4,4));
+    pb.addLabel(DataViewerPlugin.mLocalizer.msg("settings4","hours."),cc.xy(6,4));
     
     return pb.getPanel();
   }
@@ -42,6 +47,7 @@ public final class DataViewerPluginSettingsTab implements SettingsTab {
 
   public void saveSettings() {
     DataViewerPlugin.getInstance().setAcceptableGap(((Integer)mGapSpinner.getValue()).intValue());
+    DataViewerPlugin.getInstance().setAcceptableDuration(((Integer)mDurationSpinner.getValue()).intValue());
   }
 
 }
