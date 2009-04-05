8ccaf1b2176c40aa6feccf32d304ab6edc510fbf
Bananeweizen <Bananeweizen@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2009-04-05 08:48:52 +0000

    [TVB-529]: Growl plugin working on non Mac systems

--- trunk/tvbrowser/src/growlplugin/GrowlSettingsTab.java
+++ trunk/tvbrowser/src/growlplugin/GrowlSettingsTab.java
@@ -24,18 +24,14 @@
  */
 package growlplugin;
 
-import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Properties;
 
 import javax.swing.Icon;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import util.paramhandler.ParamInputField;
-import util.ui.ImageUtilities;
 import util.ui.Localizer;
 import util.ui.UiUtilities;
 
@@ -55,24 +51,20 @@ public class GrowlSettingsTab implements SettingsTab {
   /** Translator */
   private static final Localizer mLocalizer = Localizer.getLocalizerFor(GrowlSettingsTab.class);
   /** Settings to use */
-  private Properties mSettings;
+  private GrowlSettings mSettings;
   /** Input-Fields */
   private ParamInputField mTitle, mDescription;
-  /** Was the Plugin initialized correctly ? */
-  private boolean mInitialized;
   /** Instance of Growl-Plugin */
   private GrowlPlugin mGrowlPlugin;
   
   /**
    * Create the Settings-Tab
    * @param plugin The Growl-Plugin
-   * @param initialized True, if the OS is Mac and the Plugin was initialized correctly
    * @param settings Settings-Tab
    */
-  public GrowlSettingsTab(GrowlPlugin plugin, boolean initialized, Properties settings) {
+  public GrowlSettingsTab(final GrowlPlugin plugin, final GrowlSettings settings) {
     mGrowlPlugin = plugin;
     mSettings = settings;
-    mInitialized = initialized;
   }
 
   /**
@@ -81,43 +73,38 @@ public class GrowlSettingsTab implements SettingsTab {
    */
   public JPanel createSettingsPanel() {
     
-    if (!mInitialized) {
-      JPanel panel = new JPanel(new BorderLayout());
-      
-      panel.add(UiUtilities.createHelpTextArea(
-          mLocalizer.msg("notinit", "Error, System not init")), BorderLayout.NORTH);
-      
-      return panel;
-    }
-    
-    JPanel panel = new JPanel(new FormLayout("5dlu, pref:grow, 3dlu, pref, 5dlu", 
+    final JPanel panel = new JPanel(
+        new FormLayout(
+            "5dlu, pref:grow, 3dlu, pref, 5dlu",
             "5dlu, pref, 3dlu, pref, 5dlu, fill:pref:grow, 3dlu, pref, 5dlu, fill:pref:grow, 3dlu, pref, 3dlu"));
     
-    CellConstraints cc = new CellConstraints();
+    final CellConstraints cc = new CellConstraints();
     
     panel.add(UiUtilities.createHelpTextArea(
         mLocalizer.msg("help", "Help Text")), cc.xyw(2,2, 3));
     
     panel.add(DefaultComponentFactory.getInstance().createSeparator(mLocalizer.msg("title", "Title")), cc.xyw(1,4,5));
     
-    mTitle = new ParamInputField(mSettings.getProperty("title"));
+    mTitle = new ParamInputField(mSettings.getTitle());
     
     panel.add(mTitle, cc.xyw(2,6,3));
     
     panel.add(DefaultComponentFactory.getInstance().createSeparator(mLocalizer.msg("description", "Description")), cc.xyw(1,8,5));
     
-    mDescription = new ParamInputField(mSettings.getProperty("description"));
+    mDescription = new ParamInputField(mSettings.getDescription());
     
     panel.add(mDescription, cc.xyw(2,10,3));
     
-    JButton testGrowl = new JButton(mLocalizer.msg("testGrowl", "Test Growl"));
+    final JButton testGrowl = new JButton(mLocalizer.msg("testGrowl",
+        "Test Growl"));
     testGrowl.addActionListener(new ActionListener() {
 
-      public void actionPerformed(ActionEvent e) {
-        Properties prop = (Properties)mSettings.clone();
-        prop.setProperty("title", mTitle.getText());
-        prop.setProperty("description", mDescription.getText());      
-        mGrowlPlugin.getContainer().notifyGrowl(prop, Plugin.getPluginManager().getExampleProgram());
+      public void actionPerformed(final ActionEvent e) {
+        final GrowlSettings testSettings = new GrowlSettings(null);
+        testSettings.setTitle(mTitle.getText());
+        testSettings.setDescription(mDescription.getText());
+        mGrowlPlugin.getContainer().notifyGrowl(testSettings,
+            Plugin.getPluginManager().getExampleProgram());
       }
       
     });
@@ -131,10 +118,8 @@ public class GrowlSettingsTab implements SettingsTab {
    * Save the Input-Field
    */
   public void saveSettings() {
-    if (mInitialized && mSettings != null){
-      mSettings.setProperty("title", mTitle.getText());
-      mSettings.setProperty("description", mDescription.getText());      
-    }
+    mSettings.setTitle(mTitle.getText());
+    mSettings.setDescription(mDescription.getText());
   }
 
   /**
@@ -142,7 +127,7 @@ public class GrowlSettingsTab implements SettingsTab {
    * @return Icon
    */
   public Icon getIcon() {
-    return new ImageIcon(ImageUtilities.createImageFromJar("growlplugin/growlclaw.png", GrowlSettingsTab.class));
+    return mGrowlPlugin.getPluginIcon();
   }
 
   /**
