de55ec717f15c69f5e6e97a42dde339907000d49
misi67 <misi67@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2009-06-07 17:48:22 +0000

    TVBrowserExport-Plugin für PDA und Android-Handys

---
+++ tvbrowser-plugins/plugin-tvbrowsermini/src/devices/AbstractExportDevice.java
@@ -0,0 +1,269 @@
+package tvbrowsermini.devices;
+
+import devplugin.Channel;
+import devplugin.Date;
+import devplugin.Plugin;
+import devplugin.Program;
+import util.exc.ErrorHandler;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.awt.*;
+
+public abstract class AbstractExportDevice {
+
+  protected boolean elementDescription;
+  protected boolean elementShortDescription;
+  protected boolean elementGenre;
+  protected boolean elementProductionTime;
+  protected boolean elementProductionLocation;
+  protected boolean elementDirector;
+  protected boolean elementScript;
+  protected boolean elementActor;
+  protected boolean elementMusic;
+  protected boolean elementOriginalTitel;
+  protected boolean elementFSK;
+  protected boolean elementForminformation;
+  protected boolean elementShowView;
+  protected boolean elementEpisode;
+  protected boolean elementOriginalEpisode;
+  protected boolean elementModeration;
+  protected boolean elementWebside;
+  protected boolean elementVPS;
+  protected boolean elementRepetitionOn;
+  protected boolean elementRepetitionOf;
+
+  protected Properties mSettings;
+  protected Channel[] mSelectedChannels;
+  protected JProgressBar progress;
+
+  private Map<String, Integer> dbChannelIds;
+  private Map<String, Integer> dbDateIds;
+
+  protected AbstractExportDevice(Properties mSettings, Channel[] mSelectedChannels, JProgressBar progress) {
+    this.mSettings = mSettings;
+    this.mSelectedChannels = mSelectedChannels;
+    this.progress = progress;
+    loadSettings();
+  }
+
+  private void loadSettings() {
+    elementDescription = isPropertyTrue("elementDescription");
+    elementShortDescription = isPropertyTrue("elementShortDescription");
+    elementGenre = isPropertyTrue("elementGenre");
+    elementProductionTime = isPropertyTrue("elementProductionTime");
+    elementProductionLocation = isPropertyTrue("elementProductionLocation");
+    elementDirector = isPropertyTrue("elementDirector");
+    elementScript = isPropertyTrue("elementScript");
+    elementActor = isPropertyTrue("elementActor");
+    elementMusic = isPropertyTrue("elementMusic");
+    elementOriginalTitel = isPropertyTrue("elementOriginalTitel");
+    elementFSK = isPropertyTrue("elementFSK");
+    elementForminformation = isPropertyTrue("elementForminformation");
+    elementShowView = isPropertyTrue("elementShowView");
+    elementEpisode = isPropertyTrue("elementEpisode");
+    elementOriginalEpisode = isPropertyTrue("elementOriginalEpisode");
+    elementModeration = isPropertyTrue("elementModeration");
+    elementWebside = isPropertyTrue("elementWebside");
+    elementVPS = isPropertyTrue("elementVPS");
+    elementRepetitionOn = isPropertyTrue("elementRepetitionOn");
+    elementRepetitionOf = isPropertyTrue("elementRepetitionOf");
+  }
+
+  private boolean isPropertyTrue(String propertyName) {
+    return mSettings.getProperty(propertyName).equals("true");
+  }
+
+  public void export(Frame parentFrame) {
+    File file = new File(mSettings.getProperty("path") + "_temp");
+    //Wenn bereits eine DB-Datei besteht einfach löschen - das geht schneller als leeren
+    if (file.exists()) {
+      file.delete();
+    }
+    try {
+      file.createNewFile();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    String oldUserDir = System.getProperty("user.dir");
+    System.setProperty("user.dir", Plugin.getPluginManager().getTvBrowserSettings().getTvBrowserUserHome());
+    String pfad = mSettings.getProperty("path");
+    pfad = pfad.replace('\\', '/');
+    if (pfad.charAt(1) == ':') {
+      char first = pfad.charAt(0);
+      first = Character.toLowerCase(first);
+      pfad = first + pfad.substring(1, pfad.length());
+    } else {
+      pfad = "/" + pfad;
+    }
+    //SQL-Verbindung zur neuen DB-Datei
+    Connection conn = null;
+    Statement stmt = null;
+    try {
+
+      Class.forName("org.sqlite.JDBC");
+      conn = DriverManager.getConnection("jdbc:sqlite:/" + pfad + "_temp");//+mSettings.getProperty("path"));
+      System.setProperty("user.dir", oldUserDir);
+
+      stmt = conn.createStatement();
+      createTables(conn, stmt);
+
+      try {
+        exportFile(conn, parentFrame, stmt);
+        File temp = new File(mSettings.getProperty("path"));
+        if (temp.exists()) {
+          temp.delete();
+        }
+        file.renameTo(temp);
+      } catch (IOException e) {
+        e.printStackTrace();
+        ErrorHandler.handle(Plugin.mLocalizer.msg("error", "Error while exporting tv-data!"), e);
+      }
+      progress.setString("Creating indices");
+      progress.setStringPainted(true);
+      createIndices(conn, stmt);
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.out.println(e.getMessage());
+      System.out.println(e.toString());
+    } finally {
+      if (stmt != null) {
+        try {
+          stmt.close();
+        } catch (SQLException e) {
+        }
+      }
+      if (conn != null) {
+        try {
+          conn.close();
+        } catch (SQLException e) {
+        }
+      }
+    }
+  }
+
+  protected abstract void createTables(Connection connection, Statement stmt) throws SQLException;
+
+  protected abstract void createIndices(Connection conn, Statement stmt) throws SQLException;
+
+  protected abstract void exportFile(Connection connection, Frame parentFrame, Statement stmt) throws IOException;
+
+  protected void exportDates(Connection connection) throws SQLException {
+    dbDateIds = new HashMap<String, Integer>();
+    Date date = new Date();
+    date = date.addDays(-2);
+    int maxDays = Integer.parseInt(mSettings.getProperty("exportDays"));
+    if (maxDays == 0)
+      maxDays = 32;
+    PreparedStatement statement = connection.prepareStatement("INSERT INTO dates (id, datum) VALUES (?, ?)");
+    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    int id = 1;
+    for (int d = 0; d <= maxDays; d++) {
+      date = date.addDays(1);
+      statement.setInt(1, id);
+      statement.setString(2, dateFormat.format(date.getCalendar().getTime()));
+      statement.execute();
+      dbDateIds.put(dateFormat.format(date.getCalendar().getTime()), id);
+      id++;
+    }
+    statement.close();
+  }
+
+  protected void exportChannels(Connection connection) throws SQLException {
+    PreparedStatement statement = connection.prepareStatement("INSERT INTO channel (id, channelid, name, category) VALUES (?, ?, ?, ?)");
+    int id = 1;
+    dbChannelIds = new HashMap<String, Integer>();
+    for (Channel channel : mSelectedChannels) {
+      int categories = channel.getCategories();
+      String channelId = getChannelId(channel);
+      statement.setInt(1, id);
+      statement.setString(2, channelId);
+      statement.setString(3, channel.getName());
+      statement.setString(4, categories == 0 || categories == 2 ? "r" : "t");
+      statement.execute();
+      dbChannelIds.put(channelId, id);
+      id++;
+    }
+    statement.close();
+  }
+
+  protected int findDBChannel(Channel selectedChannel) {
+    return dbChannelIds.get(getChannelId(selectedChannel));
+  }
+
+  protected String getChannelId(Channel channel) {
+    return new StringBuffer(channel.getDataServiceProxy().getId()).append(":").append(channel.getGroup().getId()).append(":").append(channel.getCountry()).append(":").append(channel.getId()).toString();
+  }
+
+  protected int findDBDate(String date) {
+    return dbDateIds.get(date);
+  }
+
+  protected String encrypt(String text) {
+    String result = "";
+
+    try {
+      //Copyright by tvbrowser.org - Please do not change this part!
+      for (int i = 0; i < text.length(); i++) {
+        int position = (int) text.charAt(i);
+        position = position + 7;
+        result += (char) position;
+      }
+    }
+    catch (Exception e) {
+      result = "error";
+    }
+    result = result.replace("'", "@_@");
+    return result;
+  }
+
+  protected boolean bitSet(int num, int pattern) {
+    return (num & pattern) == pattern;
+  }
+
+  protected StringBuilder getFormatInformation(Program program, StringBuilder info) {
+    int inf = program.getInfo();
+
+    if (inf > 0) {
+      if (bitSet(inf, Program.INFO_AUDIO_DOLBY_SURROUND)) {
+        info.append("|Dolby Surround");
+      }
+      if (bitSet(inf, Program.INFO_AUDIO_DOLBY_DIGITAL_5_1)) {
+        info.append("|Dolby Digital 5.1");
+      }
+      if (bitSet(inf, Program.INFO_AUDIO_MONO)) {
+        info.append("|Mono");
+      }
+      if (bitSet(inf, Program.INFO_AUDIO_TWO_CHANNEL_TONE)) {
+        info.append("|Two Channel Tone");
+      }
+      if (bitSet(inf, Program.INFO_LIVE)) {
+        info.append("|Live");
+      }
+      if (bitSet(inf, Program.INFO_ORIGINAL_WITH_SUBTITLE)) {
+        info.append("|Subtitel");
+      }
+      if (bitSet(inf, Program.INFO_SUBTITLE_FOR_AURALLY_HANDICAPPED)) {
+        info.append("|Aurally Handicapped");
+      }
+      if (bitSet(inf, Program.INFO_VISION_16_TO_9)) {
+        info.append("|16:9");
+      }
+      if (bitSet(inf, Program.INFO_VISION_4_TO_3)) {
+        info.append("|4:3");
+      }
+      if (bitSet(inf, Program.INFO_VISION_BLACK_AND_WHITE)) {
+        info.append("|black and white");
+      }
+      if (info.length() > 0) {
+        info = info.deleteCharAt(0); //Delete first seperator
+      }
+    }
+    return info;
+  }
+}
---
+++ tvbrowser-plugins/plugin-tvbrowsermini/src/devices/PDA.java
@@ -0,0 +1,301 @@
+package tvbrowsermini.devices;
+
+import devplugin.*;
+import util.ui.UiUtilities;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Iterator;
+import java.util.Properties;
+
+public class PDA extends AbstractExportDevice {
+
+  public PDA(Properties mSettings, Channel[] mSelectedChannels, JProgressBar progress) {
+    super(mSettings, mSelectedChannels, progress);
+  }
+
+  protected void createTables(Connection connection, Statement stmt) throws SQLException {
+    stmt.addBatch("CREATE TABLE channel (id VARCHAR(100) NOT NULL PRIMARY KEY, name VARCHAR(50), category VARCHAR(1))");
+    stmt.addBatch("CREATE TABLE broadcast (id INTEGER NOT NULL PRIMARY KEY, channel_id VARCHAR(100), title VARCHAR(100), start DATETIME, end DATETIME, favorite BOOLEAN, reminder BOOLEAN)");
+    stmt.addBatch("CREATE TABLE info (broadcast_id INTEGER NOT NULL PRIMARY KEY, originaltitel VARCHAR(100), episode VARCHAR(100), originalepisode VARCHAR(100), shortdescription VARCHAR(400), description TEXT, genre VARCHAR(150), produced VARCHAR(100), location VARCHAR(100), director VARCHAR(200), moderation VARCHAR(200), script VARCHAR(200), actor VARCHAR(2000), music VARCHAR(150), fsk VARCHAR(5), form VARCHAR(400), showview VARCHAR(12), webside VARCHAR(150), vps VARCHAR(100), repetitionon VARCHAR(100), repetitionof VARCHAR(100))");
+    connection.setAutoCommit(false);
+    stmt.executeBatch();
+    connection.setAutoCommit(true);
+  }
+
+  protected void createIndices(Connection connection, Statement stmt) throws SQLException {
+    stmt.addBatch("CREATE INDEX reminder ON broadcast(reminder)");
+    stmt.addBatch("CREATE INDEX favorite ON broadcast(favorite)");
+    stmt.addBatch("CREATE INDEX channelid ON broadcast(channel_id)");
+    stmt.addBatch("CREATE INDEX end ON broadcast(end)");
+    stmt.addBatch("CREATE INDEX start ON broadcast(start)");
+    stmt.addBatch("CREATE INDEX genre ON info(genre)");
+    //stmt.addBatch("CREATE INDEX shortd ON info(shortdescription)");
+    //stmt.addBatch("CREATE INDEX desc ON info(description)");
+    //stmt.addBatch("CREATE INDEX episo ON info(episode)");
+    connection.setAutoCommit(false);
+    stmt.executeBatch();
+    connection.setAutoCommit(true);
+  }
+
+  protected void exportFile(Connection connection, Frame parentFrame, Statement stmt) throws IOException {
+    int[] positions = new int[mSelectedChannels.length];
+    int index = 0;
+    for (int i = 0; i < mSelectedChannels.length; i++) {
+      positions[i] = index;
+      index += 20000;
+    }
+
+
+    DateFormat formater = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    try {
+      Class.forName("org.sqlite.JDBC");
+      for (Channel c : mSelectedChannels) {
+        char category = 't';
+        if (c.getCategories() == 0 || c.getCategories() == 2) {
+          category = 'r';
+        }
+        stmt.addBatch("INSERT INTO channel (id, name, category) VALUES ('" + getChannelId(c) + "', '" + c.getName() + "', '" + category + "')");
+      }
+      Date date = new Date();
+      date = date.addDays(-2);
+      Calendar calStart = Calendar.getInstance();
+      Calendar calEnd = Calendar.getInstance();
+
+      int counter = 0;
+
+      int maxDays = Integer.parseInt(mSettings.getProperty("exportDays"));
+      if (maxDays == 0)
+        maxDays = 32;
+      maxDays++; //first day is always yesterday
+
+
+      for (int d = 0; d < maxDays; d++) {
+        date = date.addDays(1);
+        for (int i = 0; i < mSelectedChannels.length; i++) {
+          //for (int d = 0; d < maxDays; d++) {
+          //	date = date.addDays(1);
+          progress.setValue(progress.getValue() + 1);
+          float percent = (float) ((float) i / (float) mSelectedChannels.length) * 100;
+          percent++;
+          Channel c = mSelectedChannels[i];
+
+          Iterator<Program> it = Plugin.getPluginManager().getChannelDayProgram(date, mSelectedChannels[i]);
+          ArrayList<Program> programs = new ArrayList<Program>();
+          while ((it != null) && (it.hasNext())) {
+            programs.add(it.next());
+          }
+          for (int j = 0; j < programs.size(); j++) {
+            progress.setString(date.getLongDateString() + " " + Math.round(percent) + "%" + " (" + c.getName() + ")");
+            progress.setStringPainted(true);
+            Program program = programs.get(j);
+            if (program != null) {
+              int reminder = 0;
+              int favorite = 0;
+
+              Marker[] markerArr = program.getMarkerArr();
+              for (Marker aMarkerArr : markerArr) {
+                if (aMarkerArr.getId().equals("reminderplugin.ReminderPlugin")) {
+                  reminder = 1;
+                } else if (aMarkerArr.getId().equals("favoritesplugin.FavoritesPlugin")) {
+                  favorite = 1;
+                }
+              }
+
+              calStart.set(date.getYear(), date.getMonth() - 1, date.getDayOfMonth(), program.getHours(), program.getMinutes(), 0);
+              calEnd = (Calendar) calStart.clone();
+              calEnd.add(Calendar.MINUTE, program.getLength());
+
+              StringBuffer statementBroadcast = new StringBuffer("INSERT INTO broadcast (id, channel_id, title, start, end, favorite, reminder) VALUES ('" + positions[i] + "','" + getChannelId(mSelectedChannels[i]) + "', '" + program.getTitle().replace("'", "''") + "', '" + formater.format(calStart.getTime()) + "', '" + formater.format(calEnd.getTime()) + "'," + favorite + "," + reminder + ")");
+
+              StringBuffer statementPart1 = new StringBuffer("INSERT INTO info (broadcast_id");
+              StringBuffer statementPart2 = new StringBuffer(") VALUES (");
+              statementPart2.append("'" + positions[i] + "'");
+              boolean description = false;
+              if (elementDescription) {
+                if (program.getTextField(ProgramFieldType.DESCRIPTION_TYPE) != null) {
+                  statementPart1.append(", description");
+                  String desc = encrypt(program.getTextField(ProgramFieldType.DESCRIPTION_TYPE)).replace("'", "''");
+                  statementPart2.append(", '" + desc + "'");
+                  description = true;
+                }
+              }
+              if (elementShortDescription) {
+                if (program.getShortInfo() != null) {
+                  if (description && elementDescription) {
+                    if (program.getShortInfo().length() > 3) {
+                      if (program.getTextField(ProgramFieldType.DESCRIPTION_TYPE).indexOf(program.getShortInfo().substring(1, program.getShortInfo().length() - 3)) > 0) {
+
+                      }
+                    } else {
+                      statementPart1.append(", shortdescription");
+                      statementPart2.append(", '" + encrypt(program.getShortInfo()) + "'");
+                    }
+                  } else {
+                    statementPart1.append(", shortdescription");
+                    statementPart2.append(", '" + encrypt(program.getShortInfo()) + "'");
+                  }
+                }
+              }
+              if (elementGenre) {
+                if (program.getTextField(ProgramFieldType.GENRE_TYPE) != null) {
+                  statementPart1.append(", genre");
+                  statementPart2.append(", '" + encrypt(program.getTextField(ProgramFieldType.GENRE_TYPE)) + "'");
+                }
+              }
+              if (elementProductionTime) {
+                if (program.getIntField(ProgramFieldType.PRODUCTION_YEAR_TYPE) >= 0) {
+                  statementPart1.append(", produced");
+                  statementPart2.append(", '" + encrypt(String.valueOf(program.getIntField(ProgramFieldType.PRODUCTION_YEAR_TYPE))) + "'");
+                }
+              }
+              if (elementProductionLocation) {
+                if (program.getTextField(ProgramFieldType.ORIGIN_TYPE) != null) {
+                  statementPart1.append(", location");
+                  statementPart2.append(", '" + encrypt(program.getTextField(ProgramFieldType.ORIGIN_TYPE)) + "'");
+                }
+              }
+              if (elementDirector) {
+                if (program.getTextField(ProgramFieldType.DIRECTOR_TYPE) != null) {
+                  statementPart1.append(", director");
+                  statementPart2.append(", '" + encrypt(program.getTextField(ProgramFieldType.DIRECTOR_TYPE)) + "'");
+                }
+              }
+              if (elementScript) {
+                if (program.getTextField(ProgramFieldType.SCRIPT_TYPE) != null) {
+                  statementPart1.append(", script");
+                  statementPart2.append(", '" + encrypt(program.getTextField(ProgramFieldType.SCRIPT_TYPE)) + "'");
+                }
+              }
+              if (elementActor) {
+                if (program.getTextField(ProgramFieldType.ACTOR_LIST_TYPE) != null) {
+                  statementPart1.append(", actor");
+                  statementPart2.append(", '" + encrypt(program.getTextField(ProgramFieldType.ACTOR_LIST_TYPE)) + "'");
+                }
+              }
+              if (elementMusic) {
+                if (program.getTextField(ProgramFieldType.MUSIC_TYPE) != null) {
+                  statementPart1.append(", music");
+                  statementPart2.append(", '" + encrypt(program.getTextField(ProgramFieldType.MUSIC_TYPE)) + "'");
+                }
+              }
+              if (elementOriginalTitel) {
+                if (program.getTextField(ProgramFieldType.ORIGINAL_TITLE_TYPE) != null) {
+                  statementPart1.append(", originaltitel");
+                  statementPart2.append(", '" + encrypt(program.getTextField(ProgramFieldType.ORIGINAL_TITLE_TYPE)) + "'");
+                }
+              }
+              if (elementFSK) {
+                if (program.getIntField(ProgramFieldType.AGE_LIMIT_TYPE) >= 0) {
+                  statementPart1.append(", fsk");
+                  statementPart2.append(", '" + encrypt(String.valueOf(program.getIntField(ProgramFieldType.AGE_LIMIT_TYPE))) + "'");
+                }
+              }
+              if (elementForminformation) {
+                statementPart1.append(", form");
+                StringBuilder info = new StringBuilder("");
+                info = getFormatInformation(program, info);
+                statementPart2.append(", '" + encrypt(info.toString()) + "'");
+              }
+              if (elementShowView) {
+                if (program.getTextField(ProgramFieldType.SHOWVIEW_NR_TYPE) != null) {
+                  statementPart1.append(", showview");
+                  statementPart2.append(", '" + encrypt(program.getTextField(ProgramFieldType.SHOWVIEW_NR_TYPE)) + "'");
+                }
+              }
+              if (elementEpisode) {
+                if (program.getTextField(ProgramFieldType.EPISODE_TYPE) != null) {
+                  statementPart1.append(", episode");
+                  statementPart2.append(", '" + encrypt(program.getTextField(ProgramFieldType.EPISODE_TYPE)) + "'");
+                }
+              }
+              if (elementOriginalEpisode) {
+                if (program.getTextField(ProgramFieldType.ORIGINAL_EPISODE_TYPE) != null) {
+                  statementPart1.append(", originalepisode");
+                  statementPart2.append(", '" + encrypt(program.getTextField(ProgramFieldType.ORIGINAL_EPISODE_TYPE)) + "'");
+                }
+              }
+              if (elementModeration) {
+                if (program.getTextField(ProgramFieldType.MODERATION_TYPE) != null) {
+                  statementPart1.append(", moderation");
+                  statementPart2.append(", '" + encrypt(program.getTextField(ProgramFieldType.MODERATION_TYPE)) + "'");
+                }
+              }
+              if (elementWebside) {
+                if (program.getTextField(ProgramFieldType.URL_TYPE) != null) {
+                  statementPart1.append(", webside");
+                  statementPart2.append(", '" + encrypt(program.getTextField(ProgramFieldType.URL_TYPE)) + "'");
+                }
+              }
+              if (elementVPS) {
+                if (program.getTextField(ProgramFieldType.URL_TYPE) != null) {
+                  statementPart1.append(", vps");
+                  statementPart2.append(", '" + encrypt(program.getTimeFieldAsString(ProgramFieldType.VPS_TYPE)) + "'");
+                }
+              }
+              if (elementRepetitionOn) {
+                if (program.getTextField(ProgramFieldType.REPETITION_ON_TYPE) != null) {
+                  statementPart1.append(", repetitionon");
+                  statementPart2.append(", '" + encrypt(program.getTextField(ProgramFieldType.REPETITION_ON_TYPE)) + "'");
+                }
+              }
+              if (elementRepetitionOf) {
+                if (program.getTextField(ProgramFieldType.REPETITION_OF_TYPE) != null) {
+                  statementPart1.append(", repetitionof");
+                  statementPart2.append(", '" + encrypt(program.getTextField(ProgramFieldType.REPETITION_OF_TYPE)) + "'");
+                }
+              }
+
+              statementPart1.append(statementPart2);
+              statementPart1.append(")");
+              stmt.addBatch(statementBroadcast.toString());
+              stmt.addBatch(statementPart1.toString());
+              positions[i]++;
+              counter++;
+              if (counter >= 500) {
+                try {
+                  counter = 0;
+                  connection.setAutoCommit(false);
+                  stmt.executeBatch();
+                  connection.setAutoCommit(true);
+                  Thread.sleep(100);
+                }
+                catch (Exception e) {
+                  counter = 0;
+                  Thread.sleep(100);
+                  JOptionPane.showMessageDialog(UiUtilities.getBestDialogParent(parentFrame), e.getMessage());
+                  d = 32;
+                  j = programs.size();
+                  i = mSelectedChannels.length;
+                }
+              }
+            }
+          }
+        }
+      }
+      try {
+        progress.setValue(progress.getMaximum());
+        connection.setAutoCommit(false);
+        stmt.executeBatch();
+        connection.setAutoCommit(true);
+        connection.close();
+      }
+      catch (Exception e) {
+        JOptionPane.showMessageDialog(UiUtilities.getBestDialogParent(parentFrame), e.getMessage());
+      }
+    } catch (Exception e) {
+      JOptionPane.showMessageDialog(UiUtilities.getBestDialogParent(parentFrame), e.getMessage());
+      e.printStackTrace();
+      System.out.println(e.getMessage());
+      System.out.println(e.toString());
+    }
+  }
+}
