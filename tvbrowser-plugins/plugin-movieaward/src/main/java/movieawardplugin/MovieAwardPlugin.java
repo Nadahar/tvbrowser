cf58a52a06ca3cf601e0f53c74216e55e6a2011e
jo_mormes <jo_mormes@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2009-06-11 17:32:49 +0000

    New version

--- tvbrowser-plugins/plugin-movieaward/src/main/java/movieawardplugin/MovieAwardPlugin.java
+++ tvbrowser-plugins/plugin-movieaward/src/main/java/movieawardplugin/MovieAwardPlugin.java
@@ -76,7 +76,7 @@ final public class MovieAwardPlugin extends Plugin {
    */
   private static final Localizer mLocalizer = Localizer.getLocalizerFor(MovieAwardPlugin.class);
   private static Logger mLog = Logger.getLogger(MovieAwardPlugin.class.getName());
-  private static final Version mVersion = new Version(0, 9);
+  private static final Version mVersion = new Version(0, 10);
 
   private PluginInfo mPluginInfo;
   private ArrayList<MovieAward> mMovieAwards;
