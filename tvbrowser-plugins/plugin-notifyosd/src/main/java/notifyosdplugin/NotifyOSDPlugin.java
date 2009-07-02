8b2b0fabca9d0eb0325e141b438c28669bcf47a5
bananeweizen <bananeweizen@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2009-07-02 20:38:27 +0000

    Ubuntu notification plugin

---
+++ tvbrowser-plugins/plugin-notifyosd/src/main/java/notifyosdplugin/NotifyOSDPlugin.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright Michael Keppler
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package notifyosdplugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import util.io.ExecutionHandler;
+import util.misc.OperatingSystem;
+import util.paramhandler.ParamParser;
+import util.program.LocalPluginProgramFormating;
+import devplugin.Plugin;
+import devplugin.PluginInfo;
+import devplugin.Program;
+import devplugin.ProgramReceiveTarget;
+import devplugin.Version;
+
+public class NotifyOSDPlugin extends Plugin {
+
+  private static final util.ui.Localizer mLocalizer = util.ui.Localizer
+      .getLocalizerFor(NotifyOSDPlugin.class);
+
+  private static final Version mVersion = new Version(2, 70, 0);
+
+  private static final String TARGET = "NOTIFYOSD_TARGET";
+
+  private PluginInfo mPluginInfo;
+
+  public static Version getVersion() {
+    return mVersion;
+  }
+
+  public PluginInfo getInfo() {
+    if (mPluginInfo == null) {
+      final String name = mLocalizer.msg("name", "NotifyOSD");
+      final String desc = mLocalizer.msg("description",
+          "Show notifications using NotifyOSD.");
+      mPluginInfo = new PluginInfo(NotifyOSDPlugin.class, name, desc,
+          "Michael Keppler", "GPL 3");
+    }
+
+    return mPluginInfo;
+  }
+
+  public boolean canReceiveProgramsWithTarget() {
+    return OperatingSystem.isLinux();
+  }
+
+  public ProgramReceiveTarget[] getProgramReceiveTargets() {
+    if (canReceiveProgramsWithTarget()) {
+      final ProgramReceiveTarget target = new ProgramReceiveTarget(this,
+          mLocalizer.msg("targetName", "Show notification"), TARGET);
+      return new ProgramReceiveTarget[] { target };
+    }
+    return null;
+  }
+
+  public boolean receivePrograms(final Program[] programArr,
+      final ProgramReceiveTarget receiveTarget) {
+    if (!canReceiveProgramsWithTarget()) {
+      return false;
+    }
+    for (Program program : programArr) {
+      showNotification(program);
+    }
+    return true;
+  }
+
+  private void showNotification(final Program program) {
+    if (!notifyAvailable()) {
+      return;
+    }
+    final File curDir = new File(".");
+    String iconPath = "";
+    ArrayList<String> command = new ArrayList<String>();
+    command.add("notify-send");
+    try {
+      iconPath = "--icon=" + curDir.getCanonicalPath()
+          + "/imgs/tvbrowser128.png";
+      command.add(iconPath);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    ParamParser parser = new ParamParser();
+    final LocalPluginProgramFormating format = new LocalPluginProgramFormating(
+        mLocalizer.msg("name", "NotifyOSD"),
+        "{leadingZero(start_hour,\"2\")}:{leadingZero(start_minute,\"2\")} {title}",
+        "{maxlength(short_info,\"100\")}", "UTF-8");
+    String title = parser.analyse(format.getTitleValue(), program);
+    if (title == null) {
+      title = program.getTitle();
+    }
+    title = title.replace("\n", " ").trim();
+    command.add(title);
+    String description = parser.analyse(format.getContentValue(), program);
+    if (description == null) {
+      description = program.getShortInfo();
+    }
+    description = description.replace("\n", " ").trim();
+    command.add(description);
+    final ExecutionHandler executer = new ExecutionHandler(command
+        .toArray(new String[command.size()]));
+    try {
+      executer.execute();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private boolean notifyAvailable() {
+    final ExecutionHandler executionHandler = new ExecutionHandler(
+        "notify-send", "which");
+    try {
+      executionHandler.execute(true);
+      String location = executionHandler.getInputStreamReaderThread()
+          .getOutput();
+      if (location != null) {
+        location = location.trim();
+        if (location.length() > 0) {
+          return true;
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return false;
+  }
+
+  @Override
+  protected String getMarkIconName() {
+    return "notifyosdplugin/icons/16x16/notify.png";
+  }
+
+}
