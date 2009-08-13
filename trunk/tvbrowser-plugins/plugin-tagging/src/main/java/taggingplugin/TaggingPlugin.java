0cd69c8a98fc3894d2fa4390e28fbd0bd98566e4
bananeweizen <bananeweizen@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2009-08-13 18:15:21 +0000

    allow multiple tags, show list of tagged programs

--- trunk/tvbrowser-plugins/plugin-tagging/src/main/java/taggingplugin/TaggingPlugin.java
+++ trunk/tvbrowser-plugins/plugin-tagging/src/main/java/taggingplugin/TaggingPlugin.java
@@ -28,10 +28,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Map.Entry;
 
 import javax.swing.AbstractAction;
-import javax.swing.Action;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
@@ -50,243 +48,294 @@ import devplugin.Version;
 
 /**
  * @author bananeweizen
- *
+ * 
  */
 final public class TaggingPlugin extends Plugin {
-  private static final Version mVersion = new Version(2, 70, 0);
+	private static final Version mVersion = new Version(2, 70, 0);
 
-  private PluginInfo mPluginInfo;
+	private PluginInfo mPluginInfo;
 
-  private ImageIcon mIcon;
+	private ImageIcon mIcon;
 
-  /** The localizer for this class. */
-  private static final util.ui.Localizer mLocalizer = util.ui.Localizer
-      .getLocalizerFor(TaggingPlugin.class);
+	/** The localizer for this class. */
+	private static final util.ui.Localizer mLocalizer = util.ui.Localizer
+			.getLocalizerFor(TaggingPlugin.class);
 
-  private static final String MAIN_URL = "http://tv-browser.appspot.com/";
-  
-  private HashMap<String, String> mTags = new HashMap<String, String>();
+	private static final String MAIN_URL = "http://tv-browser.appspot.com/";
 
-  private PluginTreeNode mRootNode;
+	private HashMap<String, ArrayList<String>> mTags = new HashMap<String, ArrayList<String>>();
 
-  private boolean mStartFinished;
+	private PluginTreeNode mRootNode;
 
-  public static Version getVersion() {
-    return mVersion;
-  }
+	private boolean mStartFinished;
 
-  public PluginInfo getInfo() {
-    if (mPluginInfo == null) {
-      final String name = mLocalizer.msg("name", "Tags");
-      final String desc = mLocalizer.msg("description",
-          "Allows grouping programs with tags");
-      mPluginInfo = new PluginInfo(TaggingPlugin.class, name, desc,
-          "Michael Keppler", "GPL 3");
-    }
+	public static Version getVersion() {
+		return mVersion;
+	}
 
-    return mPluginInfo;
-  }
+	public PluginInfo getInfo() {
+		if (mPluginInfo == null) {
+			final String name = mLocalizer.msg("name", "Tags");
+			final String desc = mLocalizer.msg("description",
+					"Allows grouping programs with tags");
+			mPluginInfo = new PluginInfo(TaggingPlugin.class, name, desc,
+					"Michael Keppler", "GPL 3");
+		}
 
-  @Override
-  public ActionMenu getContextMenuActions(final Program program) {
-    // special handling of example program
-    if (program == null
-        || program.equals(getPluginManager().getExampleProgram())
-        || getPluginManager().getFilterManager() == null) {
-      return new ActionMenu(new ContextMenuAction(mLocalizer.msg("contextMenu",
-          "Tagging"), getPluginIcon()));
-    }
-    
-    ArrayList<AbstractAction> tagsAction = getTagsActions(program);
-    if (tagsAction != null && !tagsAction.isEmpty()) {
-      tagsAction.add(0, ContextMenuSeparatorAction.getInstance());
-      tagsAction.add(0, getNewTagAction(program));
-      return new ActionMenu(new ContextMenuAction(mLocalizer.msg("contextMenuTags", "Tags")),tagsAction.toArray(new AbstractAction[tagsAction.size()]));
-    }
-    return new ActionMenu(getNewTagAction(program));
-  }
-  
-  private ArrayList<AbstractAction> getTagsActions(final Program program) {
-    String title = program.getTitle();
-    ArrayList<AbstractAction> tags = new ArrayList<AbstractAction>();
-    for (Entry<String, String> titleTag : mTags.entrySet()) {
-      if (titleTag.getKey().equals(title)) {
-        tags.add(new AbstractAction(titleTag.getValue()) {
-          public void actionPerformed(ActionEvent arg0) {
-          }
-        });
-      }
-    }
-    if (!tags.isEmpty()) {
-      return tags;
-    }
-    return null;
-  }
+		return mPluginInfo;
+	}
 
-  private AbstractAction getNewTagAction(final Program program) {
-    AbstractAction action = new AbstractAction() {
-      public void actionPerformed(ActionEvent e) {
-        if (addTag(program)) {
-          updateRootNodeIfVisible();
-        }
-      }};
-    action.putValue(Action.NAME, mLocalizer.msg("contextMenuTag", "Add tag"));
-    action.putValue(Action.SMALL_ICON, getPluginIcon());
-    return action;
-  }
+	@Override
+	public ActionMenu getContextMenuActions(final Program program) {
+		// special handling of example program
+		if (program == null
+				|| program.equals(getPluginManager().getExampleProgram())
+				|| getPluginManager().getFilterManager() == null) {
+			return new ActionMenu(new ContextMenuAction(mLocalizer.msg("contextMenu",
+					"Tagging"), getPluginIcon()));
+		}
 
-  protected boolean addTag(final Program program) {
-    String tag = JOptionPane.showInputDialog(UiUtilities.getBestDialogParent(getParentFrame()), mLocalizer.msg("addTag", "Add tag for {0}", program.getTitle()));
-    if (tag != null) {
-      tag = tag.trim();
-      if (tag.length() < 3) {
-        return false;
-      }
-      if (tag.length() > 40) {
-        return false;
-      }
-      return postTagToServer(program, tag);
-    }
-    return false;
-  }
+		ArrayList<AbstractAction> tagsAction = getTagsActions(program);
+		if (tagsAction != null && !tagsAction.isEmpty()) {
+			tagsAction.add(0, ContextMenuSeparatorAction.getInstance());
+			tagsAction.add(0, getNewTagAction(program));
+			return new ActionMenu(new ContextMenuAction(mLocalizer.msg(
+					"contextMenuTags", "Tags"), getPluginIcon()), tagsAction
+					.toArray(new AbstractAction[tagsAction.size()]));
+		}
+		return new ActionMenu(getNewTagAction(program));
+	}
 
-  private boolean postTagToServer(final Program program, final String tag) {
-    try {
-      URL url = new URL(MAIN_URL + "tag?title=" + URLEncoder.encode(program.getTitle(), "UTF-8") + "&tag=" + URLEncoder.encode(tag, "UTF-8"));
-      URLConnection conn = url.openConnection();
+	private ArrayList<AbstractAction> getTagsActions(final Program program) {
+		ArrayList<String> tags = mTags.get(program.getTitle());
+		if (tags == null || tags.isEmpty()) {
+			return null;
+		}
+		ArrayList<AbstractAction> tagAction = new ArrayList<AbstractAction>();
+		for (String tag : tags) {
+			tagAction.add(getShowTagAction(tag));
+		}
+		if (!tagAction.isEmpty()) {
+			return tagAction;
+		}
+		return null;
+	}
 
-      BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-      String line;
-      while ((line = in.readLine()) != null) {
-        System.out.println(line);
-      }
-      return true;
-    } catch (MalformedURLException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    return false;
-  }
+	private AbstractAction getShowTagAction(final String tag) {
+		return new AbstractAction(tag, getPluginIcon()) {
+			public void actionPerformed(ActionEvent arg0) {
+				ProgramListDialog dialog = new ProgramListDialog(getParentFrame(), findMatchingPrograms(tag), mLocalizer.msg("programsTagged", "Programs tagged '{0}'", tag));
+				dialog.setVisible(true);
+			}
+		};
+	}
 
-  private Icon getPluginIcon() {
-    if (mIcon == null) {
-      mIcon = new ImageIcon(getClass().getResource("tag.png"));
-    }
-    return mIcon;
-  }
-  
-  @Override
-  public void handleTvDataUpdateFinished() {
-    updateRootNodeIfVisible();
-  }
+	protected Program[] findMatchingPrograms(final String wantedTag) {
+		ArrayList<Program> programs = new ArrayList<Program>();
+		final Channel[] channels = devplugin.Plugin.getPluginManager()
+				.getSubscribedChannels();
+		Date date = Date.getCurrentDate();
+		for (int days = 0; days < 30; days++) {
+			for (Channel channel : channels) {
+				final Iterator<Program> iter = Plugin.getPluginManager()
+						.getChannelDayProgram(date, channel);
+				if (iter != null) {
+					while (iter.hasNext()) {
+						final Program program = iter.next();
+						ArrayList<String> tags = mTags.get(program.getTitle());
+						if (tags != null) {
+							for (String programTag : tags) {
+								if (programTag != null && programTag.equals(wantedTag)) {
+									programs.add(program);
+									break;
+								}
+							}
+						}
+					}
+				}
+			}
+			date = date.addDays(1);
+		}
+		return programs.toArray(new Program[programs.size()]);
+	}
 
-  private void updateTagsFromServer() {
-    try {
-      URL url = new URL(MAIN_URL + "tags");
-      BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+	private AbstractAction getNewTagAction(final Program program) {
+		AbstractAction action = new AbstractAction(mLocalizer.msg("contextMenuTag", "Add tag"), createImageIcon("actions","document-new",16)) {
+			public void actionPerformed(ActionEvent e) {
+				if (addTag(program)) {
+					updateRootNodeIfVisible();
+				}
+			}
+		};
+		return action;
+	}
 
-      String inputLine;
-      while ((inputLine = in.readLine()) != null) {
-        System.out.println(inputLine);
-        int pos = inputLine.indexOf('\t');
-        if (pos >= 0) {
-          String programTitle = inputLine.substring(0, pos).trim();
-          String tag = inputLine.substring(pos + 1).trim();
-          if (!programTitle.isEmpty() && !tag.isEmpty()) {
-            mTags.put(programTitle, tag);
-          }
-        }
-      }
-      in.close();
-    } catch (MalformedURLException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
+	protected boolean addTag(final Program program) {
+		String tag = JOptionPane.showInputDialog(UiUtilities
+				.getBestDialogParent(getParentFrame()), mLocalizer.msg("addTag",
+				"Add tag for {0}", program.getTitle()));
+		if (tag != null) {
+			tag = tag.trim();
+			if (tag.length() < 3) {
+				return false;
+			}
+			if (tag.length() > 40) {
+				return false;
+			}
+			return postTagToServer(program, tag);
+		}
+		return false;
+	}
 
-  @Override
-  public PluginTreeNode getRootNode() {
-    if (mRootNode == null) {
-      mRootNode = new PluginTreeNode(this, false);
-      mRootNode.getMutableTreeNode().setShowLeafCountEnabled(false);
-      if (mStartFinished) {
-        // update the tree as the plugin view has been switched on for the first
-        // time after start
-        new Thread(new Runnable() {
-            public void run() {
-                updateRootNode();
-            }
-        }).start();
-      }
-    }
-    return mRootNode;
-  }
-  
-  @Override
-  public void handleTvBrowserStartFinished() {
-    updateRootNodeIfVisible();
-  }
+	private boolean postTagToServer(final Program program, final String tag) {
+		try {
+			URL url = new URL(MAIN_URL + "tag?title="
+					+ URLEncoder.encode(program.getTitle(), "UTF-8") + "&tag="
+					+ URLEncoder.encode(tag, "UTF-8"));
+			URLConnection conn = url.openConnection();
 
-  /**
-   * trigger an update of the plugin tree, but only if it is visible at all
-   */
-  private void updateRootNodeIfVisible() {
-    mStartFinished = true;
-    // update tree, but only if it is shown at all
-    if (mRootNode != null) {
-      updateRootNode();
-    }
-  }
+			BufferedReader in = new BufferedReader(new InputStreamReader(conn
+					.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				System.out.println(line);
+			}
+			return true;
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
 
-  private void updateRootNode() {
-    // do nothing if the tree is not visible or the main program still has work
-    // to do
-    if (!mStartFinished) {
-      return;
-    }
-    updateTagsFromServer();
-    mRootNode.removeAllChildren();
-    HashMap<String, PluginTreeNode> nodes = new HashMap<String, PluginTreeNode>(30);
-    
-    // search all programs
-    final Channel[] channels = devplugin.Plugin.getPluginManager()
-        .getSubscribedChannels();
-    Date date = Date.getCurrentDate();
-    for (int days = 0; days < 30; days++) {
-      for (Channel channel : channels) {
-        final Iterator<Program> iter = Plugin.getPluginManager()
-            .getChannelDayProgram(date, channel);
-        if (iter != null) {
-          while (iter.hasNext()) {
-            final Program program = iter.next();
-            // collect tagged programs in new nodes
-            String tag = mTags.get(program.getTitle());
-            if (tag != null) {
-              PluginTreeNode node = nodes.get(tag);
-              if (node == null) {
-                node = new PluginTreeNode(tag);
-                nodes.put(tag, node);
-              }
-              node.addProgram(program);
-            }
-          }
-        }
-      }
-      date = date.addDays(1);
-    }
-    PluginTreeNode[] nodeArray = new PluginTreeNode[nodes.size()];
-    nodes.values().toArray(nodeArray);
-    Arrays.sort(nodeArray);
-    for (PluginTreeNode node : nodeArray) {
-      mRootNode.add(node);
-    }
-    mRootNode.update();
-  }
+	private Icon getPluginIcon() {
+		if (mIcon == null) {
+			mIcon = new ImageIcon(getClass().getResource("tag.png"));
+		}
+		return mIcon;
+	}
 
-  @Override
-  public boolean canUseProgramTree() {
-    return true;
-  }
+	@Override
+	public void handleTvDataUpdateFinished() {
+		updateRootNodeIfVisible();
+	}
+
+	private void updateTagsFromServer() {
+		try {
+			URL url = new URL(MAIN_URL + "tags");
+			BufferedReader in = new BufferedReader(new InputStreamReader(url
+					.openStream()));
+
+			String inputLine;
+			while ((inputLine = in.readLine()) != null) {
+				System.out.println(inputLine);
+				int pos = inputLine.indexOf('\t');
+				if (pos >= 0) {
+					String programTitle = inputLine.substring(0, pos).trim();
+					String tag = inputLine.substring(pos + 1).trim();
+					if (!programTitle.isEmpty() && !tag.isEmpty()) {
+						ArrayList<String> list = mTags.get(programTitle);
+						if (list == null) {
+							list = new ArrayList<String>();
+							mTags.put(programTitle, list);
+						}
+						list.add(tag);
+					}
+				}
+			}
+			in.close();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public PluginTreeNode getRootNode() {
+		if (mRootNode == null) {
+			mRootNode = new PluginTreeNode(this, false);
+			mRootNode.getMutableTreeNode().setShowLeafCountEnabled(false);
+			if (mStartFinished) {
+				// update the tree as the plugin view has been switched on for the first
+				// time after start
+				new Thread(new Runnable() {
+					public void run() {
+						updateRootNode();
+					}
+				}).start();
+			}
+		}
+		return mRootNode;
+	}
+
+	@Override
+	public void handleTvBrowserStartFinished() {
+		updateRootNodeIfVisible();
+	}
+
+	/**
+	 * trigger an update of the plugin tree, but only if it is visible at all
+	 */
+	private void updateRootNodeIfVisible() {
+		mStartFinished = true;
+		// update tree, but only if it is shown at all
+		if (mRootNode != null) {
+			updateRootNode();
+		}
+	}
+
+	private void updateRootNode() {
+		// do nothing if the tree is not visible or the main program still has work
+		// to do
+		if (!mStartFinished) {
+			return;
+		}
+		updateTagsFromServer();
+		mRootNode.removeAllChildren();
+		HashMap<String, PluginTreeNode> nodes = new HashMap<String, PluginTreeNode>(
+				30);
+
+		// search all programs
+		final Channel[] channels = devplugin.Plugin.getPluginManager()
+				.getSubscribedChannels();
+		Date date = Date.getCurrentDate();
+		for (int days = 0; days < 30; days++) {
+			for (Channel channel : channels) {
+				final Iterator<Program> iter = Plugin.getPluginManager()
+						.getChannelDayProgram(date, channel);
+				if (iter != null) {
+					while (iter.hasNext()) {
+						final Program program = iter.next();
+						// collect tagged programs in new nodes
+						ArrayList<String> tags = mTags.get(program.getTitle());
+						if (tags != null) {
+							for (String tag : tags) {
+								PluginTreeNode node = nodes.get(tag);
+								if (node == null) {
+									node = new PluginTreeNode(tag);
+									nodes.put(tag, node);
+								}
+								node.addProgram(program);
+							}
+						}
+					}
+				}
+			}
+			date = date.addDays(1);
+		}
+		PluginTreeNode[] nodeArray = new PluginTreeNode[nodes.size()];
+		nodes.values().toArray(nodeArray);
+		Arrays.sort(nodeArray);
+		for (PluginTreeNode node : nodeArray) {
+			mRootNode.add(node);
+		}
+		mRootNode.update();
+	}
+
+	@Override
+	public boolean canUseProgramTree() {
+		return true;
+	}
 }
