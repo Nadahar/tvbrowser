e5aa72fcefa0b3ca29b6c3b51ea77d3a350c5007
bananeweizen <bananeweizen@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2009-10-08 17:06:13 +0000

    TVB-672: detect proxy settings

--- tvbrowser/src/tvbrowser/ui/configassistant/NetworkCardPanel.java
+++ tvbrowser/src/tvbrowser/ui/configassistant/NetworkCardPanel.java
@@ -29,6 +29,13 @@ package tvbrowser.ui.configassistant;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.Proxy.Type;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.JCheckBox;
@@ -64,7 +71,7 @@ class NetworkCardPanel extends AbstractCardPanel {
   private JLabel mPasswordLB;
   private JLabel mUserLB;
   private JCheckBox mAuthCB;
-  
+
   public NetworkCardPanel(PrevNextButtons btns) {
     super(btns);
   }
@@ -73,79 +80,110 @@ class NetworkCardPanel extends AbstractCardPanel {
     mContent = new JPanel(new BorderLayout());
 
     mContent.add(new StatusPanel(StatusPanel.NETWORK), BorderLayout.NORTH);
-    
-    JPanel content = new JPanel(new FormLayout("fill:pref:grow, fill:300dlu:grow, fill:pref:grow", "fill:pref:grow, pref, 3dlu, pref, 3dlu, pref,3dlu, pref,3dlu, pref, fill:pref:grow"));
+
+    JPanel content = new JPanel(new FormLayout("fill:pref:grow, fill:300dlu:grow, fill:pref:grow",
+        "fill:pref:grow, pref, 3dlu, pref, 3dlu, pref,3dlu, pref,3dlu, pref, fill:pref:grow"));
     content.setBorder(Borders.DLU4_BORDER);
-    
+
     CellConstraints cc = new CellConstraints();
-    
-    content.add(UiUtilities.createHtmlHelpTextArea(mLocalizer.msg("preambel", "Preambel")), cc.xy(2,2));
-    
+
+    content.add(UiUtilities.createHtmlHelpTextArea(mLocalizer.msg("preambel", "Preambel")), cc.xy(2, 2));
+
     mProxyCB = new JCheckBox(mLocalizer.msg("usingProxy", "Using Proxy"));
     mProxyCB.setSelected(Settings.propHttpProxyUseProxy.getBoolean());
 
-    content.add(mProxyCB, cc.xy(2,6));
-    
+    content.add(mProxyCB, cc.xy(2, 6));
+
     // Proxy - Subpanel
     JPanel proxyPanel = new JPanel(new FormLayout("fill:pref:grow", "pref, 3dlu, pref, 3dlu, pref, 3dlu, pref"));
     proxyPanel.setBorder(BorderFactory.createEmptyBorder(0, Sizes.dialogUnitXAsPixel(10, proxyPanel), 0, 0));
 
     mProxyHeadline = new JLabel(mLocalizer.msg("myProxy", "My Proxy"));
-    proxyPanel.add(mProxyHeadline, cc.xy(1,1));
-    
+    proxyPanel.add(mProxyHeadline, cc.xy(1, 1));
+
     JPanel hostPanel = new JPanel(new FormLayout("pref, 3dlu, pref, 3dlu, pref, 3dlu, pref", "pref"));
 
     mHostLB = new JLabel(mLocalizer.msg("host", "Host"));
-    hostPanel.add(mHostLB, cc.xy(1,1));
+    hostPanel.add(mHostLB, cc.xy(1, 1));
     mHostTF = new JTextField(20);
     mHostTF.setText(Settings.propHttpProxyHost.getString());
-    hostPanel.add(mHostTF, cc.xy(3,1));
+    hostPanel.add(mHostTF, cc.xy(3, 1));
     mPortLB = new JLabel(mLocalizer.msg("port", "Port"));
-    hostPanel.add(mPortLB, cc.xy(5,1));
+    hostPanel.add(mPortLB, cc.xy(5, 1));
     mPortTF = new JTextField(4);
-    hostPanel.add(mPortTF, cc.xy(7,1));
+    hostPanel.add(mPortTF, cc.xy(7, 1));
     mPortTF.setText(Settings.propHttpProxyPort.getString());
-    
-    proxyPanel.add(hostPanel, cc.xy(1,3));
-    
+
+    proxyPanel.add(hostPanel, cc.xy(1, 3));
+
     mAuthCB = new JCheckBox(mLocalizer.msg("auth", "authentification"));
     mAuthCB.setSelected(Settings.propHttpProxyAuthentifyAtProxy.getBoolean());
-    proxyPanel.add(mAuthCB, cc.xy(1,5));
+    proxyPanel.add(mAuthCB, cc.xy(1, 5));
 
     JPanel userPanel = new JPanel(new FormLayout("10dlu, pref, 3dlu, pref, 3dlu, pref, 3dlu, pref", "pref"));
-    
+
     mUserLB = new JLabel(mLocalizer.msg("user", "User"));
-    userPanel.add(mUserLB, cc.xy(2,1));
+    userPanel.add(mUserLB, cc.xy(2, 1));
     mUserTF = new JTextField(10);
     mUserTF.setText(Settings.propHttpProxyUser.getString());
-    userPanel.add(mUserTF, cc.xy(4,1));
+    userPanel.add(mUserTF, cc.xy(4, 1));
     mPasswordLB = new JLabel(mLocalizer.msg("password", "Password"));
-    userPanel.add(mPasswordLB, cc.xy(6,1));
+    userPanel.add(mPasswordLB, cc.xy(6, 1));
     mPasswordTF = new JPasswordField(10);
     mPasswordTF.setText(Settings.propHttpProxyPassword.getString());
-    userPanel.add(mPasswordTF, cc.xy(8,1));
+    userPanel.add(mPasswordTF, cc.xy(8, 1));
 
-    proxyPanel.add(userPanel, cc.xy(1,7));
-    
-    content.add(proxyPanel, cc.xy(2,8));
-    content.add(UiUtilities.createHtmlHelpTextArea(mLocalizer.msg("closing", "Closing")), cc.xy(2,10));
+    proxyPanel.add(userPanel, cc.xy(1, 7));
+
+    content.add(proxyPanel, cc.xy(2, 8));
+    content.add(UiUtilities.createHtmlHelpTextArea(mLocalizer.msg("closing", "Closing")), cc.xy(2, 10));
     mContent.add(content, BorderLayout.CENTER);
 
     updateFieldState();
-    
+
     mProxyCB.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         updateFieldState();
       }
     });
-    
+
     mAuthCB.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         updateFieldState();
       }
     });
-    
+
+    findProxySettings();
+
     return mContent;
+  }
+
+  private void findProxySettings() {
+    try {
+      System.getProperty("java.net.useSystemProxies", "true");
+      List<Proxy> l = ProxySelector.getDefault().select(new URI("http://www.yahoo.com/"));
+
+      for (Iterator<Proxy> iter = l.iterator(); iter.hasNext();) {
+        Proxy proxy = iter.next();
+        if (proxy.type() == Type.DIRECT) {
+          return;
+        }
+        else {
+          System.out.println("proxy hostname : " + proxy.type());
+          InetSocketAddress addr = (InetSocketAddress) proxy.address();
+
+          if (addr != null) {
+            System.out.println("proxy hostname : " + addr.getHostName());
+            System.out.println("proxy port : " + addr.getPort());
+            mHostTF.setText(addr.getHostName());
+            mPortTF.setText(String.valueOf(addr.getPort()));
+            mProxyCB.setSelected(true);
+          }
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   private void updateFieldState() {
@@ -157,7 +195,7 @@ class NetworkCardPanel extends AbstractCardPanel {
     mPortTF.setEnabled(state);
 
     mAuthCB.setEnabled(state);
-    
+
     boolean auth = mAuthCB.isSelected();
     mUserLB.setEnabled(state && auth);
     mUserTF.setEnabled(state && auth);
@@ -172,17 +210,16 @@ class NetworkCardPanel extends AbstractCardPanel {
     Settings.propHttpProxyAuthentifyAtProxy.setBoolean(mAuthCB.isSelected());
     Settings.propHttpProxyUser.setString(mUserTF.getText());
     Settings.propHttpProxyPassword.setString(new String(mPasswordTF.getPassword()));
-    
+
     TVBrowser.updateProxySettings();
-    
+
     // Check Network!
     if (!NetworkUtilities.checkConnection()) {
-      JOptionPane.showMessageDialog(mContent,
-          UiUtilities.createHtmlHelpTextArea(mLocalizer.msg("connectionError", "Connection Error")),
-          Localizer.getLocalization(Localizer.I18N_ERROR), JOptionPane.ERROR_MESSAGE);
+      JOptionPane.showMessageDialog(mContent, UiUtilities.createHtmlHelpTextArea(mLocalizer.msg("connectionError",
+          "Connection Error")), Localizer.getLocalization(Localizer.I18N_ERROR), JOptionPane.ERROR_MESSAGE);
       return false;
     }
     return true;
   }
-  
+
 }
