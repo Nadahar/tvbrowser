5cdb21ddd2407a97c17504ffbe51cb8996f850ab
bananeweizen <bananeweizen@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2009-10-25 18:24:02 +0000

    fix shortcut for scrolling program table

--- tvbrowser/src/tvbrowser/ui/mainframe/MainFrame.java
+++ tvbrowser/src/tvbrowser/ui/mainframe/MainFrame.java
@@ -788,7 +788,7 @@ public class MainFrame extends JFrame implements DateListener,DropTargetListener
 
     }, stroke, JComponent.WHEN_IN_FOCUSED_WINDOW);
 
-    stroke = KeyStroke.getKeyStroke(KeyEvent.VK_BEGIN, 0);
+    stroke = KeyStroke.getKeyStroke(KeyEvent.VK_HOME, 0);
     rootPane.registerKeyboardAction(new ActionListener() {
 
       public void actionPerformed(ActionEvent e) {
