be756ade6f8781ffc8d2708150a5f72d0a6037c1
deruzi <deruzi@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2009-11-09 13:55:33 +0000

    coding style

--- trunk/tvbrowser/src/devplugin/Date.java
+++ trunk/tvbrowser/src/devplugin/Date.java
@@ -1,20 +1,16 @@
 /*
- * TV-Browser
- * Copyright (C) 04-2003 Martin Oberhauser (darras@users.sourceforge.net)
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * CVS information:
  *  $RCSfile$
@@ -23,12 +19,6 @@
  *   $Author$
  * $Revision$
  */
-
-/**
- * TV-Browser
- * @author Martin Oberhauser
- */
-
 package devplugin;
 
 import java.io.DataInput;
@@ -40,17 +30,41 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.HashMap;
 
+/**
+ * the date of a program. it holds year, month and day.
+ *
+ * @author Martin Oberhauser
+ */
 public class Date implements Comparable<Date> {
+  /**
+   * a cache for dates.
+   */
+  private static final HashMap<Integer, String> LONG_DATE_MAP = new HashMap<Integer, String>(28);
 
+
+
+  /**
+   * the localizer for the date.
+   * TODO should be private with a getter!
+   */
   public static util.ui.Localizer mLocalizer = util.ui.Localizer.getLocalizerFor(Date.class);
 
+  /**
+   * the year, ie Calendar.get(Calendar.YEAR).
+   */
   private final int mYear;
 
+  /**
+   * the month, ie Calendar.get(Calendar.MONTH) + 1.
+   */
   private final int mMonth;
 
+  /**
+   * the day, ie Calendar.get(Calendar.DAY_OF_MONTH).
+   */
   private final int mDay;
-  
-  private static final HashMap<Integer, String> LONG_DATE_MAP = new HashMap<Integer, String>(28);
+
+
 
   /**
    * Constructs a new Date object, initialized with the current date.
@@ -62,41 +76,186 @@ public class Date implements Comparable<Date> {
     mDay = mCalendar.get(Calendar.DAY_OF_MONTH);
   }
 
-  public Date(int year, int month, int dayOfMonth) {
+  /**
+   * constructs a new date initialized with the given arguments.
+   *
+   * @param year Calendar.get(Calendar.YEAR)
+   * @param month Calendar.get(Calendar.MONTH) + 1
+   * @param dayOfMonth Calendar.get(Calendar.DAY_OF_MONTH)
+   */
+  public Date(final int year, final int month, final int dayOfMonth) {
     mYear = year;
     mMonth = month;
     mDay = dayOfMonth;
   }
 
-  public static Date createYYYYMMDD(String date, String separator) {
+  /**
+   * constructs a new date from a calendar object.
+   *
+   * @param cal the calendar to create a date from.
+   */
+  public Date(final Calendar cal) {
+    mYear = cal.get(Calendar.YEAR);
+    mMonth = cal.get(Calendar.MONTH) + 1;
+    mDay = cal.get(Calendar.DAY_OF_MONTH);
+  }
+
+  /**
+   * contructs a new date from a date object, ie clones it.
+   *
+   * @param d the date to clone
+   */
+  public Date(final Date d) {
+    mYear = d.mYear;
+    mMonth = d.mMonth;
+    mDay = d.mDay;
+  }
+
+  /**
+   * Creates a new instance from a RandomAccessFile.
+   *
+   * @param in the input to read from
+   * @throws IOException if the stream could not be read
+   * @throws ClassNotFoundException if the date could not be restored
+   * @since 2.2
+   */
+  public Date(final DataInput in) throws IOException, ClassNotFoundException {
+    int version = in.readInt();
+    if (version == 1) { // currently, version==2 is used
+      int date = in.readInt();
+      long l = (long) date * 24 * 60 * 60 * 1000;
+      java.util.Date d = new java.util.Date(l);
+      Calendar mCalendar = Calendar.getInstance();
+      mCalendar.setTime(d);
+      mYear = mCalendar.get(Calendar.YEAR);
+      mMonth = mCalendar.get(Calendar.MONTH) + 1;
+      mDay = mCalendar.get(Calendar.DAY_OF_MONTH);
+    }
+    else {
+      mYear = in.readInt();
+      mMonth = in.readInt();
+      mDay = in.readInt();
+    }
+  }
+
+  /**
+   * Creates a new instance from a stream.
+   * TODO replace this with implementation of serializable
+   *
+   * @param in the input to read from
+   * @throws IOException if the stream could not be read
+   * @throws ClassNotFoundException if the date could not be restored
+   */
+  public Date(final ObjectInputStream in) throws IOException, ClassNotFoundException {
+    int version = in.readInt();
+    if (version == 1) { // currently, version==2 is used
+      int date = in.readInt();
+      long l = (long) date * 24 * 60 * 60 * 1000;
+      java.util.Date d = new java.util.Date(l);
+      Calendar mCalendar = Calendar.getInstance();
+      mCalendar.setTime(d);
+      mYear = mCalendar.get(Calendar.YEAR);
+      mMonth = mCalendar.get(Calendar.MONTH) + 1;
+      mDay = mCalendar.get(Calendar.DAY_OF_MONTH);
+    }
+    else {
+      mYear = in.readInt();
+      mMonth = in.readInt();
+      mDay = in.readInt();
+    }
+
+  }
+
+
+
+
+
+
+  /**
+   * do not call this method repeatedly! always cache this value because this is
+   * a very slow function!
+   *
+   * @return the current date
+   */
+  public static Date getCurrentDate() {
+    return new Date();
+  }
+
+  /**
+   * create a date from a value (see getValue).
+   *
+   * @param value the value to create a date from
+   * @return the date
+   */
+  public static Date createDateFromValue(final long value) {
+    int year = (int) (value / 10000L);
+    int month = (int) (value % 10000L / 100L);
+    int day = (int) (value % 100L);
+
+    return new Date(year, month, day);
+  }
+
+
+  /**
+   * creates a date from a formatted string of the form YYYY&lt;separator&gt;MM&lt;separator&gt;DD.
+   *
+   * @param date the string to parse.
+   * @param separator the separator separating the year, month and day sections of the string
+   * @return null if the parsing failed
+   */
+  public static Date createYYYYMMDD(final String date, final String separator) {
     if (date == null)
+    {
       return null;
+    }
     String[] s = date.split(separator);
     if (s.length != 3)
+    {
       return null;
+    }
     int year = Integer.parseInt(s[0]);
     int month = Integer.parseInt(s[1]);
     int day = Integer.parseInt(s[2]);
     return new Date(year, month, day);
-
   }
 
-  public static Date createDDMMYYYY(String date, String separator) {
+
+  /**
+   * creates a date from a formatted string of the form DD&lt;separator&gt;MM&lt;separator&gt;YYYY.
+   *
+   * @param date the string to parse.
+   * @param separator the separator separating the year, month and day sections of the string
+   * @return null if the parsing failed
+   */
+  public static Date createDDMMYYYY(final String date, final String separator) {
     if (date == null)
+    {
       return null;
+    }
     String[] s = date.split(separator);
     if (s.length != 3)
+    {
       return null;
+    }
     int day = Integer.parseInt(s[0]);
     int month = Integer.parseInt(s[1]);
     int year = Integer.parseInt(s[2]);
     return new Date(year, month, day);
-
   }
 
-  public static Date createYYMMDD(String date, String separator) {
+
+  /**
+   * creates a date from a formatted string of the form YY&lt;separator&gt;MM&lt;separator&gt;DD.
+   * this method assumes that the date is in the 21st century, ie 20XX.
+   *
+   * @param date the string to parse.
+   * @param separator the separator separating the year, month and day sections of the string
+   * @return null if the parsing failed
+   */
+  public static Date createYYMMDD(final String date, final String separator) {
     return createYYYYMMDD("20" + date, separator);
   }
+
 
   /**
    * @param date
@@ -106,37 +265,47 @@ public class Date implements Comparable<Date> {
    *        the calculation is confounded by daylight savings time
    *        switchovers... Around midnight the result may not be correct.
    */
+  public int getNumberOfDaysSince(final Date date) {
+    Calendar cal1 = date.getCalendar();
+    java.util.Date utilDate1 = cal1.getTime();
+    long millis1 = utilDate1.getTime();
 
-  public int getNumberOfDaysSince(Date date) {
-    Calendar cal_1 = date.getCalendar();
-    java.util.Date utilDate_1 = cal_1.getTime();
-    long millis_1 = utilDate_1.getTime();
+    Calendar cal2 = getCalendar();
+    java.util.Date utilDate2 = cal2.getTime();
+    long millis2 = utilDate2.getTime();
 
-    Calendar cal_2 = getCalendar();
-    java.util.Date utilDate_2 = cal_2.getTime();
-    long millis_2 = utilDate_2.getTime();
-    
-    int hours = (int)((millis_2 - millis_1) / 1000L / 60L / 60L);    
-    
-    if(hours % 24 > 1)
-      return (int)(hours / 24. + 0.99);
+    int hours = (int) ((millis2 - millis1) / 1000L / 60L / 60L);
 
-    return (int) ((millis_2 - millis_1) / 1000L / 60L / 60L / 24L);
+    if (hours % 24 > 1)
+    {
+      return (int) (hours / 24. + 0.99);
+    }
+
+    return (int) ((millis2 - millis1) / 1000L / 60L / 60L / 24L);
 
   }
 
+  /**
+   * @return the year (4 digits)
+   */
   public int getYear() {
     return mYear;
   }
 
+  /**
+   * @return the month (2 digits, starting with 1 for january)
+   */
   public int getMonth() {
     return mMonth;
   }
 
+  /**
+   * @return the day of month (2 digits, starting with 1)
+   */
   public int getDayOfMonth() {
     return mDay;
   }
-  
+
   /**
    * Returns the week number within the current year.
    * @return The week number.
@@ -146,89 +315,17 @@ public class Date implements Comparable<Date> {
   public int getWeekOfYear() {
     Calendar cal = getCalendar();
     return cal.get(Calendar.WEEK_OF_YEAR);
-  } 
-
-  public Date(Calendar cal) {
-    mYear = cal.get(Calendar.YEAR);
-    mMonth = cal.get(Calendar.MONTH) + 1;
-    mDay = cal.get(Calendar.DAY_OF_MONTH);
   }
 
-  public Date(Date d) {
-    mYear = d.mYear;
-    mMonth = d.mMonth;
-    mDay = d.mDay;
-
-  }
-
-  /**
-   * do not call this method repeatedly! always cache this value because this is
-   * a very slow function!
-   * 
-   * @return
-   */
-  public static Date getCurrentDate() {
-    return new Date();
-  }
-
-  /**
-   * Creates a new instance from a RandomAccessFile.
-   * @throws IOException 
-   * @throws ClassNotFoundException 
-   * 
-   * @since 2.2
-   */
-  public Date(DataInput in) throws IOException, ClassNotFoundException {
-    int version = in.readInt();
-    if (version == 1) { // currently, version==2 is used
-      int date = in.readInt();
-      long l = (long) date * 24 * 60 * 60 * 1000;
-      java.util.Date d = new java.util.Date(l);
-      Calendar mCalendar = Calendar.getInstance();
-      mCalendar.setTime(d);
-      mYear = mCalendar.get(Calendar.YEAR);
-      mMonth = mCalendar.get(Calendar.MONTH) + 1;
-      mDay = mCalendar.get(Calendar.DAY_OF_MONTH);
-    } else {
-      mYear = in.readInt();
-      mMonth = in.readInt();
-      mDay = in.readInt();
-    }
-
-  }
-  
-  /**
-   * Creates a new instance from a stream.
-   * @throws IOException 
-   * @throws ClassNotFoundException 
-   */
-  public Date(ObjectInputStream in) throws IOException, ClassNotFoundException {
-    int version = in.readInt();
-    if (version == 1) { // currently, version==2 is used
-      int date = in.readInt();
-      long l = (long) date * 24 * 60 * 60 * 1000;
-      java.util.Date d = new java.util.Date(l);
-      Calendar mCalendar = Calendar.getInstance();
-      mCalendar.setTime(d);
-      mYear = mCalendar.get(Calendar.YEAR);
-      mMonth = mCalendar.get(Calendar.MONTH) + 1;
-      mDay = mCalendar.get(Calendar.DAY_OF_MONTH);
-    } else {
-      mYear = in.readInt();
-      mMonth = in.readInt();
-      mDay = in.readInt();
-    }
-
-  }
 
   /**
    * Writes this instance to a RandomAccessFile.
-   * @param out 
-   * @throws IOException 
-   * 
+   * @param out the file to write to
+   * @throws IOException if something went wrong
+   *
    * @since 2.2
    */
-  public void writeToDataFile(RandomAccessFile out) throws IOException {
+  public void writeToDataFile(final RandomAccessFile out) throws IOException {
     out.writeInt(2); // version
     out.writeInt(mYear);
     out.writeInt(mMonth);
@@ -237,93 +334,109 @@ public class Date implements Comparable<Date> {
 
   /**
    * Writes this instance to a stream.
-   * @throws IOException 
+   * TODO replace this with implementation of serializable
+   *
+   * @param out the stream to write to
+   * @throws IOException if something went wrong
    */
-  public void writeData(ObjectOutputStream out) throws IOException {
+  public void writeData(final ObjectOutputStream out) throws IOException {
     out.writeInt(2); // version
     out.writeInt(mYear);
     out.writeInt(mMonth);
     out.writeInt(mDay);
   }
+
+
   /**
    * A hash code implementation that returns the same code for equal Dates.
+   * @return the hash code
    */
-
+  @Override
   public int hashCode() {
     return mYear * 10000 + mMonth * 100 + mDay;
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see java.lang.Object#equals(java.lang.Object)
+   */
+  @Override
+  public boolean equals(final Object obj) {
+
+    if (obj instanceof Date) {
+      Date d = (Date) obj;
+      return d.getDayOfMonth() == getDayOfMonth() && d.getMonth() == getMonth() && d.getYear() == getYear();
+    }
+    return false;
 
   }
 
   /**
    * return the textual representation of this date with abbreviated day of week
-   * and abbreviated month name
-   * 
+   * and abbreviated month name.
+   *
    * @return date string
    */
   public String getDateString() {
     return String.valueOf(getValue());
   }
-  
-  private String getFormattedString(boolean longDay, boolean longMonth) {
+
+  /**
+   * @param longDay false for abbreviated day of week
+   * @param longMonth false for abbreviated month name
+   * @return date string
+   */
+  private String getFormattedString(final boolean longDay, final boolean longMonth) {
     Calendar cal = getCalendar();
-    
+
     SimpleDateFormat day = new SimpleDateFormat(longDay ? "EEEEEE" : "E");
-    SimpleDateFormat month = new SimpleDateFormat(longMonth ? "MMMMMM" : "MMM");    
-    
+    SimpleDateFormat month = new SimpleDateFormat(longMonth ? "MMMMMM" : "MMM");
+
     int dayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
 
     java.util.Date javaDate = new java.util.Date(getCalendar().getTimeInMillis());
-    
+
     return mLocalizer.msg("datePattern", "{0}, {1} {2}", day.format(javaDate), month.format(javaDate), Integer
         .toString(dayOfMonth));
   }
 
   /**
    * return the textual representation of this date with full day of week
-   * and full month name (neither abbreviated)
-   * 
+   * and full month name (neither abbreviated).
+   *
    * @return date string
    */
   public String getLongDateString() {
     int hashCode = hashCode();
     String result = LONG_DATE_MAP.get(hashCode);
     if (result == null) {
-      result = getFormattedString(true,true);
+      result = getFormattedString(true, true);
       LONG_DATE_MAP.put(hashCode, result);
     }
     return result;
   }
 
   /**
-   * returns the textual representation of this date with abbreviated day of week, but full month
+   * returns the textual representation of this date with abbreviated day of week, but full month.
    * @return date as string
    */
   public String getShortDayLongMonthString() {
-    return getFormattedString(false,true);
+    return getFormattedString(false, true);
   }
 
+  /**
+   * TODO is that correct? its like hashCode.
+   * @return the value as in hashCode
+   */
   public long getValue() {
     return mYear * 10000 + mMonth * 100 + mDay;
   }
 
-  public static Date createDateFromValue(long value) {
-    int year = (int) (value / 10000L);
-    int month = (int) (value % 10000L / 100L);
-    int day = (int) (value % 100L);
 
-    return new Date(year, month, day);
-  }
 
-  public boolean equals(Object obj) {
-
-    if (obj instanceof Date) {
-      Date d = (Date) obj;
-      return d.getDayOfMonth()==getDayOfMonth() && d.getMonth()==getMonth() && d.getYear()==getYear();
-    }
-    return false;
-
-  }
-
+  /**
+   * @return the corresponding calendar for this date
+   */
   public java.util.Calendar getCalendar() {
     Calendar cal = Calendar.getInstance();
     cal.set(Calendar.MONTH, mMonth - 1);
@@ -333,40 +446,49 @@ public class Date implements Comparable<Date> {
     return cal;
   }
 
+  @Override
   public String toString() {
-    return getFormattedString(false,false);
+    return getFormattedString(false, false);
   }
 
-  public Date addDays(int days) {
+  /**
+   * @param days the days to add
+   * @return a new created date object
+   */
+  public Date addDays(final int days) {
     Calendar cal = getCalendar();
     cal.add(Calendar.DAY_OF_MONTH, days);
     return new Date(cal);
   }
 
-  public int compareTo(Date otherDate) {
+  /**
+   * {@inheritDoc}
+   * @see java.lang.Comparable#compareTo(java.lang.Object)
+   */
+  public int compareTo(final Date otherDate) {
     if (this.mYear < otherDate.mYear) {
-    	return -1;
+      return -1;
     }
     else if (this.mYear > otherDate.mYear) {
-    	return 1;
+      return 1;
     }
     else if (this.mMonth < otherDate.mMonth) {
-    	return -1;
+      return -1;
     }
     else if (this.mMonth > otherDate.mMonth) {
-    	return 1;
+      return 1;
     }
     else if (this.mDay < otherDate.mDay) {
-    	return -1;
+      return -1;
     }
     else if (this.mDay > otherDate.mDay) {
-    	return 1;
+      return 1;
     }
     return 0;
   }
-  
+
   /**
-   * get the day of the week for this date
+   * get the day of the week for this date.
    * @return day of week, as Calendar.MONDAY and so on
    * @since 2.6
    */
