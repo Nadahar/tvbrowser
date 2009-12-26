195b7819b9c4f350e3a628eee68150cbf9f11059
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2009-12-26 18:00:48 +0000

    #TVB-731 : Add missing translation "Empty print queue"

--- trunk/tvbrowser/src/printplugin/EmptyQueueAction.java
+++ trunk/tvbrowser/src/printplugin/EmptyQueueAction.java
@@ -8,9 +8,12 @@ import devplugin.PluginTreeNode;
 
 
 public class EmptyQueueAction extends ButtonAction {
+    /** The localizer for this class. */
+    private static final util.ui.Localizer mLocalizer
+        = util.ui.Localizer.getLocalizerFor(EmptyQueueAction.class);
 
     public EmptyQueueAction() {
-      super.setText("Druckerwarteschlange leeren");
+      super.setText(mLocalizer.msg("emptyQueue" ,"Druckerwarteschlange leeren"));
       super.setSmallIcon(TVBrowserIcons.delete(TVBrowserIcons.SIZE_SMALL));
     }
 
