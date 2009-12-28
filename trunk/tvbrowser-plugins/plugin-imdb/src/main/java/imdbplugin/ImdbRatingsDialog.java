de2284833ebed6272c999b06571e5293bf1a9070
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2009-12-28 08:58:51 +0000

    #TVBP-156 : Add chart for vote distribution

--- trunk/tvbrowser-plugins/plugin-imdb/src/main/java/imdbplugin/ImdbRatingsDialog.java
+++ trunk/tvbrowser-plugins/plugin-imdb/src/main/java/imdbplugin/ImdbRatingsDialog.java
@@ -8,9 +8,21 @@ import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.RowSpec;
 import com.jgoodies.forms.layout.Sizes;
 import devplugin.Program;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.AxisLocation;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.general.DatasetUtilities;
 import util.ui.Localizer;
 import util.ui.UiUtilities;
 import util.ui.WindowClosingIf;
+import util.ui.html.ExtendedHTMLDocument;
+import util.ui.html.ExtendedHTMLEditorKit;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -20,7 +32,7 @@ import javax.swing.JScrollPane;
 import javax.swing.SwingUtilities;
 import java.awt.Color;
 import java.awt.Frame;
-import java.awt.Rectangle;
+import java.awt.Paint;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -52,7 +64,10 @@ public class ImdbRatingsDialog extends JDialog implements WindowClosingIf {
 
     layout.appendRow(RowSpec.decode("fill:min:grow"));
 
-    final JEditorPane editor = UiUtilities.createHtmlHelpTextArea(createText(), Color.WHITE);
+    JEditorPane editor = new JEditorPane();
+    editor.setEditorKit(new ExtendedHTMLEditorKit());
+    editor.setText(createText((ExtendedHTMLDocument)editor.getDocument()));
+    editor.setEditable(false);
     editor.setBackground(Color.WHITE);
     editor.setOpaque(true);
 
@@ -86,18 +101,18 @@ public class ImdbRatingsDialog extends JDialog implements WindowClosingIf {
     panel.add(buttonBuilder.getPanel(), cc.xy(1,panel.getRowCount()));
 
     getRootPane().setDefaultButton(okButton);
-    setSize(Sizes.dialogUnitXAsPixel(200, this),
-            Sizes.dialogUnitXAsPixel(150, this));
+    setSize(Sizes.dialogUnitXAsPixel(300, this),
+            Sizes.dialogUnitXAsPixel(200, this));
   }
 
-  private String createText() {
+  private String createText(ExtendedHTMLDocument doc) {
     StringBuilder builder = new StringBuilder();
 
     ImdbRating episode = plugin.getEpisodeRating(program);
     if (episode != null) {
       builder.append("<h3>").append(mLocalizer.msg("episodeRating", "Rating of Episode")).append(":</h3>");
       builder.append("<p style='margin-left:10px'>");
-      builder.append(makeTextForRating(episode));
+      builder.append(makeTextForRating(doc, episode));
       builder.append("</p>");
       builder.append("<br><hr>");
     }
@@ -105,13 +120,13 @@ public class ImdbRatingsDialog extends JDialog implements WindowClosingIf {
     ImdbRating rating = plugin.getProgramRating(program);
     builder.append("<h3>").append(mLocalizer.msg("rating", "Rating")).append(":</h3>");
     builder.append("<p style='margin-left:10px'>");
-    builder.append(makeTextForRating(rating));
+    builder.append(makeTextForRating(doc, rating));
     builder.append("</p>");
 
     return builder.toString();
   }
 
-  private String makeTextForRating(ImdbRating rating) {
+  private String makeTextForRating(ExtendedHTMLDocument doc, ImdbRating rating) {
     final StringBuilder builder = new StringBuilder();
     final ImdbMovie movie = plugin.getDatabase().getMovieForId(rating.getMovieId());
 
@@ -128,8 +143,10 @@ public class ImdbRatingsDialog extends JDialog implements WindowClosingIf {
     builder.append("<br>");
 
     builder.append(mLocalizer.msg("rating", "Rating")).append(": <i><b>").append(rating.getRatingText()).append("</b>/10</i> - ");
-    builder.append(mLocalizer.msg("votes", "votes")).append(" : <i>").append(rating.getVotes()).append("</i><br>");
-    builder.append(mLocalizer.msg("distribution", "Distribution")).append(": <i>").append(rating.getDistribution()).append("</i><br>");
+    builder.append(mLocalizer.msg("votes", "votes")).append(" : <i><b>").append(rating.getVotes()).append("</b></i><br><br>");
+    builder.append(mLocalizer.msg("distribution", "Distribution")).append(":<br>");
+
+    builder.append(doc.createCompTag(createChartForRating(rating))).append("<br>");
 
     ImdbAka[] akas = movie.getAkas();
     if (akas.length > 0) {
@@ -148,6 +165,58 @@ public class ImdbRatingsDialog extends JDialog implements WindowClosingIf {
     }
 
     return builder.toString();
+  }
+
+  private ChartPanel createChartForRating(ImdbRating rating) {
+    final JFreeChart chart = ChartFactory.createBarChart(
+        null,         // chart title
+        mLocalizer.msg("rating", "Rating"),                 // domain axis label
+        mLocalizer.msg("votesInPrecent","Votes in %"),                // range axis label
+        createDataSet(rating),      // data
+        PlotOrientation.HORIZONTAL, // orientation
+        false,                       // include legend
+        true,
+        false
+    );
+
+    chart.setBackgroundPaint(Color.WHITE);
+
+    final CategoryPlot plot = chart.getCategoryPlot();
+    plot.setRangeAxisLocation(AxisLocation.BOTTOM_OR_LEFT);
+
+    BarRenderer renderer = new BarRenderer(){
+      @Override
+      public Paint getItemPaint(int row, int column) {
+        return Color.yellow;
+      }
+    };
+    renderer.setDrawBarOutline(false);
+
+    plot.setRenderer(renderer);
+
+    final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+    rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+
+    return new ChartPanel(chart);
+  }
+
+  private CategoryDataset createDataSet(ImdbRating rating) {
+    final double[][] data = new double[1][10];
+
+    String dist = rating.getDistribution();
+    for (int i=0;i<10;i++){
+      if (dist.charAt(i) == '.') {
+        data[0][i] = 0;
+      } else if (dist.charAt(i) == '*') {
+        data[0][i] = 100;
+      } else {
+        data[0][i] = (Integer.parseInt(""+dist.charAt(i))) * 10 + 5;
+      }
+    }
+
+    String[] keys = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+
+    return DatasetUtilities.createCategoryDataset(new String[]{"hallo"}, keys, data);
   }
 
   public void close() {
