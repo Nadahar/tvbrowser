7174a85dd64ae08c041b29b0c259467d4924e658
bananeweizen <bananeweizen@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2010-01-05 13:24:18 +0000

    new check for textfields with index style (article at the end)

--- tvbrowser-plugins/plugin-checker/src/main/java/checkerplugin/CheckerPlugin.java
+++ tvbrowser-plugins/plugin-checker/src/main/java/checkerplugin/CheckerPlugin.java
@@ -35,6 +35,7 @@ import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
 import checkerplugin.check.AbstractCheck;
+import checkerplugin.check.ArticleCheck;
 import checkerplugin.check.DescriptionRepetitionCheck;
 import checkerplugin.check.DurationCheck;
 import checkerplugin.check.EllipsisCheck;
@@ -72,270 +73,254 @@ import devplugin.Version;
  */
 public class CheckerPlugin extends Plugin {
 
-	private static final Version mVersion = new Version(0, 3, false);
+  private static final Version mVersion = new Version(0, 3, false);
 
-	private static final util.ui.Localizer mLocalizer = util.ui.Localizer
-			.getLocalizerFor(CheckerPlugin.class);
+  private static final util.ui.Localizer mLocalizer = util.ui.Localizer.getLocalizerFor(CheckerPlugin.class);
 
-	public static Version getVersion() {
-		return mVersion;
-	}
+  public static Version getVersion() {
+    return mVersion;
+  }
 
-	private PluginInfo mPluginInfo;
+  private PluginInfo mPluginInfo;
 
-	private ImageIcon mInfoIcon = createImageIcon("status", "dialog-information",
-			16);
-	private ImageIcon mWarnIcon = createImageIcon("status", "dialog-warning", 16);
+  private ImageIcon mInfoIcon = createImageIcon("status", "dialog-information", 16);
+  private ImageIcon mWarnIcon = createImageIcon("status", "dialog-warning", 16);
 
-	private PluginTreeNode mRootNode = new PluginTreeNode(this, false);
+  private PluginTreeNode mRootNode = new PluginTreeNode(this, false);
 
-	private CheckerSettings mSettings;
+  private CheckerSettings mSettings;
 
-	private static final AbstractCheck[] ALL_CHECKS = new AbstractCheck[] {
-			new DescriptionRepetitionCheck(), new DurationCheck(),
-			new EllipsisCheck(), new EmptyLinesCheck(), new FormatCheck(),
-			new GenreCategoryCheck(), new LiveCheck(), new ModerationCheck(),
-			new NetPlayTimeCheck(), new NewCheck(), new PersonsCheck(),
-			new RepetitionCheck(), new SeriesEpisodeCheck(), new SeriesNumberCheck(),
-			new ShortDescriptionLengthCheck(), new TextFieldFormatCheck(),
-			new TitleFormatCheck(), new UrlCheck() };
+  private static final AbstractCheck[] ALL_CHECKS = new AbstractCheck[] { new ArticleCheck(),
+      new DescriptionRepetitionCheck(), new DurationCheck(), new EllipsisCheck(), new EmptyLinesCheck(),
+      new FormatCheck(), new GenreCategoryCheck(), new LiveCheck(), new ModerationCheck(), new NetPlayTimeCheck(),
+      new NewCheck(), new PersonsCheck(), new RepetitionCheck(), new SeriesEpisodeCheck(), new SeriesNumberCheck(),
+      new ShortDescriptionLengthCheck(), new TextFieldFormatCheck(), new TitleFormatCheck(), new UrlCheck() };
 
-	public PluginInfo getInfo() {
-		if (mPluginInfo == null) {
-			final String name = mLocalizer.msg("name", "Checker");
-			final String desc = mLocalizer.msg("description",
-					"Checks program data for bugs.");
-			mPluginInfo = new PluginInfo(CheckerPlugin.class, name, desc,
-					"Michael Keppler", "GPL 3");
-		}
+  public PluginInfo getInfo() {
+    if (mPluginInfo == null) {
+      final String name = mLocalizer.msg("name", "Checker");
+      final String desc = mLocalizer.msg("description", "Checks program data for bugs.");
+      mPluginInfo = new PluginInfo(CheckerPlugin.class, name, desc, "Michael Keppler", "GPL 3");
+    }
 
-		return mPluginInfo;
-	}
+    return mPluginInfo;
+  }
 
-	@Override
-	public Icon[] getMarkIconsForProgram(final Program program) {
-		if (program == null
-				|| getPluginManager().getExampleProgram().equals(program)) {
-			return new Icon[] { mWarnIcon };
-		}
-		if (!isSupportedChannel(program.getChannel())) {
-			return null;
-		}
+  @Override
+  public Icon[] getMarkIconsForProgram(final Program program) {
+    if (program == null || getPluginManager().getExampleProgram().equals(program)) {
+      return new Icon[] { mWarnIcon };
+    }
+    if (!isSupportedChannel(program.getChannel())) {
+      return null;
+    }
 
-		ArrayList<Issue> issues = getIssues(program);
-		if (issues.isEmpty()) {
-			return null;
-		}
-		for (Issue issue : issues) {
-			if (issue.getSeverity() == Issue.SEVERITY_ERROR) {
-				return new Icon[] { mWarnIcon };
-			}
-		}
-		return new Icon[] { mInfoIcon };
-	}
+    ArrayList<Issue> issues = getIssues(program);
+    if (issues.isEmpty()) {
+      return null;
+    }
+    for (Issue issue : issues) {
+      if (issue.getSeverity() == Issue.SEVERITY_ERROR) {
+        return new Icon[] { mWarnIcon };
+      }
+    }
+    return new Icon[] { mInfoIcon };
+  }
 
-	private boolean isSupportedChannel(final Channel channel) {
-		return channel.getDataServiceProxy().getInfo().getDescription().contains(
-				"TV-Browser");
-	}
+  private boolean isSupportedChannel(final Channel channel) {
+    return channel.getDataServiceProxy().getInfo().getDescription().contains("TV-Browser");
+  }
 
-	private ArrayList<Issue> getIssues(final Program program) {
-		final ArrayList<Issue> results = new ArrayList<Issue>();
+  private ArrayList<Issue> getIssues(final Program program) {
+    final ArrayList<Issue> results = new ArrayList<Issue>();
 
-		for (AbstractCheck check : ALL_CHECKS) {
-			check.check(program, results);
-		}
+    for (AbstractCheck check : ALL_CHECKS) {
+      check.check(program, results);
+    }
 
-		return results;
-	}
+    return results;
+  }
 
-	@Override
-	public ActionMenu getContextMenuActions(final Program program) {
-		if (getPluginManager().getExampleProgram().equals(program)) {
-			return new ActionMenu(new AbstractAction("Checker") {
+  @Override
+  public ActionMenu getContextMenuActions(final Program program) {
+    if (getPluginManager().getExampleProgram().equals(program)) {
+      return new ActionMenu(new AbstractAction("Checker") {
 
-				@Override
-				public void actionPerformed(final ActionEvent e) {
-					// do nothing, example program
-				}
-			});
-		}
-		if (!isSupportedChannel(program.getChannel())) {
-			return null;
-		}
-		final ArrayList<Issue> issues = getIssues(program);
-		final ArrayList<Object> actionList = new ArrayList<Object>();
-		if (issues.size() > 0) {
-			for (Issue result : issues) {
-				actionList.add(new AbstractAction(result.getMessage()) {
-					@Override
-					public void actionPerformed(final ActionEvent e) {
-						// do nothing
-					}
-				});
-			}
-		}
-		if (actionList.size() > 0) {
-			actionList.add(ContextMenuSeparatorAction.getInstance());
-		}
-		final ArrayList<Action> fieldActions = new ArrayList<Action>();
-		final Iterator<ProgramFieldType> it = program.getFieldIterator();
-		while (it.hasNext()) {
-			final ProgramFieldType field = it.next();
-			fieldActions.add(new AbstractAction(field.getLocalizedName()) {
-				public void actionPerformed(final ActionEvent e) {
-					String content = "";
-					final int format = field.getFormat();
-					if (format == ProgramFieldType.TEXT_FORMAT) {
-						content = program.getTextField(field);
-					} else if (format == ProgramFieldType.INT_FORMAT) {
-						content = program.getIntFieldAsString(field);
-					} else if (format == ProgramFieldType.TIME_FORMAT) {
-						content = program.getTimeFieldAsString(field);
-					} else if (format == ProgramFieldType.BINARY_FORMAT) {
-						content = Arrays.toString(program.getBinaryField(field));
-					}
-					JOptionPane.showMessageDialog(null, content);
-				}
-			});
-		}
-		Collections.sort(fieldActions, new Comparator<Action>() {
+        @Override
+        public void actionPerformed(final ActionEvent e) {
+          // do nothing, example program
+        }
+      });
+    }
+    if (!isSupportedChannel(program.getChannel())) {
+      return null;
+    }
+    final ArrayList<Issue> issues = getIssues(program);
+    final ArrayList<Object> actionList = new ArrayList<Object>();
+    if (issues.size() > 0) {
+      for (Issue result : issues) {
+        actionList.add(new AbstractAction(result.getMessage()) {
+          @Override
+          public void actionPerformed(final ActionEvent e) {
+            // do nothing
+          }
+        });
+      }
+    }
+    if (actionList.size() > 0) {
+      actionList.add(ContextMenuSeparatorAction.getInstance());
+    }
+    final ArrayList<Action> fieldActions = new ArrayList<Action>();
+    final Iterator<ProgramFieldType> it = program.getFieldIterator();
+    while (it.hasNext()) {
+      final ProgramFieldType field = it.next();
+      fieldActions.add(new AbstractAction(field.getLocalizedName()) {
+        public void actionPerformed(final ActionEvent e) {
+          String content = "";
+          final int format = field.getFormat();
+          if (format == ProgramFieldType.TEXT_FORMAT) {
+            content = program.getTextField(field);
+          } else if (format == ProgramFieldType.INT_FORMAT) {
+            content = program.getIntFieldAsString(field);
+          } else if (format == ProgramFieldType.TIME_FORMAT) {
+            content = program.getTimeFieldAsString(field);
+          } else if (format == ProgramFieldType.BINARY_FORMAT) {
+            content = Arrays.toString(program.getBinaryField(field));
+          }
+          JOptionPane.showMessageDialog(null, content);
+        }
+      });
+    }
+    Collections.sort(fieldActions, new Comparator<Action>() {
 
-			@Override
-			public int compare(Action o1, Action o2) {
-				return ((String) o1.getValue(Action.NAME)).compareTo((String) o2
-						.getValue(Action.NAME));
-			}
-		});
-		actionList.add(new ActionMenu(new ContextMenuAction(mLocalizer.msg(
-				"showField", "Show field")), fieldActions
-				.toArray(new Action[fieldActions.size()])));
-		if (actionList.size() > 0) {
-			return new ActionMenu(new ContextMenuAction(mLocalizer.msg("contextMenu",
-					"Checker")), actionList.toArray(new Object[actionList.size()]));
-		}
-		return null;
-	}
+      @Override
+      public int compare(Action o1, Action o2) {
+        return ((String) o1.getValue(Action.NAME)).compareTo((String) o2.getValue(Action.NAME));
+      }
+    });
+    actionList.add(new ActionMenu(new ContextMenuAction(mLocalizer.msg("showField", "Show field")), fieldActions
+        .toArray(new Action[fieldActions.size()])));
+    if (actionList.size() > 0) {
+      return new ActionMenu(new ContextMenuAction(mLocalizer.msg("contextMenu", "Checker")), actionList
+          .toArray(new Object[actionList.size()]));
+    }
+    return null;
+  }
 
-	@Override
-	public void handleTvBrowserStartFinished() {
-		if (mSettings.getAutostart()) {
-			runAllChecks();
-		}
-	}
+  @Override
+  public void handleTvBrowserStartFinished() {
+    if (mSettings.getAutostart()) {
+      runAllChecks();
+    }
+  }
 
-	private void runAllChecks() {
-		Cursor cursor = getParentFrame().getCursor();
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				getParentFrame().setCursor(
-						Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
-			}
-		});
-		try {
-			mRootNode.clear();
-			final HashMap<String, PluginTreeNode> nodes = new HashMap<String, PluginTreeNode>();
-			final Channel[] channels = devplugin.Plugin.getPluginManager()
-					.getSubscribedChannels();
-			Date currentDate = Date.getCurrentDate();
-			for (Channel channel : channels) {
-				if (!isSupportedChannel(channel)) {
-					continue;
-				}
-				Date date = currentDate;
-				for (int days = 0; days < 30; days++) {
-					final Iterator<Program> iter = Plugin.getPluginManager()
-							.getChannelDayProgram(date, channel);
-					if (iter != null) {
-						while (iter.hasNext()) {
-							final Program program = iter.next();
-							final ArrayList<Issue> issues = getIssues(program);
-							if (!issues.isEmpty()) {
-								program.mark(this);
-								for (Issue issue : issues) {
-									String message = issue.getMessage();
-									PluginTreeNode node = nodes.get(message);
-									if (node == null) {
-										node = new PluginTreeNode(issue.getMessage());
-										nodes.put(issue.getMessage(), node);
-										if (issue.getSeverity() == Issue.SEVERITY_ERROR) {
-											node.getMutableTreeNode().setIcon(mWarnIcon);
-										} else {
-											node.getMutableTreeNode().setIcon(mInfoIcon);
-										}
-									}
-									node.addProgram(program);
-								}
-							}
-						}
-					}
-					date = date.addDays(1);
-				}
-			}
-			// sort nodes and add them to the root
-			final Collection<PluginTreeNode> values = nodes.values();
-			final PluginTreeNode[] nodeArray = new PluginTreeNode[values.size()];
-			values.toArray(nodeArray);
-			Arrays.sort(nodeArray);
-			for (PluginTreeNode node : nodeArray) {
-				mRootNode.add(node);
-			}
-			mRootNode.update();
-		} finally {
-			final Cursor finalCursor = cursor;
-			SwingUtilities.invokeLater(new Runnable() {
+  private void runAllChecks() {
+    Cursor cursor = getParentFrame().getCursor();
+    SwingUtilities.invokeLater(new Runnable() {
+      @Override
+      public void run() {
+        getParentFrame().setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+      }
+    });
+    try {
+      mRootNode.clear();
+      final HashMap<String, PluginTreeNode> nodes = new HashMap<String, PluginTreeNode>();
+      final Channel[] channels = devplugin.Plugin.getPluginManager().getSubscribedChannels();
+      Date currentDate = Date.getCurrentDate();
+      for (Channel channel : channels) {
+        if (!isSupportedChannel(channel)) {
+          continue;
+        }
+        Date date = currentDate;
+        for (int days = 0; days < 30; days++) {
+          final Iterator<Program> iter = Plugin.getPluginManager().getChannelDayProgram(date, channel);
+          if (iter != null) {
+            while (iter.hasNext()) {
+              final Program program = iter.next();
+              final ArrayList<Issue> issues = getIssues(program);
+              if (!issues.isEmpty()) {
+                program.mark(this);
+                for (Issue issue : issues) {
+                  String message = issue.getMessage();
+                  PluginTreeNode node = nodes.get(message);
+                  if (node == null) {
+                    node = new PluginTreeNode(issue.getMessage());
+                    nodes.put(issue.getMessage(), node);
+                    if (issue.getSeverity() == Issue.SEVERITY_ERROR) {
+                      node.getMutableTreeNode().setIcon(mWarnIcon);
+                    } else {
+                      node.getMutableTreeNode().setIcon(mInfoIcon);
+                    }
+                  }
+                  node.addProgram(program);
+                }
+              }
+            }
+          }
+          date = date.addDays(1);
+        }
+      }
+      // sort nodes and add them to the root
+      final Collection<PluginTreeNode> values = nodes.values();
+      final PluginTreeNode[] nodeArray = new PluginTreeNode[values.size()];
+      values.toArray(nodeArray);
+      Arrays.sort(nodeArray);
+      for (PluginTreeNode node : nodeArray) {
+        mRootNode.add(node);
+      }
+      mRootNode.update();
+    } finally {
+      final Cursor finalCursor = cursor;
+      SwingUtilities.invokeLater(new Runnable() {
 
-				@Override
-				public void run() {
-					getParentFrame().setCursor(finalCursor);
-				}
-			});
-		}
-	}
+        @Override
+        public void run() {
+          getParentFrame().setCursor(finalCursor);
+        }
+      });
+    }
+  }
 
-	@Override
-	public int getMarkPriorityForProgram(final Program p) {
-		return Program.MAX_MARK_PRIORITY;
-	}
+  @Override
+  public int getMarkPriorityForProgram(final Program p) {
+    return Program.MAX_MARK_PRIORITY;
+  }
 
-	@Override
-	public boolean canUseProgramTree() {
-		return true;
-	}
+  @Override
+  public boolean canUseProgramTree() {
+    return true;
+  }
 
-	@Override
-	public PluginTreeNode getRootNode() {
-		return mRootNode;
-	}
+  @Override
+  public PluginTreeNode getRootNode() {
+    return mRootNode;
+  }
 
-	@Override
-	public ActionMenu getButtonAction() {
-		AbstractAction action = new AbstractAction(mLocalizer.msg("action.check",
-				"Start check"), mWarnIcon) {
+  @Override
+  public ActionMenu getButtonAction() {
+    AbstractAction action = new AbstractAction(mLocalizer.msg("action.check", "Start check"), mWarnIcon) {
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				runAllChecks();
-			}
-		};
-		action.putValue(Plugin.BIG_ICON, createImageIcon("status",
-				"dialog-warning", 22));
-		return new ActionMenu(action);
-	}
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        runAllChecks();
+      }
+    };
+    action.putValue(Plugin.BIG_ICON, createImageIcon("status", "dialog-warning", 22));
+    return new ActionMenu(action);
+  }
 
-	@Override
-	public SettingsTab getSettingsTab() {
-		return new CheckerSettingsTab(mSettings);
-	}
+  @Override
+  public SettingsTab getSettingsTab() {
+    return new CheckerSettingsTab(mSettings);
+  }
 
-	@Override
-	public void loadSettings(Properties properties) {
-		mSettings = new CheckerSettings(properties);
-	}
+  @Override
+  public void loadSettings(Properties properties) {
+    mSettings = new CheckerSettings(properties);
+  }
 
-	@Override
-	public Properties storeSettings() {
-		return mSettings.storeSettings();
-	}
+  @Override
+  public Properties storeSettings() {
+    return mSettings.storeSettings();
+  }
 }
