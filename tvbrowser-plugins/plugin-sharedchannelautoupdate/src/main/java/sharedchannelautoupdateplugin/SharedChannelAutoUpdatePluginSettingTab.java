de26efaaac3c929e99e2ea51c8696a363aee39f3
bananeweizen <bananeweizen@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2010-01-09 16:01:06 +0000

    import jb's plugins

---
+++ tvbrowser-plugins/plugin-sharedchannelautoupdate/src/main/java/sharedchannelautoupdateplugin/SharedChannelAutoUpdatePluginSettingTab.java
@@ -0,0 +1,34 @@
+package sharedchannelautoupdateplugin;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+import devplugin.SettingsTab;
+
+public class SharedChannelAutoUpdatePluginSettingTab implements SettingsTab {
+//  private static java.util.logging.Logger mLog = java.util.logging.Logger.getLogger(SharedChannelAutoUpdatePluginSettingTab.class.getName());
+  private static final util.ui.Localizer mLocalizer = util.ui.Localizer.getLocalizerFor(SharedChannelAutoUpdatePluginSettingTab.class);
+  private Icon mIcon;
+
+
+  public Icon getIcon() {
+    if (mIcon == null) {
+      mIcon = new ImageIcon(getClass().getResource("icons/shared.png"));
+    }
+    return mIcon;
+  }
+  
+  public String getTitle() {
+    return mLocalizer.msg("tabName", "Shared Channel Auto Updater");
+  }
+  
+  public JPanel createSettingsPanel() {
+    return new JPanel();
+  }
+    
+  public void saveSettings() {
+  }
+  
+  
+}
