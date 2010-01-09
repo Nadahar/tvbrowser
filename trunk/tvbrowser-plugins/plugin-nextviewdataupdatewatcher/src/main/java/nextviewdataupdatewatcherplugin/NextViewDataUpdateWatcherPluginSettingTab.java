05a4c92ff9b57badcd2b21fb0865818315ba803f
bananeweizen <bananeweizen@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2010-01-09 15:59:06 +0000

    import jb's plugins

---
+++ trunk/tvbrowser-plugins/plugin-nextviewdataupdatewatcher/src/main/java/nextviewdataupdatewatcherplugin/NextViewDataUpdateWatcherPluginSettingTab.java
@@ -0,0 +1,34 @@
+package nextviewdataupdatewatcherplugin;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+import devplugin.SettingsTab;
+
+public class NextViewDataUpdateWatcherPluginSettingTab implements SettingsTab {
+//  private static java.util.logging.Logger mLog = java.util.logging.Logger.getLogger(NextViewDataUpdateWatcherPluginSettingTab.class.getName());
+  private static final util.ui.Localizer mLocalizer = util.ui.Localizer.getLocalizerFor(NextViewDataUpdateWatcherPluginSettingTab.class);
+  private Icon mIcon;
+
+
+  public Icon getIcon() {
+    if (mIcon == null) {
+      mIcon = new ImageIcon(getClass().getResource("icons/nxtvepg.png"));
+    }
+    return mIcon;
+  }
+  
+  public String getTitle() {
+    return mLocalizer.msg("tabName", "Mixed Data Auto Updater");
+  }
+  
+  public JPanel createSettingsPanel() {
+    return new JPanel();
+  }
+    
+  public void saveSettings() {
+  }
+  
+  
+}
