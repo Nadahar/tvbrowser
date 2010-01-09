0eed0dd2430b2213036b71f185d304c41bc8a97a
bananeweizen <bananeweizen@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2010-01-09 15:52:41 +0000

    import jb's plugins

---
+++ tvbrowser-plugins/plugin-mixedupdatelock/src/main/java/mixedupdatelockplugin/MixedUpdateLockPlugin.java
@@ -0,0 +1,91 @@
+package mixedupdatelockplugin;
+
+import java.io.File;
+import java.util.Properties;
+
+import tvdataservice.MutableChannelDayProgram;
+
+import devplugin.PluginInfo;
+import devplugin.SettingsTab;
+import devplugin.Version;
+
+public class MixedUpdateLockPlugin extends devplugin.Plugin {
+
+//private static java.util.logging.Logger mLog = java.util.logging.Logger.getLogger(MixedUpdateLockPlugin.class.getName());
+  private static final util.ui.Localizer mLocalizer = util.ui.Localizer.getLocalizerFor(MixedUpdateLockPlugin.class);
+  private static MixedUpdateLockPlugin mInstance;
+  private String lckFileName;
+
+  public MixedUpdateLockPlugin() {
+    mInstance = this;
+    File storeDir = new File (getPluginManager().getTvBrowserSettings().getTvBrowserUserHome() + "/MixedChannels" );
+    if (!storeDir.exists()){
+      storeDir.mkdir();
+    }
+    lckFileName = storeDir + "/datamix.lck";
+    File lckFile = new File (lckFileName);
+    if (!lckFile.exists()){
+      try {
+        lckFile.createNewFile();
+      } catch (Exception e) {
+      }
+    }
+   }
+
+
+  public void handleTvDataAdded(MutableChannelDayProgram newProg) {
+    File lckFile = new File (lckFileName);
+    if (!lckFile.exists()){
+      try {
+        lckFile.createNewFile();
+      } catch (Exception e) {
+      }
+    }
+  }
+
+  public void handleTvDataUpdateFinished() {
+    File lckFile = new File (lckFileName);
+    if (lckFile.exists()){
+      lckFile.delete();
+    }
+  }
+  
+  public void loadSettings(Properties settings) {
+  }
+
+  public Properties storeSettings() {
+    return null;
+  }
+
+
+  /**
+   * Gets the Version of the implemented API
+   * Since TV-Browser 0.9.7 getAPIVersion must return 1.0 
+   */
+  public static Version getVersion() {
+    return new Version(0, 0, 0, false, null);
+  }
+
+  public PluginInfo getInfo() {
+    return new PluginInfo(MixedUpdateLockPlugin.class,
+        mLocalizer.msg("name", "Data Mixer Lock"),
+        mLocalizer.msg("desc", "Lock Update for the Data Mixing Plugins"),
+    "jb");
+
+  }
+
+  public SettingsTab getSettingsTab() {
+    return new MixedUpdateLockPluginSettingTab();
+  }
+
+  /**
+   * Returns an Instance of this Plugin
+   * 
+   * @return Instance of this Plugin
+   */
+  public static MixedUpdateLockPlugin getInstance() {
+    return mInstance;
+  }
+
+
+}
---
+++ tvbrowser-plugins/plugin-mixedupdatelock/src/main/java/mixedupdatelockplugin/MixedUpdateLockPluginSettingTab.java
@@ -0,0 +1,34 @@
+package mixedupdatelockplugin;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+import devplugin.SettingsTab;
+
+public class MixedUpdateLockPluginSettingTab implements SettingsTab {
+//  private static java.util.logging.Logger mLog = java.util.logging.Logger.getLogger(MixedUpdateLockPluginSettingTab.class.getName());
+  private static final util.ui.Localizer mLocalizer = util.ui.Localizer.getLocalizerFor(MixedUpdateLockPluginSettingTab.class);
+  private Icon mIcon;
+
+
+  public Icon getIcon() {
+    if (mIcon == null) {
+      mIcon = new ImageIcon(getClass().getResource("icons/lock.png"));
+    }
+    return mIcon;
+  }
+  
+  public String getTitle() {
+    return mLocalizer.msg("tabName", "Mixed Data Update Lock");
+  }
+  
+  public JPanel createSettingsPanel() {
+    return new JPanel();
+  }
+    
+  public void saveSettings() {
+  }
+  
+  
+}
