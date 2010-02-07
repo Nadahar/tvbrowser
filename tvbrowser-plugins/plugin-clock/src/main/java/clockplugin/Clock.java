c3f9a9705c899c0f5d087cf6f59076ca75b6e759
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2010-02-07 12:08:39 +0000

    Added Clock-Plugin

---
+++ tvbrowser-plugins/plugin-clock/src/main/java/clockplugin/Clock.java
@@ -0,0 +1,219 @@
+package clockplugin;
+
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Properties;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * The clock dialog. License: GPL
+ * 
+ * @author René Mach
+ */
+public class Clock extends JDialog implements Runnable, MouseListener, MouseMotionListener {
+
+  private static final long serialVersionUID = 1L;
+
+  private Thread mClockThread;
+  private int mShowTime;
+  private JLabel mTime;
+  private DateFormat mTimeFormat;
+  private Point mDraggingPoint;
+  private Properties mProperties;
+  private boolean mShowForever, mStop, mDontStop;
+  private JPanel mTimePanel;
+
+  /**
+   * The default construktor of the class.
+   * 
+   * @param show
+   *          The time to show the dialog in seconds.
+   * @param config
+   *          The properties for this.
+   */
+  public Clock(int show, Properties config) {
+    super(ClockPlugin.getInstance().getSuperFrame());
+    mClockThread = new Thread(this);
+    mClockThread.setPriority(Thread.MIN_PRIORITY);
+    mShowTime = show * 1000;
+    mProperties = config;
+    mStop = false;
+    mDontStop = false;
+    this.addMouseMotionListener(this);
+    
+    Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+    
+    String showForever = config.getProperty("showForever");
+    if (showForever == null || showForever.length() == 0) {
+      mShowForever = false;
+      config.setProperty("showForever", false + "");
+    } else if (showForever.equals("true"))
+      mShowForever = true;
+    else
+      mShowForever = false;
+
+    int fontsize = Integer.parseInt(config.getProperty("fontsize","28"));
+    int oldFontSize = Integer.parseInt(config.getProperty("oldFontSize", "-1"));
+    
+    Font f = new Font("Arial", Font.BOLD, fontsize);
+
+    mProperties.setProperty("oldFontSize", fontsize + "");
+
+    mTimePanel = new JPanel();
+    if (ClockPlugin.getInstance().getShowBorder())
+      mTimePanel.setBorder(BorderFactory.createEtchedBorder());
+    mTimePanel.addMouseListener(this);
+    mTimePanel.addMouseMotionListener(this);
+    mTimePanel.setLayout(new GridLayout());
+    
+    mTime = new JLabel();
+    mTime.setFont(f);
+    mTime.addMouseListener(this);
+    mTimeFormat = DateFormat.getTimeInstance(DateFormat.MEDIUM,Locale.getDefault());
+    mTime.addMouseMotionListener(this);
+    mTimePanel.add(mTime);
+
+    int xPos = Integer.parseInt(config.getProperty("xPos", "-1000"));
+    int yPos = Integer.parseInt(config.getProperty("yPos", "-1000"));
+    int width = Integer.parseInt(config.getProperty("xWidth", "-1000"));
+    int height = Integer.parseInt(config.getProperty("yHeight", "-1000"));
+    
+    this.getContentPane().setLayout(new BoxLayout(this.getContentPane(),BoxLayout.X_AXIS));
+    this.getContentPane().add(Box.createHorizontalGlue());
+    this.getContentPane().add(mTimePanel);
+    this.getContentPane().addMouseListener(this);
+    this.getContentPane().addMouseMotionListener(this);
+    this.setUndecorated(true);
+    this.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+    mTime.setText(mTimeFormat.format(new Date(System.currentTimeMillis())));
+    
+    if (width < 15 || height < 5 || fontsize != oldFontSize) {
+      this.pack();
+      this.setSize(this.getWidth() + 7, this.getHeight());
+    } else {
+      this.setSize(width, height);
+    }
+    
+    if ((xPos - width < (width * -1)) || (xPos - width > d.width) ||
+        (yPos - height < (height * -1)) || (yPos - height > d.height))
+      this.setLocation(d.width - this.getSize().width, 0);
+    else {
+      this.setLocation(xPos, yPos);
+    }
+
+    if (ClockPlugin.getInstance().getSuperFrame() != null)
+      this.setVisible(true);
+    mClockThread.start();
+    
+    ClockPlugin.getInstance().getSuperFrame().toFront();
+  }
+
+  /**
+   * 
+   * @return The clock thread.
+   */
+  public synchronized Thread getThread() {
+    return mClockThread;
+  }
+
+  /**
+   * Stop the Thread.
+   * 
+   */
+  public void stopp() {
+    mStop = true;
+  }
+
+  /**
+   * @param value
+   *          The clock should be shown forever.
+   */
+  public void setShowForever(boolean value) {
+    mShowForever = value;
+  }
+
+  public void run() {
+    try {
+      do {
+        Thread.sleep(500);
+        if (!mShowForever)
+          mShowTime -= 500;
+        mTime.setText(mTimeFormat.format(new Date(System.currentTimeMillis())));
+      } while (mShowTime > 0 && !mStop);
+    } catch (InterruptedException e) {
+    }
+    
+    try {
+      while(mDontStop)
+        Thread.sleep(200);
+    }catch(InterruptedException e) {
+    }
+  }
+
+  public void mouseClicked(MouseEvent e) {
+  }
+
+  public void mousePressed(MouseEvent e) {
+    mDraggingPoint = e.getPoint();
+    mDontStop = true;
+  }
+
+  public void mouseReleased(MouseEvent e) {
+    mDraggingPoint = null; 
+    mDontStop = false;
+    if(ClockPlugin.getInstance().getSuperFrame().isVisible())
+      ClockPlugin.getInstance().getSuperFrame().toFront();
+  }
+
+  public void mouseEntered(MouseEvent e) {
+  }
+
+  public void mouseExited(MouseEvent e) {
+  }
+
+  /**
+   * Set up the border of the clock.
+   * 
+   * @param value
+   */
+  public void setBorder(boolean value) {
+    if (value)
+      mTimePanel.setBorder(BorderFactory.createEtchedBorder());
+    else
+      mTimePanel.setBorder(BorderFactory.createEmptyBorder());
+  }
+
+  public void mouseDragged(MouseEvent e) {    
+    if (mDraggingPoint != null && !e.isShiftDown()) {
+      int xP = e.getX();
+      int yP = e.getY();
+      int x = mDraggingPoint.x - xP;
+      int y = mDraggingPoint.y - yP;
+      
+      if(x != 0 || y != 0) {
+        setLocation(getX() - x,getY() - y);
+        
+        mProperties.setProperty("xPos",getX() + "");
+        mProperties.setProperty("yPos",getY() + "");
+        mProperties.setProperty("xWidth",getWidth() + "");
+        mProperties.setProperty("yHeight",getHeight() + "");
+      }
+    }
+  }
+
+  public void mouseMoved(MouseEvent e) {}
+}
---
+++ tvbrowser-plugins/plugin-clock/src/main/java/clockplugin/ClockPlugin.java
@@ -0,0 +1,413 @@
+package clockplugin;
+
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.lang.reflect.Constructor;
+import java.util.Properties;
+import javax.swing.Action;
+import javax.swing.SwingUtilities;
+
+import devplugin.ActionMenu;
+import devplugin.ButtonAction;
+import devplugin.Plugin;
+import devplugin.PluginInfo;
+import devplugin.SettingsTab;
+import devplugin.Version;
+
+/**
+ * Clock Plugin for TV-Browser. License: GPL
+ * 
+ * @author René Mach
+ */
+public class ClockPlugin extends Plugin {
+
+  private static ClockPlugin mInstance;
+  private Properties mProperties;
+  private Clock mClock;
+  private int mShowTime;
+  private boolean mMoveOnScreen, mShowForever;
+  private TitleBarClock mTitleBarClock;
+  private Point mLocation;
+  private Dimension mParentSize;
+  
+  private static final Version mVersion = new Version(1, 76, 1, true);
+  
+  /** The localizer for this class. */
+  public static util.ui.Localizer mLocalizer = util.ui.Localizer.getLocalizerFor(ClockPlugin.class);
+
+  public static Version getVersion() {
+    return mVersion;
+  }
+  
+  /** Plugin info */
+  public PluginInfo getInfo() {
+    Class pluginInfo = PluginInfo.class;
+    
+    try {
+      Constructor constructor = pluginInfo.getConstructor(new Class[] {Class.class,String.class,String.class,String.class,String.class});
+      
+      return (PluginInfo)constructor.newInstance(new Object[] {ClockPlugin.class,"ClockPlugin",mLocalizer.msg("desc",
+      "Clock for TV-Browser"),"René Mach","GPL"});
+    } catch (Exception e) {
+      try {
+        Constructor constructor = pluginInfo.getConstructor(new Class[] {String.class,String.class,String.class,Version.class,String.class});
+        
+        return (PluginInfo)constructor.newInstance(new Object[] {"ClockPlugin",mLocalizer.msg("desc",
+        "Clock for TV-Browser"),"René Mach",mVersion,"GPL"});        
+      }catch(Exception ee) {};
+    } 
+    return new PluginInfo();
+  }
+  
+  /**
+   * Constructor.
+   */
+  public ClockPlugin() {
+    mInstance = this;
+  }
+  
+  /**
+   * 
+   * @return The instance of this Plugin.
+   */
+  public static ClockPlugin getInstance() {
+    return mInstance;
+  }
+
+  /**
+   * 
+   * @return The time to show the time dialog in seconds.
+   */
+  public int getTimeValue() {
+    String time = mProperties.getProperty("time");
+    if (time != null && time.length() != 0)
+      return Integer.parseInt(time);
+    else
+      return 5;
+  }
+
+  /**
+   * 
+   * @param time
+   *          The value the user selected to show the time dialog in seconds.
+   */
+  public void storeTimeValue(int time) {
+    mShowTime = time;
+    mProperties.setProperty("time", String.valueOf(time));
+  }
+
+  public void loadSettings(Properties p) {
+    if (p == null) {
+      mProperties = new Properties();
+      mShowTime = 5;
+    } else {
+      mProperties = p;
+      String time = p.getProperty("time");
+
+      if (time != null && time.length() > 0)
+        mShowTime = Integer.parseInt(time);
+      else
+        mShowTime = 5;
+    }
+
+    if (mProperties.getProperty("titleBarClock") != null
+        && mProperties.getProperty("titleBarClock").equals("true"))
+      mTitleBarClock = new TitleBarClock();
+    if (mProperties.getProperty("moveOnScreen") != null
+        && mProperties.getProperty("moveOnScreen").equals("true"))
+      mMoveOnScreen = true;
+    else
+      mMoveOnScreen = false;
+    if (mProperties.getProperty("showBorder") == null)
+      mProperties.setProperty("showBorder", "true");
+    if (mProperties.getProperty("showForever") != null
+        && mProperties.getProperty("showForever").equals("true"))
+      mShowForever = true;
+  }
+
+  public void onDeactivation() {
+    if (mClock != null && mClock.getThread().isAlive())
+      mClock.stopp();
+  }
+
+  public Properties storeSettings() {
+    return mProperties;
+  }
+
+  public SettingsTab getSettingsTab() {
+    return (new ClockSettingsTab());
+  }
+
+  public ActionMenu getButtonAction() {
+    ButtonAction action = new ButtonAction();
+    action.setActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        toggleOnOffClock();
+      }
+    });
+
+    action
+        .putValue(Action.NAME, mLocalizer.msg("name", "Clock for TV-Browser"));
+    action
+        .putValue(Action.SMALL_ICON, getPluginManager().getIconFromTheme(this,"apps","clock",16));
+    // big icon
+    action.putValue(BIG_ICON, getPluginManager().getIconFromTheme(this,"apps","clock",22));
+
+    return new ActionMenu(action);
+  }
+
+  /**
+   * 
+   * @return The font size of the clock.
+   */
+  public int getFontValue() {
+    String fontsize = mProperties.getProperty("fontsize");
+    if (fontsize == null || fontsize.length() == 0) {
+      mProperties.setProperty("fontsize", "28");
+      return 28;
+    } else
+      return Integer.parseInt(fontsize);
+  }
+
+  /**
+   * 
+   * @param i
+   *          The font size of the clock.
+   */
+  public void setFontValue(int i) {
+    mProperties.setProperty("fontsize", i + "");
+  }
+
+  /**
+   * 
+   * @param value
+   *          Show the clock forever?
+   */
+  public void setShowForever(boolean value) {
+    mProperties.setProperty("showForever", value + "");
+
+    if (mClock != null && mClock.getThread().isAlive())
+      mClock.setShowForever(value);
+    else if (value)
+      toggleOnOffClock();
+  }
+
+  /**
+   * 
+   * @return Show the clock forever or not.
+   */
+  public boolean getShowForever() {
+    String value = mProperties.getProperty("showForever");
+    if (value != null && value.length() > 0)
+      if (value.equals("true"))
+        return true;
+
+    return false;
+  }
+
+  /**
+   * 
+   * @param value
+   *          Clock moveOnScreen with TV-Browser?
+   */
+  public void setMoveOnScreen(boolean value) {
+    mProperties.setProperty("moveOnScreen", value + "");
+    mMoveOnScreen = value;
+    mLocation = getParentFrame().getLocation();
+  }
+
+  /**
+   * 
+   * @return Clock moveOnScreen with TV-Browser.
+   */
+  public boolean getMoveOnScreen() {
+    String value = mProperties.getProperty("moveOnScreen");
+    if (value != null && value.length() > 0)
+      if (value.equals("true"))
+        return true;
+
+    return false;
+  }
+
+  /**
+   * 
+   * @param value
+   *          Clock has border?
+   */
+  public void setShowBorder(boolean value) {
+    mProperties.setProperty("showBorder", value + "");
+    if (mClock != null && mClock.getThread().isAlive())
+      mClock.setBorder(value);
+  }
+
+  /**
+   * 
+   * @return Clock has Border.
+   */
+  public boolean getShowBorder() {
+    String value = mProperties.getProperty("showBorder");
+    if (value != null && value.length() > 0)
+      if (value.equals("true"))
+        return true;
+      else
+        return false;
+    else
+      return false;
+  }
+
+  /**
+   * 
+   * @param value
+   *          Clock in the title bar?
+   */
+  public void setTitleBarClock(boolean value) {
+    mProperties.setProperty("titleBarClock", value + "");
+    if (value) {
+      if (mTitleBarClock == null || !mTitleBarClock.isAlive())
+        mTitleBarClock = new TitleBarClock();
+    } else if (mTitleBarClock != null && mTitleBarClock.isAlive())
+      mTitleBarClock.stopp();
+  }
+
+  /**
+   * 
+   * @return Clock in the title bar.
+   */
+  public boolean getTitleBarClock() {
+    String value = mProperties.getProperty("titleBarClock");
+    if (value != null && value.length() > 0)
+      if (value.equals("true"))
+        return true;
+      else
+        return false;
+    else
+      return false;
+  }
+
+  // show the clock
+  private synchronized void toggleOnOffClock() {
+    if (getParentFrame().isUndecorated() && (mClock == null || !mClock.getThread().isAlive()))
+      return;
+    
+    if ((mClock == null || !mClock.isVisible()) && getParentFrame().isVisible()) {
+      mClock = new Clock(mShowTime, mProperties);
+      Thread t = new Thread() {
+        public void run() {
+          try {
+            while(mClock.getThread().isAlive())
+              Thread.sleep(500);
+          }catch(Exception e) {e.printStackTrace();}
+          
+          SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+              mClock.setVisible(false);
+              mClock.dispose();
+            }
+          });
+        }
+      };
+      t.setPriority(Thread.MIN_PRIORITY);
+      t.start();
+    }
+    else if(mClock != null && mClock.isVisible())
+      mClock.stopp();
+  }
+
+  public void handleTvBrowserStartFinished() {
+    if (mShowForever && mClock == null)
+      toggleOnOffClock();
+    
+    mLocation = getParentFrame().getLocation();
+    mParentSize = getParentFrame().getSize();
+    
+    getParentFrame().addComponentListener(new ComponentAdapter() {
+      boolean mWasVisible = false;
+      
+      public void componentHidden(ComponentEvent e) {
+        if(mClock != null && mClock.getThread().isAlive())
+          mClock.setShowForever(true);
+      }
+
+      public void componentResized(ComponentEvent e) {
+        if (mMoveOnScreen && !getParentFrame().isUndecorated()) {
+          Dimension d = getParentFrame().getSize();
+          
+          int diff = d.width - mParentSize.width;
+          
+          if(diff != 0) {
+            if (mClock != null && mClock.getThread().isAlive()) {
+              mClock.setLocation(mClock.getLocation().x + diff, mClock
+                  .getLocation().y);
+              mProperties.setProperty("xPos", mClock.getX() + "");
+            }
+            else {
+              int xalt = Integer.parseInt(mProperties.getProperty("xPos"));
+              mProperties.setProperty("xPos", (xalt + diff) + "");
+            }
+            mParentSize = d;
+          }
+        }
+        if(getParentFrame().isUndecorated() && mClock != null && mClock.getThread().isAlive()) {
+          mClock.stopp();
+          mClock.setVisible(false);
+          mClock.dispose();
+          mWasVisible = true;
+        }
+        else if(!getParentFrame().isUndecorated() && mWasVisible) {
+          toggleOnOffClock();
+          mWasVisible = false;
+        }
+      }
+      
+      public void componentMoved(ComponentEvent e) {
+        if (mMoveOnScreen && !getParentFrame().isUndecorated()) {
+          Point p = getParentFrame().getLocation();
+
+          if (!p.equals(mLocation) && mLocation != null) {
+            int x = mLocation.x - p.x;
+            int y = mLocation.y - p.y;
+            mLocation = p;
+
+            if (mClock != null && mClock.getThread().isAlive()) {
+              mClock.setLocation(mClock.getLocation().x - x, mClock
+                  .getLocation().y
+                  - y);
+              mProperties.setProperty("xPos", mClock.getX() + "");
+              mProperties.setProperty("yPos", mClock.getY() + "");
+            } else {
+              try {
+                int xalt = Integer.parseInt(mProperties.getProperty("xPos"));
+                int yalt = Integer.parseInt(mProperties.getProperty("yPos"));
+                mProperties.setProperty("xPos", (xalt - x) + "");
+                mProperties.setProperty("yPos", (yalt - y) + "");
+              } catch (Exception ee) {
+              }
+            }
+          }
+        }
+      }
+
+      public void componentShown(ComponentEvent e) {
+        if (!getShowForever()) {
+          if(mClock != null && mClock.getThread().isAlive())
+            mClock.setShowForever(false);
+        }
+        else if(mClock == null || !mClock.getThread().isAlive())
+          toggleOnOffClock();
+      }
+    });
+  }
+
+  /**
+   * 
+   * @return The parent frame of this Plugin.
+   */
+  public Frame getSuperFrame() {
+    return getParentFrame();
+  }
+}
