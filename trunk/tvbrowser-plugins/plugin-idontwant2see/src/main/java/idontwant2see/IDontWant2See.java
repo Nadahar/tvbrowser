733800d6b0a39fa4d2bab6acfa5588da4dc3e31c
bananeweizen <bananeweizen@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2010-02-20 12:09:24 +0000

    plugin is already released as 0.1.1.1, so the code should be at the same level

--- trunk/tvbrowser-plugins/plugin-idontwant2see/src/main/java/idontwant2see/IDontWant2See.java
+++ trunk/tvbrowser-plugins/plugin-idontwant2see/src/main/java/idontwant2see/IDontWant2See.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- * 
+ *
  * SVN information:
  *     $Date$
  *   $Author$
@@ -77,19 +77,19 @@ import devplugin.Version;
 /**
  * A very simple filter plugin to easily get rid of stupid programs in the
  * program table.
- * 
+ *
  * @author René Mach
  */
 public final class IDontWant2See extends Plugin implements AWTEventListener {
 
   private static final boolean PLUGIN_IS_STABLE = true;
-  private static final Version PLUGIN_VERSION = new Version(0, 11, 0, PLUGIN_IS_STABLE);
+  private static final Version PLUGIN_VERSION = new Version(0, 11, 1, PLUGIN_IS_STABLE);
 
   private static final String RECEIVE_TARGET_EXCLUDE_EXACT = "target_exclude_exact";
 
   static final Localizer mLocalizer = Localizer
       .getLocalizerFor(IDontWant2See.class);
-  
+
   private static Date mCurrentDate = Date.getCurrentDate();
   private PluginsProgramFilter mFilter;
   private static IDontWant2See mInstance;
@@ -111,11 +111,11 @@ public final class IDontWant2See extends Plugin implements AWTEventListener {
       + "\\((" // mandatory brackets
       + "(Fortsetzung)" + "|" + "(\\d+)" + ")\\)" + "))" + "$"); // at the end
                                                                  // only
-  
+
   public static Version getVersion() {
     return PLUGIN_VERSION;
   }
-  
+
   /**
    * Creates an instance of this plugin.
    */
@@ -124,16 +124,16 @@ public final class IDontWant2See extends Plugin implements AWTEventListener {
     mSettings = new IDontWant2SeeSettings();
     mDateWasSet = false;
   }
-  
+
   protected static IDontWant2See getInstance() {
     return mInstance;
   }
-  
+
   public void handleTvDataUpdateFinished() {
     clearCache();
     setCurrentDate();
     mDateWasSet = false;
-    
+
     for(IDontWant2SeeListEntry entry : mSettings.getSearchList()) {
       entry.resetDateWasSetFlag();
     }
@@ -146,16 +146,16 @@ public final class IDontWant2See extends Plugin implements AWTEventListener {
   private static void setCurrentDate() {
     mCurrentDate = Date.getCurrentDate();
   }
-  
+
   public byte getImportanceForProgram(Program p) {
     if(!acceptInternal(p)) {
       return mSettings.getProgramImportance();
     }
-    
+
   //TODO After 3.0 release user values from Program class
     return /*Program.DEFAULT_PROGRAM_IMPORTANCE*/ -1;
   }
-  
+
   protected boolean acceptInternal(final Program program) {
     if(!mDateWasSet) {
       mSettings.setLastUsedDate(getCurrentDate());
@@ -165,7 +165,7 @@ public final class IDontWant2See extends Plugin implements AWTEventListener {
 		if (result != null) {
 			return result;
 		}
-    
+
     // calculate lower case title only once, not for each entry again
     final String title = program.getTitle();
     final String lowerCaseTitle = title.toLowerCase();
@@ -174,10 +174,10 @@ public final class IDontWant2See extends Plugin implements AWTEventListener {
         return putCache(program, false);
       }
     }
-    
+
     return putCache(program, true);
   }
-  
+
   private boolean putCache(final Program program, final boolean matches) {
 		mMatchCache.put(program, matches);
 		return matches;
@@ -193,7 +193,7 @@ public final class IDontWant2See extends Plugin implements AWTEventListener {
                 "Removes all programs with an entered search text in the title from the program table."),
         "René Mach", "GPL");
   }
-  
+
   private int getSearchTextIndexForProgram(final Program program) {
     if (program != null) {
       // calculate lower case title only once, not for each entry again
@@ -205,15 +205,15 @@ public final class IDontWant2See extends Plugin implements AWTEventListener {
         }
       }
     }
-    
+
     return -1;
   }
-  
+
   public ActionMenu getButtonAction() {
     final ContextMenuAction baseAction = new ContextMenuAction(mLocalizer.msg(
         "name", "I don't want to see!"), createImageIcon("apps",
         "idontwant2see", 16));
-    
+
     final ContextMenuAction openExclusionList = new ContextMenuAction(
         mLocalizer.msg("editExclusionList", "Edit exclusion list"),
         createImageIcon("apps", "idontwant2see", 16));
@@ -221,22 +221,22 @@ public final class IDontWant2See extends Plugin implements AWTEventListener {
     openExclusionList.setActionListener(new ActionListener() {
       public void actionPerformed(final ActionEvent e) {
         final Window w = UiUtilities.getLastModalChildOf(getParentFrame());
-        
+
         JDialog temDlg = null;
-        
+
         if(w instanceof JDialog) {
           temDlg = new JDialog((JDialog)w,true);
         }
         else {
           temDlg = new JDialog((JFrame)w,true);
         }
-        
+
         final JDialog exclusionListDlg = temDlg;
         exclusionListDlg.setTitle(mLocalizer
             .msg("name", "I don't want to see!")
             + " - "
             + mLocalizer.msg("editExclusionList", "Edit exclusion list"));
-        
+
         UiUtilities.registerForClosing(new WindowClosingIf() {
           public void close() {
             exclusionListDlg.dispose();
@@ -246,9 +246,9 @@ public final class IDontWant2See extends Plugin implements AWTEventListener {
             return exclusionListDlg.getRootPane();
           }
         });
-        
+
         final ExclusionTablePanel exclusionPanel = new ExclusionTablePanel(mSettings);
-        
+
         final JButton ok = new JButton(Localizer
             .getLocalization(Localizer.I18N_OK));
         ok.addActionListener(new ActionListener() {
@@ -257,7 +257,7 @@ public final class IDontWant2See extends Plugin implements AWTEventListener {
             exclusionListDlg.dispose();
           }
         });
-        
+
         final JButton cancel = new JButton(Localizer
             .getLocalization(Localizer.I18N_CANCEL));
         cancel.addActionListener(new ActionListener() {
@@ -265,29 +265,29 @@ public final class IDontWant2See extends Plugin implements AWTEventListener {
             exclusionListDlg.dispose();
           }
         });
-        
+
         final FormLayout layout = new FormLayout(
             "0dlu:grow,default,3dlu,default",
             "fill:default:grow,2dlu,default,5dlu,default");
         layout.setColumnGroups(new int[][] {{2,4}});
-        
+
         final CellConstraints cc = new CellConstraints();
         final PanelBuilder pb = new PanelBuilder(layout,
             (JPanel) exclusionListDlg
             .getContentPane());
         pb.setDefaultDialogBorder();
-        
+
         pb.add(exclusionPanel, cc.xyw(1,1,4));
         pb.addSeparator("", cc.xyw(1,3,4));
         pb.add(ok, cc.xy(2,5));
         pb.add(cancel, cc.xy(4,5));
-        
+
         layoutWindow("exclusionListDlg", exclusionListDlg, new Dimension(600,
             450));
         exclusionListDlg.setVisible(true);
       }
     });
-    
+
     final ContextMenuAction undo = new ContextMenuAction(mLocalizer.msg(
         "undoLastExclusion", "Undo last exclusion"), createImageIcon("actions",
         "edit-undo", 16));
@@ -301,24 +301,24 @@ public final class IDontWant2See extends Plugin implements AWTEventListener {
               mSettings.getSearchList().remove(i);
             }
           }
-          
+
           mSettings.setLastEnteredExclusionString("");
-          
+
           updateFilter(true);
         }
       }
     });
-    
+
     return new ActionMenu(baseAction,new Action[] {openExclusionList,undo});
   }
-  
+
   public ActionMenu getContextMenuActions(final Program p) {
     if (p == null) {
       return null;
     }
     // check if this program is already hidden
     final int index = getSearchTextIndexForProgram(p);
-    
+
     // return menu to hide the program
     if (index == -1 || p.equals(getPluginManager().getExampleProgram())) {
       AbstractAction actionDontWant = getActionDontWantToSee(p);
@@ -330,7 +330,7 @@ public final class IDontWant2See extends Plugin implements AWTEventListener {
         }
         actionDontWant.putValue(Action.NAME,mLocalizer.msg("name","I don't want to see!"));
         actionDontWant.putValue(Action.SMALL_ICON,createImageIcon("apps","idontwant2see",16));
-        
+
         return new ActionMenu(actionDontWant);
       }
       else {
@@ -338,7 +338,7 @@ public final class IDontWant2See extends Plugin implements AWTEventListener {
         final ContextMenuAction baseAction = new ContextMenuAction(mLocalizer
             .msg("name", "I don't want to see!"),
             createImageIcon("apps","idontwant2see",16));
-        
+
         return new ActionMenu(baseAction, new Action[] {actionDontWant,actionInput});
       }
     }
@@ -354,7 +354,7 @@ public final class IDontWant2See extends Plugin implements AWTEventListener {
       public void actionPerformed(final ActionEvent e) {
         final int index = getSearchTextIndexForProgram(p);
         mSettings.getSearchList().remove(index);
-        
+
         updateFilter(!mSettings.isSwitchToMyFilter());
       }
     };
@@ -368,7 +368,7 @@ public final class IDontWant2See extends Plugin implements AWTEventListener {
             "caseSensitive",
             "case sensitive"));
         final JTextField input = new JTextField(p.getTitle());
-        
+
         input.addAncestorListener(new AncestorListener() {
           public void ancestorAdded(final AncestorEvent event) {
             event.getComponent().requestFocus();
@@ -422,10 +422,10 @@ public final class IDontWant2See extends Plugin implements AWTEventListener {
       }
     };
   }
-  
+
   void updateFilter(final boolean update) {
     clearCache();
-    
+
     SwingUtilities.invokeLater(new Runnable() {
       public void run() {
         if(!update) {
@@ -436,10 +436,10 @@ public final class IDontWant2See extends Plugin implements AWTEventListener {
         }
       }
     });
-    
+
     saveMe();
   }
-  
+
   public PluginsProgramFilter[] getAvailableFilter() {
     return new PluginsProgramFilter[] {getFilter()};
   }
@@ -458,28 +458,28 @@ public final class IDontWant2See extends Plugin implements AWTEventListener {
     }
     return mFilter;
   }
-  
+
   public void readData(final ObjectInputStream in) throws IOException,
       ClassNotFoundException {
     final int version = in.readInt(); // read Version
-    
+
     int n = in.readInt();
-    
+
     if(version <= 2) {
       for(int i = 0; i < n; i++) {
         final StringBuilder value = new StringBuilder("*");
         value.append(in.readUTF()).append("*");
-        
+
         mSettings.getSearchList().add(new IDontWant2SeeListEntry(value.toString(),false));
       }
-      
+
       if(version == 2) {
         n = in.readInt();
-        
+
         for(int i = 0; i < n; i++) {
           mSettings.getSearchList().add(new IDontWant2SeeListEntry(in.readUTF(),true));
         }
-        
+
         mSettings.setSimpleMenu(false);
       }
     }
@@ -487,9 +487,9 @@ public final class IDontWant2See extends Plugin implements AWTEventListener {
       for(int i = 0; i < n; i++) {
         mSettings.getSearchList().add(new IDontWant2SeeListEntry(in, version));
       }
-      
+
       mSettings.setSimpleMenu(in.readBoolean());
-      
+
       if(version >= 4) {
         mSettings.setSwitchToMyFilter(in.readBoolean());
       }
@@ -509,7 +509,7 @@ public final class IDontWant2See extends Plugin implements AWTEventListener {
       }
     }
   }
-  
+
   public void writeData(final ObjectOutputStream out) throws IOException {
     // sort the list so that often used entries are in the front (for faster lookup)
     Collections.sort(mSettings.getSearchList(), new Comparator<IDontWant2SeeListEntry>() {
@@ -518,37 +518,37 @@ public final class IDontWant2See extends Plugin implements AWTEventListener {
         return o2.getLastMatchedDate().compareTo(o1.getLastMatchedDate());
       }
     });
-    
+
     out.writeInt(7); //version
     out.writeInt(mSettings.getSearchList().size());
-    
+
     for(IDontWant2SeeListEntry entry : mSettings.getSearchList()) {
       entry.writeData(out);
     }
-    
+
     out.writeBoolean(mSettings.isSimpleMenu());
     out.writeBoolean(mSettings.isSwitchToMyFilter());
-    
+
     out.writeUTF(mSettings.getLastEnteredExclusionString());
-    
+
     mSettings.getLastUsedDate().writeData(out);
-    
+
     out.writeByte(mSettings.getProgramImportance());
   }
-  
+
   public SettingsTab getSettingsTab() {
     return new IDontWant2SeeSettingsTab(mSettings);
   }
-  
+
   protected static Date getCurrentDate() {
     return mCurrentDate;
   }
-  
+
   @SuppressWarnings("unchecked")
   public Class<? extends PluginsFilterComponent>[] getAvailableFilterComponentClasses() {
     return (Class<? extends PluginsFilterComponent>[]) new Class[] {IDontWant2SeeFilterComponent.class};
   }
-  
+
   @Override
   public boolean canReceiveProgramsWithTarget() {
     return true;
@@ -579,7 +579,7 @@ public final class IDontWant2See extends Plugin implements AWTEventListener {
     }
     return false;
   }
-  
+
   @Override
   public void onActivation() {
     mFilter = new PluginsProgramFilter(this) {
@@ -591,7 +591,7 @@ public final class IDontWant2See extends Plugin implements AWTEventListener {
         return acceptInternal(prog);
       }
     };
-    
+
     Toolkit.getDefaultToolkit().addAWTEventListener(this,
         AWTEvent.KEY_EVENT_MASK);
   }
