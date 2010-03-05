e0d6e65cb9b5444c4ece0be8d0dd0d960cadcfc7
bananeweizen <bananeweizen@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2010-03-05 09:04:25 +0000

    remove height based API from PanelBuilder again, use only standard PanelBuilder API in markings settings tab

--- trunk/tvbrowser/src/util/ui/EnhancedPanelBuilder.java
+++ trunk/tvbrowser/src/util/ui/EnhancedPanelBuilder.java
@@ -1,16 +1,16 @@
 /*
  * Copyright Michael Keppler
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -24,26 +24,42 @@ import com.jgoodies.forms.factories.FormFactory;
 import com.jgoodies.forms.layout.FormLayout;
 
 /**
- * panel builder with additional methods for typical TV-Browser UI (e.g. settings tab)
+ * Panel builder with additional methods for typical TV-Browser UI (e.g. settings tab).<br>
+ * When using this class, you should normally use {@link #addParagraph(String)} to add a section to your settings tab
+ * and afterwards use {@link #addRow()} to add standard controls into the section. For lists and other large controls
+ * you may also use {@link #addGrowingRow()} instead.
+ * <br>Don't create rows with height settings yourself!
+ *
  * @author bananeweizen
  * @since 3.0
  *
  */
 public class EnhancedPanelBuilder extends PanelBuilder {
-  public EnhancedPanelBuilder(FormLayout layout, JPanel panel) {
-    super(layout,panel);
+  public EnhancedPanelBuilder(final FormLayout layout, final JPanel parentPanel) {
+    super(layout,parentPanel);
   }
-  
-  public EnhancedPanelBuilder(FormLayout layout) {
+
+  public EnhancedPanelBuilder(final FormLayout layout) {
     super(layout);
   }
 
+  /**
+   * Create a new panel builder with the given columns.
+   * You can add rows afterwards by using {@link #addParagraph(String)}, {@link #addRow()} and {@link #addGrowingRow()}.
+   * @param encodedColumnSpecs
+   */
   public EnhancedPanelBuilder(final String encodedColumnSpecs) {
     super(new FormLayout(encodedColumnSpecs,""));
   }
 
-  public EnhancedPanelBuilder(final String encodedColumnSpecs, JPanel panel) {
-    super(new FormLayout(encodedColumnSpecs,""), panel);
+  /**
+   * Create a new panel builder with the given columns, which sits on the given panel.
+   * You can add rows afterwards by using {@link #addParagraph(String)}, {@link #addRow()} and {@link #addGrowingRow()}.
+   * @param encodedColumnSpecs
+   * @param parentPanel the finally built panel will be a child of this parent panel
+   */
+  public EnhancedPanelBuilder(final String encodedColumnSpecs, final JPanel parentPanel) {
+    super(new FormLayout(encodedColumnSpecs,""), parentPanel);
   }
 
   /**
@@ -67,10 +83,10 @@ public class EnhancedPanelBuilder extends PanelBuilder {
   }
 
   /**
-   * add a new layout row to the builders layout<br>
+   * Add a new standard layout row to the builders layout.
    * It is separated from the preceding row with a LINE_GAP. Use {@link #getRow()} to address this line
    * afterwards.
-   * 
+   *
    * @return the builder
    */
   public PanelBuilder addRow() {
@@ -88,7 +104,7 @@ public class EnhancedPanelBuilder extends PanelBuilder {
   }
 
   /**
-   * add a new growing layout row to the builders layout<br>
+   * Add a new growing layout row to the builders layout.
    * It is separated from the preceding row by LINE_GAP and will grow to take the available space.
    * @return the builder
    */
@@ -97,26 +113,16 @@ public class EnhancedPanelBuilder extends PanelBuilder {
   }
 
   /**
-   * add a new layout row with the given height to the builders layout<br>
-   * It is separated from the preceding row with a LINE_GAP. Use {@link #getRow()} to address this line
-   * afterwards.
+   * Add a new layout row with the given height to the builders layout.
+   * It is separated from the preceding row with a LINE_GAP.
+   * Use {@link #getRow()} to address this line afterwards.<br>
+   * This method should normally not be used! Use {@link #addRow()} or {@link #addGrowingRow()} instead.
+   * The necessary sizes for rows will be calculated by the PanelBuilder.
    * @param rowHeightCode row height
    * @return the builder
    */
   public PanelBuilder addRow(final String rowHeightCode) {
     appendRow(FormFactory.LINE_GAP_ROWSPEC);
-    appendRow(rowHeightCode);
-    incrementRowNumber();
-    return this;
-  }
-  
-  /**
-   * add a new layout row with the given height to the builders layout<br>
-   * Use {@link #getRowCount()} to address this line
-   * @param rowHeightCode row height
-   * @return the builder
-   */
-  public PanelBuilder addSingleRow(final String rowHeightCode) {
     appendRow(rowHeightCode);
     incrementRowNumber();
     return this;
