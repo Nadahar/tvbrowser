965d7da9c9ba3f40e63d66def1d3acc3a464c636
troggan <troggan@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2010-03-14 08:54:45 +0000

    [TVB-865] fix createDDMMYYYY , add Test

---
+++ trunk/tvbrowser/test/src/devplugin/DateTest.java
@@ -0,0 +1,65 @@
+package devplugin;
+
+import junit.framework.TestCase;
+
+public class DateTest extends TestCase {
+
+  public void testCreateDDMMYYYY() {
+    Date d = Date.createDDMMYYYY("12.12.2009", "\\.");
+    assertEquals(d.getDayOfMonth(), 12);
+    assertEquals(d.getMonth(), 12);
+    assertEquals(d.getYear(), 2009);
+
+    d = Date.createDDMMYYYY("12.12", "\\.");
+    assertNull(d);
+
+    d = Date.createDDMMYYYY("12.1222.2009", "\\.");
+    assertNull(d);
+
+    d = Date.createDDMMYYYY("12.12.09", "\\.");
+    assertEquals(d.getDayOfMonth(), 12);
+    assertEquals(d.getMonth(), 12);
+    assertEquals(d.getYear(), 9);
+
+    d = Date.createDDMMYYYY(null, "\\.");
+    assertNull(d);
+  }
+
+  public void testCreateYYMMDD() {
+    Date d = Date.createYYMMDD("2009.12.12", "\\.");
+    assertNull(d);
+
+    d = Date.createYYMMDD("12.12", "\\.");
+    assertNull(d);
+
+    d = Date.createYYMMDD("09.12.12", "\\.");
+    assertEquals(d.getDayOfMonth(), 12);
+    assertEquals(d.getMonth(), 12);
+    assertEquals(d.getYear(), 2009);
+
+    d = Date.createYYMMDD(null, "\\.");
+    assertNull(d);
+  }
+
+  public void testCreateYYYYMMDD() {
+    Date d = Date.createYYYYMMDD("2009.12.12", "\\.");
+    assertEquals(d.getDayOfMonth(), 12);
+    assertEquals(d.getMonth(), 12);
+    assertEquals(d.getYear(), 2009);
+
+    d = Date.createYYYYMMDD("12.12", "\\.");
+    assertNull(d);
+
+    d = Date.createYYYYMMDD("09.12.12", "\\.");
+    assertEquals(d.getDayOfMonth(), 12);
+    assertEquals(d.getMonth(), 12);
+    assertEquals(d.getYear(), 9);
+
+    d = Date.createDDMMYYYY("2009.12.1222", "\\.");
+    assertNull(d);
+
+    d = Date.createYYYYMMDD(null, "\\.");
+    assertNull(d);
+  }
+
+}
