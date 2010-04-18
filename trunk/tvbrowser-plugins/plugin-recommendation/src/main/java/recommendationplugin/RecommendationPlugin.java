503ff4a31b57f8063e48348b0ace46d55f1e51c9
bananeweizen <bananeweizen@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2010-04-18 06:51:54 +0000

    TVBP-184: ConcurrentModificationException in Recommendations

--- trunk/tvbrowser-plugins/plugin-recommendation/src/main/java/recommendationplugin/RecommendationPlugin.java
+++ trunk/tvbrowser-plugins/plugin-recommendation/src/main/java/recommendationplugin/RecommendationPlugin.java
@@ -1,16 +1,16 @@
 /*
  * Copyright Michael Keppler
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -101,7 +101,7 @@ public final class RecommendationPlugin extends Plugin {
         }
       }
     }
-    
+
     initializeWeighting(new FavoriteWeighting());
     initializeWeighting(new ReminderWeighting());
   }
@@ -159,7 +159,7 @@ public final class RecommendationPlugin extends Plugin {
     UiUtilities.centerAndShow(dialog);
   }
 
-  void updateRecommendations() {
+  synchronized void updateRecommendations() {
     mRootNode.clear();
     mRecommendations = new ArrayList<ProgramWeight>();
 
