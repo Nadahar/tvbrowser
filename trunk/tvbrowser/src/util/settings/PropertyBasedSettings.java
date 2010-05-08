88ca8b97bc6fd5e6cfb8fe7edcc8470dfa3a6939
bananeweizen <bananeweizen@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2010-05-08 10:28:54 +0000

    additional string array methods in settings

--- trunk/tvbrowser/src/util/settings/PropertyBasedSettings.java
+++ trunk/tvbrowser/src/util/settings/PropertyBasedSettings.java
@@ -1,16 +1,16 @@
 /*
  * Copyright Michael Keppler
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -18,12 +18,15 @@ package util.settings;
 
 import java.util.Properties;
 
+import org.apache.commons.lang.StringUtils;
+
 /**
  * @author bananeweizen
- * 
+ *
  */
 public class PropertyBasedSettings {
 
+  private static final String ARRAY_SEPARATOR = "|||";
   private Properties mProperties;
 
   public PropertyBasedSettings(final Properties properties) {
@@ -38,35 +41,51 @@ public class PropertyBasedSettings {
     return mProperties;
   }
 
-  protected String get(final String key) {
-    return get(key, null);
+  private void internalSet(final String key, final String value) {
+    mProperties.setProperty(key, value);
   }
 
-  protected void set(final String key, final String value) {
-    mProperties.setProperty(key, value);
+  private String internalGet(final String key, final String defaultValue) {
+    return mProperties.getProperty(key, defaultValue);
   }
 
   protected void remove(final String key) {
     mProperties.remove(key);
   }
 
+  protected String get(final String key) {
+    return internalGet(key, "");
+  }
+
   protected String get(final String key, final String defaultValue) {
-    return mProperties.getProperty(key, defaultValue);
+    return internalGet(key, defaultValue);
+  }
+
+  protected void set(final String key, final String value) {
+    internalSet(key, value);
   }
 
   protected boolean get(final String key, final boolean defaultValue) {
-    return Boolean.parseBoolean(get(key, String.valueOf(defaultValue)));
+    return Boolean.parseBoolean(internalGet(key, String.valueOf(defaultValue)));
   }
 
   protected void set(final String key, final boolean value) {
-    set(key, String.valueOf(value));
+    internalSet(key, String.valueOf(value));
   }
 
   protected int get(final String key, final int defaultValue) {
-    return Integer.valueOf(get(key, String.valueOf(defaultValue)));
+    return Integer.valueOf(internalGet(key, String.valueOf(defaultValue)));
   }
 
-  protected void set(final String key, final int defaultValue) {
-    set(key, String.valueOf(defaultValue));
+  protected void set(final String key, final int value) {
+    internalSet(key, String.valueOf(value));
+  }
+
+  protected void set(final String key, final String[] value) {
+    internalSet(key, StringUtils.join(value, ARRAY_SEPARATOR));
+  }
+
+  protected String[] get(final String key, final String[] defaultValue) {
+    return StringUtils.split(internalGet(key, StringUtils.join(defaultValue, ARRAY_SEPARATOR)), ARRAY_SEPARATOR);
   }
 }
