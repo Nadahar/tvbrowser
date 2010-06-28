25b9099eef9c48c2c5c5c77b1e38c0907cd865a0
bananeweizen <bananeweizen@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2010-06-28 17:04:35 +0000

    TVBP-191 TVBP-192, have automatic updates of recommendations, explain them in their tree label

--- tvbrowser-plugins/plugin-recommendation/src/main/java/recommendationplugin/RecommendationNode.java
+++ tvbrowser-plugins/plugin-recommendation/src/main/java/recommendationplugin/RecommendationNode.java
@@ -1,16 +1,16 @@
 /*
  * Copyright Michael Keppler
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -29,7 +29,7 @@ public class RecommendationNode extends PluginTreeNode implements Comparable<Rec
   }
 
   private static String getLabel(final ProgramWeight programWeight) {
-    return programWeight.getProgram().getTitle();
+    return programWeight.getProgram().getTitle() + " (" + 100 * programWeight.getWeight() / ProgramWeight.getMaxWeight() + "%)";
   }
 
   public int compareTo(final RecommendationNode other) {
