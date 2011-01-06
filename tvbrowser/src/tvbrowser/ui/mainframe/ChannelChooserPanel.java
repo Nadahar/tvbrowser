07738fcd5aaae23646be9083b413c3175a8b3bc7
bananeweizen <bananeweizen@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2011-01-06 11:49:59 +0000

    TVB-1000: drag'n drop of channels leads to channel group no longer visible (all channels enabled)

--- tvbrowser/src/tvbrowser/ui/mainframe/ChannelChooserPanel.java
+++ tvbrowser/src/tvbrowser/ui/mainframe/ChannelChooserPanel.java
@@ -63,6 +63,7 @@ public class ChannelChooserPanel extends JPanel implements ListDropAction {
   private JList mList;
   private MainFrame mParent;
   private boolean disableSync = false;
+  private ChannelFilterComponent mChannelFilter;
 
   /**
    * @param frame
@@ -80,7 +81,7 @@ public class ChannelChooserPanel extends JPanel implements ListDropAction {
     ListDragAndDropHandler dnDHandler = new ListDragAndDropHandler(mList,
         mList, this);
     new DragAndDropMouseListener(mList, mList, this, dnDHandler);
-    
+
     mList.addListSelectionListener(new ListSelectionListener() {
       public void valueChanged(ListSelectionEvent e) {
         if (!disableSync) {
@@ -89,7 +90,7 @@ public class ChannelChooserPanel extends JPanel implements ListDropAction {
         disableSync  = false;
       }
     });
-    
+
     mList.addKeyListener(new KeyAdapter() {
       public void keyPressed(KeyEvent e) {
         if(e.getKeyCode() == KeyEvent.VK_SPACE) {
@@ -97,25 +98,25 @@ public class ChannelChooserPanel extends JPanel implements ListDropAction {
         }
       }
     });
-    
+
     mList.addMouseListener(new MouseAdapter() {
       public void mouseClicked(MouseEvent e) {
         if(SwingUtilities.isLeftMouseButton(e)) {
           showChannel();
         }
       }
-      
+
       public void mousePressed(MouseEvent e) {
         if(SwingUtilities.isRightMouseButton(e)) {
           mList.setSelectedIndex(mList.locationToIndex(e.getPoint()));
         }
         showPopupMenu(e);
       }
-      
+
       public void mouseReleased(MouseEvent e) {
         showPopupMenu(e);      }
     });
-    
+
     mList.addMouseWheelListener(new MouseWheelListener() {
       public void mouseWheelMoved(MouseWheelEvent e) {
         int selected = mList.getSelectedIndex() + e.getWheelRotation();
@@ -130,20 +131,20 @@ public class ChannelChooserPanel extends JPanel implements ListDropAction {
       }
     });
   }
-  
+
   private void showChannel() {
     Channel selectedChannel = (Channel) mList.getSelectedValue();
     if (selectedChannel != null) {
       mParent.showChannel(selectedChannel);
     }
   }
-  
+
   private void showPopupMenu(MouseEvent e) {
     if(e.isPopupTrigger()) {
       new ChannelContextMenu(e,(Channel)mList.getModel().getElementAt(mList.locationToIndex(e.getPoint())),this);
     }
   }
-  
+
   public void updateChannelChooser() {
     mList.setCellRenderer(new ChannelListCellRenderer(Settings.propShowChannelIconsInChannellist.getBoolean(),
         Settings.propShowChannelNamesInChannellist.getBoolean()));
@@ -167,17 +168,18 @@ public class ChannelChooserPanel extends JPanel implements ListDropAction {
 
     ChannelList.setSubscribeChannels(channelArr);
     Settings.propSubscribedChannels.setChannelArray(channelArr);
-    
+
     if (!Settings.propTrayUseSpecialChannels.getBoolean()) {
       Channel[] tempArr = new Channel[channelArr.length > 10 ? 10 : channelArr.length];
       System.arraycopy(channelArr, 0, tempArr, 0, tempArr.length);
       Settings.propTraySpecialChannels.setChannelArray(tempArr);
     }
-    
+
     ChannelList.reload();
     DefaultProgramTableModel model = MainFrame.getInstance().getProgramTableModel();
     model.setChannels(ChannelList.getSubscribedChannels());
     MainFrame.getInstance().updateChannellist();
+    setChannelGroup(mChannelFilter);
   }
 
   public void selectChannel(Channel channel) {
@@ -187,6 +189,7 @@ public class ChannelChooserPanel extends JPanel implements ListDropAction {
   }
 
   public void setChannelGroup(ChannelFilterComponent channelFilter) {
+    mChannelFilter = channelFilter;
     Channel[] channels = null;
     if (channelFilter != null) {
       channels = channelFilter.getChannels();
