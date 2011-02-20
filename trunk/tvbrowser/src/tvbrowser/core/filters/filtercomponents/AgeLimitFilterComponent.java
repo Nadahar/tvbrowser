0f228d5824412977499e384163d0e48c44d2d6c1
bananeweizen <bananeweizen@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2011-02-20 19:03:37 +0000

    TVB-1013: Date filter component not editable

--- trunk/tvbrowser/src/tvbrowser/core/filters/filtercomponents/AgeLimitFilterComponent.java
+++ trunk/tvbrowser/src/tvbrowser/core/filters/filtercomponents/AgeLimitFilterComponent.java
@@ -1,16 +1,16 @@
 /*
  * Copyright Michael Keppler
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -42,7 +42,7 @@ public class AgeLimitFilterComponent extends AbstractFilterComponent {
     this("", "");
   }
 
-  private int mRequiredAge;
+  private int mRequiredAge = 14;
   private JSpinner mAgeSpinner;
 
   public boolean accept(final Program program) {
@@ -54,7 +54,7 @@ public class AgeLimitFilterComponent extends AbstractFilterComponent {
     JPanel content = new JPanel(new BorderLayout());
 
     mAgeSpinner = new JSpinner(new SpinnerNumberModel(16, 6, 21, 1));
-    mAgeSpinner.setValue(14);
+    mAgeSpinner.setValue(mRequiredAge);
     content.add(UiUtilities.createHelpTextArea(mLocalizer
         .msg("description", "")), BorderLayout.NORTH);
     content.add(mAgeSpinner, BorderLayout.SOUTH);
