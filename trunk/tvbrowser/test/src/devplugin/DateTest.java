f301fcba2675fe691ee4a307cb410ce1536bf728
bananeweizen <bananeweizen@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2011-02-26 09:27:42 +0000

    wrong usage of pattern quoting in date test remove numberformatexception during unit test

--- trunk/tvbrowser/test/src/devplugin/DateTest.java
+++ trunk/tvbrowser/test/src/devplugin/DateTest.java
@@ -5,60 +5,60 @@ import junit.framework.TestCase;
 public class DateTest extends TestCase {
 
   public void testCreateDDMMYYYY() {
-    Date d = Date.createDDMMYYYY("12.12.2009", "\\.");
+    Date d = Date.createDDMMYYYY("12.12.2009", ".");
     assertEquals(d.getDayOfMonth(), 12);
     assertEquals(d.getMonth(), 12);
     assertEquals(d.getYear(), 2009);
 
-    d = Date.createDDMMYYYY("12.12", "\\.");
+    d = Date.createDDMMYYYY("12.12", ".");
     assertNull(d);
 
-    d = Date.createDDMMYYYY("12.1222.2009", "\\.");
+    d = Date.createDDMMYYYY("12.1222.2009", ".");
     assertNull(d);
 
-    d = Date.createDDMMYYYY("12.12.09", "\\.");
+    d = Date.createDDMMYYYY("12.12.09", ".");
     assertEquals(d.getDayOfMonth(), 12);
     assertEquals(d.getMonth(), 12);
     assertEquals(d.getYear(), 9);
 
-    d = Date.createDDMMYYYY(null, "\\.");
+    d = Date.createDDMMYYYY(null, ".");
     assertNull(d);
   }
 
   public void testCreateYYMMDD() {
-    Date d = Date.createYYMMDD("2009.12.12", "\\.");
+    Date d = Date.createYYMMDD("2009.12.12", ".");
     assertNull(d);
 
-    d = Date.createYYMMDD("12.12", "\\.");
+    d = Date.createYYMMDD("12.12", ".");
     assertNull(d);
 
-    d = Date.createYYMMDD("09.12.12", "\\.");
+    d = Date.createYYMMDD("09.12.12", ".");
     assertEquals(d.getDayOfMonth(), 12);
     assertEquals(d.getMonth(), 12);
     assertEquals(d.getYear(), 2009);
 
-    d = Date.createYYMMDD(null, "\\.");
+    d = Date.createYYMMDD(null, ".");
     assertNull(d);
   }
 
   public void testCreateYYYYMMDD() {
-    Date d = Date.createYYYYMMDD("2009.12.12", "\\.");
+    Date d = Date.createYYYYMMDD("2009.12.12", ".");
     assertEquals(d.getDayOfMonth(), 12);
     assertEquals(d.getMonth(), 12);
     assertEquals(d.getYear(), 2009);
 
-    d = Date.createYYYYMMDD("12.12", "\\.");
+    d = Date.createYYYYMMDD("12.12", ".");
     assertNull(d);
 
-    d = Date.createYYYYMMDD("09.12.12", "\\.");
+    d = Date.createYYYYMMDD("09.12.12", ".");
     assertEquals(d.getDayOfMonth(), 12);
     assertEquals(d.getMonth(), 12);
     assertEquals(d.getYear(), 9);
 
-    d = Date.createDDMMYYYY("2009.12.1222", "\\.");
+    d = Date.createDDMMYYYY("2009.12.1222", ".");
     assertNull(d);
 
-    d = Date.createYYYYMMDD(null, "\\.");
+    d = Date.createYYYYMMDD(null, ".");
     assertNull(d);
   }
 
