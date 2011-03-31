cd5d10d3703fb699e7b37b4d43efba695d93effc
bananeweizen <bananeweizen@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2011-03-31 16:41:52 +0000

    new version with ZDF neo

--- trunk/tvbrowser-plugins/plugin-teletext/src/main/java/teletextplugin/TeleTextPlugin.java
+++ trunk/tvbrowser-plugins/plugin-teletext/src/main/java/teletextplugin/TeleTextPlugin.java
@@ -37,7 +37,7 @@ import devplugin.Version;
  *
  */
 public final class TeleTextPlugin extends Plugin {
-  private static final Version mVersion = new Version(2, 70, 14);
+  private static final Version mVersion = new Version(2, 70, 15);
 
   /**
    * created lazily on first access
