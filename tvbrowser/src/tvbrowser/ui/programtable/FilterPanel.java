51f819b014b7fa495dfaf6ded9f4c1d74054a1c7
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2011-09-18 10:26:21 +0000

    [TVB-1075] Make filter panel transparent.

--- tvbrowser/src/tvbrowser/ui/programtable/FilterPanel.java
+++ tvbrowser/src/tvbrowser/ui/programtable/FilterPanel.java
@@ -2,6 +2,8 @@ package tvbrowser.ui.programtable;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -17,6 +19,7 @@ import javax.swing.UIManager;
 
 import tvbrowser.core.filters.FilterManagerImpl;
 import tvbrowser.ui.mainframe.MainFrame;
+import util.ui.persona.Persona;
 import devplugin.ProgramFilter;
 
 /**
@@ -46,8 +49,46 @@ public class FilterPanel extends JPanel {
     setLayout(new BorderLayout());
     setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
     setBackground(Color.WHITE);
+    setOpaque(false);
 
-    mFilterLabel = new JLabel();
+    mFilterLabel = new JLabel() {
+      protected void paintComponent(Graphics g) {
+        if(Persona.getInstance().getHeaderImage() != null) {
+
+          String info = mLocalizer.msg("filterActive", "Active Filter:") + ": ";
+          
+          Font boldFont = getFont().deriveFont(Font.BOLD);
+          
+          FontMetrics metrics = g.getFontMetrics(boldFont);
+          int textWidth = metrics.stringWidth(info);
+          int baseLine = getBaseline(getWidth(),getHeight());
+        
+      
+        if(!Persona.getInstance().getShadowColor().equals(Persona.getInstance().getTextColor())) {
+          g.setColor(Persona.getInstance().getShadowColor());
+          
+          g.setFont(boldFont);
+          g.drawString(info,getIconTextGap()+getInsets().left+1,baseLine+1);
+          g.drawString(info,getIconTextGap()+getInsets().left+2,baseLine+2);
+          
+          g.setFont(getFont());
+          g.drawString(mCurrentName,getIconTextGap()+getInsets().left+1+textWidth,baseLine+1);
+          g.drawString(mCurrentName,getIconTextGap()+getInsets().left+2+textWidth,baseLine+2);
+        }
+        
+        g.setColor(Persona.getInstance().getTextColor());
+        
+        g.setFont(boldFont);
+        g.drawString(info,getIconTextGap()+getInsets().left,baseLine);
+        
+        g.setFont(getFont());
+        g.drawString(mCurrentName,getIconTextGap()+getInsets().left+textWidth,baseLine);
+      }
+      else {
+        super.paintComponent(g);
+      }
+      }
+    };
     mFilterLabel.addKeyListener(keyListener);
     
     mFilterLabel.setHorizontalAlignment(SwingConstants.LEFT);
@@ -97,9 +138,62 @@ public class FilterPanel extends JPanel {
     int width = getWidth();
     int height = getHeight();
 
+    Color c2 = Persona.getInstance().getTextColor() != null ? Persona.getInstance().getTextColor() : UIManager.getColor("List.selectionBackground");
+    Color c = Persona.getInstance().getAccentColor() != null ? Persona.getInstance().getAccentColor() : getBackground();
+    
+    if(Persona.getInstance().getHeaderImage() != null) {
+      double test = (0.2126 * Persona.getInstance().getTextColor().getRed()) + (0.7152 * Persona.getInstance().getTextColor().getGreen()) + (0.0722 * Persona.getInstance().getTextColor().getBlue());
+      int alpha = 100;
+      
+      if(test <= 30) {
+        c = Color.white;
+        alpha = 200;
+      }
+      else if(test <= 40) {
+        c = c.brighter().brighter().brighter().brighter().brighter().brighter();
+        alpha = 200;
+      }
+      else if(test <= 60) {
+        c = c.brighter().brighter().brighter();
+        alpha = 160;
+      }
+      else if(test <= 100) {
+        c = c.brighter().brighter();
+        alpha = 140;
+      }
+      else if(test <= 145) {
+        alpha = 120;
+      }
+      else if(test <= 170) {
+        c = c.darker();
+        alpha = 120;
+      }
+      else if(test <= 205) {
+        c = c.darker().darker();
+        alpha = 120;
+      }
+      else if(test <= 220){
+        c = c.darker().darker().darker();
+        alpha = 100;
+      }
+      else if(test <= 235){
+        c = c.darker().darker().darker().darker();
+        alpha = 100;
+      }
+      else {
+        c = Color.black;
+        alpha = 100;
+      }
+      
+      
+      
+      c = new Color(c.getRed(),c.getGreen(),c.getBlue(),alpha);
+      c2 = new Color(c2.getRed(),c2.getGreen(),c2.getBlue(),200);
+    }
+    
     // Create the gradient paint
     GradientPaint paint =
-        new GradientPaint((float)width / 3, 0, getBackground(), width, height, UIManager.getColor("List.selectionBackground"), false);
+        new GradientPaint((float)width / 3, 0, c, width, height, c2, false);
 
     g2d.setPaint(paint);
     g2d.fillRect(0, 0, width, height);
