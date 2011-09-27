00d7be74c3ff12bcb78096eb465a11f4fe90be5e
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2011-09-27 14:03:28 +0000

    [TVB-1065] Save setting for closing with X too

--- tvbrowser/src/newsplugin/NewsDialog.java
+++ tvbrowser/src/newsplugin/NewsDialog.java
@@ -31,6 +31,8 @@ import java.awt.FlowLayout;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.net.URL;
 import java.text.DateFormat;
 import java.util.ArrayList;
@@ -102,6 +104,12 @@ public class NewsDialog implements WindowClosingIf {
    */
   public NewsDialog(Window parent, ArrayList<News> newsList, int newNewsCount) {
     mDialog = UiUtilities.createDialog(parent, false);
+    mDialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+    mDialog.addWindowListener(new WindowAdapter() {
+      public void windowClosing(WindowEvent e) {
+        close();
+      }
+    });
     mNewsList = newsList;
     mNewNewsCount = newNewsCount;
 
