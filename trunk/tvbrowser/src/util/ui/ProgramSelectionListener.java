679f17e82ea279854255186fd39cef56dc93be16
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2012-08-06 15:51:28 +0000

    [TVB-1131] Created new components for program selection.

---
+++ trunk/tvbrowser/src/util/ui/ProgramSelectionListener.java
@@ -0,0 +1,42 @@
+/*
+ * TV-Browser
+ * Copyright (C) 2012 TV-Browser team (dev@tvbrowser.org)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * CVS information:
+ *     $Date$
+ *   $Author$
+ * $Revision$
+ */
+package util.ui;
+
+import devplugin.Program;
+
+/**
+ * A listener that informs about
+ * selection events of Programs.
+ * 
+ * @author René Mach
+ * @since 3.2
+ */
+public interface ProgramSelectionListener {
+  /**
+   * Informs about a selection of a program.
+   * <p>
+   * @param prog The selected program.
+   */
+  public void programSeleted(Program prog);
+}
