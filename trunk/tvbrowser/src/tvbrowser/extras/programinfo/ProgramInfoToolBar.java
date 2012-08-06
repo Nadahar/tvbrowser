8d59a0553a06abb9ce513d9a3b7888aa221b0b6e
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2012-08-06 20:24:12 +0000

    [TVB-1131] Use new tool bar for program navigation

--- trunk/tvbrowser/src/tvbrowser/extras/programinfo/ProgramInfoToolBar.java
+++ trunk/tvbrowser/src/tvbrowser/extras/programinfo/ProgramInfoToolBar.java
@@ -1,3 +1,26 @@
+/*
+ * TV-Browser
+ * Copyright (C) 2012 TV-Browser team (dev@tvbrowser.org)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * CVS information:
+ *     $Date$
+ *   $Author$
+ * $Revision$
+ */
 package tvbrowser.extras.programinfo;
 
 import java.awt.Color;
@@ -5,16 +28,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
-import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 
-import com.jgoodies.forms.factories.Borders;
 import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
 
@@ -24,6 +44,11 @@ import util.ui.ProgramSelectionButton;
 import util.ui.ProgramSelectionListener;
 import util.ui.TVBrowserIcons;
 
+/**
+ * A tool bar panel for the program info dialog.
+ * 
+ * @author René Mach
+ */
 public class ProgramInfoToolBar extends JPanel implements ProgramSelectionListener {
   private JButton mHistPrevious;
   private JButton mHistNext;
