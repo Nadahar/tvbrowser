1338d48ac7d5308e460ab464c6932019e4020520
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2012-09-14 10:28:11 +0000

    Plugin is stable.

--- tvbrowser-plugins/plugin-simplemarker/src/main/java/simplemarkerplugin/SimpleMarkerPlugin.java
+++ tvbrowser-plugins/plugin-simplemarker/src/main/java/simplemarkerplugin/SimpleMarkerPlugin.java
@@ -78,7 +78,7 @@ import devplugin.Version;
  * @author René Mach
  */
 public class SimpleMarkerPlugin extends Plugin {
-  private static final Version mVersion = new Version(3,20,false);
+  private static final Version mVersion = new Version(3,20,true);
 
   /** The localizer for this class. */
   private static final util.ui.Localizer mLocalizer = util.ui.Localizer.getLocalizerFor(SimpleMarkerPlugin.class);
