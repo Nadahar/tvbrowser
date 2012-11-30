97df0b9f38291b0d4755c5118f4bab41e96a6a4b
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2012-11-30 23:22:05 +0000

    Removed unneeded imports.

--- tvbrowser/src/util/ui/LocalizerClassloader.java
+++ tvbrowser/src/util/ui/LocalizerClassloader.java
@@ -29,8 +29,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.util.Enumeration;
-import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
