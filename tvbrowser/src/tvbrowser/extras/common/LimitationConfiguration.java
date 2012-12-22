43cfbec4ab909a26419fe4bce8992814337b2c4e
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2012-12-22 09:54:22 +0000

    [TVB-1169] Missing channel in limitation is not update when channel is available again

--- tvbrowser/src/tvbrowser/extras/common/LimitationConfiguration.java
+++ tvbrowser/src/tvbrowser/extras/common/LimitationConfiguration.java
@@ -169,7 +169,7 @@ public class LimitationConfiguration {
    * Tries to load the channels again.
    */
   public void reValidateChannels() {
-    ArrayList<Channel> channelList = new ArrayList<>(mChannelArr.length);
+    ArrayList<Channel> channelList = new ArrayList<Channel>(mChannelArr.length);
     
     for(ChannelItem item : mChannelItemList) {
       item.reValidate();
