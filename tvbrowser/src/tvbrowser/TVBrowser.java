6685a670dc1d855166c44bb9040585bd4f948bae
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2012-12-23 08:54:52 +0000

    Changed version to 3.3beta1.

--- tvbrowser/src/tvbrowser/TVBrowser.java
+++ tvbrowser/src/tvbrowser/TVBrowser.java
@@ -84,7 +84,6 @@ import tvbrowser.core.tvdataservice.TvDataServiceProxy;
 import tvbrowser.core.tvdataservice.TvDataServiceProxyManager;
 import tvbrowser.extras.common.InternalPluginProxyIf;
 import tvbrowser.extras.common.InternalPluginProxyList;
-import tvbrowser.extras.favoritesplugin.FavoritesPlugin;
 import tvbrowser.extras.programinfo.ProgramInfo;
 import tvbrowser.extras.reminderplugin.ReminderPlugin;
 import tvbrowser.extras.searchplugin.SearchPlugin;
@@ -139,14 +138,15 @@ public class TVBrowser {
 
   private static final boolean mIsStable = false;
   private static final int mMajorVersion = 3;
-  private static final int mMinorVersion = 21;
-  private static final int mSubMinorVersion = 51;
+  private static final int mMinorVersion = 29;
+  private static final int mSubMinorVersion = 95;
 
   /* If you want to change the version string, add it to the beginning of this array.
      We need the old version strings to import the settings.
   */
   /** The string array with the names of the earlier versions. */
   private static final String[] ALL_VERSIONS = new String[]{
+          "3.3beta1",
           "3.2.1.51 SVN",
           "3.2.1.50 SVN",
           "3.2.1", "3.2.1RC1", "3.2.1beta2", "3.2.1beta1", "3.2.0.50 SVN",
