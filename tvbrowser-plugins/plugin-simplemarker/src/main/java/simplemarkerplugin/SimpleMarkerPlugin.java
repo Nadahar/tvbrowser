14f092d58e46ec47d4fc0978184c2f234051ae1e
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2013-01-03 13:14:35 +0000

    Fixed NPE.

--- tvbrowser-plugins/plugin-simplemarker/src/main/java/simplemarkerplugin/SimpleMarkerPlugin.java
+++ tvbrowser-plugins/plugin-simplemarker/src/main/java/simplemarkerplugin/SimpleMarkerPlugin.java
@@ -78,7 +78,7 @@ import devplugin.Version;
  * @author René Mach
  */
 public class SimpleMarkerPlugin extends Plugin {
-  private static final Version mVersion = new Version(3,20,1,true);
+  private static final Version mVersion = new Version(3,20,2,true);
 
   /** The localizer for this class. */
   private static final util.ui.Localizer mLocalizer = util.ui.Localizer.getLocalizerFor(SimpleMarkerPlugin.class);
