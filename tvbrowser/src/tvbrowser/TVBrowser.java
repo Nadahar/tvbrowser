12d23f4101f11563956ce2eee3cd32392dee6192
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2013-03-10 22:12:00 +0000

    Changed version to 3.3RC2

--- tvbrowser/src/tvbrowser/TVBrowser.java
+++ tvbrowser/src/tvbrowser/TVBrowser.java
@@ -147,6 +147,7 @@ public class TVBrowser {
   */
   /** The string array with the names of the earlier versions. */
   private static final String[] ALL_VERSIONS = new String[]{
+	      "3.3RC2",
           "3.3RC1",
           "3.3beta1",
           "3.2.1.51 SVN",
