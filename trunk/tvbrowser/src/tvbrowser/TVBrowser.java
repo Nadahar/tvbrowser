cf405223a2598f977660ebe138fb734150ac964a
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2013-03-22 09:59:31 +0000

    Changed version to 3.3.0.50 SVN.

--- trunk/tvbrowser/src/tvbrowser/TVBrowser.java
+++ trunk/tvbrowser/src/tvbrowser/TVBrowser.java
@@ -137,16 +137,17 @@ public class TVBrowser {
 
   private static String curLookAndFeel;
 
-  private static final boolean mIsStable = true;
+  private static final boolean mIsStable = false;
   private static final int mMajorVersion = 3;
   private static final int mMinorVersion = 30;
-  private static final int mSubMinorVersion = 0;
+  private static final int mSubMinorVersion = 50;
 
   /* If you want to change the version string, add it to the beginning of this array.
      We need the old version strings to import the settings.
   */
   /** The string array with the names of the earlier versions. */
   private static final String[] ALL_VERSIONS = new String[]{
+	      "3.3.0.50 SVN",
 	      "3.3",
 	      "3.3RC2",
           "3.3RC1",
