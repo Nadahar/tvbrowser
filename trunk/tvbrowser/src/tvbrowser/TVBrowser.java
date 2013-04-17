fa45578cdae2e0ada67bf77dc0e69ec21b6eeefc
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2013-04-17 10:17:46 +0000

    Added version string for 3.3a.

--- trunk/tvbrowser/src/tvbrowser/TVBrowser.java
+++ trunk/tvbrowser/src/tvbrowser/TVBrowser.java
@@ -148,6 +148,7 @@ public class TVBrowser {
   /** The string array with the names of the earlier versions. */
   private static final String[] ALL_VERSIONS = new String[]{
 	      "3.3.0.50 SVN",
+	      "3.3a",
 	      "3.3",
 	      "3.3RC2",
           "3.3RC1",
