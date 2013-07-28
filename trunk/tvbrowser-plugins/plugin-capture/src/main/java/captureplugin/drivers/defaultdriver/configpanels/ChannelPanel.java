6bc8efa588099a01c4102ccb07e07356ccea260d
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2013-07-28 10:01:56 +0000

    [TVBP-253] Add again after accidently deleting

--- trunk/tvbrowser-plugins/plugin-capture/src/main/java/captureplugin/drivers/defaultdriver/configpanels/ChannelPanel.java
+++ trunk/tvbrowser-plugins/plugin-capture/src/main/java/captureplugin/drivers/defaultdriver/configpanels/ChannelPanel.java
@@ -25,7 +25,16 @@
 package captureplugin.drivers.defaultdriver.configpanels;
 
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -37,12 +46,15 @@ import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableCellEditor;
 
 import util.ui.ChannelTableCellRenderer;
+import util.ui.ExtensionFileFilter;
 import util.ui.Localizer;
 import captureplugin.drivers.defaultdriver.DeviceConfig;
 
 import com.jgoodies.forms.builder.PanelBuilder;
 import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
+
+import devplugin.Channel;
 
 /**
  * This Panel makes it possible to assign external names to channels
@@ -53,6 +65,7 @@ public class ChannelPanel extends JPanel {
     private static final Localizer mLocalizer = Localizer.getLocalizerFor(ChannelPanel.class);
 
     private JTable mChannelTable = new JTable();
+    private ChannelTableModel mTableModel;
 
     /** Settings */
     private DeviceConfig mData;
@@ -71,11 +84,11 @@ public class ChannelPanel extends JPanel {
      */
     private void createPanel() {
       CellConstraints cc = new CellConstraints();
-      PanelBuilder pb = new PanelBuilder(new FormLayout("5dlu,pref:grow,5dlu","pref,5dlu,fill:default:grow"),this);
+      PanelBuilder pb = new PanelBuilder(new FormLayout("5dlu,default,pref:grow,5dlu","pref,5dlu,fill:default:grow,5dlu,default"),this);
       pb.setDefaultDialogBorder();
 
-      ChannelTableModel tableModel = new ChannelTableModel(mData);
-      mChannelTable.setModel(tableModel);
+      mTableModel = new ChannelTableModel(mData);
+      mChannelTable.setModel(mTableModel);
       mChannelTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
       mChannelTable.getTableHeader().setReorderingAllowed(false);
       mChannelTable.getColumnModel().getColumn(0).setCellRenderer(new ChannelTableCellRenderer());
@@ -115,10 +128,101 @@ public class ChannelPanel extends JPanel {
           }
         }
       });
+      
+      JButton tryMapping = new JButton(mLocalizer.msg("tryMapping", "Try mapping of channels from text file with external names"));
+      tryMapping.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+          tryMapping();
+        }
+      });
         
-      pb.addSeparator(mLocalizer.msg("ChannelNames", "Channel Names"), cc.xyw(1,1,3));
-      pb.add(sp, cc.xy(2,3));
+      pb.addSeparator(mLocalizer.msg("ChannelNames", "Channel Names"), cc.xyw(1,1,4));
+      pb.add(sp, cc.xyw(2,3,2));
+      pb.add(tryMapping, cc.xy(2,5));
     }
+    
+    /**
+     * @since 3.1.1
+     */
+    private void tryMapping() {
+      JFileChooser chooser = new JFileChooser();
+      chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+      chooser.setDialogTitle(mLocalizer.msg("tryMappingTitle", "Load external channel names from text file"));
+      
+      ExtensionFileFilter filter = new ExtensionFileFilter("txt", mLocalizer.msg("fileType", "Text files (*.txt [UTF-8])"));
 
-   
+      chooser.setFileFilter(filter);
+      
+      if (chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+        ArrayList<String> externalChannelList = new ArrayList<String>(0);
+
+        try {
+          BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(chooser.getSelectedFile()),"UTF-8"));
+          
+          String line = null;
+          
+          // seems to be needed to prevent additional char in first line
+          in.read();
+          
+          while((line = in.readLine()) != null) {
+            externalChannelList.add(line.trim());
+          }
+          
+          in.close();
+        } catch (IOException e) {e.printStackTrace();}
+        
+        if(!externalChannelList.isEmpty()) {
+          guessChannels(externalChannelList.toArray(new String[externalChannelList.size()]));
+        }
+      }
+    }
+    
+    /**
+     * Taken from WtvcgScheduler2 an changed for CapturePlugin
+     * <p>
+     * @param externalChannels The names of the external channels.
+     * @since 3.1.1
+     */
+    private void guessChannels(String[] externalChannels) {
+      for(int i = 0; i < mTableModel.getRowCount(); i++) {
+        Channel ch = (Channel)mTableModel.getValueAt(i,0);
+        
+        int foundLength = -1;
+        String foundValue = null;
+        
+        for(int j = 0; j < externalChannels.length; j++) {
+          String externalChannel = externalChannels[j].replaceAll("\\p{Punct}|\\s+","").toLowerCase();
+          String internalChannel = ch.getName().replaceAll("\\p{Punct}|\\s+","").toLowerCase();
+          
+          if(internalChannel.indexOf(externalChannel) != -1) {
+            if(externalChannel.length() > foundLength) {
+              foundLength = externalChannel.length();
+              foundValue = externalChannels[j];
+            }
+          }
+          else if(externalChannel.indexOf(internalChannel) != -1) {
+            if(internalChannel.length() > foundLength) {
+              foundLength = internalChannel.length();
+              foundValue = externalChannels[j];
+            }
+          }
+          
+          if(internalChannel.equals(externalChannel)){
+            foundValue = externalChannels[j];
+            break;
+          }
+        }
+        
+        if(foundValue != null) {
+          if(foundValue.indexOf(":") >= 0) {
+            foundValue = foundValue.substring(0,foundValue.indexOf(":"));
+          }
+          
+          mTableModel.setValueAt(foundValue,i,1);
+        }
+      }
+      
+      mChannelTable.repaint();
+    }
 }
