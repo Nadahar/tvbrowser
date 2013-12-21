f08f1b82313e75919148d225ffe95df8031507ee
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2013-12-21 16:44:35 +0000

    Fix NFE in timer helper.

--- trunk/tvbrowser-plugins/plugin-capture/src/main/java/captureplugin/CapturePlugin.java
+++ trunk/tvbrowser-plugins/plugin-capture/src/main/java/captureplugin/CapturePlugin.java
@@ -79,7 +79,7 @@ import devplugin.Version;
  *         adopted by fishhead
  */
 public class CapturePlugin extends devplugin.Plugin {
-  private static final Version mVersion = new Version(3,14,1,false);
+  private static final Version mVersion = new Version(3,14,2,false);
 
     /**
      * Translator
