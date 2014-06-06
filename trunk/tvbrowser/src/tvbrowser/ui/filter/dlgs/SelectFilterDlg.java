4a660dcd11fc14c01293cd138a5f8a8f4121d65f
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2014-06-06 15:21:42 +0000

    Click on X has to do the same as click on Close.

--- trunk/tvbrowser/src/tvbrowser/ui/filter/dlgs/SelectFilterDlg.java
+++ trunk/tvbrowser/src/tvbrowser/ui/filter/dlgs/SelectFilterDlg.java
@@ -34,6 +34,9 @@ import java.awt.Font;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.DefaultListCellRenderer;
 
 import javax.swing.BorderFactory;
@@ -166,6 +169,13 @@ public class SelectFilterDlg extends JDialog implements ActionListener, WindowCl
     updateBtns();
     
     setLayout(new BorderLayout());
+    setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+    addWindowListener(new WindowAdapter() {
+      @Override
+      public void windowClosing(WindowEvent e) {
+        close();
+      }
+    });
     
     add(pb.getPanel(), BorderLayout.CENTER);
     
