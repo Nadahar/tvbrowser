0ee6dfbdf193220a77fcc6c50b4aab5b746aebbe
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2014-07-02 20:54:27 +0000

    Added license.

--- tvbrowser-plugins/plugin-androidsync/src/main/java/androidsync/AndroidSync.java
+++ tvbrowser-plugins/plugin-androidsync/src/main/java/androidsync/AndroidSync.java
@@ -1,3 +1,26 @@
+/*
+ * AndroidSync - Plugin for TV-Browser
+ * Copyright (C) 2014 René Mach
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * SVN information:
+ *     $Date$
+ *   $Author$
+ * $Revision$
+ */
 package androidsync;
 
 import java.awt.Window;
@@ -68,6 +91,11 @@ import devplugin.SettingsTab;
 import devplugin.ThemeIcon;
 import devplugin.Version;
 
+/**
+ * Exports and imports channels, markings and reminders to and from Android version of TV-Browser.
+ * 
+ * @author René Mach
+ */
 public class AndroidSync extends Plugin {
   private static final Localizer mLocalizer = Localizer.getLocalizerFor(AndroidSync.class);
   
