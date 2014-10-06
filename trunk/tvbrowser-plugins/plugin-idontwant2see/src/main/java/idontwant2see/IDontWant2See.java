e388ab02e6805b1e6326bc02f107725e1e1d0cbc
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2014-10-06 11:02:43 +0000

    Fix URL for download of exclusion list.

--- trunk/tvbrowser-plugins/plugin-idontwant2see/src/main/java/idontwant2see/IDontWant2See.java
+++ trunk/tvbrowser-plugins/plugin-idontwant2see/src/main/java/idontwant2see/IDontWant2See.java
@@ -104,10 +104,10 @@ import devplugin.Version;
  */
 public final class IDontWant2See extends Plugin implements AWTEventListener {
   private static final String DONT_WANT_TO_SEE_SYNC_ADDRESS = "http://android.tvbrowser.org/data/scripts/syncUp.php?type=dontWantToSee";
-  private static final String DONT_WANT_TO_SEE_IMPORT_SYNC_ADDRESS = "http://android.tvbrowser.org/data/scripts/syncDpwn.php?type=dontWantToSee";
+  private static final String DONT_WANT_TO_SEE_IMPORT_SYNC_ADDRESS = "http://android.tvbrowser.org/data/scripts/syncDown.php?type=dontWantToSee";
   
   private static final boolean PLUGIN_IS_STABLE = true;
-  private static final Version PLUGIN_VERSION = new Version(0, 15, 6, PLUGIN_IS_STABLE);
+  private static final Version PLUGIN_VERSION = new Version(0, 15, 7, PLUGIN_IS_STABLE);
 
   private static final String RECEIVE_TARGET_EXCLUDE_EXACT = "target_exclude_exact";
 
