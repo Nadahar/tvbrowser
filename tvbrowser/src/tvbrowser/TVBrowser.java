8f2e2a0c0c734603e35e5f2efcd4200751007ae5
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2014-12-10 11:35:04 +0000

    Changed version to 3.4.0.99 RC.

--- tvbrowser/src/tvbrowser/TVBrowser.java
+++ tvbrowser/src/tvbrowser/TVBrowser.java
@@ -144,13 +144,14 @@ public class TVBrowser {
   private static final boolean mIsStable = false;
   private static final int mMajorVersion = 3;
   private static final int mMinorVersion = 40;
-  private static final int mSubMinorVersion = 98;
+  private static final int mSubMinorVersion = 99;
 
   /* If you want to change the version string, add it to the beginning of this array.
      We need the old version strings to import the settings.
   */
   /** The string array with the names of the earlier versions. */
   private static final String[] ALL_VERSIONS = new String[] {
+          "3.4.0.99 RC",
           "3.4.0.98 RC",
           "3.4.0.97 RC",
           "3.4.0.96 RC",
