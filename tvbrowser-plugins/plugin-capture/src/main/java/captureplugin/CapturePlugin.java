8410aa048e5c34921da8bbd33a66df416deb06e6
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2015-01-22 18:00:47 +0000

    Applied patch by Ronald Nikel.

--- tvbrowser-plugins/plugin-capture/src/main/java/captureplugin/CapturePlugin.java
+++ tvbrowser-plugins/plugin-capture/src/main/java/captureplugin/CapturePlugin.java
@@ -79,7 +79,7 @@ import devplugin.Version;
  *         adopted by fishhead
  */
 public class CapturePlugin extends devplugin.Plugin {
-  private static final Version mVersion = new Version(3,14,11,false);
+  private static final Version mVersion = new Version(3,14,12,false);
 
     /**
      * Translator
