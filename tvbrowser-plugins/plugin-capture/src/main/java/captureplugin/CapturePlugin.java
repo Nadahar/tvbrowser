d326cd5be98e56ffe1c33741e9a89fe9f777c5df
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2015-01-25 17:11:33 +0000

    New version compiled against TV-Browser 3.4.1.

--- tvbrowser-plugins/plugin-capture/src/main/java/captureplugin/CapturePlugin.java
+++ tvbrowser-plugins/plugin-capture/src/main/java/captureplugin/CapturePlugin.java
@@ -79,7 +79,7 @@ import devplugin.Version;
  *         adopted by fishhead
  */
 public class CapturePlugin extends devplugin.Plugin {
-  private static final Version mVersion = new Version(3,14,12,false);
+  private static final Version mVersion = new Version(3,14,13,false);
 
     /**
      * Translator
