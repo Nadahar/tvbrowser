b8279cd1481accfb7de3303d31dc32a1cc04e548
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2016-01-30 13:42:06 +0000

    Show restart button if icon showing changed for program panel.

--- tvbrowser/src/tvbrowser/ui/settings/ChannelIconAndNameSettingsTab.java
+++ tvbrowser/src/tvbrowser/ui/settings/ChannelIconAndNameSettingsTab.java
@@ -23,16 +23,23 @@
  */
 package tvbrowser.ui.settings;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
 import javax.swing.ButtonGroup;
 import javax.swing.Icon;
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
+import tvbrowser.TVBrowser;
 import tvbrowser.core.Settings;
+import tvbrowser.ui.mainframe.MainFrame;
 import util.settings.ProgramPanelSettings;
 
 import com.jgoodies.forms.builder.PanelBuilder;
@@ -67,21 +74,29 @@ public class ChannelIconAndNameSettingsTab implements SettingsTab {
   private JCheckBox mShowSortNumberInProgramTable;
   private JCheckBox mShowSortNumberInChannelLists;
   
+  private static int INDEX_ICONS_PROGRAM_PANEL = -1;
+  
+  private SettingsDialog mDialogSettings;
+  
+  public ChannelIconAndNameSettingsTab(SettingsDialog dialogSettings) {
+    mDialogSettings = dialogSettings;
+  }
+  
   /**
    * Creates the settings panel for this tab.
    */
   public JPanel createSettingsPanel() {
-    PanelBuilder pb = new PanelBuilder(new FormLayout("5dlu, default:grow, 5dlu",
+    PanelBuilder pb = new PanelBuilder(new FormLayout("5dlu, default:grow, default, 5dlu",
     "default,5dlu,default,default,default,5dlu,default,default,10dlu,default," +
-    "5dlu,default,default,10dlu,default,5dlu,default,default,default,5dlu,default"));
+    "5dlu,default,default,10dlu,default,5dlu,default,default,default,5dlu,default,fill:0dlu:grow,default"));
     pb.border(Borders.DIALOG);
 
     int y = 1;
     
-    pb.addSeparator(mLocalizer.msg("programTable","Program table"), CC.xyw(1,y++,3));
-    pb.add(mShowIconAndNameInProgramTable = new JRadioButton(mLocalizer.msg("showIconAndName","Show channel icon and channel name"), Settings.propShowChannelIconsInProgramTable.getBoolean() && Settings.propShowChannelNamesInProgramTable.getBoolean()), CC.xy(2,++y));
-    pb.add(mShowOnlyIconInProgramTable = new JRadioButton(mLocalizer.msg("showOnlyIcon","Show channel icon"), Settings.propShowChannelIconsInProgramTable.getBoolean() && !Settings.propShowChannelNamesInProgramTable.getBoolean()), CC.xy(2,++y));
-    pb.add(mShowOnlyNameInProgramTable = new JRadioButton(mLocalizer.msg("showOnlyName","Show channel name"), Settings.propShowChannelNamesInProgramTable.getBoolean() && !Settings.propShowChannelIconsInProgramTable.getBoolean()), CC.xy(2,++y));
+    pb.addSeparator(mLocalizer.msg("programTable","Program table"), CC.xyw(1,y++,4));
+    pb.add(mShowIconAndNameInProgramTable = new JRadioButton(mLocalizer.msg("showIconAndName","Show channel icon and channel name"), Settings.propShowChannelIconsInProgramTable.getBoolean() && Settings.propShowChannelNamesInProgramTable.getBoolean()), CC.xyw(2,++y,2));
+    pb.add(mShowOnlyIconInProgramTable = new JRadioButton(mLocalizer.msg("showOnlyIcon","Show channel icon"), Settings.propShowChannelIconsInProgramTable.getBoolean() && !Settings.propShowChannelNamesInProgramTable.getBoolean()), CC.xyw(2,++y,2));
+    pb.add(mShowOnlyNameInProgramTable = new JRadioButton(mLocalizer.msg("showOnlyName","Show channel name"), Settings.propShowChannelNamesInProgramTable.getBoolean() && !Settings.propShowChannelIconsInProgramTable.getBoolean()), CC.xyw(2,++y,2));
     
     y += 2;
     
@@ -103,9 +118,13 @@ public class ChannelIconAndNameSettingsTab implements SettingsTab {
     
     y += 2;
     
-    pb.addSeparator(mLocalizer.msg("programPanels", "Program panels"),CC.xyw(1,y++,3));
-    pb.add(mShowIconInProgramPanelPlugins = new JRadioButton(mLocalizer.msg("showLogoPlugins", "Plugins decide showing of channel logos"), Settings.propShowChannelLogoForProgramPanel.getInt() == ProgramPanelSettings.SHOW_CHANNEL_LOGO_PLUGINS_CONTROL), CC.xy(2,++y));
-    pb.add(mShowIconInProgramPanelNever = new JRadioButton(mLocalizer.msg("showLogoNever", "Channel logos are never shown"), Settings.propShowChannelLogoForProgramPanel.getInt() == ProgramPanelSettings.SHOW_CHANNEL_LOGO_NEVER), CC.xy(2,++y));
+    pb.addSeparator(mLocalizer.msg("programPanels", "Program panels"),CC.xyw(1,y++,4));
+    pb.add(mShowIconInProgramPanelPlugins = new JRadioButton(mLocalizer.msg("showLogoPlugins", "Plugins decide showing of channel logos"), Settings.propShowChannelLogoForProgramPanel.getInt() == ProgramPanelSettings.SHOW_CHANNEL_LOGO_PLUGINS_CONTROL), CC.xyw(2,++y,2));
+    pb.add(mShowIconInProgramPanelNever = new JRadioButton(mLocalizer.msg("showLogoNever", "Channel logos are never shown"), Settings.propShowChannelLogoForProgramPanel.getInt() == ProgramPanelSettings.SHOW_CHANNEL_LOGO_NEVER), CC.xyw(2,++y,2));
+    
+    if(INDEX_ICONS_PROGRAM_PANEL == -1) {
+      INDEX_ICONS_PROGRAM_PANEL = Settings.propShowChannelLogoForProgramPanel.getInt();
+    }
     
     ButtonGroup programPanels = new ButtonGroup();
     programPanels.add(mShowIconInProgramPanelPlugins);
@@ -113,19 +132,55 @@ public class ChannelIconAndNameSettingsTab implements SettingsTab {
     
     y += 2;
     
-    pb.addSeparator(mLocalizer.msg("channelLists","Channel lists"), CC.xyw(1,y++,3));
-    pb.add(mShowIconAndNameInChannelLists = new JRadioButton(mLocalizer.msg("showIconAndName","Show channel icon and channel name"), Settings.propShowChannelIconsInChannellist.getBoolean() && Settings.propShowChannelNamesInChannellist.getBoolean()), CC.xy(2,++y));
-    pb.add(mShowOnlyIconInChannelLists = new JRadioButton(mLocalizer.msg("showOnlyIcon","Show channel icon"), Settings.propShowChannelIconsInChannellist.getBoolean() && !Settings.propShowChannelNamesInChannellist.getBoolean()), CC.xy(2,++y));
-    pb.add(mShowOnlyNameInChannelLists = new JRadioButton(mLocalizer.msg("showOnlyName","Show channel name"), Settings.propShowChannelNamesInChannellist.getBoolean() && !Settings.propShowChannelIconsInChannellist.getBoolean()), CC.xy(2,++y));
+    pb.addSeparator(mLocalizer.msg("channelLists","Channel lists"), CC.xyw(1,y++,4));
+    pb.add(mShowIconAndNameInChannelLists = new JRadioButton(mLocalizer.msg("showIconAndName","Show channel icon and channel name"), Settings.propShowChannelIconsInChannellist.getBoolean() && Settings.propShowChannelNamesInChannellist.getBoolean()), CC.xyw(2,++y,2));
+    pb.add(mShowOnlyIconInChannelLists = new JRadioButton(mLocalizer.msg("showOnlyIcon","Show channel icon"), Settings.propShowChannelIconsInChannellist.getBoolean() && !Settings.propShowChannelNamesInChannellist.getBoolean()), CC.xyw(2,++y,2));
+    pb.add(mShowOnlyNameInChannelLists = new JRadioButton(mLocalizer.msg("showOnlyName","Show channel name"), Settings.propShowChannelNamesInChannellist.getBoolean() && !Settings.propShowChannelIconsInChannellist.getBoolean()), CC.xyw(2,++y,2));
     
     y += 2;
     
-    pb.add(mShowSortNumberInChannelLists = new JCheckBox(mLocalizer.msg("showChannelNumber", "Show sort number"), Settings.propShowSortNumberInProgramLists.getBoolean()), CC.xy(2, y++));
+    pb.add(mShowSortNumberInChannelLists = new JCheckBox(mLocalizer.msg("showChannelNumber", "Show sort number"), Settings.propShowSortNumberInProgramLists.getBoolean()), CC.xyw(2, y++, 2));
     
-    ButtonGroup channelLists = new ButtonGroup();
+    final ButtonGroup channelLists = new ButtonGroup();
     channelLists.add(mShowIconAndNameInChannelLists);
     channelLists.add(mShowOnlyIconInChannelLists);
     channelLists.add(mShowOnlyNameInChannelLists);
+
+    final JLabel restartInfo = new JLabel(mLocalizer.msg("restartNote", "For the changes to take effect TV-Browser has to be restarted."));
+    restartInfo.setForeground(Color.red);
+    restartInfo.setVisible(INDEX_ICONS_PROGRAM_PANEL != Settings.propShowChannelLogoForProgramPanel.getInt());
+
+    final JButton restart = new JButton(mLocalizer.msg("restart", "Restart now"));
+    restart.setVisible(restartInfo.isVisible());
+    restart.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        mDialogSettings.saveSettings();
+        TVBrowser.addRestart();
+        MainFrame.getInstance().quit();
+      }
+    });
+    
+    final ItemListener pluginProgramPanelLogoListener = new ItemListener() {
+      @Override
+      public void itemStateChanged(ItemEvent e) {
+        if(e.getStateChange() == ItemEvent.SELECTED) {
+          boolean showRestart = (mShowIconInProgramPanelPlugins.equals(e.getItem()) && INDEX_ICONS_PROGRAM_PANEL != ProgramPanelSettings.SHOW_CHANNEL_LOGO_PLUGINS_CONTROL)
+              || (mShowIconInProgramPanelNever.equals(e.getItem()) && INDEX_ICONS_PROGRAM_PANEL != ProgramPanelSettings.SHOW_CHANNEL_LOGO_NEVER);
+          
+          restartInfo.setVisible(showRestart);
+          restart.setVisible(showRestart);
+        }
+      }
+    };
+    
+    mShowIconInProgramPanelNever.addItemListener(pluginProgramPanelLogoListener);
+    mShowIconInProgramPanelPlugins.addItemListener(pluginProgramPanelLogoListener);
+    
+    y++;
+    
+    pb.add(restartInfo, CC.xyw(1, y, 2));
+    pb.add(restart, CC.xy(3, y));
     
     return pb.getPanel();
   }
