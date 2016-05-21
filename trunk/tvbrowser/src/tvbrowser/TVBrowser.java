e38acaef70cb02d3f94e0b25475f3efc6875268b
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2016-05-21 20:24:07 +0000

    Changed version to 3.4.3.96 RC1.

--- trunk/tvbrowser/src/tvbrowser/TVBrowser.java
+++ trunk/tvbrowser/src/tvbrowser/TVBrowser.java
@@ -152,7 +152,7 @@ public class TVBrowser {
   private static final boolean mIsStable = false;
   private static final int mMajorVersion = 3;
   private static final int mMinorVersion = 43;
-  private static final int mSubMinorVersion = 95;
+  private static final int mSubMinorVersion = 96;
 
   /* If you want to change the version string, add it to the beginning of this array.
      We need the old version strings to import the settings.
@@ -166,6 +166,7 @@ public class TVBrowser {
   */
   /** The string array with the names of the earlier versions. */
   private static final String[] ALL_VERSIONS = new String[] {
+          "3.4.3.96 RC1",
           "3.4.3.95 Beta1",
           "3.4.3.52 SVN",
           "3.4.3.51 SVN",
