cffa7465d9f81bb7bca7df6ae9b00ce5b4616144
v6ph1 <v6ph1@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2016-10-15 21:49:44 +0000

    Update read pearls after phpbb-update TODO: write pearls

--- tvbrowser-plugins/plugin-tvpearl/src/main/java/tvpearlplugin/TVPGrabber.java
+++ tvbrowser-plugins/plugin-tvpearl/src/main/java/tvpearlplugin/TVPGrabber.java
@@ -51,7 +51,7 @@ public class TVPGrabber
    * regular expression to grab the content of a TV pearl
    */
   private static final Pattern PATTERN_CONTENT = Pattern
-      .compile("<p class=\"author\"><a href=\"([^\"]*)\">.*?<a href=\"./memberlist.php?[^\"]*\"[^>]*>(.*?)</a></strong> &raquo; (.*?)</p>[\\r\\n\\t ]*?<div class=\"content\">([\\w\\W]*?)(<dl class=\"postprofile\"|<div[^>]*class=\"signature\">|<div class=\"notice\")");
+      .compile("<p class=\"author\"><a href=\"([^\"]*)\">.*?<a href=\"./memberlist.php?[^\"]*\"[^>]*>(.*?)</a></strong> &raquo; </span>(.*?)</p>[\\r\\n\\t ]*?<div class=\"content\">([\\w\\W]*?)(<dl class=\"postprofile\"|<div[^>]*class=\"signature\">|<div class=\"notice\")");
 
   /**
    * regular expression to grab the URL of the next forum page
