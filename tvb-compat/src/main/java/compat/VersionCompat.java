c2a86818eaffa4bfa05c055c31889062f2d2426e
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2017-05-24 13:32:18 +0000

    Version 0.1

---
+++ tvb-compat/src/main/java/compat/VersionCompat.java
@@ -0,0 +1,44 @@
+/*
+ * TV-Browser
+ * Copyright (C) 2017 TV-Browser team (dev@tvbrowser.org)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * SVN information:
+ *     $Date: 2014-06-17 15:59:09 +0200 (Di, 17 Jun 2014) $
+ *   $Author: ds10 $
+ * $Revision: 8152 $
+ */
+package compat;
+
+import devplugin.Version;
+import tvbrowser.TVBrowser;
+
+/**
+ * Compatibility class for TV-Browser version infos.
+ * 
+ * @author René Mach
+ * @since 0.1
+ */
+public final class VersionCompat {
+  
+  /**
+   * @return <code>true</code> if the used TV-Browser is at least a 
+   * TV-Browser 4, <code>false</code> otherwise.
+   */
+  public static boolean isAtLeastTvBrowser4() {
+    return TVBrowser.VERSION.compareTo(new Version(3,44,95,false)) >= 0;
+  }
+}
