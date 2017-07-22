3644e8c052520c5992d9675cedd30dbea65b572a
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2017-07-22 15:45:19 +0000

    Canged version to 4.0.0.95 Beta1.

--- trunk/tvbrowser/src/tvbrowser/TVBrowser.java
+++ trunk/tvbrowser/src/tvbrowser/TVBrowser.java
@@ -152,7 +152,7 @@ public class TVBrowser {
   private static final boolean mIsStable = false;
   private static final int mMajorVersion = 4;
   private static final int mMinorVersion = 00;
-  private static final int mSubMinorVersion = 50;
+  private static final int mSubMinorVersion = 95;
 
   /* If you want to change the version string, add it to the beginning of this array.
      We need the old version strings to import the settings.
@@ -166,7 +166,8 @@ public class TVBrowser {
   */
   /** The string array with the names of the earlier versions. */
   private static final String[] ALL_VERSIONS = new String[] {
-          "4.0.0.50 SVN",
+		  "4.0.0.95 Beta1",
+		  "4.0.0.50 SVN",
           "4", "3.4.4.98 RC3", "3.4.4.97 RC2", "3.4.4.95 Beta1", "3.4.4.50 SVN",
           "3.4.4", "3.4.3.96 RC1", "3.4.3.95 Beta1", "3.4.3.52 SVN", "3.4.3.51 SVN", "3.4.3.50 SVN",
 	        "3.4.3", "3.4.2.50 SVN", "3.4.2", "3.4.1.96 RC1", "3.4.1.95 Beta1", "3.4.1.50-SVN",
