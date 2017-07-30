7585c457487a0320453b693c8405206c17ffb3d7
ds10 <ds10@3b0f13be-b878-488a-9e93-cd1a97d469cd>
2017-07-30 16:19:05 +0000

    Fixed multiple program entries.

--- trunk/tvbrowser-plugins/plugin-simplemarker/src/main/java/simplemarkerplugin/SimpleMarkerPlugin.java
+++ trunk/tvbrowser-plugins/plugin-simplemarker/src/main/java/simplemarkerplugin/SimpleMarkerPlugin.java
@@ -94,7 +94,7 @@ import util.ui.WindowClosingIf;
  * @author René Mach
  */
 public class SimpleMarkerPlugin extends Plugin {
-  private static final Version mVersion = new Version(3,26,5,true);
+  private static final Version mVersion = new Version(3,26,6,true);
 
   /** The localizer for this class. */
   private static final util.ui.Localizer mLocalizer = util.ui.Localizer.getLocalizerFor(SimpleMarkerPlugin.class);
